e88d24032cc0812e596b32b770ce2ebdd4768f93
Antti Leppä <antti.leppa@foyt.fi>
2013-04-17 10:40:25 +0300

    ** Added calendarsvisible and calendarsettings widgets into calendar plugin ** Added contentToolsTop widget-space into calendar ** Added support for changing calendar firstDay (between sunday and monday)

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/calendarsvisible.js
@@ -0,0 +1,104 @@
+(function() {
+  
+  CalendarsVisibleWidgetController = $.klass(WidgetController, {
+    init: function () {
+      this._super(arguments);
+      
+      this._calendarCheckboxClickListener = $.proxy(this._onCalendarCheckboxClick, this);
+    },
+    
+    setup: function (widgetElement) {
+      this._widgetElement = $(widgetElement);
+      
+      this._overlayVisible = false;
+      
+      this._calendarsVisibleButton = this._widgetElement.find(".calendarsVisibleButton");
+      this._calendarsVisibleButton.click($.proxy(this._onCalendarsVisibleButtonClick, this));
+      $(document).mousedown($.proxy(this._onDocumentMouseDown, this));
+      
+      var _this = this;
+      RESTful.doGet(CONTEXTPATH + "/rest/calendar/calendars")
+        .success($.proxy(this._onCalendarsLoad, this));
+    },
+    
+    _onCalendarsLoad: function (data, textStatus, jqXHR) {
+      this._calendars = data;
+    },
+    
+    _onCalendarsVisibleButtonClick: function (event) {
+      if (this._overlayVisible == false) {
+        var json = {};
+        
+        var _this = this;
+        renderDustTemplate('/calendar/calendarsvisible.dust', { calendars : this._calendars }, function (text) {
+          var widgetOffset = _this._widgetElement.offset();
+          var widgetHeight = _this._widgetElement.outerHeight();
+          var widgetWidth =  _this._widgetElement.outerWidth();
+          var marginTop = 4;
+          
+          var calendarsVisibleContainer = $(text).appendTo(document.body);
+          $(calendarsVisibleContainer)
+            .css({
+              opacity: 0
+            })
+            .offset({
+              left: widgetOffset.left + widgetWidth - calendarsVisibleContainer.outerWidth(),
+              top: widgetOffset.top + widgetHeight + marginTop
+            })
+            .hide()
+            .css({
+              opacity: 1
+            })
+            .show("blind", { direction: "vertical" }, 300);
+          
+          _this._overlayVisible = true;
+          
+          calendarsVisibleContainer.find('input[type="checkbox"]').click(_this._calendarCheckboxClickListener);
+        });
+      }
+    },
+    _onDocumentMouseDown: function (event) {
+      if (this._overlayVisible) {
+        var closest = $(event.target).closest('.visibleCalendars');
+        
+        if (closest.length == 0) {
+          $('.visibleCalendars')
+            .hide("blind", { direction: "vertical" }, 300, function () {
+              $(this).remove()
+            });
+          this._overlayVisible = false;
+        }
+      }
+    },
+    _onCalendarCheckboxClick: function (event) {
+      var target = event.target;
+      var calendarId = $(target).data('calendar-id');
+      if ($(target).is(":checked")) {
+        RESTful.doPut(CONTEXTPATH + '/rest/calendar/calendars/' + calendarId, {
+          data: {
+            "visible": true
+          }
+        })
+        .success(function (data, textStatus, jqXHR) {
+          $(document).trigger($.Event("calendarVisibleWidget:calendarShow", {
+            calendarId: calendarId
+          })); 
+        });
+      } else {
+        RESTful.doPut(CONTEXTPATH + '/rest/calendar/calendars/' + calendarId, {
+          data: {
+            "visible": false
+          }
+        })
+        .success(function (data, textStatus, jqXHR) {
+          $(document).trigger($.Event("calendarVisibleWidget:calendarHide", {
+            calendarId: calendarId
+          })); 
+        });
+      }
+    }
+  });
+  
+  addWidgetController('calendarsVisibleWidget', CalendarsVisibleWidgetController);
+  
+}).call(this);
