54fc7e562c05e0980259ae95c187c235740343e3
Antti Leppä <antti.leppa@foyt.fi>
2013-04-17 10:33:55 +0300

    ** Added updateName and updateCategory methods into CalendarRAO

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/calendar/dao/CalendarDAO.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/calendar/dao/CalendarDAO.java
@@ -3,10 +3,23 @@ package fi.muikku.plugins.calendar.dao;
 import fi.muikku.dao.DAO;
 import fi.muikku.plugin.PluginDAO;
 import fi.muikku.plugins.calendar.model.Calendar;
+import fi.muikku.plugins.calendar.model.CalendarCategory;
 
 @DAO
 public class CalendarDAO extends PluginDAO<Calendar> {
 
 	private static final long serialVersionUID = -1305486704450948743L;
+
+	public Calendar updateName(Calendar calendar, String name) {
+		calendar.setName(name);
+		getEntityManager().persist(calendar);
+		return calendar;
+	}
+
+	public Calendar updateCategory(Calendar calendar, CalendarCategory calendarCategory) {
+		calendar.setCalendarCategory(calendarCategory);
+		getEntityManager().persist(calendar);
+		return calendar;
+	}
 
 }
