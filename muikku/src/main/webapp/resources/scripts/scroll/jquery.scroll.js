b5b5974f8bf6e49edf38dbbdc0b2a0947f87e6a7
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2013-04-23 11:57:34 +0300

    Ui-muutoksia jQuery Waypoint - plugari ja sticky shortcut scrollijavascripti

---
+++ muikku/src/main/webapp/resources/scripts/scroll/jquery.scroll.js
@@ -0,0 +1,14 @@
+/* Pins elements on scroll */
+
+/* Uses jQuery Waypoints plugin and sticky-shortcut */
+
+
+
+$(document).ready(function() {
+$('#staticNavigationFade').waypoint('sticky', {
+  stuckClass : 'stuckStNav'
+});
+});
+
+
+
