e2e47bb925ff08e3be2c3ed5193d563531abe61d
Antti Leppä <antti.leppa@foyt.fi>
2013-05-24 10:31:18 +0300

    ** Disablded and deprecated dock plugin

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/dock/DockPluginDescriptor.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/dock/DockPluginDescriptor.java
@@ -7,21 +7,18 @@ import javax.ejb.Stateful;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import fi.muikku.WidgetLocations;
 import fi.muikku.controller.EnvironmentController;
 import fi.muikku.controller.UserController;
 import fi.muikku.controller.WidgetController;
-import fi.muikku.model.widgets.DefaultWidget;
-import fi.muikku.model.widgets.Widget;
-import fi.muikku.model.widgets.WidgetLocation;
-import fi.muikku.model.widgets.WidgetVisibility;
 import fi.muikku.plugin.PluginDescriptor;
 
 @ApplicationScoped
 @Stateful
+@Deprecated
 public class DockPluginDescriptor implements PluginDescriptor {
 
-  private static final String ENVIRONMENT_DOCK_WIDGET_NAME = "environmentdock";
+  @SuppressWarnings("unused")
+	private static final String ENVIRONMENT_DOCK_WIDGET_NAME = "environmentdock";
 
   @Inject
   private EnvironmentController environmentController;
@@ -39,7 +36,7 @@ public class DockPluginDescriptor implements PluginDescriptor {
 
   @Override
   public void init() {
-
+/**
     Widget environmentDockWidget = widgetController.findWidget(ENVIRONMENT_DOCK_WIDGET_NAME);
     if (environmentDockWidget == null) {
       environmentDockWidget = widgetController.createWidget(ENVIRONMENT_DOCK_WIDGET_NAME, WidgetVisibility.EVERYONE);
@@ -54,6 +51,7 @@ public class DockPluginDescriptor implements PluginDescriptor {
     if (environmentDockDefaultWidget == null) {
       environmentDockDefaultWidget = widgetController.createDefaultWidget(environmentDockTopWidgetLocation, environmentDockWidget);
     }
+**/    
   }
 
   @Override
