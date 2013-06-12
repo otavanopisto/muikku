ee1da29a06466903d8aad37b47496ecc17fa5e97
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2013-06-12 15:02:29 +0300

    - Course Picker seeker result

---
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/coursepicker/CoursePickerSeekerResultProvider.java
@@ -0,0 +1,34 @@
+package fi.muikku.plugins.coursepicker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import fi.muikku.i18n.LocaleController;
+import fi.muikku.plugins.seeker.DefaultSeekerResultImpl;
+import fi.muikku.plugins.seeker.SeekerResult;
+import fi.muikku.plugins.seeker.SeekerResultProvider;
+import fi.muikku.session.SessionController;
+
+public class CoursePickerSeekerResultProvider implements SeekerResultProvider {
+
+  @Inject
+  private LocaleController localeController;
+  
+  @Inject
+  private SessionController sessionController;
+  
+  @Override
+  public List<SeekerResult> search(String searchTerm) {
+    List<SeekerResult> result = new ArrayList<SeekerResult>();
+    
+    String searchTerms = localeController.getText(sessionController.getLocale(), "plugin.coursepicker.seekersearchterms").toLowerCase();
+    
+    if (searchTerms.contains(searchTerm.toLowerCase()))
+      result.add(new DefaultSeekerResultImpl(localeController.getText(sessionController.getLocale(), "plugin.coursepicker.coursepicker"), 
+          null, "/coursepicker/coursepicker.jsf", null));
+    
+    return result; 
+  }
+}
