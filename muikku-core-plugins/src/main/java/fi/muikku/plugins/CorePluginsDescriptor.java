34e9dc7dfb2bfadb30a5e7372297505f3f66e07f
Antti Leppä <antti.leppa@foyt.fi>
2013-06-28 09:09:05 +0300

    ** Removed deprecated dock plugin

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/CorePluginsDescriptor.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/CorePluginsDescriptor.java
@@ -18,7 +18,6 @@ import fi.muikku.plugins.calendar.CalendarPluginDescriptor;
 import fi.muikku.plugins.communicator.CommunicatorPluginDescriptor;
 import fi.muikku.plugins.coursepicker.CoursePickerPluginDescriptor;
 import fi.muikku.plugins.defaults.DefaultsPluginDescriptor;
-import fi.muikku.plugins.dock.DockPluginDescriptor;
 import fi.muikku.plugins.forum.ForumPluginDescriptor;
 import fi.muikku.plugins.internallogin.InternalLoginPluginDescriptor;
 import fi.muikku.plugins.language.LanguagePluginDescriptor;
@@ -48,7 +47,6 @@ public class CorePluginsDescriptor implements PluginLibraryDescriptor {
       CoursePickerPluginDescriptor.class,
       ForumPluginDescriptor.class,
       WallPluginDescriptor.class,
-      DockPluginDescriptor.class,
       SeekerPluginDescriptor.class
 		));
 	}
