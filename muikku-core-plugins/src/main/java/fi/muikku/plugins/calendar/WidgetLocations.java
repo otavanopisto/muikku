14fe10c0cb1d43374c8c898db5f8036fe7af417c
Antti Leppä <antti.leppa@foyt.fi>
2013-06-28 11:17:53 +0300

    ** Fixed calendar plugin widget space names

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/calendar/WidgetLocations.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/calendar/WidgetLocations.java
@@ -2,10 +2,10 @@ package fi.muikku.plugins.calendar;
 
 public class WidgetLocations {
 
-	public static final String CALENDAR_CONTENT_TOOLS_TOP_LEFT = "calendar.contentToolsTopLeft";
-	public static final String CALENDAR_CONTENT_TOOLS_TOP_RIGHT = "calendar.contentToolsTopRight";
-	public static final String CALENDAR_CONTENT_SIDEBAR_LEFT = "calendar.contentSidebarLeft";
+	public static final String CALENDAR_CONTENT_TOOLS_TOP_LEFT = "calendar.content-tools-top.left";
+	public static final String CALENDAR_CONTENT_TOOLS_TOP_RIGHT = "calendar.content-tools-top.right";
+	public static final String CALENDAR_CONTENT_SIDEBAR_LEFT = "calendar.content-sidebar.left";
 	public static final String CALENDAR_CONTENT = "calendar.content";
-	public static final String CALENDAR_CONTENT_SIDEBAR_RIGHT = "calendar.contentSidebarRight";
+	public static final String CALENDAR_CONTENT_SIDEBAR_RIGHT = "calendar.content-sidebar.right";
   
 }
