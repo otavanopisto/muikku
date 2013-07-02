091b65a1a66652a503d37fbfedea7a2586eb53ec
Antti Leppä <antti.leppa@foyt.fi>
2013-07-02 12:58:39 +0300

    ** Added initial view for user adding functionality

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/settingsuseradd.js
@@ -0,0 +1,45 @@
+(function() {
+  
+  SettingsUserAddWidgetController = $.klass(WidgetController, {
+    init: function () {
+      this._super(arguments);
+    },
+    
+    setup: function (widgetElement) {
+      this._widgetElement = $(widgetElement);
+      this._widgetElement.find('a').click($.proxy(this._onAddUserLinkClick, this));
+    },
+    destroy: function () {
+      this._super(arguments);
+    },
+    _onAddUserLinkClick: function (event) {
+      renderDustTemplate('/settings/users-add-dialog.dust', {}, function (text) {
+        var buttons = new Object();
+        
+        buttons["Cancel"] = function (event) {
+          $(this).dialog("close");
+        };
+        
+        buttons["Create"] = function (event) {
+          var form = $(this).find('form');
+          var userEntity = {
+            firstName: form.find('input[name="firstName"]').val(),
+            lastName: form.find('input[name="lastName"]').val(),
+            email: form.find('input[name="email"]').val()
+          };
+          
+          alert(JSON.stringify(userEntity));
+          
+          $(this).dialog("close");
+        };
+        
+        $(text).dialog({
+          buttons: buttons
+        });
+      });
+    }
+  });
+  
+  addWidgetController('settingsUserAddWidget', SettingsUserAddWidgetController);
+  
+}).call(this);
