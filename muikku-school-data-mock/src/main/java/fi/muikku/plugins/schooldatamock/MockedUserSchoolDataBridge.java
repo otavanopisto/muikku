65250530db7bbdc1dfde92f5895324e6144642e2
Antti Leppä <antti.leppa@foyt.fi>
2013-07-03 13:18:14 +0300

    ** Added UserImage operations into school-data-mock plugin

--- muikku-school-data-mock/src/main/java/fi/muikku/plugins/schooldatamock/MockedUserSchoolDataBridge.java
+++ muikku-school-data-mock/src/main/java/fi/muikku/plugins/schooldatamock/MockedUserSchoolDataBridge.java
@@ -1,5 +1,8 @@
 package fi.muikku.plugins.schooldatamock;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,11 +14,13 @@ import javax.ejb.Stateful;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import fi.muikku.plugins.hsqldb.HSQLDBPluginController;
 import fi.muikku.plugins.schooldatamock.entities.MockedUser;
 import fi.muikku.plugins.schooldatamock.entities.MockedUserEmail;
+import fi.muikku.plugins.schooldatamock.entities.MockedUserImage;
 import fi.muikku.schooldata.UserSchoolDataBridge;
 import fi.muikku.schooldata.entity.User;
 import fi.muikku.schooldata.entity.UserEmail;
@@ -201,32 +206,114 @@ public class MockedUserSchoolDataBridge implements UserSchoolDataBridge {
 
 	@Override
 	public UserImage createUserImage(String userIdentifier, String contentType, byte[] content) {
-		// TODO Auto-generated method stub
+		Long userId = NumberUtils.createLong(userIdentifier);
+		
+		try {
+			PreparedStatement preparedStatement = executeInsert("insert into UserImage (user_id, content_type, content) values (?, ?, ?)", userId, contentType, content);
+			ResultSet resultSet = preparedStatement.getGeneratedKeys();
+			if (resultSet.next()) {
+				String identifier = String.valueOf(resultSet.getLong(1));
+			  return new MockedUserImage(identifier, userIdentifier, contentType, content);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Proper error handling
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
 		return null;
 	}
 
 	@Override
 	public UserImage findUserImage(String identifier) {
-		// TODO Auto-generated method stub
+		Long id = NumberUtils.createLong(identifier);
+		
+		try {
+			ResultSet resultSet = executeSelect("select user_id, content_type, content from UserImage where id = ?", id);
+			if (resultSet.next()) {
+				String userId = resultSet.getString(1);
+				String contentType = resultSet.getString(2);
+				Blob contentBlob = resultSet.getBlob(3);
+				
+				InputStream contentStream = contentBlob.getBinaryStream();
+				try {
+					byte[] content = IOUtils.toByteArray(contentStream);
+					return new MockedUserImage(identifier, userId, contentType, content);
+				} finally {
+					contentStream.close();
+				}
+			}
+		} catch (SQLException | IOException e) {
+			// TODO Proper error handling
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
 		return null;
 	}
 
 	@Override
 	public List<UserImage> listUserImagesByUserIdentifier(String userIdentifier) {
-		// TODO Auto-generated method stub
-		return null;
+		List<UserImage> result = new ArrayList<UserImage>();
+		
+		Long userId = NumberUtils.createLong(userIdentifier);
+		
+		try {
+			ResultSet resultSet = executeSelect("select id, user_id, content_type, content from UserImage where user_id = ?", userId);
+			while (resultSet.next()) {
+				String identifier = resultSet.getString(1);
+				String uid = resultSet.getString(2);
+				String contentType = resultSet.getString(3);
+				Blob contentBlob = resultSet.getBlob(4);
+				
+				InputStream contentStream = contentBlob.getBinaryStream();
+				try {
+					byte[] content = IOUtils.toByteArray(contentStream);
+					result.add(new MockedUserImage(identifier, uid, contentType, content));
+				} finally {
+					contentStream.close();
+				}
+			}
+		} catch (SQLException | IOException e) {
+			// TODO Proper error handling
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+		return result;
 	}
 
 	@Override
 	public UserImage updateUserImage(UserImage userImage) {
-		// TODO Auto-generated method stub
-		return null;
+		Long id = NumberUtils.createLong(userImage.getIdentifier());
+		
+		try {
+			PreparedStatement preparedStatement = executeUpdate("update UserImage set content_type = ?, content = ? where id = ?", userImage.getContentType(), userImage.getContent(), id);
+			if (preparedStatement.getUpdateCount() == 1) {
+				return userImage;
+			} else {
+				// TODO Proper error handling
+				throw new RuntimeException("UserEmail updating failed");
+			}
+		} catch (SQLException e) {
+			// TODO Proper error handling
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public void removeUserImage(String identifier) {
-		// TODO Auto-generated method stub
+		Long id = NumberUtils.createLong(identifier);
 		
+		try {
+			executeDelete("delete from UserImage where id = ?", id);
+		} catch (SQLException e) {
+			// TODO Proper error handling
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
