d590cd027e3e905c60a1968b8dccf2fd58027bb5
Antti Leppä <antti.leppa@foyt.fi>
2013-07-08 10:42:33 +0300

    ** Added muikku-data project

---
+++ muikku-data/test/003_create_users_mysql.sql
@@ -0,0 +1,8 @@
+/* Andreas Fuccibani */
+
+insert into UserEntity (id, archived) values (1, false); 
+insert into EnvironmentUser (id, archived, role_id, user_id) values (1, false, 2, 1); 
+insert into LocalUser (id, firstName, lastName, archived) values (1, 'Andreas', 'Fuccibani', false);
+insert into LocalUserEmail (user_id, address, archived) values (1, 'andreas.fuccibani@superdupermangobanana.fi', false);
+insert into InternalAuth (id, password, userEntityId) values (1, md5(md5('qwe')), 1);
+insert into UserSchoolDataIdentifier (identifier, dataSource_id, userEntity_id) values ('1', (select id from SchoolDataSource where identifier = 'LOCAL'), 1);
