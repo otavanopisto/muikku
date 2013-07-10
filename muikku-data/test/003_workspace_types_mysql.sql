eed88a0f8ffb77d2988f5d7f6ac6dcae3b0d2bce
Antti Leppä <antti.leppa@foyt.fi>
2013-07-10 07:36:27 +0300

    ** Added more test data for workspace types

--- muikku-data/test/003_workspace_types_mysql.sql
+++ muikku-data/test/003_workspace_types_mysql.sql
@@ -1,2 +1,3 @@
 INSERT INTO WorkspaceTypeEntity (id, name) values (1, 'Course');
 INSERT INTO WorkspaceTypeEntity (id, name) values (2, 'Group work');
+INSERT INTO WorkspaceTypeEntity (id, name) values (3, 'Game');
