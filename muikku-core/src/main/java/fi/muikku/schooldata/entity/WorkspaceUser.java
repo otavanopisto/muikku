f3234a2701a607c522cefa4db15c7af93c017b76
Antti Leppä <antti.leppa@foyt.fi>
2013-07-11 06:31:04 +0300

    ** Added school data entity WorkspaceUser ** Added listWorkspaceUsers method into WorkspaceSchoolDataBridge interface

---
+++ muikku-core/src/main/java/fi/muikku/schooldata/entity/WorkspaceUser.java
@@ -0,0 +1,14 @@
+package fi.muikku.schooldata.entity;
+
+import fi.tranquil.TranquilEntity;
+
+@TranquilEntity
+public interface WorkspaceUser extends SchoolDataEntity {
+
+	public String getIdentifier();
+
+	public String getUserIdentifier();
+	
+	public String getWorkspaceIdentifier();
+	
+}
