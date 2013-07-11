6b355c2f405ec1786919ee7545f00c3ba193ea0b
Antti Leppä <antti.leppa@foyt.fi>
2013-07-11 11:57:38 +0300

    ** Added roles settings navigation widget and empty view where it leads to

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/settings/SettingsPluginDescriptor.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/settings/SettingsPluginDescriptor.java
@@ -26,27 +26,38 @@ public class SettingsPluginDescriptor implements PluginDescriptor, LocalizedPlug
 	private static final String DOCK_SETTINGS_WIDGET_LOCATION = fi.muikku.WidgetLocations.ENVIRONMENT_DOCK_TOP_CENTER;
 	private static final int DOCK_SETTINGS_WIDGET_MINIMUM_SIZE = 1;
 	private static final String DOCK_SETTINGS_WIDGET_NAME = "docksettings";
-	
+  private static final WidgetVisibility DOCK_SETTINGS_VISIBILITY = WidgetVisibility.AUTHENTICATED;
+  
 	private static final String USERS_WIDGET_LOCATION = WidgetLocations.SETTINGS_CONTENT_SIDEBAR_LEFT;
 	private static final int USERS_WIDGET_MINIMUM_SIZE = 1;
 	private static final String USERS_WIDGET_NAME = "settings-navigation-users";
+  private static final WidgetVisibility USERS_WIDGET_VISIBILITY = WidgetVisibility.AUTHENTICATED;
+  
+	private static final String ROLES_WIDGET_LOCATION = WidgetLocations.SETTINGS_CONTENT_SIDEBAR_LEFT;
+	private static final int ROLES_WIDGET_MINIMUM_SIZE = 1;
+	private static final String ROLES_WIDGET_NAME = "settings-navigation-roles";
+  private static final WidgetVisibility ROLES_WIDGET_VISIBILITY = WidgetVisibility.AUTHENTICATED;
 	
 	private static final String WORKSPACES_WIDGET_LOCATION = WidgetLocations.SETTINGS_CONTENT_SIDEBAR_LEFT;
 	private static final int WORKSPACES_WIDGET_MINIMUM_SIZE = 1;
 	private static final String WORKSPACES_WIDGET_NAME = "settings-navigation-workspaces";
+  private static final WidgetVisibility WORKSPACES_WIDGET_VISIBILITY = WidgetVisibility.AUTHENTICATED;
 	
 	private static final String WORKSPACETYPES_WIDGET_LOCATION = WidgetLocations.SETTINGS_CONTENT_SIDEBAR_LEFT;
 	private static final int WORKSPACETYPES_WIDGET_MINIMUM_SIZE = 1;
 	private static final String WORKSPACETYPES_WIDGET_NAME = "settings-navigation-workspace-types";
+  private static final WidgetVisibility WORKSPACETYPES_WIDGET_VISIBILITY = WidgetVisibility.AUTHENTICATED;
 	
 	private static final String PLUGINS_WIDGET_LOCATION = WidgetLocations.SETTINGS_CONTENT_SIDEBAR_LEFT;
   private static final int PLUGINS_WIDGET_MINIMUM_SIZE = 1;
   private static final String PLUGINS_WIDGET_NAME = "settings-navigation-plugins";
-	
+  private static final WidgetVisibility PLUGINS_WIDGET_VISIBILITY = WidgetVisibility.AUTHENTICATED;
+  
 	private static final String USERS_ADD_WIDGET_LOCATION = WidgetLocations.SETTINGS_USERS_CONTENT_TOOLS_TOP_RIGHT;
 	private static final int USERS_ADD_WIDGET_MINIMUM_SIZE = 1;
 	private static final String USERS_ADD_WIDGET_NAME = "settings-users-add";
-	
+	private static final WidgetVisibility USERS_ADD_WIDGET_VISIBILITY = WidgetVisibility.AUTHENTICATED;
+			
 	@Inject
 	private WidgetController widgetController;
 	
@@ -61,22 +72,23 @@ public class SettingsPluginDescriptor implements PluginDescriptor, LocalizedPlug
 		 * Dock widget
 		 */
 		
-		widgetController.ensureDefaultWidget(widgetController.ensureWidget(DOCK_SETTINGS_WIDGET_NAME, DOCK_SETTINGS_WIDGET_MINIMUM_SIZE, WidgetVisibility.AUTHENTICATED), DOCK_SETTINGS_WIDGET_LOCATION);
+		widgetController.ensureDefaultWidget(widgetController.ensureWidget(DOCK_SETTINGS_WIDGET_NAME, DOCK_SETTINGS_WIDGET_MINIMUM_SIZE, DOCK_SETTINGS_VISIBILITY), DOCK_SETTINGS_WIDGET_LOCATION);
 		
 		/**
 		 * Settings navigation widgets
 		 */
 		
-		widgetController.ensureDefaultWidget(widgetController.ensureWidget(USERS_WIDGET_NAME, USERS_WIDGET_MINIMUM_SIZE, WidgetVisibility.AUTHENTICATED), USERS_WIDGET_LOCATION);
-		widgetController.ensureDefaultWidget(widgetController.ensureWidget(WORKSPACES_WIDGET_NAME, WORKSPACES_WIDGET_MINIMUM_SIZE, WidgetVisibility.AUTHENTICATED), WORKSPACES_WIDGET_LOCATION);
-		widgetController.ensureDefaultWidget(widgetController.ensureWidget(WORKSPACETYPES_WIDGET_NAME, WORKSPACETYPES_WIDGET_MINIMUM_SIZE, WidgetVisibility.AUTHENTICATED), WORKSPACETYPES_WIDGET_LOCATION);
-    widgetController.ensureDefaultWidget(widgetController.ensureWidget(PLUGINS_WIDGET_NAME, PLUGINS_WIDGET_MINIMUM_SIZE, WidgetVisibility.AUTHENTICATED), PLUGINS_WIDGET_LOCATION);
+		widgetController.ensureDefaultWidget(widgetController.ensureWidget(USERS_WIDGET_NAME, USERS_WIDGET_MINIMUM_SIZE, USERS_WIDGET_VISIBILITY), USERS_WIDGET_LOCATION);
+		widgetController.ensureDefaultWidget(widgetController.ensureWidget(ROLES_WIDGET_NAME, ROLES_WIDGET_MINIMUM_SIZE, ROLES_WIDGET_VISIBILITY), ROLES_WIDGET_LOCATION);
+		widgetController.ensureDefaultWidget(widgetController.ensureWidget(WORKSPACES_WIDGET_NAME, WORKSPACES_WIDGET_MINIMUM_SIZE, WORKSPACES_WIDGET_VISIBILITY), WORKSPACES_WIDGET_LOCATION);
+		widgetController.ensureDefaultWidget(widgetController.ensureWidget(WORKSPACETYPES_WIDGET_NAME, WORKSPACETYPES_WIDGET_MINIMUM_SIZE, WORKSPACETYPES_WIDGET_VISIBILITY), WORKSPACETYPES_WIDGET_LOCATION);
+    widgetController.ensureDefaultWidget(widgetController.ensureWidget(PLUGINS_WIDGET_NAME, PLUGINS_WIDGET_MINIMUM_SIZE, PLUGINS_WIDGET_VISIBILITY), PLUGINS_WIDGET_LOCATION);
 
 		/**
 		 * Settings / users
 		 */
 
-		widgetController.ensureDefaultWidget(widgetController.ensureWidget(USERS_ADD_WIDGET_NAME, USERS_ADD_WIDGET_MINIMUM_SIZE, WidgetVisibility.AUTHENTICATED), USERS_ADD_WIDGET_LOCATION);
+    widgetController.ensureDefaultWidget(widgetController.ensureWidget(USERS_ADD_WIDGET_NAME, USERS_ADD_WIDGET_MINIMUM_SIZE, USERS_ADD_WIDGET_VISIBILITY), USERS_ADD_WIDGET_LOCATION);
 	}
 
 	@Override
