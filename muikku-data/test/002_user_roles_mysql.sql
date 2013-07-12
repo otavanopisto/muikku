bcdcc8805c9c18c62c99fc1e4469b66e7f12dc42
Antti Leppä <antti.leppa@foyt.fi>
2013-07-12 07:30:54 +0300

    ** Fixed test data scripts

--- muikku-data/test/002_user_roles_mysql.sql
+++ muikku-data/test/002_user_roles_mysql.sql
@@ -1,6 +1,7 @@
-INSERT INTO `UserRole` VALUES (2,'Manager'),(3,'Teacher'),(4,'Student'),(5,'Workspace Teacher'),(6,'Workspace Student');
-INSERT INTO EnvironmentUserRole values (2);
-INSERT INTO EnvironmentUserRole values (3);
-INSERT INTO EnvironmentUserRole values (4);
-INSERT INTO CourseUserRole values (5);
-INSERT INTO CourseUserRole values (6);
+INSERT INTO RoleEntity VALUES (1,'EVERYONE'), (2,'Manager'), (3,'Teacher'), (4,'Student'), (5,'Workspace Teacher'), (6,'Workspace Student');
+INSERT INTO EnvironmentRoleEntity (id, environmentRoleType) values (1, 'EVERYONE');
+INSERT INTO EnvironmentRoleEntity (id, environmentRoleType) values (2, 'NORMAL');
+INSERT INTO EnvironmentRoleEntity (id, environmentRoleType) values (3, 'NORMAL');
+INSERT INTO EnvironmentRoleEntity (id, environmentRoleType) values (4, 'NORMAL');
+INSERT INTO WorkspaceRoleEntity values (5);
+INSERT INTO WorkspaceRoleEntity values (6);
