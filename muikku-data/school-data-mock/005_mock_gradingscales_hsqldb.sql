476d7aab48f7f55967b05284b064c0005525a85c
Antti Leppä <antti.leppa@foyt.fi>
2013-07-16 12:30:12 +0300

    ** Changed grading scale test data to fit better the existing data

--- muikku-data/school-data-mock/005_mock_gradingscales_hsqldb.sql
+++ muikku-data/school-data-mock/005_mock_gradingscales_hsqldb.sql
@@ -1,24 +1,13 @@
 insert into GradingScale (name) values 
-  ('Academic grading - WES'),
-  ('Academic grading - Finland');
+  ('Lawful Good / Chaotic Evil');
 
 insert into GradingScaleItem (grading_scale_id, name) values 
-  (0, 'A+'),
-  (0, 'A'),
-  (0, 'A-'),
-  (0, 'B+'),
-  (0, 'B'),
-  (0, 'B-'),
-  (0, 'C+'),
-  (0, 'C'),
-  (0, 'C-'),
-  (0, 'D+'),
-  (0, 'D'),
-  (0, 'D-'),
-  (0, 'F'),
-  (1, '0'),
-  (1, '1'),
-  (1, '2'),
-  (1, '3'),
-  (1, '4'),
-  (1, '5');
+  (0, 'Lawful Good'),
+  (0, 'Neutral Good'),
+  (0, 'Chaotic Good'),
+  (0, 'Lawful Neutral'),
+  (0, 'True Neutral'),
+  (0, 'Chaotic Neutral'),
+  (0, 'Lawful Evil'),
+  (0, 'Neutral Evil'),
+  (0, 'Chaotic Evil');
