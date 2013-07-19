0704204654f657927b7254851f45408afe55e1cb
Antti Leppä <antti.leppa@foyt.fi>
2013-07-19 09:49:04 +0300

    ** Added more test data for subjects and course identifiers testing purposes

---
+++ muikku-data/school-data-mock/003_mock_subjects_courseidentifiers_hsqldb.sql
@@ -0,0 +1,27 @@
+/* Subject */
+
+insert into Subject (name) values 
+  ('Elvish'), 
+  ('Language Of Men'), 
+  ('Dwarfish'), 
+  ('Ents'), 
+  ('Orc'), 
+  ('Eä'), 
+  ('Fencing'), 
+  ('Orcology'), 
+  ('Dark Magic');
+
+insert into CourseIdentifier (code, subject_id) values
+  ('MEL-ELF-1', 0),
+  ('MEL-ELF-2', 0),
+  ('MEL-ELF-3', 0),
+  ('MEL-ELF-4', 0),
+  ('MEL-ELF-5', 0),
+  ('MEL-MEN-1', 1),
+  ('MEL-MEN-2', 1),
+  ('MEL-MEN-3', 1),
+  ('MEL-DWF-1', 2),
+  ('MEL-DWF-2', 2),
+  ('MEL-ENT-1', 3),
+  ('MEL-ORC-1', 4),
+  ('MEL-ORC-2', 4);
---
+++ muikku-data/school-data-mock/004_mock_workspaces_hsqldb.sql
@@ -0,0 +1,36 @@
+/* Workspace Types */
+
+insert into WorkspaceType (name) values
+  ('GENERIC'),
+  ('COURSE'),
+  ('GROUPWORK'),
+  ('GAME');
+
+/** Language Courses **/
+
+insert into Workspace (course_identifier_id, name, type_id) values
+	(0, 'Elvish 1 - Primitive Quendian', 1),
+	(1, 'Elvish 2 - Common Eldarin, Quenya and Goldogrin', 1),
+	(2, 'Elvish 3 - Noldorin', 1),
+	(3, 'Elvish 4 - Telerin, Ilkorin, Doriathrin and the Avarin', 1),
+	(4, 'Elvish 5 - Sindarin', 1),
+	(5, 'Language Of Men 1 - Soval Pharë', 1),
+	(6, 'Language Of Men 2 - Taliska', 1),
+	(7, 'Language Of Men 3 - Adûnaic', 1),
+	(8, 'Dwarfish 1 - Basics of Khuzdul', 1),
+	(9, 'Dwarfish 2 - Iglishmêk', 1),
+	(10, 'Entish 1 - Fundamental Entish', 1),
+	(11, 'Orc 1 - Orcish', 1),
+	(12, 'Orc 2 - Black Speech', 1);
+	
+/* Group Work Workspaces */
+	
+insert into Workspace (course_identifier_id, name, type_id) values
+	(null, 'Public House - Green Dragon', 2),
+	(null, 'Hidden kingdom of Gondolin', 2),
+	(null, 'Minas Morgul - City of the Nazgûl', 2);	
+	
+/* Games */	
+	
+insert into Workspace (course_identifier_id, name, type_id) values
+	(null, 'MERP', 3);	
