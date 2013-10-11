dcf4d98c979037cf5b98619136762094fd069b8b
Antti Leppä <antti.leppa@foyt.fi>
2013-10-11 10:29:38 +0300

    ** Added support for relative urls into data plugin

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/data/DataPluginController.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/data/DataPluginController.java
@@ -57,6 +57,10 @@ public class DataPluginController {
 	  			String url = script.getAttribute("url");
 	  			String handler = script.getAttribute("handler");
 	  			Map<String, String> parameters = new HashMap<String, String>();
+	  			
+	  			if (!url.matches("\b(([a-z]{1,}):\\/\\/)")) {
+	  				url = new File(file.getParent(), url).toURI().toString();
+	  			}
 
 	  			NodeList scriptChildNodes = script.getChildNodes();
 			  	for (int j= 0, jl = scriptChildNodes.getLength(); j< jl; j++) {
