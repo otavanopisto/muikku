e3b66efb5c5a29ba76bbd85d87f00121e6dd1462
Antti Leppä <antti.leppa@foyt.fi>
2013-10-16 13:26:47 +0300

    ** Added WorkspaceMaterialController

---
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialController.java
@@ -0,0 +1,94 @@
+package fi.muikku.plugins.workspace;
+
+import java.util.List;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import fi.muikku.model.workspace.WorkspaceEntity;
+import fi.muikku.plugins.material.model.Material;
+import fi.muikku.plugins.workspace.dao.WorkspaceFolderDAO;
+import fi.muikku.plugins.workspace.dao.WorkspaceMaterialDAO;
+import fi.muikku.plugins.workspace.model.WorkspaceFolder;
+import fi.muikku.plugins.workspace.model.WorkspaceMaterial;
+
+@Dependent
+@Stateful
+public class WorkspaceMaterialController {
+	
+	@Inject
+	private WorkspaceMaterialDAO workspaceMaterialDAO;
+
+	@Inject
+	private WorkspaceFolderDAO workspaceFolderDAO;
+	
+	/* Material */
+	
+	public WorkspaceMaterial createWorkspaceMaterial(WorkspaceFolder folder, Material material) {
+		return workspaceMaterialDAO.create(folder, material);
+	}
+	
+	public WorkspaceMaterial findWorkspaceMaterialById(Long workspaceMaterialId) {
+		return workspaceMaterialDAO.findById(workspaceMaterialId);
+	}
+	
+  public WorkspaceMaterial findWorkspaceMaterialByFolderAndUrlName(WorkspaceFolder folder, String urlName) {
+		return workspaceMaterialDAO.findByFolderAndUrlName(folder, urlName);
+	}
+
+	public WorkspaceMaterial findWorkspaceMaterialByWorkspaceEntityAndPath(WorkspaceEntity workspaceEntity, String path) {
+		String[] pathElements = path.split("/");
+		WorkspaceFolder parent = findWorkspaceFolderByWorkspaceEntity(workspaceEntity);
+		
+		for (int i = 0, l = pathElements.length; i < l - 1; i++) {
+			String pathElement = pathElements[i];
+			parent = findWorkspaceFolderByParentAndUrlName(parent, pathElement);
+		}
+		
+		return findWorkspaceMaterialByFolderAndUrlName(parent, pathElements[pathElements.length - 1]);
+	}
+
+	public List<WorkspaceMaterial> listWorkspaceMaterialsByFolder(WorkspaceFolder folder) {
+		return workspaceMaterialDAO.listByFolder(folder);
+	}
+	
+	/* Folder */
+	
+	public WorkspaceFolder createWorkspaceFolder(WorkspaceEntity workspaceEntity) {
+		if (workspaceEntity != null) {
+  		return workspaceFolderDAO.create(workspaceEntity.getId(), null);
+		} 
+		
+		return null;
+	}
+
+	public WorkspaceFolder createWorkspaceFolder(WorkspaceFolder parent) {
+		if (parent != null) {
+  		return workspaceFolderDAO.create(null, parent);
+		}
+		
+		return null;
+	}
+	
+	public WorkspaceFolder findWorkspaceFolderById(Long workspaceFolderId) {
+		return workspaceFolderDAO.findById(workspaceFolderId);
+	}
+	
+	public WorkspaceFolder findWorkspaceFolderByParentAndUrlName(WorkspaceFolder parent, String urlName) {
+		return workspaceFolderDAO.findByParentAndUrlName(parent, urlName);
+	}
+	
+	public WorkspaceFolder findWorkspaceFolderByWorkspaceEntity(WorkspaceEntity workspaceEntity) {
+		return workspaceFolderDAO.findByWorkspaceEntityId(workspaceEntity.getId());
+	}
+	
+	public List<WorkspaceFolder> listWorkspaceFoldersByWorkspaceEntity(WorkspaceEntity workspaceEntity) {
+		return workspaceFolderDAO.listByWorkspaceEntityId(workspaceEntity.getId());
+	}
+	
+	public List<WorkspaceFolder> listWorkspaceFoldersByParent(WorkspaceFolder parent) {
+		return workspaceFolderDAO.listByParent(parent);
+	}
+	
+}
