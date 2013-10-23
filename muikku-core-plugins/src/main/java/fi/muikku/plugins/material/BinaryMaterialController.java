72376952e41730b87597ee3477bce12ea826c74e
Antti Leppä <antti.leppa@foyt.fi>
2013-10-23 10:06:37 +0300

    ** Added support for creating binary materials into BinaryMaterialController

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/BinaryMaterialController.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/BinaryMaterialController.java
@@ -14,6 +14,10 @@ public class BinaryMaterialController {
 
 	@Inject
 	private BinaryMaterialDAO binaryMaterialDAO;
+
+	public BinaryMaterial createBinaryMaterial(String title, String urlName, String contentType, byte[] content) {
+		return binaryMaterialDAO.create(title, urlName, contentType, content);
+	}
 	
 	public BinaryMaterial finBinaryMaterialById(Long id) {
 		return binaryMaterialDAO.findById(id);
--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/dao/BinaryMaterialDAO.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/dao/BinaryMaterialDAO.java
@@ -6,5 +6,16 @@ import fi.muikku.plugins.material.model.BinaryMaterial;
 public class BinaryMaterialDAO extends PluginDAO<BinaryMaterial> {
 
 	private static final long serialVersionUID = 1L;
+
+	public BinaryMaterial create(String title, String urlName, String contentType, byte[] content) {
+		BinaryMaterial binaryMaterial = new BinaryMaterial();
+		binaryMaterial.setContent(content);
+		binaryMaterial.setContentType(contentType);
+		binaryMaterial.setTitle(title);
+		binaryMaterial.setType("binary");
+		binaryMaterial.setUrlName(urlName);
+		
+		return persist(binaryMaterial);
+	}
 	
 }
