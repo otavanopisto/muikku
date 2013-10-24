40025a965c9b6384ea490be21049c216fea89797
Antti Leppä <antti.leppa@foyt.fi>
2013-10-24 06:25:20 +0300

    ** Added missing getTitle method into Material entity

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/model/Material.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/model/Material.java
@@ -30,6 +30,10 @@ public class Material {
   public void setType(String type) {
     this.type = type;
   }
+  
+  public String getTitle() {
+		return title;
+	}
 
   public void setTitle(String title) {
     this.title = title;
