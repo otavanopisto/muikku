55fc73531d7f3e0ae81132cb495351c7db3a424d
Antti Leppä <antti.leppa@foyt.fi>
2013-10-24 06:37:54 +0300

    ** Added initial version of deus nex machina structure parser ** Added general deus nex machina utilities and exceptions

---
+++ muikku-deus-nex-machina/src/main/java/fi/muikku/plugins/dnm/parser/structure/model/Folder.java
@@ -0,0 +1,8 @@
+package fi.muikku.plugins.dnm.parser.structure.model;
+
+public class Folder extends ResourceContainer {
+
+	public Folder() {
+		setType(Type.FOLDER);	
+	}
+}
---
+++ muikku-deus-nex-machina/src/main/java/fi/muikku/plugins/dnm/parser/structure/model/Resource.java
@@ -0,0 +1,50 @@
+package fi.muikku.plugins.dnm.parser.structure.model;
+
+public abstract class Resource {
+
+	public Integer getNo() {
+		return no;
+	}
+	
+	public void setNo(Integer no) {
+		this.no = no;
+	}
+
+	public Type getType() {
+		return type;
+	}
+	
+	public void setType(Type type) {
+		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+	
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	private Integer no;
+	private Type type;
+	private String path;
+	private String title;
+	private String name;
+}
