4065b11f77c2b96aafd156e70db22179838a18d5
Antti Leppä <antti.leppa@foyt.fi>
2013-11-08 07:08:52 +0200

    ** Moved workspace member related methods into WorkspaceMembersBackingBean

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMembersBackingBean.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMembersBackingBean.java
@@ -1,6 +1,8 @@
 package fi.muikku.plugins.workspace;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
@@ -14,7 +16,11 @@ import com.ocpsoft.pretty.faces.annotation.URLMapping;
 import com.ocpsoft.pretty.faces.annotation.URLMappings;
 
 import fi.muikku.model.workspace.WorkspaceEntity;
+import fi.muikku.schooldata.UserController;
 import fi.muikku.schooldata.WorkspaceController;
+import fi.muikku.schooldata.entity.User;
+import fi.muikku.schooldata.entity.UserEmail;
+import fi.muikku.schooldata.entity.WorkspaceUser;
 
 @Named
 @Stateful
@@ -30,6 +36,9 @@ public class WorkspaceMembersBackingBean {
   @Inject
   private WorkspaceController workspaceController;
 
+  @Inject
+  private UserController userController;
+  
   @Inject
   @Named
   private WorkspaceNavigationBackingBean workspaceNavigationBackingBean;
@@ -49,6 +58,21 @@ public class WorkspaceMembersBackingBean {
     workspaceNavigationBackingBean.setWorkspaceUrlName(urlName);
 
     workspaceId = workspaceEntity.getId();
+    
+    workspaceUsers = new ArrayList<>();
+    
+    for (WorkspaceUser workspaceUser : workspaceController.listWorkspaceUsers(workspaceEntity)) {
+      User user = userController.findUser(workspaceUser.getUserSchoolDataSource(), workspaceUser.getUserIdentifier());
+      if (user != null) {
+        List<UserEmail> userEmails = userController.listUserEmails(user);
+        List<String> emails = new ArrayList<>(userEmails.size());
+        for (UserEmail userEmail : userEmails) {
+          emails.add(userEmail.getAddress());
+        }
+        
+        workspaceUsers.add(new WorkspaceUserBean(workspaceUser.getIdentifier(), user.getIdentifier(), user.getFirstName(), user.getLastName(), emails));
+      }
+    }
   }
   
   public Long getWorkspaceId() {
@@ -66,7 +90,69 @@ public class WorkspaceMembersBackingBean {
   public void setWorkspaceUrlName(String workspaceUrlName) {
     this.workspaceUrlName = workspaceUrlName;
   }
+  
+  public List<WorkspaceUserBean> getWorkspaceUsers() {
+    return workspaceUsers;
+  }
 
   private Long workspaceId;
   private String workspaceUrlName;
+  private List<WorkspaceUserBean> workspaceUsers;
+  
+  public class WorkspaceUserBean {
+
+    public WorkspaceUserBean(String identifier, String userIdentifier, String firstName, String lastName, List<String> emails) {
+      this.identifier = identifier;
+      this.userIdentifier = userIdentifier;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.emails = emails;
+    }
+
+    public String getIdentifier() {
+      return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+      this.identifier = identifier;
+    }
+
+    public String getUserIdentifier() {
+      return userIdentifier;
+    }
+
+    public void setUserIdentifier(String userIdentifier) {
+      this.userIdentifier = userIdentifier;
+    }
+
+    public String getFirstName() {
+      return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+      this.firstName = firstName;
+    }
+
+    public String getLastName() {
+      return lastName;
+    }
+
+    public void setLastName(String lastName) {
+      this.lastName = lastName;
+    }
+    
+    public List<String> getEmails() {
+      return emails;
+    }
+    
+    public void setEmails(List<String> emails) {
+      this.emails = emails;
+    }
+
+    private String identifier;
+    private String userIdentifier;
+    private String firstName;
+    private String lastName;
+    private List<String> emails;
+  }
 }
