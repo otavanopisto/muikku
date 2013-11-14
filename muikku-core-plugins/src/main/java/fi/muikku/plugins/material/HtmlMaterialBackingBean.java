5a608f2c65d66b621ca76956b4a5117e270b99a8
Antti Leppä <antti.leppa@foyt.fi>
2013-11-14 09:44:11 +0200

    ** Added HtmlMaterialBeforeSerialize event (fired before html material is serialized back to text) ** Moved html material serialization into HtmlMaterialController from HtmlMaterialBackingBean

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/HtmlMaterialBackingBean.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/HtmlMaterialBackingBean.java
@@ -2,26 +2,14 @@ package fi.muikku.plugins.material;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.io.StringWriter;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import fi.muikku.plugins.material.model.HtmlMaterial;
@@ -40,8 +28,7 @@ public class HtmlMaterialBackingBean implements Serializable {
     
   	HtmlMaterial htmlMaterial = htmlMaterialController.findHtmlMaterialById(materialId);
   	if (htmlMaterial != null) { 
-      Document processedHtmlDocument = htmlMaterialController.getProcessedHtmlDocument(htmlMaterial);
-      return serializeDocument(processedHtmlDocument);
+  	  return htmlMaterialController.getSerializedHtmlDocument(htmlMaterial);
   	}
   	
   	return null;
@@ -49,33 +36,5 @@ public class HtmlMaterialBackingBean implements Serializable {
   
   public void save(ActionEvent event) {
     
-  }
-  
-  private static String serializeDocument(Document document) throws TransformerException, XPathExpressionException {
-    StringWriter writer = new StringWriter();
-
-    Node bodyNode = findNodeByXPath(document.getDocumentElement(), "//BODY");
-    if (bodyNode == null) {
-      bodyNode = document.getDocumentElement();
-    }  
-    
-    if (bodyNode instanceof Element) {
-      TransformerFactory transformerFactory = TransformerFactory.newInstance();
-      Transformer transformer = transformerFactory.newTransformer();
-      transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-
-      Element bodyElement = (Element) bodyNode;
-
-      NodeList bodyChildNodes = bodyElement.getChildNodes();
-      for (int i = 0, l = bodyChildNodes.getLength(); i < l; i++) {
-        transformer.transform(new DOMSource(bodyChildNodes.item(i)), new StreamResult(writer));
-      }
-    }
-    
-    return writer.getBuffer().toString();
-  }
-
-  private static Node findNodeByXPath(Node contextNode, String expression) throws XPathExpressionException {
-    return (Node) XPathFactory.newInstance().newXPath().evaluate(expression, contextNode, XPathConstants.NODE);
   }
 }
--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/HtmlMaterialController.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/HtmlMaterialController.java
@@ -2,20 +2,35 @@ package fi.muikku.plugins.material;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 import javax.ejb.Stateless;
 
 import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.apache.commons.lang3.StringUtils;
 import org.cyberneko.html.parsers.DOMParser;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import fi.muikku.plugins.material.dao.HtmlMaterialDAO;
+import fi.muikku.plugins.material.events.HtmlMaterialBeforeSerializeEvent;
+import fi.muikku.plugins.material.events.HtmlMaterialProcessEvent;
 import fi.muikku.plugins.material.model.HtmlMaterial;
 
 @Dependent
@@ -24,6 +39,9 @@ public class HtmlMaterialController {
   
   @Inject
   private Event<HtmlMaterialProcessEvent> processEvent;
+
+  @Inject
+  private Event<HtmlMaterialBeforeSerializeEvent> beforeSerializeEvent;
 
 	@Inject
 	private HtmlMaterialDAO htmlMaterialDAO;
@@ -59,5 +77,40 @@ public class HtmlMaterialController {
     }
     
     return null;
+  }
+
+  public String getSerializedHtmlDocument(HtmlMaterial htmlMaterial) throws SAXException, IOException, XPathExpressionException, TransformerException {
+    Document processedHtmlDocument = getProcessedHtmlDocument(htmlMaterial);
+    HtmlMaterialBeforeSerializeEvent event = new HtmlMaterialBeforeSerializeEvent(htmlMaterial.getId(), processedHtmlDocument);
+    beforeSerializeEvent.fire(event);
+    return serializeDocument(event.getDocument());
+  }
+    
+  private String serializeDocument(Document document) throws TransformerException, XPathExpressionException {
+    StringWriter writer = new StringWriter();
+
+    Node bodyNode = findNodeByXPath(document.getDocumentElement(), "//BODY");
+    if (bodyNode == null) {
+      bodyNode = document.getDocumentElement();
+    }  
+    
+    if (bodyNode instanceof Element) {
+      TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      Transformer transformer = transformerFactory.newTransformer();
+      transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+
+      Element bodyElement = (Element) bodyNode;
+
+      NodeList bodyChildNodes = bodyElement.getChildNodes();
+      for (int i = 0, l = bodyChildNodes.getLength(); i < l; i++) {
+        transformer.transform(new DOMSource(bodyChildNodes.item(i)), new StreamResult(writer));
+      }
+    }
+    
+    return writer.getBuffer().toString();
+  }
+
+  private Node findNodeByXPath(Node contextNode, String expression) throws XPathExpressionException {
+    return (Node) XPathFactory.newInstance().newXPath().evaluate(expression, contextNode, XPathConstants.NODE);
   }
 }
