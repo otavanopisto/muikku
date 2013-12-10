b7977d194e78a520697c139dff29c24baa1aa9a6
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2013-12-10 13:58:26 +0200

    added rightanswer entity

---
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/rightanswer/model/RightAnswer.java
@@ -0,0 +1,80 @@
+package fi.muikku.plugins.material.rightanswer.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import fi.muikku.plugins.material.queryfields.model.QueryField;
+
+@Entity
+public class RightAnswer {
+	
+    public Double getPoints() {
+		return points;
+	}
+
+	public void setPoints(Double points) {
+		this.points = points;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public boolean isCaseSensitive() {
+		return caseSensitive;
+	}
+
+	public void setCaseSensitive(boolean caseSensitive) {
+		this.caseSensitive = caseSensitive;
+	}
+
+	public boolean isNormalizeWhitespace() {
+		return normalizeWhitespace;
+	}
+
+	public void setNormalizeWhitespace(boolean normalizeWhitespace) {
+		this.normalizeWhitespace = normalizeWhitespace;
+	}
+
+	public QueryField getField() {
+		return field;
+	}
+
+	public void setField(QueryField field) {
+		this.field = field;
+	}
+
+	@NotNull
+	@Column(nullable = false)
+	@NotEmpty
+	private Double points;
+	
+	@NotNull
+	@Column(nullable = false)
+	@NotEmpty
+    private String text;
+	
+	@NotNull
+	@Column(nullable = false)
+	@NotEmpty
+    private boolean caseSensitive;
+	
+	@NotNull
+	@Column(nullable = false)
+	@NotEmpty
+    private boolean normalizeWhitespace;
+	
+	@ManyToOne
+	@JoinColumn(name = "field_id")
+	private QueryField field;
+		
+}
