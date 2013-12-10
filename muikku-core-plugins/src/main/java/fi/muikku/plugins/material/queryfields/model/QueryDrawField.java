1d36ca132fcfb111cae0b16fd8328f2d567e5247
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2013-12-10 14:18:45 +0200

    Added more field entities

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/queryfields/model/QueryDrawField.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/queryfields/model/QueryDrawField.java
@@ -9,7 +9,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @PrimaryKeyJoinColumn(name="id")
-public class QueryTextField extends QueryField{
+public class QueryDrawField extends QueryField{
 	
     public String getText() {
 		return text;
@@ -18,9 +18,21 @@ public class QueryTextField extends QueryField{
 		this.text = text;
 	}
 
+	public String getCanvasHtml() {
+		return canvasHtml;
+	}
+	public void setCanvasHtml(String canvasHtml) {
+		this.canvasHtml = canvasHtml;
+	}
+
 	@NotEmpty
 	@NotNull
 	@Column (nullable = false)
 	private String text;
+	
+	@NotEmpty
+	@NotNull
+	@Column (nullable = false)
+	private String canvasHtml;
 	
 }
