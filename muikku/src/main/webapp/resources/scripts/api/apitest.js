c28727ef47bc71ab78634f46f9b7b8dfebd833df
Antti Leppä <antti.leppa@foyt.fi>
2013-12-18 13:22:47 +0200

    ** Added demo cases for delete and update operations via new REST API

--- muikku/src/main/webapp/resources/scripts/api/apitest.js
+++ muikku/src/main/webapp/resources/scripts/api/apitest.js
@@ -112,15 +112,89 @@
         }
       });
         
-    $('#post-test').click(function (event) {
-      api.workspace.workspaces.create({
-        "schoolDataSource": "MOCK",
-        "name": "Test Workspace",
-        "description": "Workspace created via new REST API",
-        "workspaceTypeId": "2",
-        "courseIdentifierIdentifier": "2"
+    $('#crud-test').click(function (event) {
+      api.workspace.workspaces
+        .create({
+          "schoolDataSource": "MOCK",
+          "name": "Test Workspace - " + new Date().getTime(),
+          "description": "Workspace created via new REST API",
+          "workspaceTypeId": "2",
+          "courseIdentifierIdentifier": "2"
+        })
+        .callback(function (err, result) {
+          if (err) {
+            alert(err);
+          } else {
+            $('#crud-test-result').append($('<div>').html('<div>Created a workspace: ' + JSON.stringify(result) + '</div><div><a data-sds="' + result.schoolDataSource + '" data-id="' + result.identifier + '" class="crud-test-update" href="javascript:void(null)">Update</a>&nbsp;|&nbsp;<a data-sds="' + result.schoolDataSource + '" data-id="' + result.identifier + '" class="crud-test-delete" href="javascript:void(null)">Delete</a></div>'));
+          }
+        });
+      
+    });
+    
+    $(document).on("click", ".crud-test-update", function (event) {
+      var link = $(event.target);
+      var identifier = link.data('id');
+      var schoolDataSource = link.data('sds');
+      
+      api.workspace.workspaceEntities
+        .read({
+          schoolDataSource: schoolDataSource,
+          identifier: identifier
+        })
+        .add('$', 'id', 'workspace', api.workspace.workspaces)
+        .callback(function (err, result) {
+          if (err) {
+            alert(err);
+          } else {
+            if (result.length == 1) {
+              var workspace = result[0].workspace;
+              workspace.name += '-modified';
+              api.workspace.workspaces
+                .update(result[0].id, workspace)
+                .callback(function (updErr, updResult) {
+                  if (updErr) {
+                    alert(updErr);
+                  } else {
+                    $('#crud-test-result').append($('<div>').html('<div>Updated workspace: ' + JSON.stringify(updResult) + '</div><div><a data-sds="' + updResult.schoolDataSource + '" data-id="' + updResult.identifier + '" class="crud-test-update" href="javascript:void(null)">Update</a>&nbsp;|&nbsp;<a data-sds="' + updResult.schoolDataSource + '" data-id="' + updResult.identifier + '" class="crud-test-delete" href="javascript:void(null)">Delete</a></div>'));
+                  }
+                });
+            }
+          }
+        });
+    });
+    
+    $(document).on("click", ".crud-test-delete", function (event) {
+      var link = $(event.target);
+      var identifier = link.data('id');
+      var schoolDataSource = link.data('sds');
+      
+      api.workspace.workspaceEntities
+      .read({
+        schoolDataSource: schoolDataSource,
+        identifier: identifier
+      })
+      .add('$', 'id', 'workspace', api.workspace.workspaces)
+      .callback(function (err, result) {
+        if (err) {
+          alert(err);
+        } else {
+          if (result.length == 1) {
+            var workspace = result[0].workspace;
+            workspace.name += '-modified';
+            api.workspace.workspaces
+              .del(result[0].id)
+              .callback(function (delErr) {
+                if (delErr) {
+                  alert(delErr);
+                } else {
+                  $('#crud-test-result').append($('<div>').html('<div>Deleted workspace</div>'));
+                }
+              });
+          }
+        }
       });
     });
+    
   });
   
 })(window);
