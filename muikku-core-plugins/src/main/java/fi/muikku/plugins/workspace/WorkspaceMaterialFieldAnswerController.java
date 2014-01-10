ca889a8824b3eb8f9c01a17e0a874b750617d867
Antti Leppä <antti.leppa@foyt.fi>
2014-01-10 15:07:07 +0200

    ** Added methods for finding, creating and updating select field answers into WorkspaceMaterialFieldAnswerController

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialFieldAnswerController.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialFieldAnswerController.java
@@ -2,15 +2,24 @@ package fi.muikku.plugins.workspace;
 
 import javax.inject.Inject;
 
+import fi.muikku.plugins.materialfields.model.QuerySelectField;
 import fi.muikku.plugins.materialfields.model.QueryTextField;
+import fi.muikku.plugins.materialfields.model.SelectFieldOption;
+import fi.muikku.plugins.workspace.dao.WorkspaceMaterialSelectFieldAnswerDAO;
 import fi.muikku.plugins.workspace.dao.WorkspaceMaterialTextFieldAnswerDAO;
 import fi.muikku.plugins.workspace.model.WorkspaceMaterialReply;
+import fi.muikku.plugins.workspace.model.WorkspaceMaterialSelectFieldAnswer;
 import fi.muikku.plugins.workspace.model.WorkspaceMaterialTextFieldAnswer;
 
 public class WorkspaceMaterialFieldAnswerController {
 
   @Inject
   private WorkspaceMaterialTextFieldAnswerDAO workspaceMaterialTextFieldAnswerDAO;
+
+  @Inject
+  private WorkspaceMaterialSelectFieldAnswerDAO workspaceMaterialSelectFieldAnswerDAO;
+  
+  /* TextField */
   
   public WorkspaceMaterialTextFieldAnswer createWorkspaceMaterialTextFieldAnswer(QueryTextField queryField, WorkspaceMaterialReply reply, String value) {
     return workspaceMaterialTextFieldAnswerDAO.create(queryField, reply, value);
@@ -22,6 +31,20 @@ public class WorkspaceMaterialFieldAnswerController {
   
   public WorkspaceMaterialTextFieldAnswer updateWorkspaceMaterialTextFieldAnswerValue(WorkspaceMaterialTextFieldAnswer workspaceMaterialTextFieldAnswer, String value) {
     return workspaceMaterialTextFieldAnswerDAO.updateValue(workspaceMaterialTextFieldAnswer, value);
+  }
+  
+  /* SelectField */
+
+  public WorkspaceMaterialSelectFieldAnswer createWorkspaceMaterialSelectFieldAnswer(QuerySelectField queryField,  WorkspaceMaterialReply reply, SelectFieldOption value) {
+    return workspaceMaterialSelectFieldAnswerDAO.create(queryField, reply, value);
+  }
+
+  public WorkspaceMaterialSelectFieldAnswer findWorkspaceMaterialSelectFieldAnswerByQueryFieldAndReply(QuerySelectField queryField,  WorkspaceMaterialReply reply) {
+    return workspaceMaterialSelectFieldAnswerDAO.findByQueryFieldAndReply(queryField, reply);
+  }
+
+  public WorkspaceMaterialSelectFieldAnswer updateWorkspaceMaterialSelectFieldAnswerValue(WorkspaceMaterialSelectFieldAnswer workspaceMaterialSelectFieldAnswer, SelectFieldOption value) {
+    return workspaceMaterialSelectFieldAnswerDAO.updateValue(workspaceMaterialSelectFieldAnswer, value);
   }
 
 }
