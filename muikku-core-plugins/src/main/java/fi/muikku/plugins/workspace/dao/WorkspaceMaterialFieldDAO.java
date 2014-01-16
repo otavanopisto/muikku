99ad2eb33a9a5e0470f6c727993b4f1fedca1c6d
Antti Leppä <antti.leppa@foyt.fi>
2014-01-16 14:26:59 +0200

    ** Added listByWorkspaceMaterial method into WorkspaceMaterialFieldDAO

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/dao/WorkspaceMaterialFieldDAO.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/dao/WorkspaceMaterialFieldDAO.java
@@ -9,11 +9,7 @@ import javax.persistence.criteria.Root;
 
 import fi.muikku.dao.DAO;
 import fi.muikku.plugin.PluginDAO;
-import fi.muikku.plugins.material.model.Material;
 import fi.muikku.plugins.materialfields.model.QueryField;
-import fi.muikku.plugins.materialfields.model.QuerySelectField;
-import fi.muikku.plugins.materialfields.model.SelectFieldOption;
-import fi.muikku.plugins.materialfields.model.SelectFieldOption_;
 import fi.muikku.plugins.workspace.model.WorkspaceMaterial;
 import fi.muikku.plugins.workspace.model.WorkspaceMaterialField;
 import fi.muikku.plugins.workspace.model.WorkspaceMaterialField_;
@@ -35,7 +31,6 @@ public class WorkspaceMaterialFieldDAO extends PluginDAO<WorkspaceMaterialField>
   }
   
   public List<WorkspaceMaterialField> findMaterialFieldByMaterial(WorkspaceMaterial material){
-    
     EntityManager entityManager = getEntityManager();
 
     CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
@@ -46,6 +41,37 @@ public class WorkspaceMaterialFieldDAO extends PluginDAO<WorkspaceMaterialField>
 
     return entityManager.createQuery(criteria).getResultList();
     
+  }
+
+  public WorkspaceMaterialField findMaterialFieldByWorkspaceMaterialAndName(WorkspaceMaterial workspaceMaterial, String name) {
+    EntityManager entityManager = getEntityManager();
+
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<WorkspaceMaterialField> criteria = criteriaBuilder.createQuery(WorkspaceMaterialField.class);
+    Root<WorkspaceMaterialField> root = criteria.from(WorkspaceMaterialField.class);
+    criteria.select(root);
+    criteria.where(
+      criteriaBuilder.and(
+        criteriaBuilder.equal(root.get(WorkspaceMaterialField_.material), workspaceMaterial),
+        criteriaBuilder.equal(root.get(WorkspaceMaterialField_.name), name)
+      )
+    );
+
+    return getSingleResult(entityManager.createQuery(criteria));    
+  }
+
+  public List<WorkspaceMaterialField> listByWorkspaceMaterial(WorkspaceMaterial workspaceMaterial) {
+    EntityManager entityManager = getEntityManager();
+
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<WorkspaceMaterialField> criteria = criteriaBuilder.createQuery(WorkspaceMaterialField.class);
+    Root<WorkspaceMaterialField> root = criteria.from(WorkspaceMaterialField.class);
+    criteria.select(root);
+    criteria.where(
+      criteriaBuilder.equal(root.get(WorkspaceMaterialField_.material), workspaceMaterial)
+    );
+
+    return entityManager.createQuery(criteria).getResultList();
   }
 
 }
