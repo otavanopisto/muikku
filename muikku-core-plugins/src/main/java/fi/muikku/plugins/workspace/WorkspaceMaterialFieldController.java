eec8d9c9830bf19e3061e4c7f220128101c13cdd
Antti Leppä <antti.leppa@foyt.fi>
2014-01-16 14:28:15 +0200

    ** Added method for listing workspace material fields into WorkspaceMaterialFieldController ** Added method for creating workspace material fields into WorkspaceMaterialFieldController

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialFieldController.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialFieldController.java
@@ -1,11 +1,27 @@
 package fi.muikku.plugins.workspace;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
+import org.apache.commons.lang3.math.NumberUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import fi.muikku.plugins.material.HtmlMaterialController;
+import fi.muikku.plugins.material.MaterialController;
+import fi.muikku.plugins.material.model.HtmlMaterial;
+import fi.muikku.plugins.material.model.Material;
+import fi.muikku.plugins.materialfields.QueryFieldController;
+import fi.muikku.plugins.materialfields.model.QueryField;
 import fi.muikku.plugins.workspace.dao.WorkspaceMaterialFieldDAO;
 import fi.muikku.plugins.workspace.model.WorkspaceMaterial;
 import fi.muikku.plugins.workspace.model.WorkspaceMaterialField;
@@ -15,10 +31,66 @@ import fi.muikku.plugins.workspace.model.WorkspaceMaterialField;
 public class WorkspaceMaterialFieldController {
 
   @Inject
+  private HtmlMaterialController htmlMaterialController;
+  
+  @Inject
+  private QueryFieldController queryFieldController;
+  
+  @Inject
+  private MaterialController materialController;
+  
+  @Inject
   private WorkspaceMaterialFieldDAO workspaceMaterialFieldDAO;
   
   public List<WorkspaceMaterialField> findWorkspaceMaterialFieldsByMaterial(WorkspaceMaterial material){
     return workspaceMaterialFieldDAO.findMaterialFieldByMaterial(material);
+  }
+
+  public List<WorkspaceMaterialField> listWorkspaceMaterialFieldsByWorkspaceMaterial(WorkspaceMaterial workspaceMaterial) {
+    return workspaceMaterialFieldDAO.listByWorkspaceMaterial(workspaceMaterial);
+  }
+  
+  public void createWorkspaceMaterialFields(String fieldPrefix, WorkspaceMaterial workspaceMaterial) throws SAXException, IOException, XPathExpressionException, MaterialQueryIntegrityExeption {
+    if (workspaceMaterial.getMaterial() instanceof HtmlMaterial) {
+      HtmlMaterial htmlMaterial = (HtmlMaterial) workspaceMaterial.getMaterial();
+      
+      Document htmlDocument = htmlMaterialController.getProcessedHtmlDocument(fieldPrefix, htmlMaterial);
+      NodeList formFieldNodes = (NodeList) XPathFactory.newInstance().newXPath().evaluate("//INPUT|//TEXTAREA|//SELECT", htmlDocument, XPathConstants.NODESET);
+      htmlMaterialController.assignMaterialFieldNames(formFieldNodes, fieldPrefix, true);
+      
+      for (int i = 0, l = formFieldNodes.getLength(); i < l; i++) {
+        if (formFieldNodes.item(i) instanceof Element) {
+          Element formFieldElement = (Element) formFieldNodes.item(i);
+          String unencodedName = formFieldElement.getAttribute("data-unencoded-name");
+          String encodedName = formFieldElement.getAttribute("name");
+          
+          String[] nameParts = unencodedName.split(":");
+          String fieldName = nameParts[nameParts.length - 2];
+          Material fieldMaterial = null;
+          
+          if (nameParts.length > 2) {
+            Long fieldMaterialId = NumberUtils.createLong(nameParts[nameParts.length - 3]);
+            Material material = materialController.findMaterialById(fieldMaterialId);
+            if (material == null) {
+              throw new MaterialQueryIntegrityExeption("Embedded field " + unencodedName + " points to non-existing material");
+            }
+
+            fieldMaterial = material;
+          } else {
+            fieldMaterial = htmlMaterial;
+          }
+         
+          QueryField queryField = queryFieldController.findQueryTextFieldByMaterialAndName(fieldMaterial, fieldName); 
+          if (queryField == null) {
+            throw new MaterialQueryIntegrityExeption("Material #" + fieldMaterial.getId() + " does not contain field '" + fieldName + "'");
+          }
+          
+          if (workspaceMaterialFieldDAO.findMaterialFieldByWorkspaceMaterialAndName(workspaceMaterial, encodedName) == null) {
+            workspaceMaterialFieldDAO.create(encodedName, queryField, workspaceMaterial);
+          }
+        }
+      }
+    }
   }
   
 }
