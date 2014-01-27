10e4fadef6ad28f001c818335197ae15fdd77f9e
ilmoeuro <ilmo.euro@gmail.com>
2014-01-27 13:31:20 +0200

    ** Added dummy mail bridge plugin

--- muikku-core/src/main/java/fi/muikku/mail/MailAttachment.java
+++ muikku-core/src/main/java/fi/muikku/mail/MailAttachment.java
@@ -1,5 +1,7 @@
 package fi.muikku.mail;
 
+import org.apache.commons.codec.binary.Hex;
+
 public class MailAttachment {
   
   public MailAttachment(String mimeType, byte[] content) {
@@ -13,6 +15,10 @@ public class MailAttachment {
   
   public byte[] getContent() {
     return content;
+  }
+  
+  public String toString() {
+    return "Type: " + mimeType + " Content: 0x" + Hex.encodeHexString(content);
   }
 
   private final String mimeType;
---
+++ muikku-dummy-mail-bridge-plugin/src/main/java/fi/muikku/plugins/DummyMailBridgePluginLibraryDescriptor.java
@@ -0,0 +1,30 @@
+package fi.muikku.plugins;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import fi.muikku.plugin.PluginDescriptor;
+import fi.muikku.plugin.PluginLibraryDescriptor;
+import fi.muikku.plugins.mail.dummy.DummyMailBridgePluginDescriptor;
+
+public class DummyMailBridgePluginLibraryDescriptor implements PluginLibraryDescriptor {
+
+	@Inject
+	private Logger logger;
+
+	@Override
+	public String getName() {
+		return "h2db";
+	}
+
+	@Override
+	public List<Class<? extends PluginDescriptor>> getPlugins() {
+		return new ArrayList<Class<? extends PluginDescriptor>>(
+		  Arrays.asList(DummyMailBridgePluginDescriptor.class)
+		);
+	}
+}
