e4205541054098035bc9a071de34531a31fa73f4
ilmoeuro <ilmo.euro@gmail.com>
2014-01-30 11:23:38 +0200

    Renamed: sendMailSimple -> sendMail

--- muikku-core/src/main/java/fi/muikku/mail/Mailer.java
+++ muikku-core/src/main/java/fi/muikku/mail/Mailer.java
@@ -2,37 +2,33 @@ package fi.muikku.mail;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.activation.MimeType;
-import javax.activation.MimeTypeParseException;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 
+
 @RequestScoped
 public class Mailer {
-  
+
   @Inject
   private Logger logger;
-  
+
   @Inject
   private MailBridge bridge;
-  
+
   public void sendMail(MailType mailType, String from, List<String> to, List<String> cc, List<String> bcc, String content, List<MailAttachment> attachments) {
     bridge.sendMail(mailType, from, to, cc, bcc, content, attachments);
   }
-  
-  public void sendMailSimple(String from, String to, String content) {
+
+  public void sendMail(String from, String to, String content) {
     List<String> toList = new ArrayList<>();
     toList.add(to);
     this.sendMail(MailType.PLAINTEXT, from, toList, new ArrayList<String>(), new ArrayList<String>(), content, new ArrayList<MailAttachment>());
-
   }
   
   public void sendMailToGroup(String from, List<String> bcc, String content) {
-      this.sendMail(MailType.PLAINTEXT, from, new ArrayList<String>(), new ArrayList<String>(), bcc, content, new ArrayList<MailAttachment>());
+    this.sendMail(MailType.PLAINTEXT, from, new ArrayList<String>(), new ArrayList<String>(), bcc, content, new ArrayList<MailAttachment>());
   }
-  
-  
+
 }
