114fe8f8a5335ff70d72648bee9516abbe2f8b20
Antti Leppä <antti.leppa@foyt.fi>
2014-02-03 14:08:26 +0200

    ** Removed checklist field rendering from WorkspaceSelectFieldHandler

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/fieldhandler/WorkspaceSelectFieldHandler.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/fieldhandler/WorkspaceSelectFieldHandler.java
@@ -57,10 +57,6 @@ public class WorkspaceSelectFieldHandler extends AbstractWorkspaceFieldHandler {
       case "radio_horz":
         renderRadioField(ownerDocument, objectElement, selectFieldMeta, parameterName, fieldAnswer, false);
       break;
-      case "checklist":
-        // TODO: Add proper checklist support
-        renderSelectField(ownerDocument, objectElement, selectFieldMeta, parameterName, fieldAnswer);
-      break;
     }
   }
 
