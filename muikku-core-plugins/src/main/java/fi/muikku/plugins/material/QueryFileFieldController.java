a64130ebcab684de50598c20f6cca83784319c0b
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2014-02-04 14:33:30 +0200

    ** Added entitiy, DAO and controller for QueryFileField

---
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/QueryFileFieldController.java
@@ -0,0 +1,32 @@
+package fi.muikku.plugins.material;
+
+import javax.ejb.Stateless;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import fi.muikku.plugins.material.dao.QueryFileFieldDAO;
+import fi.muikku.plugins.material.dao.QueryTextFieldDAO;
+import fi.muikku.plugins.material.model.Material;
+import fi.muikku.plugins.material.model.QueryFileField;
+import fi.muikku.plugins.material.model.QueryTextField;
+
+@Stateless
+@Dependent
+public class QueryFileFieldController {
+
+  @Inject
+  private QueryFileFieldDAO queryFileFieldDAO;
+
+  public QueryFileField createQueryFileField(Material material, String name) {
+    return queryFileFieldDAO.create(material, name);
+  }
+
+  public QueryFileField findQueryFileFieldbyId(Long id) {
+    return queryFileFieldDAO.findById(id);
+  }
+
+  public QueryFileField findQueryFileFieldByMaterialAndName(Material material, String name) {
+    return queryFileFieldDAO.findByMaterialAndName(material, name);
+  }
+
+}
