08184e85d311dbc192886f7a645034724cd93eea
Antti Leppä <antti.leppa@foyt.fi>
2014-02-19 13:18:17 +0200

    ** Changed WorkspaceMaterialDeleteListener to remove all related replies from WorkspaceMaterial

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialDeleteListener.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialDeleteListener.java
@@ -9,18 +9,26 @@ import javax.inject.Inject;
 import fi.muikku.plugins.workspace.events.WorkspaceMaterialDeleteEvent;
 import fi.muikku.plugins.workspace.model.WorkspaceMaterial;
 import fi.muikku.plugins.workspace.model.WorkspaceMaterialField;
+import fi.muikku.plugins.workspace.model.WorkspaceMaterialReply;
 
 @Stateless
 public class WorkspaceMaterialDeleteListener {
 
   @Inject
   private WorkspaceMaterialFieldController workspaceMaterialFieldController;
-  
+
+  @Inject
+  private WorkspaceMaterialReplyController workspaceMaterialReplyController;
   public void onWorkspaceMaterialDelete(@Observes WorkspaceMaterialDeleteEvent event) {
     WorkspaceMaterial workspaceMaterial = event.getWorkspaceNode();
     List<WorkspaceMaterialField> workspaceMaterialFields = workspaceMaterialFieldController.listWorkspaceMaterialFieldsByWorkspaceMaterial(workspaceMaterial);
     for (WorkspaceMaterialField workspaceMaterialField : workspaceMaterialFields) {
       workspaceMaterialFieldController.deleteWorkspaceMaterialField(workspaceMaterialField);
+    }
+    
+    List<WorkspaceMaterialReply> workspaceMaterialReplies = workspaceMaterialReplyController.listWorkspaceMaterialRepliesByWorkspaceMaterial(workspaceMaterial);
+    for (WorkspaceMaterialReply workspaceMaterialReply : workspaceMaterialReplies) {
+      workspaceMaterialReplyController.deleteWorkspaceMaterialReply(workspaceMaterialReply);
     }
   }
   
