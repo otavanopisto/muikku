8833f4c4652f32ee15d328ccbaf8e739f358e6b4
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2014-04-08 14:53:08 +0300

    ** Renamed IndexEvents to EntityEvents

--- muikku-persistence/src/main/java/fi/muikku/dao/events/IndexAddEvent.java
+++
@@ -1,9 +0,0 @@
-package fi.muikku.dao.events;
-
-public class IndexAddEvent extends IndexEvent<Object>{
-
-  public IndexAddEvent(Object field) {
-    super(field);
-  }
-
-}
--- muikku-persistence/src/main/java/fi/muikku/dao/events/IndexRemoveEvent.java
+++
@@ -1,9 +0,0 @@
-package fi.muikku.dao.events;
-
-public class IndexRemoveEvent extends IndexEvent<Object>{
-
-  public IndexRemoveEvent(Object field) {
-    super(field);
-  }
-
-}
