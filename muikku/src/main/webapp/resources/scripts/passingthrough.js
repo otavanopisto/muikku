a007ff7f608c685ad1f6f42ad275adaa29767486
avukkonen <antti.ukkonen@otavanopisto.fi>
2014-04-10 20:35:03 +0300

    Search javascript

--- muikku/src/main/webapp/resources/scripts/passingthrough.js
+++ muikku/src/main/webapp/resources/scripts/passingthrough.js
@@ -68,35 +68,72 @@ $(document).ready(function() {
     $("div[class*='wi-dock-static-navi']").mouseenter(function() {
     	var tooltip = $(this).find("[class*='dock-navi-tt-container']");
     	var innerTooltip = tooltip.children('div');
-    	tooltip.stop().show("fade", 100);
-      
+    	var hasSearch = $(this).has("input[id='seeker']").length ? "1" : "0";
+    	var search = $(".wi-dock-search");
+
+
+
+
+    	
+        
+        if(hasSearch == "1"){
+        	search.show("drop", 100);     	
+	    }else{
+	    	tooltip.stop().show("blind", 100, function(){
+	    		
+	
+	
+	    		
+	    	} );
+	    }
+
+    	
         // Offsets
         var iconOffset = tooltip.parent().offset().left + 15;
         var tOffset = tooltip.offset().left;
         var paddingOffset = tOffset + 20;
-
-        // Sets tooltip's width same as view port width 
-        // and left position is adjusted accordingly
-        tooltip.css({
-        	width: $(window).width(),
-        	left:-tOffset + 'px'
-        });
-      
+        
+        
         // Sets tooltip's inner elements padding-left same as tooltip's left position 
         // and sets background-position relative to icons position
         innerTooltip.css({
         	paddingLeft: paddingOffset + 'px',
         	backgroundPosition: iconOffset + 'px 0px'
         });
-      
+        
+        
+        // Sets tooltip's width same as view port width 
+        // and left position is adjusted accordingly
+        tooltip.css({
+        	width: $(window).width(),
+        	left:-tOffset + 'px'
+        });
+    	
+    	
+
+            
+
+        
     });
 
     $("div[class*='wi-dock-static-navi']").mouseleave(function() {
     	var tooltip =  $(this).find("[class*='dock-navi-tt-container']");
-    	tooltip.stop().hide();
-    	tooltip.css({
-    		left: "0px"
-        });
+    	var hasSearch = $(this).has("input[id='seeker']").length ? "1" : "0";
+    	var search = $(".wi-dock-search");
+
+    	
+        if(hasSearch == "1"){
+            	search.hide("drop", 100);     	
+        }else{
+        	
+        	tooltip.stop().hide();
+        	tooltip.css({
+        		left: "0px"
+            });
+        	
+        }
+    
+    
     });
 
 });
