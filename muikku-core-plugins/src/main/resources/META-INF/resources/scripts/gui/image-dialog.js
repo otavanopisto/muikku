9597fb4332f061c1bf849198d47c99e3a6d4b568
ilmoeuro <ilmo.euro@gmail.com>
2014-05-13 15:14:32 +0300

    Crop shows target size & preview zoom; aspect ratio dropdown

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/image-dialog.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/image-dialog.js
@@ -52,8 +52,7 @@
       okButtonText: 'Ok',
       cancelButtonText: 'Cancel',
       uploadHintText: 'Change image by clicking here or by dragging image file into this box',
-      imageWidth: 192,
-      imageHeight: 128
+      imageMaxSize: 200
     },
     _create : function() {
       if (!window.FileReader) {
@@ -79,8 +78,38 @@
         .appendTo(this.element);
 
       $('<div>')
+              .addClass('image-dialog-info-container')
+              .append(
+                    $('<select>')
+                    .addClass('image-dialog-aspect')
+                    .append(
+                        $('<option>')
+                        .val('1.0')
+                        .html('1:1'),
+                        $('<option>')
+                        .val('1.5')
+                        .html('3:2'),
+                        $('<option>')
+                        .val('0.75')
+                        .html('2:3'),
+                        $('<option>')
+                        .val('0.707107')
+                        .html('1:&radic;2'),
+                        $('<option>')
+                        .val('1.707107')
+                        .html('&radic;2:1'),
+                        $('<option>')
+                        .val('NaN')
+                        .html('Free')
+                    )
+                    .on('change', $.proxy(this._onAspectChange, this))
+            )
+            .appendTo(this.element);
+
+      $('<div>')
         .addClass('image-dialog-preview-container')
         .append($('<canvas>').addClass('image-dialog-preview'))
+        .append($('<div>').addClass('image-dialog-preview-info'))
         .appendTo(this.element);
 
       $('<div>')
@@ -121,42 +150,84 @@
       });
     },
 
-    _onImageLoad: function (event) {
-      var imageElement = $(event.target);
-
+    _refresh: function (imageElement) {
       if (this._jCropApi) {
         this._jCropApi.destroy();
       }
 
-      var originalWidth = $(imageElement).width();
-      var originalHeight = $(imageElement).height();
-      var xRatio = originalWidth / this.element.find('.image-dialog-image-container').width();
-      var yRatio = originalHeight / this.element.find('.image-dialog-image-container').height();
-      var ratio = xRatio > yRatio ? xRatio : yRatio;
+      var originalWidth = imageElement.get(0).naturalWidth;
+      var originalHeight = imageElement.get(0).naturalHeight;
+      var container = this.element.find('.image-dialog-image-container');
+      var invRatio = this._calculateScaleFactor(
+              originalWidth,
+              originalHeight,
+              container.width(),
+              container.height());
+      var ratio = 1/invRatio;
       var newWidth = originalWidth / ratio;
       var newHeight = originalHeight / ratio;
+      var aspect = +this.element.find('.image-dialog-aspect').val();
+      var cropInitialSize = (this.options.imageMaxSize / aspect) | 0;
+      if (isNaN(aspect)) {
+          cropInitialSize = this.options.imageMaxSize;
+      }
 
-      var _this = this;
-      $(imageElement)
-        .attr({
-          'width': newWidth,
-          'height': newHeight,
-          'data-ratio': ratio
-        })
-        .Jcrop({
-          setSelect: [0, 0, this.options.imageWidth, this.options.imageHeight],
+      var scaleFactor = this._calculateScaleFactor(
+            cropInitialSize,
+            cropInitialSize,
+            originalWidth,
+            originalHeight);
+
+      cropInitialSize *= scaleFactor;
+
+      var jCropOptions = {
           minSize: [1, 1],
           onChange: $.proxy(this._onCropChange, this),
           onSelect: $.proxy(this._onCropChange, this),
           allowSelect: true,
           allowMove: true,
           allowResize: true
-        }, function () {
+        };
+
+      if (isNaN(aspect)) {
+          jCropOptions['setSelect'] =
+                  [0, 0, cropInitialSize, cropInitialSize];
+      } else {
+          jCropOptions['setSelect'] =
+                  [0, 0, cropInitialSize*aspect, cropInitialSize];
+          jCropOptions['aspectRatio'] = aspect;
+      }
+
+      var _this = this;
+      $(imageElement)
+        .data('ratio', ratio)
+        .attr('width', newWidth)
+        .width(newWidth)
+        .attr('height', newHeight)
+        .height(newHeight)
+        .Jcrop(jCropOptions, function () {
           _this._jCropApi = this;
           _this.element.find('.image-dialog-image-container').removeClass('image-dialog-image-loading');
+          _this.element.find('.image-dialog-zoom').html((1.0/ratio).toFixed(2) + "x");
+          _this._refreshPreviewInfo();
         });
+    },
 
-        this.element.find('.image-dialog-zoom').html((1.0/ratio).toFixed(2) + "x");
+    _refreshPreviewInfo: function() {
+        var width = +this.element.find('.image-dialog-preview').attr('width');
+        var height = +this.element.find('.image-dialog-preview').attr('height');
+        var cssWidthPx = this.element.find('.image-dialog-preview').css('width');
+        var cssWidth = parseInt(cssWidthPx, 10);
+        var zoom = cssWidth / width;
+
+        this.element.find('.image-dialog-preview-info').html(
+                width.toFixed(0) + "x" + height.toFixed(0) +
+                " (" + zoom.toFixed(2) + "x)"
+        );
+    },
+
+    _onImageLoad: function (event) {
+      this._refresh($(event.target));
     },
 
     _onUploadFieldChange: function (event) {
@@ -217,17 +288,31 @@
         }
       }
 
-      this.element.find('.image-dialog-image')
-        .removeAttr('width')
-        .removeAttr('height');
-
       if (data && ((rotate != 0) || flip)) {
         flipAndRotateImage(src, rotate, flip, $.proxy(function (dataUrl) {
-          this.element.find('.image-dialog-image').attr('src', dataUrl);
+          this.element.find('.image-dialog-image')
+                   .attr('src', dataUrl);
         }, this));
       } else {
-        this.element.find('.image-dialog-image').attr('src', src);
+        this.element.find('.image-dialog-image')
+                   .attr('src', src);
       }
+    },
+
+    _calculateScaleFactor: function(width, height, maxWidth, maxHeight) {
+        var xScaleFactor = 1;
+        var yScaleFactor = 1;
+
+        if (width > maxWidth) {
+            xScaleFactor = maxWidth/width;
+        }
+        if (height > maxHeight) {
+            yScaleFactor = maxHeight/height;
+        }
+
+        return xScaleFactor < yScaleFactor
+                          ? xScaleFactor
+                          : yScaleFactor;
     },
 
     _onCropChange: function(coords) {
@@ -235,21 +320,42 @@
         var previewCanvas = this.element.find('.image-dialog-preview');
         var maxWidth = this.element.find('.image-dialog-preview-container').width();
         var maxHeight = this.element.find('.image-dialog-preview-container').height();
-        var canvasWidth;
-        var canvasHeight;
-        if (coords.w > coords.h) {
-        	canvasWidth = maxWidth;
-        	canvasHeight = maxWidth * (coords.h / coords.w);
-        } else {
-        	canvasHeight = maxHeight;
-        	canvasWidth = maxWidth * (coords.w / coords.h);
-        }
         var ratio = this.element.find('.image-dialog-image').data('ratio');
 
+        var canvasWidth = coords.w * ratio;
+        var canvasHeight = coords.h * ratio;
+        if (canvasWidth > maxWidth || canvasHeight > maxHeight) {
+            if (coords.w > coords.h) {
+                canvasWidth = maxWidth;
+                canvasHeight = maxWidth * (coords.h / coords.w);
+            } else {
+                canvasWidth = maxHeight * (coords.w / coords.h);
+                canvasHeight = maxHeight;
+            }
+        }
+
+        var finalWidth = coords.w * ratio;
+        var finalHeight = coords.h * ratio;
+
+        var scaleFactor = this._calculateScaleFactor(
+                finalWidth,
+                finalHeight,
+                this.options.imageMaxSize,
+                this.options.imageMaxSize);
+
+        finalWidth *= scaleFactor;
+        finalHeight *= scaleFactor;
+
         previewCanvas.attr({
-          width: canvasWidth,
-          height: canvasHeight
+          width: finalWidth,
+          height: finalHeight
         });
+        previewCanvas.css({
+          width: canvasWidth + "px",
+          height: canvasHeight + "px"
+        });
+
+        this._refreshPreviewInfo();
 
         var previewContext = previewCanvas.get(0).getContext('2d');
         previewContext.drawImage(this.element.find('.image-dialog-image').get(0),
@@ -259,9 +365,14 @@
                                  coords.h * ratio,
                                  0,
                                  0,
-                                 canvasWidth,
-                                 canvasHeight);
+                                 finalWidth,
+                                 finalHeight);
       }
+    },
+
+    _onAspectChange: function(event) {
+        this._jCropApi.destroy();
+        this._refresh(this.element.find('.image-dialog-image'));
     },
 
     _destroy : function() {
