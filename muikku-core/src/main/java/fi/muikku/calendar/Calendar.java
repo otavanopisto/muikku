8f8d579ea052432276c162b02e93c7b560930be1
Antti Leppä <antti.leppa@foyt.fi>
2014-06-03 10:08:14 +0300

    ** Updated Calendar interface in core

--- muikku-core/src/main/java/fi/muikku/calendar/Calendar.java
+++ muikku-core/src/main/java/fi/muikku/calendar/Calendar.java
@@ -2,8 +2,34 @@ package fi.muikku.calendar;
 
 public interface Calendar {
 
+  /**
+   * Returns calendar identifier
+   * 
+   * Identifier must be unique within service provider.
+   * 
+   * @return calendar identifier
+   */
+  public String getId();
+  
+  /**
+   * Returns name of calendar service provider
+   * 
+   * @return name of calendar service provider
+   */
+  public String getServiceProvider();
+  
+  /**
+   * Returns a summary (title) of the calendar
+   * 
+   * @return a summary (title) of the calendar
+   */
   public String getSummary();
   
+  /**
+   * Returns description of the calendar
+   * 
+   * @return description of the calendar
+   */
   public String getDescription();
   
 }
