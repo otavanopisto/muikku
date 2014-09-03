b74dd10a427acea63eac23cfafe77a9b48cfcb3b
Antti Leppä <antti.leppa@foyt.fi>
2014-09-03 07:32:24 +0300

    ** Removed deprecated muikku-plugin-core module

--- muikku-h2db-plugin/src/main/java/fi/muikku/plugins/H2DBPluginLibraryDescriptor.java
+++ muikku-h2db-plugin/src/main/java/fi/muikku/plugins/H2DBPluginLibraryDescriptor.java
@@ -3,18 +3,12 @@ package fi.muikku.plugins;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Logger;
-
-import javax.inject.Inject;
 
 import fi.muikku.plugin.PluginDescriptor;
 import fi.muikku.plugin.PluginLibraryDescriptor;
 import fi.muikku.plugins.h2db.H2DBPluginDescriptor;
 
 public class H2DBPluginLibraryDescriptor implements PluginLibraryDescriptor {
-
-	@Inject
-	private Logger logger;
 
 	@Override
 	public String getName() {
