ffce3ea16eda6f1e37565880b8167802710d2063
Antti Leppä <antti.leppa@foyt.fi>
2014-09-03 08:11:54 +0300

    ** Changed plugins to use their own persistence units because shared PUs are not permitted by JPA

--- muikku-plugin/src/main/java/fi/muikku/plugin/PluginDAO.java
+++ muikku-plugin/src/main/java/fi/muikku/plugin/PluginDAO.java
@@ -1,36 +1,9 @@
 package fi.muikku.plugin;
 
-import javax.annotation.Resource;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.transaction.Status;
-import javax.transaction.UserTransaction;
 import fi.muikku.dao.AbstractDAO;
 
-public class PluginDAO<T> extends AbstractDAO<T> {
+public abstract class PluginDAO<T> extends AbstractDAO<T> {
 
-  private static final long serialVersionUID = -146157476338894997L;
-
-  @Resource
-  private UserTransaction userTransaction;
-
-  @SuppressWarnings("cdi-ambiguous-dependency")
-  @Inject
-  @PluginPersistence
-  private EntityManager pluginEntityManager;
-
-  @Override
-  protected EntityManager getEntityManager() {
-    try {
-      boolean hasTransaction = userTransaction.getStatus() == Status.STATUS_ACTIVE;
-      if (hasTransaction) {
-        pluginEntityManager.joinTransaction();
-      }
-    } catch (Exception e) {
-      // TODO exception handling
-    }
-
-    return pluginEntityManager;
-  }
-
+  private static final long serialVersionUID = 1L;
+ 
 }
