2d03f43d201692d12fef3d8428ad9c697b24df8b
Antti Leppä <antti.leppa@foyt.fi>
2014-09-04 06:47:39 +0300

    ** Fixed mongo-log plugin to work with new plugin system

--- muikku-mongo-log/src/main/java/fi/muikku/plugins/mongolog/MongoLogPluginDescriptor.java
+++ muikku-mongo-log/src/main/java/fi/muikku/plugins/mongolog/MongoLogPluginDescriptor.java
@@ -1,16 +1,7 @@
 package fi.muikku.plugins.mongolog;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.ejb.Stateful;
-import javax.enterprise.context.ApplicationScoped;
-
 import fi.muikku.plugin.PluginDescriptor;
 
-@ApplicationScoped
-@Stateful
 public class MongoLogPluginDescriptor implements PluginDescriptor {
 	
 	@Override
@@ -18,15 +9,4 @@ public class MongoLogPluginDescriptor implements PluginDescriptor {
 		return "mongo-log";
 	}
 	
-	@Override
-	public void init() {
-	}
-
-	@Override
-	public List<Class<?>> getBeans() {
-    return new ArrayList<Class<?>>(Arrays.asList(
-        MongoLogProvider.class
-    ));
-	}
-
 }
