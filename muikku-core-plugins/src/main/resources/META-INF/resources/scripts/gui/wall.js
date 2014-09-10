8f3e5be013e752deed95cbddedf5fe50cd2a6aa2
Antti Leppä <antti.leppa@foyt.fi>
2014-09-10 08:47:36 +0300

    ** Fixed wall widgets to work with new rest end-points

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/wall.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/wall.js
@@ -1,55 +1,67 @@
 (function() {
   
-  WallWidgetController = $.klass(WidgetController, {
-    initialize: function () {
-    },
-    setup: function (widgetElement) {
-      widgetElement = $(widgetElement);
-      this._widgetElement = widgetElement;
-      this._wallId = widgetElement.find("input[name='wallId']").val();
-      this._userId = widgetElement.find("input[name='userId']").val();
-      this._wallEntriesContainer = widgetElement.find(".wallEntries");
-      
-      this._listWallEntries();
-      
-      $(document).on('wall_newWallEntry', $.proxy(this._onNewWallEntryNotification, this));
-    },
-    deinitialize: function () {
-    },
-    _listWallEntries: function () {
-      var _this = this;
-
-      RESTful.doGet(CONTEXTPATH + "/rest/wall/{wallId}/listWallEntries", {
-        parameters: {
-          'wallId': this._wallId
-        }
-      }).success(function (data, textStatus, jqXHR) {
-        _this._wallEntriesContainer.children().remove();
+  function getFeedItemController(item) {
+    var controllerClass = window._wallEntryControllers[item.type];
+    return new controllerClass();
+  }
+  
+  $(document).on('wall_newWallEntry', function (event, data) {
+    $('.wallWidget>input[name="wallId"]').each(function (index, wallId) {
+      if ($(wallId).val() == data.wallId) {
+        var item = {
+          type: 'wallEntries',
+          entry: data.newEntryData
+        };
         
-        for (var i = 0, len = data.length; i < len; i++) {
-          var dataNode = data[i];
-          var renderer = _this._getWallItemRenderer(dataNode.wallFeedItemName);
-          var element = renderer.render(dataNode);
-          _this._wallEntriesContainer.append(element);
-        }
-      });
-    },
-    _getWallItemRenderer: function (wallItemName) {
-      var controllerClass = window._wallEntryControllers[wallItemName];
-
-      return new controllerClass();
-    },
-    _onNewWallEntryNotification: function (e, eventInfo) {
-      var _this = this;
-      if (eventInfo.wallId == _this._wallId) {
-        renderDustTemplate('wall/wallentry.dust', eventInfo.newEntryData, function (text) {
-          _this._wallEntriesContainer.append($.parseHTML(text));
+        getFeedItemController(item).render(item, function (renderErr, html) {
+          if (renderErr) {
+            $('.notification-queue').notificationQueue('notification', 'error', renderErr);  
+          } else {
+            $(wallId).closest('.wallWidget').find('.wallEntries').prepend(html); 
+          }
         });
       }
-    }
+    });
   });
   
-  addWidgetController('wallWidget', WallWidgetController);
+  $(document).ready(function () {
+    var wallId = $('.wallWidget>input[name="wallId"]').val();
+    
+    mApi().wall.walls.feed.read(wallId)
+      .on('$', function (items, callback) {
+        async.parallel($.map(items, function(item) {
+          return function (itemCallback) {
+            mApi().wall.walls[item.type].read(wallId, item.identifier).callback(function (entryErr, entry) {
+              if (entryErr) {
+                $('.notification-queue').notificationQueue('notification', 'error', entryErr);
+              } else {
+                item.entry = entry;
+                itemCallback();
+              }
+            });
+          }
+        }), function () {
+          callback();
+        });
+      })
+      .callback(function (err, result) {
+        if (err) {
+          $('.notification-queue').notificationQueue('notification', 'error', err);
+        } else {
+          for (var i = 0, l = result.length; i < l; i++) {
+            var item = result[i];
+            getFeedItemController(item).render(item, function (renderErr, html) {
+              if (renderErr) {
+                $('.notification-queue').notificationQueue('notification', 'error', renderErr);  
+              } else {
+                $('.wallEntries').append(html); 
+              }
+            });
+          }
+        }
+      });
+  });
+  
 }).call(this);
 
 WallEntryController = $.klass({
