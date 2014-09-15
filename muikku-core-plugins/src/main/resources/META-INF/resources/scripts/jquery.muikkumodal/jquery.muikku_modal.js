f6df53087a4f6ff37480e972aff9412f51ed39a5
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2014-09-15 11:23:36 +0300

    Modal plugin

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/jquery.muikkumodal/jquery.muikku_modal.js
@@ -0,0 +1,23 @@
+(function($){
+
+  
+
+  $.fn.muikku_modal = function(prop){
+    
+    var options = $.extend({
+      height : "600",
+      width : "960",
+      title:"Muikku 3 M0Dal",
+      description: "Lorem ipsum dolor sit amet.",
+      top: "20%",
+      left: "20%",
+    },prop);
+        
+    return this.click(function(e){
+      
+    });
+            
+    return this;
+  };
+  
+})(jQuery);
