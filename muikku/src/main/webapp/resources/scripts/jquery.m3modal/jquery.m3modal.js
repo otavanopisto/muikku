54c7e73eaa6dd48009992f7687d4bb65561a939b
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2014-09-16 11:14:40 +0300

    Modal name change

--- muikku/src/main/webapp/resources/scripts/jquery.m3modal/jquery.m3modal.js
+++ muikku/src/main/webapp/resources/scripts/jquery.m3modal/jquery.m3modal.js
@@ -1,11 +1,11 @@
 (function($){
 
-  $.fn.muikku_modal = function(prop){
+  $.fn.m3modal = function(prop){
     
     var options = $.extend({
       height : "600",
       width : "960",
-      title:"Muikku 3 M0Dal",
+      title:"Muikku 3 Modal",
       description: "Lorem ipsum dolor sit amet.",
       top: "20%",
       left: "20%",
