a34e9e591c8e443ba46c9266d9a053137202c512
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2014-09-16 15:07:09 +0300

    modal Js plugin

--- muikku/src/main/webapp/resources/scripts/jquery.m3modal/jquery.m3modal.js
+++ muikku/src/main/webapp/resources/scripts/jquery.m3modal/jquery.m3modal.js
@@ -12,9 +12,53 @@
     },prop);
         
     return this.click(function(e){
-      
+      bgrElem();
+      contElem();
+      elemStyles();     
     });
             
+    function bgrElem(){
+    	var bgE = $('<div class="md-background"></div>');   	
+    	$(bgE).appendTo('body');
+    }
+    
+    function elemStyles(){
+     // Content box 	
+      $('md-box').css({
+        'position' : 'absolute',
+        'left' :options.left,
+        'top' :options.top,
+        'display' : 'none',
+        'height' : options.height +'px',
+        'width' : options.width + 'px',     
+      });
+      //  Close button 
+      $('md-close').css({
+          'position' : 'relative',
+          'right' : '10px',
+          'top' : '10px',
+          'display' : 'block', 
+        });
+      $('md-close').addClass('icon-close');     
+      
+     // Background
+      
+      var pH = $(document).height; 
+      var pW = $(window).width; 
+      
+      $('md-background').css({
+          'position' : 'absolute',
+          'left' : '0',
+          'top' : '0',
+          'height' : pageHeight,
+          'width' : pageWidth,
+          'z-index' : '1+'
+        });      
+    }
+    
+    function contElem(){}
+        
+    
     return this;
   };
   
