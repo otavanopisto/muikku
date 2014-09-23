f569bf0f4c4783d7ead5ce872a4c2c0c3a3f7cb5
Antti Leppä <antti.leppa@foyt.fi>
2014-09-23 08:51:55 +0300

    ** Added plugin descriptor for pyramus school data plugin

---
+++ muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/SchoolDataPyramusPluginDescriptor.java
@@ -0,0 +1,32 @@
+package fi.muikku.plugins.schooldatapyramus;
+
+import javax.inject.Inject;
+
+import fi.muikku.model.base.SchoolDataSource;
+import fi.muikku.plugin.PluginDescriptor;
+import fi.muikku.schooldata.SchoolDataController;
+
+public class SchoolDataPyramusPluginDescriptor implements PluginDescriptor {
+
+  public static final String SCHOOL_DATA_SOURCE = "PYRAMUS";
+  
+  @Inject
+  private SchoolDataController schoolDataController;
+  
+  @Override
+  public void init() {
+    /**
+     * Ensure that SchoolDataSource is defined
+     */
+    
+    SchoolDataSource schoolDataSource = schoolDataController.findSchoolDataSource(SCHOOL_DATA_SOURCE);
+    if (schoolDataSource == null) {
+      schoolDataController.createSchoolDataSource(SCHOOL_DATA_SOURCE);
+    }
+  }
+  
+  @Override
+  public String getName() {
+    return "school-data-pyramus";
+  }
+}
