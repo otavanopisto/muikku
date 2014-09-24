502f318c9e77172bf649149d9880712633765471
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2014-09-24 10:47:37 +0300

    stored changes temporarily

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/seeker/SeekerRESTService.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/seeker/SeekerRESTService.java
@@ -1,10 +1,21 @@
 package fi.muikku.plugins.seeker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
 import fi.muikku.plugin.PluginRESTService;
+import fi.muikku.session.SessionController;
 
 @Path("/seeker")
 @RequestScoped
@@ -12,40 +23,29 @@ import fi.muikku.plugin.PluginRESTService;
 @Produces ("application/json")
 public class SeekerRESTService extends PluginRESTService {
 
-//FIXME: Re-enable this service
-//
-//  @Inject 
-//  private TranquilityBuilderFactory tranquilityBuilderFactory;
-//
-//  @Inject
-//  private SessionController sessionController;
-//
-//  @Inject
-//  @Any
-//  private Instance<SeekerResultProvider> seekerResultProviders;
-//  
-//  @GET
-//  @Path ("/search")
-//  public Response search(
-//      @QueryParam("searchString") String searchString
-//      ) {
-//    List<SeekerResult> results = new ArrayList<SeekerResult>();
-//    
-//    for (SeekerResultProvider provider : seekerResultProviders) {
-//      List<SeekerResult> result = provider.search(searchString);
-//      if (result != null) {
-//        results.addAll(result);
-//      }
-//    }
-//    
-//    TranquilityBuilder tranquilityBuilder = tranquilityBuilderFactory.createBuilder();
-//    Tranquility tranquility = tranquilityBuilder.createTranquility()
-//      .addInstruction(tranquilityBuilder.createPropertyTypeInstruction(TranquilModelType.COMPLETE));
-//    
-//    return Response.ok(
-//      tranquility.entities(results)
-//    ).build();
-//  }
-//
+  @Inject
+  private SessionController sessionController;
+
+  @Inject
+  @Any
+  private Instance<SeekerResultProvider> seekerResultProviders;
+  
+  @GET
+  @Path ("/search")
+  public Response search(
+      @QueryParam("searchString") String searchString
+      ) {
+    List<SeekerResult> results = new ArrayList<SeekerResult>();
+    
+    for (SeekerResultProvider provider : seekerResultProviders) {
+      List<SeekerResult> result = provider.search(searchString);
+      if (result != null) {
+        results.addAll(result);
+      }
+    }
+    
+    return Response.ok(results).build();
+  }
+
 
 }
