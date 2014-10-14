a0370117758af465bde28acd5889c27f7351004f
Antti Leppä <antti.leppa@foyt.fi>
2014-10-14 09:54:52 +0300

    ** Removed deprecated GenericRoleController ** Added (as replacement for the GenericRoleController) teacherLoggedIn, managerLoggedIn, administratorLoggedIn and studentLoggedIn properties into SessionBackingBean

--- muikku/src/main/java/fi/muikku/session/SessionBackingBean.java
+++ muikku/src/main/java/fi/muikku/session/SessionBackingBean.java
@@ -1,17 +1,41 @@
 package fi.muikku.session;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import fi.muikku.model.users.EnvironmentRoleArchetype;
+import fi.muikku.model.users.EnvironmentUser;
+import fi.muikku.model.users.UserEntity;
+import fi.muikku.schooldata.UserController;
 
 @RequestScoped
 @Named
 @Stateful
 public class SessionBackingBean {
 
+  @Inject
+  private UserController userController;
+  
 	@Inject
 	private SessionController sessionController;
+	
+	@PostConstruct
+	public void init() {
+	  loggedUserRoleArchetype = null;
+
+    if (sessionController.isLoggedIn()) {
+	    UserEntity loggedUser = sessionController.getUser();
+	    if (loggedUser != null) {
+	      EnvironmentUser environmentUser = userController.findEnvironmentUserByUserEntity(loggedUser);
+	      if (environmentUser != null) {
+	        loggedUserRoleArchetype = environmentUser.getRole().getArchetype();
+	      }
+	    }
+	  }
+	}
 	
 	public boolean getLoggedIn() {
 		return sessionController.isLoggedIn();
@@ -25,4 +49,21 @@ public class SessionBackingBean {
 		return "theme-muikku";
 	}
 	
+	public boolean getTeacherLoggedIn() {
+    return loggedUserRoleArchetype == EnvironmentRoleArchetype.TEACHER;
+  }
+	
+	public boolean getManagerLoggedIn() {
+    return loggedUserRoleArchetype == EnvironmentRoleArchetype.MANAGER;
+  }
+	
+	public boolean getAdministratorLoggedIn() {
+    return loggedUserRoleArchetype == EnvironmentRoleArchetype.ADMINISTRATOR;
+  }
+	
+	public boolean getStudentLoggedIn() {
+	  return loggedUserRoleArchetype == EnvironmentRoleArchetype.STUDENT;
+	}
+	
+	private EnvironmentRoleArchetype loggedUserRoleArchetype;
 }
