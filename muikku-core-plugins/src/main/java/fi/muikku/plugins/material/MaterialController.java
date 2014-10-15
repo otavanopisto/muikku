b4ef04f901e05e07d05c2109f1ca0a1075a71aa6
pkukkon <pasi.kukkonen@otavanopisto.fi>
2014-10-15 10:33:00 +0300

    - actually working version of the material cloning framework...

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/MaterialController.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/MaterialController.java
@@ -1,6 +1,7 @@
 package fi.muikku.plugins.material;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Iterator;
 
 import javax.ejb.Stateless;
@@ -30,15 +31,27 @@ public class MaterialController {
 	
   public <T> T cloneMaterial(T material) {
     Iterator<MaterialCloneOperation<?>> operations = cloneOperations.iterator();
-	  
+
     while (operations.hasNext()) {
-      @SuppressWarnings("unchecked") MaterialCloneOperation<T> operation = (MaterialCloneOperation<T>) operations.next();
-      Class<?> operationMaterialType = (Class<?>) ((ParameterizedType) operation.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-      if (material.getClass().isAssignableFrom(operationMaterialType)) {
-        return operation.clone(material);
+      @SuppressWarnings("unchecked")
+      MaterialCloneOperation<T> operation = (MaterialCloneOperation<T>) operations.next();
+      @SuppressWarnings("rawtypes")
+      Class<? extends MaterialCloneOperation> operationClass = operation.getClass();
+
+      for (Type genericInterface : operationClass.getGenericInterfaces()) {
+        if (genericInterface instanceof ParameterizedType) {
+          ParameterizedType parameterizedGenericInterface = (ParameterizedType) genericInterface;
+      
+          Type type = parameterizedGenericInterface.getActualTypeArguments()[0];
+          Class<?> operationMaterialType = (Class<?>) type;
+
+          if (material.getClass().isAssignableFrom(operationMaterialType)) {
+            return operation.clone(material);
+          }
+        }
       }
     }
-	  
+    
     return null;
 	}
 
