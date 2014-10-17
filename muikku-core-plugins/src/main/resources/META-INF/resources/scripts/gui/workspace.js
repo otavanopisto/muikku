ddac70531568c6b11c2e460a0d63151730a0e84b
jjlankinen <jari.lankinen@otavanopisto.fi>
2014-10-17 14:27:02 +0300

    * Added workspace material's TOC functionality

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace.js
@@ -23,7 +23,68 @@ $(document).ready(function() {
         left:naviLeftPos + 'px'
       })
     });
+    
+    if ($('.wi-dock-workspace-navi-button-materials-toc').length > 0) {
+      var tocButton = $('.wi-dock-workspace-navi-button-materials-toc');
+      var tocWrapper = tocButton.children('.workspace-materials-toc-wrapper');
+      var tocButtonLabel = $('.wi-dock-workspace-navi-button-materials-toc .workspace-navi-tt-container-materials-toc'); 
 
+      
+      $(tocButton).click(function(e) {
+        
+        if ($('.workspace-materials-toc-wrapper:hidden').length !== 0) {
+         
+          tocButtonLabel.hide().css({
+            opacity:0
+          });
+          
+          tocButton.addClass('wi-dock-workspace-navi-button-selected');
+          
+          tocWrapper
+          .show()
+          .clearQueue()
+          .stop()
+          .animate({
+            opacity: 1
+          }, {
+            duration : 400,
+            easing : "easeInOutQuad"
+          });
+          
+        } else { 
+          
+          $(".workspace-materials-toc-wrapper").bind('click', function(e) {
+            e.stopPropagation();
+          });
+          
+          tocButtonLabel
+          .show()
+          .animate({
+            opacity: 1
+          }, {
+            duration : 100,
+            easing : "easeInOutQuad"
+          });
+          
+          tocWrapper
+          .clearQueue()
+          .stop()
+          .animate({
+            opacity: 0
+          }, {
+            duration : 200,
+            easing : "easeInOutQuad",
+            complete : function(){
+              $(this).hide();
+              tocButton.removeClass('wi-dock-workspace-navi-button-selected');
+            }
+          });
+          
+        }
+        
+      });
+
+    }
   }
   
 });
