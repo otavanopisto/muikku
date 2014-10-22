201984103bfcb76762a3e3dab87d69ec60f3d5e1
jjlankinen <jari.lankinen@otavanopisto.fi>
2014-10-22 13:39:08 +0300

    * Added material reading view's toc * Added scripts to hide/show toc in material reading view * Changed workspace.reading.contentArea to more simplified version as of workspace.reading.content (this affacts few .xhtml files within workspaces plugin) * Removed unececcary div ele,ents from workspace.xhtml, they should not be needed to create functional widgetSpace * Added nececcary styles for the above changes

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace.js
@@ -24,14 +24,14 @@ $(document).ready(function() {
       })
     });
     
-    // Functionality for workspace's TOC
+    // Functionality for workspace's material's TOC
     if ($('.wi-dock-workspace-navi-button-materials-toc').length > 0) {
       var tocButton = $('.wi-dock-workspace-navi-button-materials-toc');
       var tocWrapper = tocButton.children('.workspace-materials-toc-wrapper');
       var tocButtonLabel = $('.wi-dock-workspace-navi-button-materials-toc .workspace-navi-tt-container-materials-toc'); 
 
       // Some hiding and showing for TOC by click event
-      $(tocButton).click(function(e) {
+      $(tocButton).click(function() {
         
         if ($('.workspace-materials-toc-wrapper:hidden').length !== 0) {
          
@@ -86,6 +86,100 @@ $(document).ready(function() {
       });
 
     }
+
   }
+  
+  // Workspace's materials's reading view
+  if ($('#workspaceMaterialReadingTOCWrapper').length > 0) {
+    var height = $(window).height();
+    var closedTocWrapper = $('#workspaceMaterialReadingTOCClosed');
+    var openTocWrapper = $('#workspaceMaterialReadingTOCOpen');
+    var tocOpeningButton = $('.workspace-material-reading-toc-opening-button');
+    var tocClosingButton = $('.workspace-material-reading-toc-closing-button');
+
+    if (closedTocWrapper.length > 0) {
+      closedTocWrapper
+      .show()
+      .css({
+        height: height
+      });
+    }
+    
+    if (openTocWrapper.length > 0) {
+      openTocWrapper
+      .hide()
+      .css({
+        height: height,
+        "margin-left" : "-350px"
+      });
+    }
+    
+    $(window).resize(function(){
+      height = $(window).height();
+      openTocWrapper.height(height);
+      closedTocWrapper.height(height);
+    });
+    
+    $(tocOpeningButton).click(function() {
+      closedTocWrapper
+      .clearQueue()
+      .stop()
+      .animate({
+        "margin-left" : "-55px"
+      }, {
+        duration : 150,
+        easing : "easeInOutQuad",
+        complete : function(){
+          $(this).hide();
+          
+          openTocWrapper
+          .show()
+          .clearQueue()
+          .stop()
+          .animate({
+            opacity:0.95,
+            "margin-left" : "0"
+          }, {
+            duration:300,
+            easing: "easeInOutQuad"
+          });
+          
+        }
+      });
+    });
+    
+    $(tocClosingButton).click(function() {
+      openTocWrapper
+      .clearQueue()
+      .stop()
+      .animate({
+        "margin-left" : "-350px",
+        opacity: 1
+      }, {
+        duration : 150,
+        easing : "easeInOutQuad",
+        complete : function(){
+          $(this).hide();
+          
+          closedTocWrapper
+          .show()
+          .clearQueue()
+          .stop()
+          .animate({
+            "margin-left" : "0"
+          }, {
+            duration:300,
+            easing: "easeInOutQuad"
+          });
+          
+        }
+      });
+    });
+    
+
+    
+  }
+
+  
   
 });
