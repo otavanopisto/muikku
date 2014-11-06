7322ca13f6d8cd1b3d7f5f1286533ef7b608f19c
ilmoeuro <ilmo.euro@gmail.com>
2014-11-06 11:50:49 +0200

    ** GET for material REST service

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/rest/MaterialRESTService.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/rest/MaterialRESTService.java
@@ -2,71 +2,38 @@ package fi.muikku.plugins.material.rest;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import fi.muikku.plugin.PluginRESTService;
+import fi.muikku.plugins.material.HtmlMaterialController;
+import fi.muikku.plugins.material.model.HtmlMaterial;
 
 @RequestScoped
 @Path("/materials")
 @Stateful
 @Produces ("application/json")
 public class MaterialRESTService extends PluginRESTService {
-// FIXME: Re-enable this service
-//
-//	@SuppressWarnings("cdi-ambiguous-dependency")
-//	@Inject
-//	private TranquilityBuilderFactory tranquilityBuilderFactory;
-//
-//	@Inject
-//	private HtmlMaterialController htmlMaterialController;
-//	
-//	// 
-//	// HtmlMaterial 
-//	// 
-//	
-//	@POST
-//	@Path ("/html/")
-//	public Response createHtmlMaterial(HtmlMaterialCompact htmlMaterial) {
-//	  if (htmlMaterial.getId() != null) {
-//      return Response.status(Status.BAD_REQUEST).entity("id can not be specified when creating new HtmlMaterial").build();
-//    }
-//	  
-//	  if (StringUtils.isBlank(htmlMaterial.getHtml())) {
-//      return Response.status(Status.BAD_REQUEST).entity("html is required when creating new HtmlMaterial").build();
-//    }
-//	  
-//	  if (StringUtils.isBlank(htmlMaterial.getTitle())) {
-//      return Response.status(Status.BAD_REQUEST).entity("title is required when creating new HtmlMaterial").build();
-//    }
-//	  
-//	  if (StringUtils.isBlank(htmlMaterial.getUrlName())) {
-//      return Response.status(Status.BAD_REQUEST).entity("urlName is required when creating new HtmlMaterial").build();
-//    }
-//	  
-//    return Response.ok(
-//        tranquilityBuilderFactory.createBuilder()
-//          .createTranquility()
-//          .entity(htmlMaterialController.createHtmlMaterial(htmlMaterial.getUrlName(), htmlMaterial.getTitle(), htmlMaterial.getHtml()))
-//    ).build();
-//	}
-//	
-//	@DELETE
-//  @Path ("/html/{ID}")
-//	public Response deleteHtmlMaterial(@PathParam ("ID") Long htmlMaterialId) {
-//	  // TODO: Security
-//    
-//    if (htmlMaterialId == null) {
-//      return Response.status(Status.NOT_FOUND).entity("Html material not found").build();
-//    }
-//    
-//    HtmlMaterial htmlMaterial = htmlMaterialController.findHtmlMaterialById(htmlMaterialId);
-//    if (htmlMaterial == null) {
-//      return Response.status(Status.NOT_FOUND).entity("Html material not found").build();
-//    }
-//    
-//    htmlMaterialController.deleteHtmlMaterial(htmlMaterial);
-//    
-//    return Response.noContent().build();
-//	}
-//  
+	@Inject
+	private HtmlMaterialController htmlMaterialController;
+	
+	// 
+	// HtmlMaterial 
+	// 
+	
+	@GET
+	@Path ("/html/{id}")
+	@Produces(MediaType.TEXT_HTML)
+	public String findMaterial(@PathParam("id") long id) {
+	  HtmlMaterial material = htmlMaterialController.findHtmlMaterialById(id);
+	  if (material == null) {
+	    throw new NotFoundException();
+	  }
+	  return material.getHtml();
+	}
 }
