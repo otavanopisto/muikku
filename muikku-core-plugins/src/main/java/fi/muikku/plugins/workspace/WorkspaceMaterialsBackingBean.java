9fb35afee5d2b0fd7dd628837ed3106d42aeeb59
pkukkon <pasi.kukkonen@otavanopisto.fi>
2014-11-06 11:19:00 +0200

    - added titles to material beans

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialsBackingBean.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialsBackingBean.java
@@ -108,10 +108,12 @@ public class WorkspaceMaterialsBackingBean {
       Material material = ((WorkspaceMaterial) workspaceNode).getMaterial();
       materialNode.setMaterialId(material.getId());
       materialNode.setMaterialType(material.getType());
+      materialNode.setMaterialTitle(material.getTitle());
     }
     else if (workspaceNode instanceof WorkspaceFolder) {
       // TODO questionable hard-coding 
       materialNode.setMaterialType("folder");
+      materialNode.setMaterialTitle(((WorkspaceFolder) workspaceNode).getTitle());
     }
     else {
       throw new IllegalArgumentException("Unsupported workspace node: " + workspaceNode.getClass());
@@ -196,12 +198,20 @@ public class WorkspaceMaterialsBackingBean {
     public void setMaterialPath(String materialPath) {
       this.materialPath = materialPath;
     }
+    public String getMaterialTitle() {
+      return materialTitle;
+    }
+    public void setMaterialTitle(String materialTitle) {
+      this.materialTitle = materialTitle;
+    }
     // The id of the WorkspaceNode this material represents
     private Long workspaceMaterialId;
     // The id of the Material referenced by WorkspaceNode; null for folders
     private Long materialId;
     // The type of the Material referenced by WorkspaceNode, e.g. folder, html, binary
     private String materialType;
+    // Material title
+    private String materialTitle;
     // Relative path of the WorkspaceNode 
     private String materialPath;
   }
