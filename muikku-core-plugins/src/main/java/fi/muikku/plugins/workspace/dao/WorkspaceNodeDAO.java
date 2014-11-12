3dddd17d25477c6857dc19ac8ecc2406863cb87b
pkukkon <pasi.kukkonen@otavanopisto.fi>
2014-11-12 14:20:17 +0200

    - workspace node sorting support

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/dao/WorkspaceNodeDAO.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/dao/WorkspaceNodeDAO.java
@@ -31,6 +31,54 @@ public class WorkspaceNodeDAO extends CorePluginsDAO<WorkspaceNode> {
    
     return getSingleResult(entityManager.createQuery(criteria));
 	}
+	
+	public Integer getMaximumOrderNumber(WorkspaceNode parent) {
+    EntityManager entityManager = getEntityManager();
+    
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Integer> criteria = criteriaBuilder.createQuery(Integer.class);
+    Root<WorkspaceNode> root = criteria.from(WorkspaceNode.class);
+    criteria.select(criteriaBuilder.max(root.get(WorkspaceNode_.orderNumber)));
+    criteria.where(
+      criteriaBuilder.equal(root.get(WorkspaceNode_.parent), parent)
+    );
+   
+    return entityManager.createQuery(criteria).getSingleResult();
+	}
+	
+	public List<WorkspaceNode> listByOrderNumberEqualOrGreater(WorkspaceNode node) {
+    EntityManager entityManager = getEntityManager();
+    
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<WorkspaceNode> criteria = criteriaBuilder.createQuery(WorkspaceNode.class);
+    Root<WorkspaceNode> root = criteria.from(WorkspaceNode.class);
+    criteria.select(root);
+    criteria.where(
+      criteriaBuilder.and(
+        criteriaBuilder.equal(root.get(WorkspaceNode_.parent), node.getParent()),
+        criteriaBuilder.greaterThanOrEqualTo(root.get(WorkspaceNode_.orderNumber), node.getOrderNumber())
+      )
+    );
+   
+    return entityManager.createQuery(criteria).getResultList();
+	}
+
+	public List<WorkspaceNode> listByOrderNumberGreater(WorkspaceNode node) {
+    EntityManager entityManager = getEntityManager();
+    
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<WorkspaceNode> criteria = criteriaBuilder.createQuery(WorkspaceNode.class);
+    Root<WorkspaceNode> root = criteria.from(WorkspaceNode.class);
+    criteria.select(root);
+    criteria.where(
+      criteriaBuilder.and(
+        criteriaBuilder.equal(root.get(WorkspaceNode_.parent), node.getParent()),
+        criteriaBuilder.greaterThan(root.get(WorkspaceNode_.orderNumber), node.getOrderNumber())
+      )
+    );
+   
+    return entityManager.createQuery(criteria).getResultList();
+  }
 
 	public List<WorkspaceNode> listByParent(WorkspaceNode parent) {
     EntityManager entityManager = getEntityManager();
@@ -45,5 +93,10 @@ public class WorkspaceNodeDAO extends CorePluginsDAO<WorkspaceNode> {
    
     return entityManager.createQuery(criteria).getResultList();
   }
+	
+	public WorkspaceNode updateOrderNumber(WorkspaceNode node, Integer orderNumber) {
+	  node.setOrderNumber(orderNumber);
+	  return persist(node);
+	}
 
 }
