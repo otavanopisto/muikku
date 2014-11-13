2ed9b336863d57ff6bb5539c47d0da7ab03151e7
Antti Leppä <antti.leppa@foyt.fi>
2014-11-13 13:15:44 +0200

    ** Added title and contentType properties into HtmlRestMaterial class ** Added support for creating html materials via rest

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/rest/HtmlMaterialRESTService.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/rest/HtmlMaterialRESTService.java
@@ -4,12 +4,14 @@ import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Document;
 
 import fi.muikku.plugin.PluginRESTService;
@@ -27,19 +29,48 @@ public class HtmlMaterialRESTService extends PluginRESTService {
   @Inject
   private HtmlMaterialController htmlMaterialController;
 
+  @POST
+  @Path("/")
+  public Response createMaterial(HtmlRestMaterial entity) {
+    if (StringUtils.isBlank(entity.getContentType())) {
+      return Response.status(Status.BAD_REQUEST).entity("contentType is missing").build();
+    }
+    
+    if (StringUtils.isBlank(entity.getTitle())) {
+      return Response.status(Status.BAD_REQUEST).entity("title is missing").build();
+    }
+    
+    HtmlMaterial htmlMaterial = htmlMaterialController.createHtmlMaterial(entity.getTitle(), entity.getHtml(), entity.getContentType(), 0l);
+    try {
+      String html = null;
+      if (StringUtils.isNoneBlank(htmlMaterial.getHtml())) {
+        Document processedHtmlDocument = htmlMaterialController.getProcessedHtmlDocument(htmlMaterial);
+        html = htmlMaterialController.getSerializedHtmlDocument(processedHtmlDocument, htmlMaterial);
+      }
+      
+      return Response.ok(new HtmlRestMaterial(htmlMaterial.getId(), htmlMaterial.getTitle(), htmlMaterial.getContentType(), html)).build();
+    } catch (Exception e) {
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+    }
+  }
+  
   @GET
   @Path("/{id}")
-  public Response findMaterial(@PathParam("id") long id) {
-    HtmlMaterial material = htmlMaterialController.findHtmlMaterialById(id);
-    if (material == null) {
+  public Response findMaterial(@PathParam("id") Long id) {
+    HtmlMaterial htmlMaterial = htmlMaterialController.findHtmlMaterialById(id);
+    if (htmlMaterial == null) {
       return Response.status(Status.NOT_FOUND).build();
     } else {
       try {
-        Document processedHtmlDocument = htmlMaterialController.getProcessedHtmlDocument(material);
-        String html = htmlMaterialController.getSerializedHtmlDocument(processedHtmlDocument, material);
-        return Response.ok().entity(new HtmlRestMaterial(id, html)).build();
+        String html = null;
+        if (StringUtils.isNoneBlank(htmlMaterial.getHtml())) {
+          Document processedHtmlDocument = htmlMaterialController.getProcessedHtmlDocument(htmlMaterial);
+          html = htmlMaterialController.getSerializedHtmlDocument(processedHtmlDocument, htmlMaterial);
+        }
+        
+        return Response.ok(new HtmlRestMaterial(htmlMaterial.getId(), htmlMaterial.getTitle(), htmlMaterial.getContentType(), html)).build();
       } catch (Exception e) {
-        return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
       }
     }
   }
