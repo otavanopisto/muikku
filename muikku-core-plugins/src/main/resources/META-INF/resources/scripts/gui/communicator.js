d39aab09c26758d9260ea6e39bf1c2e2a5d9e9bf
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2014-11-18 08:52:08 +0200

    Messages listing

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/communicator.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/communicator.js
@@ -1,63 +1,81 @@
- 
+
 $(document).ready(function(){
 
 	$(".bt-mainFunction").m3modal({
-	        		title : "Veistin! ",
-	        		description : "Voit lähettää veistejä kaikille kavereillesi tai muuten vaan jos ei sellaisia ole!",
-	        		content: $('<div><div><select name="msgTemplates"><option>Ei pohjia</option></select></div><div><div><input type="textfield" value="vastaanottajat" name="msgRecipients"></input></div><div><input type="textfield" value="aihe" name="msgSubject"></input></div></div><div><textarea value="" name="msgContent"></textarea></div></div>'),
-	        		modalgrid : 24,
-	        		contentgrid : 24,
-	     		
-	        		options: [
-						{
-						    caption: "Lähetä myös itselle",
-							name : "mailSelf",
-							type : "checkbox",
-							action: function (e) {			
-											}
-						},
-						{
-						    caption: "Lisää allekirjoitus",
-							name : "addSignature",
-							type : "checkbox",
-							action: function (e) {			
-											}
-						},					
-							 ],
-	            		
-	        	    buttons: [
-	        		  {
-	        		    caption: "Lähetä",
-	        		    name : "sendMail",
-	        		    action: function (e) {
-	        
-	        		    }
-	        		  },
-	        		  {
-	            		 caption: "Tallenna luonnos",
-	            		 name : "saveMail",
-	            		 action: function (e) {
-	            
-	            		}
-	        		  }
-	        		]
-	        	});
+		title : "Veistin! ",
+		description : "Voit lähettää veistejä kaikille kavereillesi tai muuten vaan jos ei sellaisia ole!",
+		content: $('<div><div><select name="msgTemplates"><option>Ei pohjia</option></select></div><div><div><input type="textfield" value="vastaanottajat" name="msgRecipients"></input></div><div><input type="textfield" value="aihe" name="msgSubject"></input></div></div><div><textarea value="" name="msgContent"></textarea></div></div>'),
+		modalgrid : 24,
+		contentgrid : 24,
 
-	
-	/* Can't get the userId, so commented
-
-        var msgC = $('mf-content-main');
-        var uId = ;
-        RESTful.doGet(CONTEXTPATH + "/rest/communicator/{userId}/items", {
-          parameters: {
-            'userId': this._userId
+		options: [
+ 				{
+          caption : "Lähetä myös itselle",
+          name : "mailSelf",
+          type : "checkbox",
+          action : function(e) {
           }
-        }).success(function (data, textStatus, jqXHR) {
-          renderDustTemplate('communicator/communicator_items.dust', data, function (text) {
-        	  msgC.append($.parseHTML(text));
-          });
-        });
-      
+        }, {
+          caption : "Lisää allekirjoitus",
+          name : "addSignature",
+          type : "checkbox",
+          action : function(e) {
+          }
+        }, 
+      ],
 
-*/
+      buttons : [ 
+        {
+          caption : "Lähetä",
+          name : "sendMail",
+          action : function(e) {
+            var subject = e.contentElement.find("input[name='msgSubject']").val();
+            var content = e.contentElement.find("textarea[name='msgContent']").val();
+            var tagStr = "tagi viesti"; // TODO: Tag content
+            var tags = tagStr != undefined ? tagStr.split(' ') : [];
+            var recipientIds = [10];
+            var groupIds = [];
+            
+            mApi().communicator.messages.create({
+              categoryName: "message",
+              caption : subject,
+              content : content,
+              tags : tags,
+              recipientIds : recipientIds,
+              recipientGroupIds : groupIds
+            })
+            .callback(function (err, result) {
+            });
+          }
+        }, {
+          caption : "Tallenna luonnos",
+          name : "saveMail",
+          action : function(e) {
+          }
+        } 
+      ]
+    });
+
+    
+    mApi().communicator.items.read()
+      .on('$', function (item, itemCallback) {
+        mApi().communicator.communicatormessages.sender.read(item.id)
+          .callback(function (err, user) {
+            item.senderFullName = user.firstName + ' ' + user.lastName;
+            item.senderHasPicture = user.hasImage;
+          });
+        mApi().communicator.messages.messagecount.read(item.communicatorMessageId)
+          .callback(function (err, count) {
+            item.messageCount = count;
+          });
+
+        itemCallback();
+      })
+      .callback(function (err, result) {
+        renderDustTemplate('communicator/communicator_items.dust', result, function (text) {
+          $('.mf-content-main').append($.parseHTML(text));
+        });
+      });
+
+    
 });
