4814f99c9cd6b08449f586ff9039fd4e37cc21b5
ilmo.euro <ilmo.euro@gmail.com>
2014-11-19 13:46:06 +0200

    ** Made material loading sequential, not parallel

--- muikku/src/main/webapp/resources/scripts/gui/workspace-materials.js
+++ muikku/src/main/webapp/resources/scripts/gui/workspace-materials.js
@@ -1,24 +1,30 @@
 (function() {
 
   $(document).ready(function() {
-    // Workspace Material's page loading
-    $('.workspace-materials-view-page').each(function(index, node) {
-      var materialType = $(node).data('material-type');
+
+    function loadMaterial(sel, $node) {
+      var materialType = $node.data('material-type');
       switch (materialType) {
       case 'html':
-        var materialId = $(node).data('material-id');
-        mApi().materials.html.read(materialId).callback(
-          function (err, html) {
-            $(node).html(html.html);
-          });
+        var materialId = $node.data('material-id');
+        mApi().materials.html.read(materialId).callback(function(err, html) {
+          $node.html(html.html);
+          var next = $node.next(sel);
+          if (next.length) {
+            loadMaterial(sel, next);
+          }
+        });
         break;
       case 'folder':
-        $(node).append($('<p>').html($(node).data('material-title')));
+        $node.append($('<p>').html($node.data('material-title')));
         break;
       default:
         break;
       }
-    });
+    }
+
+    // Workspace Material's page loading
+    loadMaterial('.workspace-materials-view-page', $('.workspace-materials-view-page').first());
 
     // Workspace material's Reading View page loading
     $('.workspace-materials-reading-view-page').each(function(index, node) {
@@ -26,10 +32,9 @@
       switch (materialType) {
       case 'html':
         var materialId = $(node).data('material-id');
-        mApi().materials.html.read(materialId).callback(
-          function (err, html) {
-            $(node).html(html.html);
-          });
+        mApi().materials.html.read(materialId).callback(function(err, html) {
+          $(node).html(html.html);
+        });
         break;
       case 'folder':
         $(node).append($('<p>').html($(node).data('material-title')));
@@ -48,18 +53,18 @@
 
       $('a[href="' + hash + '"]').click(function(event) {
         $('html, body').stop().animate({
-          scrollTop: $section.offset().top - 100
-        },{
-          duration: 500,
+          scrollTop : $section.offset().top - 100
+        }, {
+          duration : 500,
           easing : "easeInOutQuad",
-          complete: function() {
+          complete : function() {
             window.location.hash = hash;
           }
         });
         event.preventDefault();
       });
     });
-    
+
     /* Smooth scrolling in workspace Material's Reading View */
     var $sections = $('.workspace-materials-reading-view-page');
 
@@ -69,11 +74,11 @@
 
       $('a[href="' + hash + '"]').click(function(event) {
         $('html, body').stop().animate({
-          scrollTop: $section.offset().top - 29
-        },{
-          duration: 500,
+          scrollTop : $section.offset().top - 29
+        }, {
+          duration : 500,
           easing : "easeInOutQuad",
-          complete: function() {
+          complete : function() {
             window.location.hash = hash;
           }
         });
@@ -81,45 +86,47 @@
       });
     });
 
-    /* Highlighting toc item at appropriate time when we scroll to the corresponding section */
-    $('.workspace-materials-view-page')
-      .waypoint(function(direction) {
-        var $links = $('a[href="#' + this.id + '"]');
-        $links.toggleClass('active', direction === 'down');
-      }, {
-        offset: '60%'
-      })
-      .waypoint(function(direction) {
-        var $links = $('a[href="#' + this.id + '"]');
-        $links.toggleClass('active', direction === 'up');
-      }, {
-        offset: function() {
-          return -$(this).height() + 250;
-        }
-      });
-    
-    /* Highlighting toc item at appropriate time when we scroll to the corresponding section - Reading View */
-    $('.workspace-materials-reading-view-page')
-      .waypoint(function(direction) {
-        var $links = $('a[href="#' + this.id + '"]');
-        $links.toggleClass('active', direction === 'down');
-      }, {
-        offset: '60%'
-      })
-      .waypoint(function(direction) {
-        var $links = $('a[href="#' + this.id + '"]');
-        $links.toggleClass('active', direction === 'up');
-      }, {
-        offset: function() {
-          return -$(this).height() + 250;
-        }
-      });
-    
+    /*
+     * Highlighting toc item at appropriate time when we scroll to the
+     * corresponding section
+     */
+    $('.workspace-materials-view-page').waypoint(function(direction) {
+      var $links = $('a[href="#' + this.id + '"]');
+      $links.toggleClass('active', direction === 'down');
+    }, {
+      offset : '60%'
+    }).waypoint(function(direction) {
+      var $links = $('a[href="#' + this.id + '"]');
+      $links.toggleClass('active', direction === 'up');
+    }, {
+      offset : function() {
+        return -$(this).height() + 250;
+      }
+    });
+
+    /*
+     * Highlighting toc item at appropriate time when we scroll to the
+     * corresponding section - Reading View
+     */
+    $('.workspace-materials-reading-view-page').waypoint(function(direction) {
+      var $links = $('a[href="#' + this.id + '"]');
+      $links.toggleClass('active', direction === 'down');
+    }, {
+      offset : '60%'
+    }).waypoint(function(direction) {
+      var $links = $('a[href="#' + this.id + '"]');
+      $links.toggleClass('active', direction === 'up');
+    }, {
+      offset : function() {
+        return -$(this).height() + 250;
+      }
+    });
+
   });
 
-// TODO enable?
-//  jsPlumb.ready(function() {
-//    $(".muikku-connect-field-table").muikkuConnectField();
-//  });
+  // TODO enable?
+  // jsPlumb.ready(function() {
+  // $(".muikku-connect-field-table").muikkuConnectField();
+  // });
 
 }).call(this);
