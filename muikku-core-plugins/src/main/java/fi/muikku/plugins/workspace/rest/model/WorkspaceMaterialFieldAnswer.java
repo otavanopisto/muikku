d2f3dfb7dd3ee8800883b412f0050f4453921c24
Antti Leppä <antti.leppa@foyt.fi>
2014-11-26 15:05:45 +0200

    ** Changed workspace answer rest resource to accept materialId, fieldName, embedId and value istead of name and value pair

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/rest/model/WorkspaceMaterialFieldAnswer.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/rest/model/WorkspaceMaterialFieldAnswer.java
@@ -2,31 +2,51 @@ package fi.muikku.plugins.workspace.rest.model;
 
 public class WorkspaceMaterialFieldAnswer {
 
-  public WorkspaceMaterialFieldAnswer(){
+  public WorkspaceMaterialFieldAnswer() {
   }
-  
-  public WorkspaceMaterialFieldAnswer(String field, Object value) {
-    this.field = field;
+
+  public WorkspaceMaterialFieldAnswer(Long materialId, String embedId, String fieldName, String value) {
+    super();
+    this.materialId = materialId;
+    this.embedId = embedId;
+    this.fieldName = fieldName;
     this.value = value;
   }
-
-  public String getField() {
-    return field;
+  
+  public Long getMaterialId() {
+    return materialId;
+  }
+  
+  public void setMaterialId(Long materialId) {
+    this.materialId = materialId;
   }
 
-  public void setField(String field) {
-    this.field = field;
+  public String getEmbedId() {
+    return embedId;
   }
 
-  public Object getValue() {
+  public void setEmbedId(String embedId) {
+    this.embedId = embedId;
+  }
+
+  public String getFieldName() {
+    return fieldName;
+  }
+
+  public void setFieldName(String fieldName) {
+    this.fieldName = fieldName;
+  }
+
+  public String getValue() {
     return value;
   }
 
-  public void setValue(Object value) {
+  public void setValue(String value) {
     this.value = value;
   }
-
-  private String field;
-  private Object value;
-
+  
+  private Long materialId;
+  private String fieldName;
+  private String embedId;
+  private String value;
 }
