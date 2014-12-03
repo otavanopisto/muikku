9325c11c23d23521eca48a7b5106364497209954
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2014-12-03 11:49:49 +0200

    - Changed user search stuff for communicator

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/communicator.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/communicator.js
@@ -1,13 +1,150 @@
+$.widget("custom.communicatorautocomplete", $.ui.autocomplete, {
+  _renderMenu: function(ul, items) {
+    var _this = this;
+    var currentCategory = "";
+  
+    $.each(items, function(index, item) {
+      if (item.category != currentCategory) {
+        if (item.category)
+          ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+        currentCategory = item.category;
+      }
+      _this._renderItemData(ul, item);
+    });
+  },
+  _renderItem: function(ul, item) {
+    var imageUrl = CONTEXTPATH + "/themes/default/gfx/fish.jpg";
+    if (item.image)
+      imageUrl = item.image;
+    
+    var inner_html = 
+      '<a><div class="communicator_autocomplete_item_container">' + 
+      '<span class="communicator_autocomplete_item_image"><img width="25" height="25" src="' + imageUrl + '"></span>' +
+      '<span class="communicator_autocomplete_item_label">' + item.label + '</span></div></a>';
+    return $( "<li></li>" ).data( "item.autocomplete", item ).append(inner_html).appendTo( ul );
+  }
+});
+
+$.fn.extend({
+  /**
+   * Thanks to
+   * https://github.com/Kasheftin/jquery-textarea-caret/blob/master/jquery.textarea.caret.js
+   */
+  
+  insertAtCaret: function(myValue) {
+    return this.each(function(i) {
+      if (document.selection) {
+        this.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+        this.focus();
+      }
+      else 
+      if (this.selectionStart || this.selectionStart == "0") {
+        var startPos = this.selectionStart;
+        var endPos = this.selectionEnd;
+        var scrollTop = this.scrollTop;
+        this.value = this.value.substring(0,startPos) + myValue + this.value.substring(endPos,this.value.length);
+        this.focus();
+        this.selectionStart = startPos + myValue.length;
+        this.selectionEnd = startPos + myValue.length;
+        this.scrollTop = scrollTop;
+      }
+      else {
+        this.value += myValue;
+        this.focus();
+      }
+    }
+  )}
+});
+
 
 $(document).ready(function(){
 	
 	$(".bt-mainFunction").m3modal({
 		title : "Veistin! ",
 		description : "Voit lähettää veistejä kaikille kavereillesi tai muuten vaan jos ei sellaisia ole!",
-		content: $('<div><div><select name="msgTemplates"><option>Ei pohjia</option></select></div><div><div><input type="textfield" value="vastaanottajat" name="msgRecipients"></input></div><div><input type="textfield" value="aihe" name="msgSubject"></input></div></div><div><textarea value="" name="msgContent"></textarea></div></div>'),
+		content: $('<div><div><select name="msgTemplates"><option>Ei pohjia</option></select></div><div><div><input type="textfield" value="vastaanottajat" name="msgRecipients" id="msgRecipients"></input></div><div><input type="textfield" value="aihe" name="msgSubject"></input></div></div><div><textarea value="" name="msgContent"></textarea></div></div>'),
 		modalgrid : 24,
 		contentgrid : 24,
+		
+		onBeforeOpen: function (modal) {
+		  var resps = $("#msgRecipients");
+		  var _this = this;
+		  
+		  resps.communicatorautocomplete({
+        source: function (request, response) {
+          response(_this._doSearch(request.term));
+        },
+        select: function (event, ui) {
+//          _this._selectRecipient(event, ui.item);
+//          $(this).val("");
+          return false;
+        }
+      });
+		  
+		},
 
+    _searchUsers: function (searchTerm) {
+      var _this = this;
+      var users = new Array();
+
+      console.log("searchUsers");
+      
+      RESTful.doGet(CONTEXTPATH + "/rest/user/searchUsers", {
+        parameters: {
+          'searchString': searchTerm
+        }
+      }).success(function (data, textStatus, jqXHR) {
+        for (var i = 0, l = data.length; i < l; i++) {
+          var img = undefined;
+          
+          if (data[i].hasImage)
+            img = CONTEXTPATH + "/picture?userId=" + data[i].id;
+          
+          users.push({
+            category: getLocaleText("plugin.communicator.users"),
+            label: data[i].firstName + " " + data[i].lastName,
+            id: data[i].id,
+            image: img,
+            type: "USER"
+          });
+        }
+      });
+
+      return users;
+    },
+//    _searchGroups: function (searchTerm) {
+//      var _this = this;
+//      var userGroups = new Array();
+//
+//      RESTful.doGet(CONTEXTPATH + "/rest/usergroup/searchGroups", {
+//        parameters: {
+//          'searchString': searchTerm
+//        }
+//      }).success(function (data, textStatus, jqXHR) {
+//        for (var i = 0, l = data.length; i < l; i++) {
+//          userGroups.push({
+//            category: getLocaleText("plugin.communicator.usergroups"),
+//            label: data[i].name,
+//            id: data[i].id,
+//            memberCount: data[i].memberCount,
+//            image: undefined, // TODO usergroup image
+//            type: "GROUP"
+//          });
+//        }
+//      });
+//
+//      return userGroups;
+//    },
+    _doSearch: function (searchTerm) {
+//      var groups = this._searchGroups(searchTerm);
+//      var users = this._searchUsers(searchTerm);
+//      
+//      return $.merge(groups, users);
+      return this._searchUsers(searchTerm);
+    },
+		
 		options: [
  				{
           caption : "Lähetä myös itselle",
