bc28fd1301a699373bde5f2e5f70a8fffc4a60e2
jjlankinen <jari.lankinen@otavanopisto.fi>
2014-12-08 08:07:35 +0200

    * Changed materials reading view's navigation to the left side

--- muikku/src/main/webapp/resources/scripts/gui/workspace-materials-reading.js
+++ muikku/src/main/webapp/resources/scripts/gui/workspace-materials-reading.js
@@ -43,6 +43,194 @@
         return -$(this).height() + 250;
       }
     });
+    
+    // Workspace's materials's reading view
+    if ($('#workspaceMaterialsReadingTOCWrapper').length > 0) {
+      
+      var height = $(window).height();
+      var tocWrapper = $('#workspaceMaterialsReadingTOCContainer');
+      var navWrapper = $('#workspaceMaterialsReadingNav');
+      var tocOpenCloseButton = $('.wi-workspace-materials-reading-navi-button-toc > .icon-navicon');
+      var tocPinButton = $('#workspaceMaterialsReadingTOCPinicon');
+      var contentPageContainer = $('#contentWorkspaceMaterialsReading');
+      
+      var contentOffset;
+      var windowMinWidth;
+      var tocWrapperWidth = tocWrapper.width();
+      var navWrapperWidth = navWrapper.width();
+      var tocWrapperLeftMargin = "-" + (tocWrapperWidth - navWrapperWidth) + "px";
+      var contentMinLeftOffset = tocWrapperWidth + navWrapperWidth + 10;
+      
+      if (tocWrapper.length > 0) {
+        // If we have tocWrapper lets hide it first and set negative margin for later animation
+        tocWrapper
+        .hide()
+        .css({
+          height: height,
+          "margin-left" : tocWrapperLeftMargin
+        });
+      }
+      
+      $(window).resize(function(){
+        height = $(window).height();
+        tocWrapper.height(height);
+        contentOffset = contentPageContainer.offset();
+        windowMinWidth = contentPageContainer.width() + contentMinLeftOffset*2;
+        
+        // Lets prevent page content to slide under TOC when browser window is been resized
+        if ($('#workspaceMaterialsReadingTOCContainer:visible').length !== 0) {
+          
+          if (contentOffset.left < contentMinLeftOffset) {
+            contentPageContainer.css({
+              paddingLeft: contentMinLeftOffset,
+              paddingRight: "10px"
+            });
+          } 
+        } else {
+          contentPageContainer.css({
+            paddingLeft: navWrapperWidth,
+            paddingRight: "10px"
+          });
+        }
+        
+      });
+      
+      // TOC pin button click handling
+      var tocPinned = 0;
+      $(tocPinButton).click(function() {
+        if (tocPinned == 0) {
+          tocPinButton.addClass('selected');
+          tocPinned = 1;  
+        } else {
+          tocPinButton.removeClass('selected');
+          tocPinned = 0;
+        }
+        
+      });
+      
+      // Prevent icon-navicon link from working normally
+      $(tocOpenCloseButton).bind('click', function(e) {
+        e.stopPropagation();
+      });
+
+      $(tocOpenCloseButton).click(function() {
+        
+        // If tocWrapper is visible
+        if ($('#workspaceMaterialsReadingTOCContainer:visible').length !== 0) {
+          contentPageContainer
+          .animate({
+            paddingLeft: navWrapperWidth,
+            paddingRight: "10px"
+          },{
+            duration:500,
+            easing: "easeInOutQuint"
+          });
+          
+          tocWrapper
+          .clearQueue()
+          .stop()
+          .animate({
+            "margin-left" : tocWrapperLeftMargin,
+          }, {
+            duration:500,
+            easing: "easeInOutQuint",
+            complete: function () {
+              $(this).hide();
+            }
+          });
+        // If tocWrapper is not visible  
+        } else {
+          contentPageContainer
+          .animate({
+            paddingLeft: contentMinLeftOffset,
+            paddingRight: "10px"
+          },{
+            duration:500,
+            easing: "easeInOutQuint"
+          });
+          tocWrapper
+          .show()
+          .clearQueue()
+          .stop()
+          .animate({
+            "margin-left" : navWrapperWidth,
+          }, {
+            duration:500,
+            easing: "easeInOutQuint",
+            complete: function () {
+
+              // Lets hide wrapper when user clicks anywhere in the document
+              $(document).bind('click', function(){
+                // Need to check if toc is pinned or not
+                if (tocPinned == 0) {
+                  
+                  contentPageContainer
+                  .animate({
+                    paddingLeft: navWrapperWidth,
+                    paddingRight: "10px"
+                  },{
+                    duration:600,
+                    easing: "easeInOutQuint"
+                  });
+                  
+                  tocWrapper
+                  .clearQueue()
+                  .stop()
+                  .animate({
+                    "margin-left" : tocWrapperLeftMargin
+                  }, {
+                    duration : 600,
+                    easing : "easeInOutQuint",
+                    complete: function() {
+                      $(this).hide();
+                      $(document).unbind('click');
+                    }
+                  });
+                }
+              });
+
+              // Preventing TOC wrapper to disappear if user clicks inside wrapper
+              $(tocWrapper).bind('click', function(e) {
+                e.stopPropagation();
+              });
+              
+            }
+          });
+        }
+        
+      });
+      
+      $('.workspace-materials-toc-content-inner').on('DOMMouseScroll mousewheel', function(ev) {
+        var $this = $(this),
+          scrollTop = this.scrollTop,
+          scrollHeight = this.scrollHeight,
+          height = $this.height(),
+          delta = (ev.type == 'DOMMouseScroll' ?
+            ev.originalEvent.detail * -40 :
+            ev.originalEvent.wheelDelta),
+          up = delta > 0;
+
+        var prevent = function() {
+          ev.stopPropagation();
+          ev.preventDefault();
+          ev.returnValue = false;
+          return false;
+        }
+
+        if (!up && -delta > scrollHeight - height - scrollTop) {
+          // Scrolling down, but this will take us past the bottom.
+          $this.scrollTop(scrollHeight);
+
+          return prevent();
+        } else if (up && delta > scrollTop) {
+          // Scrolling up, but this will take us past the top.
+          $this.scrollTop(0);
+          return prevent();
+        }
+      });
+
+    }
+    
   });
 
   $(document).on('click', '.muikku-save-page', function (event, data) {
