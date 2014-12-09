45ff1c2e7b22bc10a160e2485b5ee1d74d192a94
ilmo.euro <ilmo.euro@gmail.com>
2014-12-09 12:17:23 +0200

    ** Text-field editor with type attr in <object> element

--- muikku/src/main/webapp/resources/scripts/ckplugins/muikku-textfield/plugin.js
+++ muikku/src/main/webapp/resources/scripts/ckplugins/muikku-textfield/plugin.js
@@ -1,30 +1,79 @@
-(function() {
-  'use strict'
-  
-  /* global CKEDITOR */
+/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben, Otavan opisto. All rights reserved.
+ */
+
+( function() {
+  function isMuikkuTextField( element ) {
+    var attributes = element.attributes;
+
+    return (attributes.type == 'application/vnd.muikku.field.text');
+  }
+
+  function createFakeElement( editor, realElement ) {
+    var fake = editor.createFakeParserElement( realElement, 'muikku-text-field', 'muikkutextfield', true );
+    fake.attributes.src = "//placehold.it/150x20";
+    return fake;
+  }
 
   CKEDITOR.plugins.add( 'muikku-textfield', {
-    icons: 'muikkutextfield',
-    init: function(editor) {
-      /* stash the type */
-      editor.filter.addTransformations(
-        [
-          [
-		 				{
-		 					element: 'object',
-		 					left: function(element) {
-                return (element.attributes['type'] == 'application/vnd.muikku.field.text');
-		 					},
-		 					right: function(element,tools) {
-		 					  var fakeElement = editor.createFakeParserElement(element, 'muikku-text-field', 'object');
-		 					  fakeElement.attributes['src'] = 'https://placehold.it/100x20';
-		 					  fakeElement.attributes['style'] = 'margin-left: 1em; margin-right: 1em;';
-		 					  element.replaceWith(fakeElement);
-		 					}
-		 				}
-		 			]
-		 		]
-      );
+    requires: 'dialog,fakeobjects',
+    onLoad: function() {
     },
-  });
-}).call(this);
+    init: function( editor ) {
+      var allowed = 'object[type];' +
+        'param[name,value];';
+
+      editor.addCommand( 'muikkutextfield', new CKEDITOR.dialogCommand( 'muikkutextfield', {
+        allowedContent: allowed,
+        requiredContent: 'object'
+      } ) );
+      editor.ui.addButton && editor.ui.addButton( 'MuikkuTextField', {
+        label: editor.lang.common.flash,
+        command: 'muikkutextfield',
+        toolbar: 'insert,20'
+      } );
+      CKEDITOR.dialog.add( 'muikkutextfield', this.path + 'dialogs/muikkutextfield.js' );
+
+      // If the "menu" plugin is loaded, register the menu items.
+      if ( editor.addMenuItems ) {
+        editor.addMenuItems( {
+          flash: {
+            label: editor.lang.flash.properties,
+            command: 'muikkutextfield',
+            group: 'muikku'
+          }
+        } );
+      }
+
+      editor.on( 'doubleclick', function( evt ) {
+        var element = evt.data.element;
+
+        if ( element.is( 'img' ) && element.data( 'cke-real-element-type' ) == 'muikkutextfield' )
+          evt.data.dialog = 'muikkutextfield';
+      } );
+
+      // If the "contextmenu" plugin is loaded, register the listeners.
+      if ( editor.contextMenu ) {
+        editor.contextMenu.addListener( function( element, selection ) {
+          if ( element && element.is( 'img' ) && !element.isReadOnly() && element.data( 'cke-real-element-type' ) == 'muikkutextfield' )
+            return { flash: CKEDITOR.TRISTATE_OFF };
+        } );
+      }
+    },
+
+    afterInit: function( editor ) {
+      var dataProcessor = editor.dataProcessor,
+        dataFilter = dataProcessor && dataProcessor.dataFilter;
+
+      if ( dataFilter ) {
+        dataFilter.addRules( {
+          elements: {
+            'cke:object': function( element ) {
+              return createFakeElement( editor, element );
+            }
+          }
+        }, 5 );
+      }
+    }
+  } );
+} )();
