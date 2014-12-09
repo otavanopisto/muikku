4d13cc5c6374a788eba96e12fcfd72f4a118d1f5
ilmo.euro <ilmo.euro@gmail.com>
2014-12-09 08:50:05 +0200

    ** Fake elements in material field editors

--- muikku/src/main/webapp/resources/scripts/ckplugins/muikku-textfield/plugin.js
+++ muikku/src/main/webapp/resources/scripts/ckplugins/muikku-textfield/plugin.js
@@ -6,45 +6,23 @@
   CKEDITOR.plugins.add( 'muikku-textfield', {
     icons: 'muikkutextfield',
     init: function(editor) {
-        editor.filter.addTransformations( [
-		 			[
+      /* stash the type */
+      editor.filter.addTransformations(
+        [
+          [
 		 				{
 		 					element: 'object',
 		 					left: function(element) {
-		 					  console.log("object type: %o", element.attributes['type']);
                 return (element.attributes['type'] == 'application/vnd.muikku.field.text');
 		 					},
 		 					right: function(element,tools) {
-                var metaType;
-                var metaContent;
-                for (var i=0, l=element.children.length; i<l; i++) {
-                  var child = element.children[i];
-		 					    console.log("child: %o", child);
-                  if (child.name == 'cke:param') {
-                    switch(child.attributes['name']) {
-                      case 'type':
-                        metaType = child.attributes['value'];
-                        break;
-                      case 'content':
-                        metaContent = child.attributes['value'];
-                        break;
-                    }
-                  }
-                }
-                var result = new CKEDITOR.htmlParser.element(
-                    'input',
-                    {
-                      'type': 'text',
-                      'data-muikku-meta-type': metaType,
-                      'data-muikku-meta-content': metaContent
-                    }
-                );
-                element.replaceWith(result);
+		 					  var fakeElement = editor.createFakeParserElement(element, 'muikku-text-field', 'object');
+		 					  element.replaceWith(fakeElement);
 		 					}
 		 				}
 		 			]
 		 		]
       );
-    }
+    },
   });
 }).call(this);
