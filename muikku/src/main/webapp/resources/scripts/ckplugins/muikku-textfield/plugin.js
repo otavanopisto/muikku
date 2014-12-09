f07264904849e899bf74e36d2f01d2a9914865d9
ilmo.euro <ilmo.euro@gmail.com>
2014-12-09 13:13:35 +0200

    ** Separated allowed/required content to its own feature

--- muikku/src/main/webapp/resources/scripts/ckplugins/muikku-textfield/plugin.js
+++ muikku/src/main/webapp/resources/scripts/ckplugins/muikku-textfield/plugin.js
@@ -21,20 +21,23 @@
     },
     init: function( editor ) {
       var allowed = 'object[type];param[name,value];';
-
-      editor.addCommand( 'muikkutextfield', new CKEDITOR.dialogCommand( 'muikkutextfield', {
+      
+      editor.addFeature({
+        name: 'muikkutextfieldfeature',
         allowedContent: allowed,
         requiredContent: 'object'
-      } ) );
-      editor.ui.addButton && editor.ui.addButton( 'MuikkuTextField', {
+      });
+      editor.addCommand('muikkutextfield', new CKEDITOR.dialogCommand('muikkutextfield', {
+      }));
+      editor.ui.addButton && editor.ui.addButton('MuikkuTextField', {
         label: "Muikku text field properties",
         command: 'muikkutextfield',
         toolbar: 'insert,20'
-      } );
-      CKEDITOR.dialog.add( 'muikkutextfield', this.path + 'dialogs/muikkutextfield.js' );
+      });
+      CKEDITOR.dialog.add('muikkutextfield', this.path + 'dialogs/muikkutextfield.js');
 
       // If the "menu" plugin is loaded, register the menu items.
-      if ( editor.addMenuItems ) {
+      if (editor.addMenuItems) {
         editor.addMenuItems( {
           muikku: {
             label: editor.lang.flash.properties,
@@ -44,15 +47,15 @@
         } );
       }
 
-      editor.on( 'doubleclick', function( evt ) {
+      editor.on('doubleclick', function(evt) {
         var element = evt.data.element;
 
-        if ( element.is( 'img' ) && element.data( 'cke-real-element-type' ) == 'muikkutextfield' )
+        if (element.is( 'img' ) && element.data( 'cke-real-element-type' ) == 'muikkutextfield')
           evt.data.dialog = 'muikkutextfield';
       } );
 
       // If the "contextmenu" plugin is loaded, register the listeners.
-      if ( editor.contextMenu ) {
+      if (editor.contextMenu) {
         editor.contextMenu.addListener( function( element, selection ) {
           if ( element && element.is( 'img' ) && !element.isReadOnly() && element.data( 'cke-real-element-type' ) == 'muikkutextfield' )
             return { flash: CKEDITOR.TRISTATE_OFF };
@@ -60,18 +63,18 @@
       }
     },
 
-    afterInit: function( editor ) {
+    afterInit: function(editor) {
       var dataProcessor = editor.dataProcessor,
         dataFilter = dataProcessor && dataProcessor.dataFilter;
 
-      if ( dataFilter ) {
+      if (dataFilter) {
         dataFilter.addRules( {
           elements: {
             'cke:object': function( element ) {
               return createFakeElement( editor, element );
             }
           }
-        }, 5 );
+        }, 5);
       }
     }
   } );
