fe89736ff27084bf21e4f3e6510c061e98ef342d
ilmo.euro <ilmo.euro@gmail.com>
2014-12-11 13:54:06 +0200

    ** Persistent text field name

--- muikku/src/main/webapp/resources/scripts/ckplugins/muikku-textfield/plugin.js
+++ muikku/src/main/webapp/resources/scripts/ckplugins/muikku-textfield/plugin.js
@@ -84,16 +84,16 @@
         this.setupContent(contentJson);
       },
       onOk : function(event) {
-        var object = new CKEDITOR.dom.element('object');
-        object.setAttribute('type', 'application/vnd.muikku.field.text');
-        var paramType = new CKEDITOR.dom.element('param');
-        paramType.setAttribute('name', 'type');
-        paramType.setAttribute('value', 'application/json');
-        var paramContent = new CKEDITOR.dom.element('param');
-        paramContent.setAttribute('name', 'content');
+        var oldJson = editor.getMuikkuFieldDefinition(editor.getSelection().getStartElement());
+        var name;
+        if (oldJson.name) {
+          name = oldJson.name;
+        } else {
+          name = editor.createRandomMuikkuFieldName();
+        }
         var rightAnswers = [];
         var formAnswers = this.getContentElement('info', 'answers').getValue().split('\n');
-        for (var i=0, l=rightAnswersSource.length; i<l; i++) {
+        for (var i=0, l=formAnswers.length; i<l; i++) {
           var formAnswer = formAnswers[i];
           rightAnswers.push({
             // TODO
@@ -103,16 +103,24 @@
             'normalizeWhitespace': true
           });
         }
-        paramContent.setAttribute('value', JSON.stringify({
-          'name': editor.createRandomMuikkuFieldName(),
+        var newJson = {
+          'name': name,
           'rightAnswers': rightAnswers,
           'columns': this.getContentElement('info', 'width').getValue(),
           'hint': this.getContentElement('info', 'hint').getValue(),
           'help': this.getContentElement('info', 'help').getValue()
-        }));
+        };
+        
+        var object = new CKEDITOR.dom.element('object');
+        object.setAttribute('type', 'application/vnd.muikku.field.text');
+        var paramType = new CKEDITOR.dom.element('param');
+        paramType.setAttribute('name', 'type');
+        paramType.setAttribute('value', 'application/json');
+        var paramContent = new CKEDITOR.dom.element('param');
+        paramContent.setAttribute('name', 'content');
+        paramContent.setAttribute('value', JSON.stringify(newJson));
         object.append(paramType);
         object.append(paramContent);
-        
         var fakeElement = createFakeElement(editor, object);
         editor.insertElement(fakeElement);
       },
