f91ce1c0897f9f67e0c91ee62174ade3589135ae
pkukkon <pasi.kukkonen@otavanopisto.fi>
2014-12-11 12:13:54 +0200

    - checkbox field localization

---
+++ muikku/src/main/webapp/resources/scripts/ckplugins/muikku-checkbox/lang/en.js
@@ -0,0 +1,9 @@
+CKEDITOR.plugins.setLang('muikku-checkbox', 'en', {
+  uiElement: 'Rastituslista',
+  toolbarMenu: 'Checkbox List',
+  propertiesMenu: 'Checkbox List Properties',
+  propertiesDialogTitle: 'Checkbox List Properties', 
+  propertiesDialogOrientation: 'Orientation',
+  propertiesDialogOrientationHor: 'Horizontal',
+  propertiesDialogOrientationVer: 'Vertical',
+});
