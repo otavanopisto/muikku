baa3e1a41c1b88849db0f1297ac3e65b4a5507d4
Antti Leppä <antti.leppa@foyt.fi>
2014-12-15 09:48:22 +0200

    ** Changed LoggedInInterceptor to redirect user into login page when user is not logged in

--- muikku-security/src/main/java/fi/muikku/security/LoggedInInterceptor.java
+++ muikku-security/src/main/java/fi/muikku/security/LoggedInInterceptor.java
@@ -1,12 +1,15 @@
 package fi.muikku.security;
 
 import java.io.Serializable;
+import java.net.URLEncoder;
 
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Interceptor for @LoggedIn annotation. Checks that the user calling the annotated method has logged in. 
@@ -21,16 +24,25 @@ public class LoggedInInterceptor implements Serializable {
   private static final long serialVersionUID = -2098163583725766390L;
 
   @Inject
+  private HttpServletRequest httpServletRequest;
+
+  @Inject
   private Instance<Identity> identityInstance;
 
   @AroundInvoke
   public Object checkLogin(InvocationContext ctx) throws Exception {
     Identity identity = identityInstance.get();
     if (identity != null) {
-      if (identity.isLoggedIn())
+      if (identity.isLoggedIn()) {
         return ctx.proceed();
-      else
-        throw new AuthorizationException("Not logged in");
+      } else { 
+        String redirectUrl = (String) httpServletRequest.getAttribute(RequestDispatcher.FORWARD_REQUEST_URI);
+        if (redirectUrl == null) {
+          redirectUrl = httpServletRequest.getRequestURL().toString();
+        }
+        
+        return "/login.jsf?faces-redirect=true&redirectUrl=" + URLEncoder.encode(redirectUrl, "UTF-8");
+      }
     } else
       throw new RuntimeException("LoggedInInterceptor - Identity bean not found");
   }
