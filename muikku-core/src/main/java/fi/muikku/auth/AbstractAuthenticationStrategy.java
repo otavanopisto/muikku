61a9525c2bf7637b0eb0d3a56ca840d1a75c9195
Antti Leppä <antti.leppa@foyt.fi>
2014-12-16 15:29:27 +0200

    ** Changed login to use default user identity when logging in

--- muikku-core/src/main/java/fi/muikku/auth/AbstractAuthenticationStrategy.java
+++ muikku-core/src/main/java/fi/muikku/auth/AbstractAuthenticationStrategy.java
@@ -81,6 +81,7 @@ public abstract class AbstractAuthenticationStrategy implements AuthenticationPr
 
     UserEntity emailUser = emailUsers.size() == 1 ? emailUsers.get(0) : null;
     boolean newAccount = false;
+    User activeUser = null;
 
     UserIdentification userIdentification = userIdentificationController.findUserIdentificationByAuthSourceAndExternalId(authSource, externalId);
     if (userIdentification != null) {
@@ -88,6 +89,9 @@ public abstract class AbstractAuthenticationStrategy implements AuthenticationPr
       if (emailUser != null && emailUser.getId() != userIdentification.getUser().getId()) {
         return new AuthenticationResult(Status.CONFLICT, ConflictReason.EMAIL_BELONGS_TO_ANOTHER_USER);
       }
+      
+      // TODO: This is probably not the best way to resolve active user....
+      activeUser = userSchoolDataController.listUsersByEmails(emails).get(0);
     } else {
       // User has not used this auth source before
       if (emailUser != null) {
@@ -105,36 +109,24 @@ public abstract class AbstractAuthenticationStrategy implements AuthenticationPr
           }
           
           // Your guess for default identity is the first of returned users.
-          User user = users.get(0);
-          
-          SchoolDataSource schoolDataSource = schoolDataController.findSchoolDataSource(user.getSchoolDataSource());
-          userEntityController.updateDefaultSchoolDataSource(userEntity, schoolDataSource);
-          userEntityController.updateDefaultIdentifier(userEntity, user.getIdentifier());
+          activeUser = users.get(0);
         } else {
-          String defaultIdentifier = null;
-          SchoolDataSource defaultSchoolDataSource = null;
-          
           // If user could not be found from any of the sources we create new and attach those 
           for (SchoolDataSource schoolDataSource : schoolDataController.listSchoolDataSources()) {
             User user = userSchoolDataController.createUser(schoolDataSource, firstName, lastName);
-            if (user != null) {
-              userSchoolDataIdentifierController.createUserSchoolDataIdentifier(user.getSchoolDataSource(), user.getIdentifier(), userEntity);
-              if ((defaultIdentifier == null) && (defaultSchoolDataSource == null)) {
-                defaultIdentifier = user.getIdentifier();
-                defaultSchoolDataSource = schoolDataSource;
-              }
+            if (user != null && activeUser == null) {
+              activeUser = user;
             }
-          }
-          
-          if ((defaultIdentifier != null) && (defaultSchoolDataSource != null)) {
-            userEntityController.updateDefaultSchoolDataSource(userEntity, defaultSchoolDataSource);
-            userEntityController.updateDefaultIdentifier(userEntity, defaultIdentifier);
           }
         }
         
         userIdentification = userIdentificationController.createUserIdentification(userEntity, authSource, externalId);
         newAccount = true;
       }
+    }
+    
+    if (activeUser == null) {
+      throw new AuthenticationHandleException("Active user could not be found");
     }
 
     List<String> existingAddresses = userEmailEntityController.listAddressesByUserEntity(userIdentification.getUser());
@@ -144,17 +136,23 @@ public abstract class AbstractAuthenticationStrategy implements AuthenticationPr
       }
     }
 
-    return login(userIdentification, newAccount);
+    return login(userIdentification, activeUser, newAccount);
   }
 
-  private AuthenticationResult login(UserIdentification userIdentification, boolean newAccount) {
-    UserEntity user = userIdentification.getUser();
+  private AuthenticationResult login(UserIdentification userIdentification, User user, boolean newAccount) {
+    UserEntity userEntity = userIdentification.getUser();
     UserEntity loggedUser = sessionController.getLoggedUser();
-    if ((loggedUser == null) || loggedUser.getId().equals(user.getId())) {
-      sessionController.login(user.getId());
-      userEntityController.updateLastLogin(user);
+    
+    SchoolDataSource schoolDataSource = schoolDataController.findSchoolDataSource(user.getSchoolDataSource());
+    userEntityController.updateDefaultSchoolDataSource(userEntity, schoolDataSource);
+    userEntityController.updateDefaultIdentifier(userEntity, user.getIdentifier());
+    sessionController.setActiveUserIdentifier(schoolDataSource.getIdentifier(), user.getIdentifier());;
+    
+    if ((loggedUser == null) || loggedUser.getId().equals(userEntity.getId())) {
+      sessionController.login(userEntity.getId());
+      userEntityController.updateLastLogin(userEntity);
       HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-      userLoggedInEvent.fire(new LoginEvent(user.getId(), this, req.getRemoteAddr()));
+      userLoggedInEvent.fire(new LoginEvent(userEntity.getId(), this, req.getRemoteAddr()));
       return new AuthenticationResult(newAccount ? Status.NEW_ACCOUNT : Status.LOGIN);
     } else {
       return new AuthenticationResult(Status.CONFLICT, ConflictReason.LOGGED_IN_AS_DIFFERENT_USER);
--- muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/PyramusUserSchoolDataBridge.java
+++ muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/PyramusUserSchoolDataBridge.java
@@ -23,6 +23,8 @@ import fi.muikku.schooldata.entity.UserEmail;
 import fi.muikku.schooldata.entity.UserImage;
 import fi.muikku.schooldata.entity.UserProperty;
 import fi.pyramus.rest.model.CourseStaffMemberRole;
+import fi.pyramus.rest.model.Person;
+import fi.pyramus.rest.model.StaffMember;
 import fi.pyramus.rest.model.Student;
 import fi.pyramus.rest.model.StudyProgramme;
 import fi.pyramus.rest.model.UserRole;
@@ -70,7 +72,16 @@ public class PyramusUserSchoolDataBridge implements UserSchoolDataBridge {
 	  
 	  return entityFactory.createEntity(students, studyProgrammes.toArray(new StudyProgramme[0]));
 	}
-	
+
+  private User createStudentEntity(Student student) {
+    List<User> users = createStudentEntities(new Student[] { student } );
+    if (users.isEmpty()) {
+      return null;
+    }
+    
+    return users.get(0);
+  }
+  
 	@Override
 	public User findUser(String identifier) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
 	  Long studentId = identifierMapper.getPyramusStudentId(identifier);
@@ -88,12 +99,31 @@ public class PyramusUserSchoolDataBridge implements UserSchoolDataBridge {
 
 	@Override
 	public List<User> listUsersByEmail(String email) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
+	  Map<Long, User> userMap = new HashMap<Long, User>();
+	  Long personId = null;
+	  
+	  for (Student student : pyramusClient.get("/students/students?email=" + email, Student[].class)) {
+	    userMap.put(student.getId(), createStudentEntity(student));
+	    personId = student.getPersonId();
+	  }
+	  
+	  for (StaffMember staffMember : pyramusClient.get("/staff/members?email=" + email, fi.pyramus.rest.model.StaffMember[].class)) {
+	    userMap.put(staffMember.getId(), entityFactory.createEntity(staffMember));
+      personId = staffMember.getPersonId();
+	  }
+	  
 	  List<User> result = new ArrayList<User>();
-
-    result.addAll(createStudentEntities(pyramusClient.get("/students/students?email=" + email, Student[].class)));
-    result.addAll(entityFactory.createEntity(pyramusClient.get("/staff/members?email=" + email, fi.pyramus.rest.model.StaffMember[].class)));
-
-    return result;
+	  
+	  if (personId != null) {
+	    Person person = pyramusClient.get("/persons/persons/" + personId, fi.pyramus.rest.model.Person.class);
+	    if (userMap.containsKey(person.getDefaultUserId())) {
+	      result.add(userMap.remove(person.getDefaultUserId()));
+	    }
+    }
+	  
+	  result.addAll(userMap.values());
+	  
+	  return result;
 	}
 
 	@Override
