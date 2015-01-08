08e825b67e33cf8ed39e6d667437f607a9f29ca5
ilmo.euro <ilmo.euro@gmail.com>
2015-01-08 12:05:56 +0200

    ** Correct current revision number for HTML material REST model

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/HtmlMaterialController.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/HtmlMaterialController.java
@@ -5,6 +5,7 @@ import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
+import fi.muikku.plugins.material.coops.dao.HtmlMaterialRevisionDAO;
 import fi.muikku.plugins.material.dao.HtmlMaterialDAO;
 import fi.muikku.plugins.material.events.HtmlMaterialCreateEvent;
 import fi.muikku.plugins.material.events.HtmlMaterialDeleteEvent;
@@ -14,37 +15,49 @@ import fi.muikku.plugins.material.model.HtmlMaterial;
 @Dependent
 @Stateless
 public class HtmlMaterialController {
+
+  @Inject
+  private HtmlMaterialDAO htmlMaterialDAO;
   
-	@Inject
-	private HtmlMaterialDAO htmlMaterialDAO;
-	
-	@Inject
-	private Event<HtmlMaterialCreateEvent> materialCreateEvent;
-	
-	@Inject
+  @Inject
+  private HtmlMaterialRevisionDAO htmlMaterialRevisionDAO;
+
+  @Inject
+  private Event<HtmlMaterialCreateEvent> materialCreateEvent;
+
+  @Inject
   private Event<HtmlMaterialDeleteEvent> materialDeleteEvent;
-  
+
   @SuppressWarnings("unused")
   @Inject
   private Event<HtmlMaterialUpdateEvent> materialUpdateEvent;
-  
-	public HtmlMaterial createHtmlMaterial(String title, String html, String contentType, Long revisionNumber) {
-		HtmlMaterial material = htmlMaterialDAO.create(title, html, contentType, revisionNumber);
-		materialCreateEvent.fire(new HtmlMaterialCreateEvent(material));
-		return material;
-	}
-	
-	public HtmlMaterial findHtmlMaterialById(Long id) {
-		return htmlMaterialDAO.findById(id);
-	}
+
+  public HtmlMaterial createHtmlMaterial(String title, String html,
+      String contentType, Long revisionNumber) {
+    HtmlMaterial material = htmlMaterialDAO.create(title, html, contentType,
+        revisionNumber);
+    materialCreateEvent.fire(new HtmlMaterialCreateEvent(material));
+    return material;
+  }
+
+  public HtmlMaterial findHtmlMaterialById(Long id) {
+    return htmlMaterialDAO.findById(id);
+  }
 
   public void deleteHtmlMaterial(HtmlMaterial htmlMaterial) {
     materialDeleteEvent.fire(new HtmlMaterialDeleteEvent(htmlMaterial));
     htmlMaterialDAO.delete(htmlMaterial);
   }
-	
-	public HtmlMaterial updateHtmlMaterialHtml(HtmlMaterial htmlMaterial, String html){
-	  return htmlMaterialDAO.updateData(htmlMaterial, html);
-	}
-	
+
+  public HtmlMaterial updateHtmlMaterialHtml(HtmlMaterial htmlMaterial,
+      String html) {
+    return htmlMaterialDAO.updateData(htmlMaterial, html);
+  }
+  
+  public long lastHtmlMaterialRevision(HtmlMaterial htmlMaterial) {
+    return htmlMaterialRevisionDAO.maxRevisionByHtmlMaterial(htmlMaterial);
+  }
+  
+  
+
 }
