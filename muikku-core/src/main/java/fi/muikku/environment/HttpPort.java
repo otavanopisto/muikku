cde0e7f0ed74a849fcdd976bcb5fbc8c6bd190e0
Antti Leppä <antti.leppa@foyt.fi>
2015-01-08 11:01:30 +0200

    ** Reduced HttpPort and HttpsPort targets into MEHTOD and FIELD

--- muikku-core/src/main/java/fi/muikku/environment/HttpPort.java
+++ muikku-core/src/main/java/fi/muikku/environment/HttpPort.java
@@ -8,12 +8,10 @@ import javax.inject.Qualifier;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Qualifier
-@Target({ TYPE, METHOD, PARAMETER, FIELD })
+@Target({ METHOD, FIELD })
 @Retention(RUNTIME)
 @Documented
 public @interface HttpPort {
