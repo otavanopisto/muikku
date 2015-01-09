59dee6774b5a311b367a9c56ba7f8ea59c237090
Antti Leppä <antti.leppa@foyt.fi>
2015-01-09 06:37:42 +0200

    ** Fixed bug in HtmlMaterialUpdateEvent that caused both html properites to be null

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/events/HtmlMaterialUpdateEvent.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/events/HtmlMaterialUpdateEvent.java
@@ -6,6 +6,8 @@ public class HtmlMaterialUpdateEvent extends MaterialUpdateEvent<HtmlMaterial> {
 
   public HtmlMaterialUpdateEvent(HtmlMaterial material, String oldHtml, String newHtml) {
     super(material);
+    this.oldHtml = oldHtml;
+    this.newHtml = newHtml;
   }
   
   public String getOldHtml() {
