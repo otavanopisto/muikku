d19e5aed6b9ba484465e96f580141a5e68cd3e67
Antti Leppä <antti.leppa@foyt.fi>
2015-01-16 13:24:41 +0200

    ** Moved pyramus rest client into its own file

--- muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/rest/SystemPyramusClient.java
+++ muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/rest/SystemPyramusClient.java
@@ -1,5 +1,7 @@
 package fi.muikku.plugins.schooldatapyramus.rest;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -7,12 +9,28 @@ import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.ext.ContextResolver;
 
 import org.apache.commons.lang3.StringUtils;
+import org.jboss.resteasy.client.jaxrs.cache.BrowserCacheFeature;
 import org.joda.time.DateTime;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 import fi.muikku.controller.PluginSettingsController;
 import fi.muikku.plugins.schooldatapyramus.SchoolDataPyramusPluginDescriptor;
@@ -22,7 +40,9 @@ import fi.muikku.plugins.schooldatapyramus.rest.qualifier.PyramusSystem;
 
 @ApplicationScoped
 @PyramusSystem
-class SystemPyramusClient extends AbstractPyramusClient implements PyramusClient {
+@Singleton
+@Lock (LockType.WRITE)
+class SystemPyramusClient extends PyramusRestClient implements PyramusClient {
 
   private static final int EXPIRE_SLACK = 3;
   
@@ -31,6 +51,9 @@ class SystemPyramusClient extends AbstractPyramusClient implements PyramusClient
 
   @Inject
   private Logger logger;
+
+  @Inject
+  private PyramusRestClient restClient;
 
   @Inject
   private SystemOauthController systemOauthController;
@@ -54,31 +77,63 @@ class SystemPyramusClient extends AbstractPyramusClient implements PyramusClient
   }
 
   @Override
-  protected String getAccessToken() {
-
-    SystemAccessToken systemAccessToken = systemOauthController.getSystemAccessToken();
-    if (systemAccessToken == null) {
-      AccessToken createdAccessToken = createAccessToken(authCode);
-      accessToken = createdAccessToken.getAccessToken();
-      accessTokenExpires = new DateTime().plusSeconds(createdAccessToken.getExpiresIn());
-      systemOauthController.createSystemAccessToken(accessToken, accessTokenExpires.getMillis(), createdAccessToken.getRefreshToken());
-    } else {
-      if (System.currentTimeMillis() > systemAccessToken.getExpires()) {
-        AccessToken refreshedAccessToken = refreshAccessToken(systemAccessToken.getRefreshToken());
-        accessToken = refreshedAccessToken.getAccessToken();
-        accessTokenExpires = new DateTime().plusSeconds(refreshedAccessToken.getExpiresIn() - EXPIRE_SLACK);
-        systemOauthController.refreshSystemAccessToken(systemAccessToken, accessToken, accessTokenExpires.getMillis());
-      } else {
-        accessToken = systemAccessToken.getAccessToken();
-        accessTokenExpires = new DateTime(systemAccessToken.getExpires());
-      }
+  public <T> T post(String path, Entity<?> entity, Class<T> type) {
+    Client client = obtainClient();
+    try {
+      return restClient.post(client, getAccessToken(), path, entity, type);
+    } finally {
+      releaseClient(client);
     }
-
-    return accessToken;
   }
 
   @Override
-  protected Client obtainClient() {
+  public <T> T post(String path, T entity) {
+    Client client = obtainClient();
+    try {
+      return restClient.post(client, getAccessToken(), path, entity);
+    } finally {
+      releaseClient(client);
+    }
+  }
+
+  @Override
+  public <T> T get(String path, Class<T> type) {
+    Client client = obtainClient();
+    try {
+      return restClient.get(client, getAccessToken(), path, type);
+    } finally {
+      releaseClient(client);
+    }
+  }
+
+  private String getAccessToken() {
+    Client client = obtainClient();
+    try {
+      SystemAccessToken systemAccessToken = systemOauthController.getSystemAccessToken();
+      if (systemAccessToken == null) {
+        AccessToken createdAccessToken = restClient.createAccessToken(client, authCode);
+        accessToken = createdAccessToken.getAccessToken();
+        accessTokenExpires = new DateTime().plusSeconds(createdAccessToken.getExpiresIn());
+        systemOauthController.createSystemAccessToken(accessToken, accessTokenExpires.getMillis(), createdAccessToken.getRefreshToken());
+      } else {
+        if (System.currentTimeMillis() > systemAccessToken.getExpires()) {
+          AccessToken refreshedAccessToken = restClient.refreshAccessToken(client, systemAccessToken.getRefreshToken());
+          accessToken = refreshedAccessToken.getAccessToken();
+          accessTokenExpires = new DateTime().plusSeconds(refreshedAccessToken.getExpiresIn() - EXPIRE_SLACK);
+          systemOauthController.refreshSystemAccessToken(systemAccessToken, accessToken, accessTokenExpires.getMillis());
+        } else {
+          accessToken = systemAccessToken.getAccessToken();
+          accessTokenExpires = new DateTime(systemAccessToken.getExpires());
+        }
+      }
+  
+      return accessToken;
+    } finally {
+      releaseClient(client);
+    }
+  }
+
+  private Client obtainClient() {
     if (pooledClients.isEmpty()) {
       return buildClient();
     }
@@ -86,9 +141,63 @@ class SystemPyramusClient extends AbstractPyramusClient implements PyramusClient
     return pooledClients.remove(pooledClients.size() - 1);
   }
 
-  @Override
-  protected void releaseClient(Client client) {
+  private void releaseClient(Client client) {
     pooledClients.add(client);
+  }
+  
+  private Client buildClient() {
+    // TODO: trust all only on development environment
+
+    ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+    
+    TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+      public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+        return null;
+      }
+
+      public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+      }
+
+      public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+      }
+    } };
+
+    SSLContext sslContext = null;
+    try {
+      sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+    } catch (NoSuchAlgorithmException | KeyManagementException e) {
+      logger.log(Level.SEVERE, "Failed to initialize trust all certificate manager", e);
+    }
+
+    HostnameVerifier fakeHostnameVerifier = new HostnameVerifier() {
+      @Override
+      public boolean verify(String hostname, SSLSession session) {
+        return true;
+      }
+    };
+    
+    ClientBuilder builder = clientBuilder
+        .sslContext(sslContext)
+        .hostnameVerifier(fakeHostnameVerifier)
+        .register(new JacksonConfigurator())
+        .register(new BrowserCacheFeature());
+    
+    return builder.build();
+  }
+
+
+  private class JacksonConfigurator implements ContextResolver<ObjectMapper> {
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+      ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.registerModule(new JodaModule());
+      objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+      
+      return objectMapper;
+    }
+
   }
 
   private List<Client> pooledClients;
--- muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/rest/UserPyramusClient.java
+++ muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/rest/UserPyramusClient.java
@@ -1,33 +1,60 @@
 package fi.muikku.plugins.schooldatapyramus.rest;
 
 import java.io.Serializable;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.ext.ContextResolver;
+
+import org.jboss.resteasy.client.jaxrs.cache.BrowserCacheFeature;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 import fi.muikku.plugins.schooldatapyramus.rest.qualifier.PyramusUser;
 import fi.muikku.session.SessionController;
 import fi.muikku.session.local.LocalSession;
 
+@Lock(LockType.WRITE)
 @SessionScoped
 @PyramusUser
-class UserPyramusClient extends AbstractPyramusClient implements PyramusClient, Serializable {
+class UserPyramusClient implements PyramusClient, Serializable {
 
   private static final long serialVersionUID = -2643693371146903250L;
   
   private static final int EXPIRE_SLACK = 3;
   
   @Inject
+  private Logger logger;
+
+  @Inject
   @LocalSession
   private SessionController sessionController;
+
+  @Inject
+  private PyramusRestClient restClient;
   
   @PostConstruct
   public void init() {
@@ -40,31 +67,63 @@ class UserPyramusClient extends AbstractPyramusClient implements PyramusClient, 
       pooledClient.close();
     }
   }
-  
+
   @Override
-  protected String getAccessToken() {
-    
+  public <T> T post(String path, Entity<?> entity, Class<T> type) {
+    Client client = obtainClient();
+    try {
+      return restClient.post(client, getAccessToken(), path, entity, type);
+    } finally {
+      releaseClient(client);
+    }
+  }
+
+  @Override
+  public <T> T post(String path, T entity) {
+    Client client = obtainClient();
+    try {
+      return restClient.post(client, getAccessToken(), path, entity);
+    } finally {
+      releaseClient(client);
+    }
+  }
+
+  @Override
+  public <T> T get(String path, Class<T> type) {
+    Client client = obtainClient();
+    try {
+      return restClient.get(client, getAccessToken(), path, type);
+    } finally {
+      releaseClient(client);
+    }
+  }
+  
+  private String getAccessToken() {
     fi.muikku.session.AccessToken accessToken = sessionController.getOAuthAccessToken("pyramus");
     
     if(accessToken == null){
       return null;
     }
-    Date expires = accessToken.getExpires();   
-    if(expires.before(new Date())){
-      AccessToken refreshedAccessToken = refreshAccessToken(accessToken.getRefreshToken());
-      Calendar calendar = new GregorianCalendar();
-      calendar.setTime(new Date());
-      calendar.add(Calendar.SECOND, (refreshedAccessToken.getExpiresIn() - EXPIRE_SLACK));
-      sessionController.addOAuthAccessToken("pyramus", calendar.getTime(), refreshedAccessToken.getAccessToken(), refreshedAccessToken.getRefreshToken());
-      return refreshedAccessToken.getAccessToken();
+    
+    Client client = obtainClient();
+    try {
+      Date expires = accessToken.getExpires();   
+      if(expires.before(new Date())){
+        AccessToken refreshedAccessToken = restClient.refreshAccessToken(client, accessToken.getRefreshToken());
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.SECOND, (refreshedAccessToken.getExpiresIn() - EXPIRE_SLACK));
+        sessionController.addOAuthAccessToken("pyramus", calendar.getTime(), refreshedAccessToken.getAccessToken(), refreshedAccessToken.getRefreshToken());
+        return refreshedAccessToken.getAccessToken();
+      }
+      
+      return accessToken.getToken();
+    } finally {
+      releaseClient(client);
     }
-    
-    return accessToken.getToken();
-    
   }
 
-  @Override
-  protected Client obtainClient() {
+  private Client obtainClient() {
     if (pooledClients.isEmpty()) {
       return buildClient();
     }
@@ -72,11 +131,63 @@ class UserPyramusClient extends AbstractPyramusClient implements PyramusClient, 
     return pooledClients.remove(pooledClients.size() - 1);
   }
 
-  @Override
-  protected void releaseClient(Client client) {
+  private void releaseClient(Client client) {
     pooledClients.add(client);
   }
   
   private List<Client> pooledClients;
   
+  private Client buildClient() {
+    // TODO: trust all only on development environment
+
+    ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+    
+    TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+      public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+        return null;
+      }
+
+      public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+      }
+
+      public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+      }
+    } };
+
+    SSLContext sslContext = null;
+    try {
+      sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+    } catch (NoSuchAlgorithmException | KeyManagementException e) {
+      logger.log(Level.SEVERE, "Failed to initialize trust all certificate manager", e);
+    }
+
+    HostnameVerifier fakeHostnameVerifier = new HostnameVerifier() {
+      @Override
+      public boolean verify(String hostname, SSLSession session) {
+        return true;
+      }
+    };
+    
+    ClientBuilder builder = clientBuilder
+        .sslContext(sslContext)
+        .hostnameVerifier(fakeHostnameVerifier)
+        .register(new JacksonConfigurator())
+        .register(new BrowserCacheFeature());
+    
+    return builder.build();
+  }
+
+
+  private class JacksonConfigurator implements ContextResolver<ObjectMapper> {
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+      ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.registerModule(new JodaModule());
+      objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+      
+      return objectMapper;
+    }
+  }
 }
