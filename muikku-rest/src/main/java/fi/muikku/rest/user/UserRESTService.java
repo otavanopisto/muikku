d77a4032fc7e7e1f4abaabcf15e4c17a1b9cb509
Antti Leppä <antti.leppa@foyt.fi>
2015-01-26 10:25:24 +0200

    ** Fixed issue #85 in /user/searchUsers endpoint ** Renamed /user/users endpoint to match default rest layout

--- muikku-rest/src/main/java/fi/muikku/rest/user/UserRESTService.java
+++ muikku-rest/src/main/java/fi/muikku/rest/user/UserRESTService.java
@@ -5,17 +5,19 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import javax.ejb.Stateless;
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+
+import org.apache.commons.lang3.StringUtils;
 
 import fi.muikku.model.users.UserEntity;
 import fi.muikku.rest.AbstractRESTService;
@@ -26,7 +28,6 @@ import fi.muikku.users.UserController;
 import fi.muikku.users.UserEntityController;
 
 @Path("/user")
-@Stateless
 @Produces ("application/json")
 @Consumes ("application/json")
 public class UserRESTService extends AbstractRESTService {
@@ -41,6 +42,42 @@ public class UserRESTService extends AbstractRESTService {
   @Any
   private Instance<SearchProvider> searchProviders;
 
+  @GET
+  @Path ("/users")
+  public Response searchUsers(@QueryParam("searchString") String searchString, @QueryParam("firstResult") @DefaultValue ("0") Integer firstResult, @QueryParam("maxResults") @DefaultValue ("10") Integer maxResults) {
+    try {
+      SearchProvider elasticSearchProvider = getProvider("elastic-search");
+      if (elasticSearchProvider != null) {
+        String[] fields = new String[] { "firstName", "lastName" };
+        SearchResult result = null;
+        
+        if (StringUtils.isBlank(searchString)) {
+          result = elasticSearchProvider.matchAllSearch(firstResult, maxResults, User.class);
+        } else {
+          result = elasticSearchProvider.search(searchString, fields, firstResult, maxResults, User.class);
+        }
+        
+        List<Map<String, Object>> results = result.getResults();
+        boolean hasImage = false;
+        
+        List<fi.muikku.rest.model.User> ret = new ArrayList<fi.muikku.rest.model.User>();
+  
+        for (Map<String, Object> o : results) {
+          String[] id = ((String) o.get("id")).split("/", 2);
+          UserEntity userEntity = userEntityController.findUserEntityByDataSourceAndIdentifier(id[1], id[0]);
+  
+          ret.add(new fi.muikku.rest.model.User(userEntity.getId(), (String) o.get("firstName"), (String) o.get("lastName"), hasImage));
+        }
+        
+        return Response.ok(ret).build();
+      }
+    } catch (Exception e) {
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
+    
+    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+  }
+  
   @GET
   @Path ("/users/{ID}")
   public Response findUser(@PathParam ("ID") Long id) {
@@ -80,35 +117,6 @@ public class UserRESTService extends AbstractRESTService {
 ////    ).build();
 ////  }
 //  
-
-  @GET
-  @Path ("/searchUsers")
-  public Response searchUsers(
-      @QueryParam("searchString") String searchString
-      ) {
-
-    SearchProvider elasticSearchProvider = getProvider("elastic-search");
-    if (elasticSearchProvider != null) {
-      String[] fields = new String[] { "firstName", "lastName" };
-      SearchResult result = elasticSearchProvider.search(searchString, fields, 0, 10, User.class);
-      
-      List<Map<String, Object>> results = result.getResults();
-      boolean hasImage = false;
-      
-      List<fi.muikku.rest.model.User> ret = new ArrayList<fi.muikku.rest.model.User>();
-
-      for (Map<String, Object> o : results) {
-        String[] id = ((String) o.get("id")).split("/", 2);
-        UserEntity userEntity = userEntityController.findUserEntityByDataSourceAndIdentifier(id[1], id[0]);
-
-        ret.add(new fi.muikku.rest.model.User(userEntity.getId(), (String) o.get("firstName"), (String) o.get("lastName"), hasImage));
-      }
-      
-      return Response.ok(ret).build();
-    }
-    
-    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-  }
   
   private SearchProvider getProvider(String name) {
     Iterator<SearchProvider> i = searchProviders.iterator();
