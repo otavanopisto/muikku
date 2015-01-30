b2ef91bdd4ff07bfc86d49857d5ff2092f834390
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2015-01-30 12:16:13 +0200

    Solved a mistake

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialController.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialController.java
@@ -352,7 +352,7 @@ public class WorkspaceMaterialController {
       }
     } catch (Exception e) {
       Throwable cause = e;
-      while (cause != null) {
+      while (e.getCause() != null) {
         cause = cause.getCause();
         if (cause instanceof WorkspaceMaterialContainsAnswersExeption) {
           throw (WorkspaceMaterialContainsAnswersExeption) cause;
--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/model/WorkspaceMaterialAssignmentType.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/model/WorkspaceMaterialAssignmentType.java
@@ -3,5 +3,5 @@ package fi.muikku.plugins.workspace.model;
 public enum WorkspaceMaterialAssignmentType {
   EXERCISE,
   EVALUATED,
-  MIXED, // Do not use.
+  MIXED, // Do not use
 }
