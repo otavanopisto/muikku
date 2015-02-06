162c0f931a0edd12b3a187d6ad2fb5e4dbc00e91
ilmo.euro <ilmo.euro@gmail.com>
2015-02-06 13:54:02 +0200

    ** Visits in front page

---
+++ muikku/src/main/webapp/resources/scripts/gui/widget-visits.js
@@ -0,0 +1,24 @@
+ 
+$(document).ready(function(){
+
+    mApi().workspace.workspaces
+    .read({ minVisits: 1, orderBy: "numVisits" })
+    .callback( function (err, workspaces) {
+    	
+    	if( err ){
+            $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('TODO: Virheilmoitus', err));
+    	}else{
+        renderDustTemplate('frontpage/widget_visits.dust', {
+        	
+        	workspaces :  workspaces
+        	
+        
+        }, function (text) {
+            $('#widgetVisits').append($.parseHTML(text));
+          });
+        
+    	}
+    });
+    
+	});
+
