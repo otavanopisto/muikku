920eb23cd2ed3ccc9c4899c42591994b530e0104
Antti Leppä <antti.leppa@foyt.fi>
2015-02-06 08:21:03 +0200

    ** Added supprot for attaching extra parameters into SchoolDataWorkspaceDiscoveredEvent

--- muikku-core/src/main/java/fi/muikku/schooldata/events/SchoolDataWorkspaceDiscoveredEvent.java
+++ muikku-core/src/main/java/fi/muikku/schooldata/events/SchoolDataWorkspaceDiscoveredEvent.java
@@ -1,12 +1,15 @@
 package fi.muikku.schooldata.events;
 
+import java.util.Map;
+
 public class SchoolDataWorkspaceDiscoveredEvent {
 
-  public SchoolDataWorkspaceDiscoveredEvent(String dataSource, String identifier, String name) {
+  public SchoolDataWorkspaceDiscoveredEvent(String dataSource, String identifier, String name, Map<String, Object> extra) {
     super();
     this.dataSource = dataSource;
     this.identifier = identifier;
     this.name = name;
+    this.extra = extra;
   }
 
   public String getDataSource() {
@@ -28,9 +31,14 @@ public class SchoolDataWorkspaceDiscoveredEvent {
   public Long getDiscoveredWorkspaceEntityId() {
     return discoveredWorkspaceEntityId;
   }
+  
+  public Map<String, Object> getExtra() {
+    return extra;
+  }
 
   private String dataSource;
   private String identifier;
   private String name;
   private Long discoveredWorkspaceEntityId;
+  private Map<String, Object> extra;
 }
