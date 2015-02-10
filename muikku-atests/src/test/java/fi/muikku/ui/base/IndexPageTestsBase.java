1d1d0f0be09324bba3e1c0a0d085d72d01b17548
petja-laitila <petja.laitila@otavanopisto.fi>
2015-02-10 09:13:53 +0200

    ** Mocking start

--- muikku-atests/src/test/java/fi/muikku/ui/base/IndexPageTestsBase.java
+++ muikku-atests/src/test/java/fi/muikku/ui/base/IndexPageTestsBase.java
@@ -13,14 +13,30 @@ import fi.muikkku.ui.AbstractUITest;
 
 public class IndexPageTestsBase extends AbstractUITest {
 
-//  @Before
-//  public void requiredPyramusMocks() {
-//    stubFor(get(urlEqualTo("/1/students/students/1234"))
-//        .withHeader("Accept", equalTo("text/json"))
-//        .willReturn(aResponse().withStatus(200)
-//        .withHeader("Content-Type", "text/json")
-//        .withBody("{\"id\":1234,\"abstractStudentId\":8,\"firstName\":\"Testii\",\"lastName\":\"Userii\",\"nickname\":null,\"additionalInfo\":null,\"additionalContactInfo\":null,\"nationalityId\":null,\"languageId\":null,\"municipalityId\":null,\"schoolId\":null,\"activityTypeId\":null,\"examinationTypeId\":null,\"educationalLevelId\":null,\"studyTimeEnd\":null,\"studyProgrammeId\":3,\"previousStudies\":null,\"education\":null,\"lodging\":false,\"studyStartDate\":\"2010-09-29T00:00:00.000+03:00\",\"studyEndDate\":null,\"studyEndReasonId\":null,\"studyEndText\":null,\"variables\":{},\"tags\":[],\"archived\":false}")));
-//  }
+  @Before
+  public void requiredPyramusMocks() {
+    stubFor(get(urlPathEqualTo("/users/authorize.page"))
+        .withQueryParam("client_id", matching(".*"))
+        .withQueryParam("response_type", matching(".*"))
+        .withQueryParam("redirect_uri", matching(".*"))
+        //        .withHeader("Accept", equalTo("text/json"))
+        .willReturn(aResponse()
+          .withStatus(200)
+          .withHeader("Content-Type", "text/html")
+          .withHeader("Location", "https://dev.muikku.fi:8443/login?_stg=rsp&code=322322323232332")));
+
+    stubFor(get(urlEqualTo("/oauth/token"))
+        .willReturn(aResponse()
+          .withBody("{\"expires_in\":3600,\"refresh_token\":\"322322323232332\",\"access_token\":\"322322323232332\"}")
+          .withStatus(200)));
+
+    stubFor(get(urlEqualTo("/whoami"))
+        .willReturn(aResponse()
+          .withHeader("Content-Type", "text/json")
+          .withBody("{\"id\":5,\"firstName\":\"Test\",\"lastName\":\"User\",\"emails\":[\"testuser@made.up\"]}")
+          .withStatus(200)));
+    
+  }
 
   @Test
   public void IndexPageTest() throws IOException {
