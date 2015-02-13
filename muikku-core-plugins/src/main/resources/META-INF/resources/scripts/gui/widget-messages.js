5487e7f21e372e9d9994194ac411af62984554df
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2015-02-13 10:31:48 +0200

    Fixed the bug 108 Moved 3 widget js-files from mukku project to core-plugins

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/widget-messages.js
@@ -0,0 +1,23 @@
+ 
+$(document).ready(function(){
+
+	
+    mApi().communicator.items.read()
+      .callback(function (err, messages) {
+    	  
+      	if( err ){
+            $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('TODO: Virheilmoitus', err));
+    	}else{    	  
+        renderDustTemplate('frontpage/widget_messages.dust', 
+    		{
+    	   	 messages : messages
+    		}, function (text) {
+
+    		$('#widgetMessages').append($.parseHTML(text));
+          
+        });
+    	}
+      });
+      
+	});
+ 	
