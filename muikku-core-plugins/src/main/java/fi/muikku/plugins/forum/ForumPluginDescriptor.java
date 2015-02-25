64fd54a6beceff9cca7b5ef6c6c2f3f009ce9420
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2015-02-25 08:54:05 +0200

    - Removed default forumarea due to scope problems

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/forum/ForumPluginDescriptor.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/forum/ForumPluginDescriptor.java
@@ -1,26 +1,11 @@
 package fi.muikku.plugins.forum;
 
-import javax.inject.Inject;
-
 import fi.muikku.plugin.PluginDescriptor;
-import fi.muikku.plugins.forum.model.ForumArea;
-import fi.muikku.plugins.forum.model.ForumAreaGroup;
 
 public class ForumPluginDescriptor implements PluginDescriptor {
 
-  @Inject
-  private ForumController forumController;
-  
   @Override
   public void init() {
-    ForumAreaGroup forumAreaGroup = forumController.findForumAreaGroup(1l);
-    
-    if (forumAreaGroup == null)
-      forumAreaGroup = forumController.createForumAreaGroup("Yleinen");
-    
-    ForumArea forumArea = forumController.getForumArea(1l);
-    if (forumArea == null)
-      forumController.createEnvironmentForumArea("Yleinen keskustelu", forumAreaGroup.getId());
   }
 
 	@Override
