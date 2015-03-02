5c5710cacba8ad6e2c59bfa089de287fd37feac6
petja-laitila <petja.laitila@otavanopisto.fi>
2015-03-02 14:10:27 +0200

    ** Muikku tests update   - muikku version   - moved atests module

--- muikku-atests/src/test/java/fi/muikku/ui/base/IndexPageTestsBase.java
+++ muikku-atests/src/test/java/fi/muikku/ui/base/IndexPageTestsBase.java
@@ -17,66 +17,97 @@ public class IndexPageTestsBase extends AbstractUITest {
 
   @Before
   public void requiredPyramusMocks() {
+    
+    stubFor(get(urlMatching("/dnm"))
+      .willReturn(
+        aResponse()
+        .withHeader("Content-Type", "application/json")
+        .withBody("")
+        .withStatus(204)));
+    
     stubFor(get(urlMatching("/users/authorize.*"))
-//        .withQueryParam("client_id", matching("*"))
-//        .withQueryParam("response_type", matching("*"))
-//        .withQueryParam("redirect_uri", matching("*"))
-        //        .withHeader("Accept", equalTo("text/json"))
-        .willReturn(aResponse()
+    // .withQueryParam("client_id", matching("*"))
+    // .withQueryParam("response_type", matching("*"))
+    // .withQueryParam("redirect_uri", matching("*"))
+      // .withHeader("Accept", equalTo("text/json"))
+      .willReturn(
+        aResponse()
           .withStatus(302)
           .withHeader("Content-Length", "0")
           .withHeader("Location", "http://dev.muikku.fi:8080/login?_stg=rsp&code=1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")));
-    
-    stubFor(post(urlMatching("/1/oauth/token"))
-        .willReturn(aResponse()
+
+    stubFor(post(urlMatching("/1/oauth/token")).willReturn(
+      aResponse().withHeader("Content-Type", "application/json")
+        .withBody("{\"expires_in\":3600,\"refresh_token\":\"12312ewsdf34fsd234r43rfsw32rf33e\",\"access_token\":\"ur84ur839843ruwf39843ru39ru37y2e\"}")
+        .withStatus(200)));
+
+    stubFor(get(urlMatching("/1/system/whoami")).willReturn(
+      aResponse().withHeader("Content-Type", "application/json")
+        .withBody("{\"id\":1,\"firstName\":\"Test\",\"lastName\":\"User\",\"emails\":[\"testuser@made.up\"]}")
+        .withStatus(200)));
+
+    stubFor(get(urlMatching("/1/students/students/.*"))
+      .willReturn(
+        aResponse()
           .withHeader("Content-Type", "application/json")
-          .withBody("{\"expires_in\":3600,\"refresh_token\":\"12312ewsdf34fsd234r43rfsw32rf33e\",\"access_token\":\"ur84ur839843ruwf39843ru39ru37y2e\"}")
+          .withBody("{\"id\":1,\"personId\":1,\"firstName\":\"Test\",\"lastName\":\"User\",\"nickname\":null,\"additionalInfo\":null,\"additionalContactInfo\":null,\"nationalityId\":null,\"languageId\":null,\"municipalityId\":null,\"schoolId\":null,\"activityTypeId\":null,\"examinationTypeId\":null,\"educationalLevelId\":null,\"studyTimeEnd\":null,\"studyProgrammeId\":1,\"previousStudies\":null,\"education\":null,\"lodging\":false,\"studyStartDate\":null,\"studyEndDate\":null,\"studyEndReasonId\":null,\"studyEndText\":null,\"variables\":{},\"tags\":[],\"archived\":false}")
           .withStatus(200)));
 
-    stubFor(get(urlMatching("/1/system/whoami"))
-        .willReturn(aResponse()
-          .withHeader("Content-Type", "application/json")
-          .withBody("{\"id\":1,\"firstName\":\"Test\",\"lastName\":\"User\",\"emails\":[\"testuser@made.up\"]}")
-          .withStatus(200)));
-    
-    stubFor(get(urlMatching("/1/students/students/.*"))
-        .willReturn(aResponse()
-        .withHeader("Content-Type", "application/json")
-        .withBody("{\"id\":1,\"personId\":1,\"firstName\":\"Test\",\"lastName\":\"User\",\"nickname\":null,\"additionalInfo\":null,\"additionalContactInfo\":null,\"nationalityId\":null,\"languageId\":null,\"municipalityId\":null,\"schoolId\":null,\"activityTypeId\":null,\"examinationTypeId\":null,\"educationalLevelId\":null,\"studyTimeEnd\":null,\"studyProgrammeId\":1,\"previousStudies\":null,\"education\":null,\"lodging\":false,\"studyStartDate\":null,\"studyEndDate\":null,\"studyEndReasonId\":null,\"studyEndText\":null,\"variables\":{},\"tags\":[],\"archived\":false}")
-        .withStatus(200)));
-    
-    stubFor(get(urlMatching("/1/students/students/.*/emails"))
-        .willReturn(aResponse()
-        .withHeader("Content-Type", "application/json")
+    stubFor(get(urlMatching("/1/students/students/.*/emails")).willReturn(
+      aResponse().withHeader("Content-Type", "application/json")
         .withBody("{\"id\":1,\"contactTypeId\":2,\"defaultAddress\":true,\"address\":\"testuser@made.up\"}")
         .withStatus(200)));
 
     stubFor(get(urlEqualTo("/1/students/students?email=testuser@made.up"))
-//        .withQueryParam("email", matching(".*"))
-        .willReturn(aResponse()
-        .withHeader("Content-Type", "application/json")
-        .withBody("[{\"id\":1,\"personId\":1,\"firstName\":\"Test\",\"lastName\":\"User\",\"nickname\":\"Student\",\"additionalInfo\":null,\"additionalContactInfo\":null,\"nationalityId\":null,\"languageId\":null,\"municipalityId\":1,\"schoolId\":null,\"activityTypeId\":null,\"examinationTypeId\":null,\"educationalLevelId\":null,\"studyTimeEnd\":null,\"studyProgrammeId\":1,\"previousStudies\":null,\"education\":null,\"lodging\":false,\"studyStartDate\":null,\"studyEndDate\":null,\"studyEndReasonId\":null,\"studyEndText\":null,\"variables\":{},\"tags\":[],\"archived\":false}]")
-        .withStatus(200)));
+    // .withQueryParam("email", matching(".*"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody("[{\"id\":1,\"personId\":1,\"firstName\":\"Test\",\"lastName\":\"User\",\"nickname\":\"Student\",\"additionalInfo\":null,\"additionalContactInfo\":null,\"nationalityId\":null,\"languageId\":null,\"municipalityId\":1,\"schoolId\":null,\"activityTypeId\":null,\"examinationTypeId\":null,\"educationalLevelId\":null,\"studyTimeEnd\":null,\"studyProgrammeId\":1,\"previousStudies\":null,\"education\":null,\"lodging\":false,\"studyStartDate\":null,\"studyEndDate\":null,\"studyEndReasonId\":null,\"studyEndText\":null,\"variables\":{},\"tags\":[],\"archived\":false}]")
+          .withStatus(200)));
 
     stubFor(get(urlMatching("/1/persons/persons/.*"))
-        .willReturn(aResponse()
-        .withHeader("Content-Type", "application/json")
-        .withBody("{\"id\":1,\"birthday\":\"1990-02-04T00:00:00.000+02:00\",\"socialSecurityNumber\":\"345345-3453\",\"sex\":\"MALE\",\"secureInfo\":false,\"basicInfo\":null,\"defaultUserId\":1}")
-        .withStatus(200)));
-    
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody("{\"id\":1,\"birthday\":\"1990-02-04T00:00:00.000+02:00\",\"socialSecurityNumber\":\"345345-3453\",\"sex\":\"MALE\",\"secureInfo\":false,\"basicInfo\":null,\"defaultUserId\":1}")
+          .withStatus(200)));
+
+    stubFor(get(urlEqualTo("/1/students/students?filterArchived=false&firstResult=0&maxResults=100"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody("[{\"id\":1,\"personId\":1,\"firstName\":\"Test\",\"lastName\":\"User\",\"nickname\":null,\"additionalInfo\":null,\"additionalContactInfo\":null,\"nationalityId\":null,\"languageId\":null,\"municipalityId\":null,\"schoolId\":null,\"activityTypeId\":null,\"examinationTypeId\":null,\"educationalLevelId\":null,\"studyTimeEnd\":null,\"studyProgrammeId\":1,\"previousStudies\":null,\"education\":null,\"lodging\":false,\"studyStartDate\":null,\"studyEndDate\":null,\"studyEndReasonId\":null,\"studyEndText\":null,\"variables\":{},\"tags\":[],\"archived\":false}]")
+          .withStatus(200)));
     
     stubFor(get(urlMatching("/1/staff/members"))
-        .withQueryParam("email", matching("staff@made.up"))
-        .willReturn(aResponse()
-        .withHeader("Content-Type", "application/json")
-        .withBody("[{\"id\":5,\"personId\":5,\"additionalContactInfo\":null,\"firstName\":\"Test\",\"lastName\":\"Staffmember\",\"title\":null,\"role\":\"ADMINISTRATOR\",\"variables\":{},\"tags\":[]}]")
-        .withStatus(200)));
+      .withQueryParam("email", matching("staff@made.up"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody("[{\"id\":5,\"personId\":5,\"additionalContactInfo\":null,\"firstName\":\"Test\",\"lastName\":\"Staffmember\",\"title\":null,\"role\":\"ADMINISTRATOR\",\"variables\":{},\"tags\":[]}]")
+          .withStatus(200)));
     
     stubFor(get(urlMatching("/1/courses/staffMemberRoles"))
-        .willReturn(aResponse()
-        .withHeader("Content-Type", "application/json")
-        .withBody("")
-        .withStatus(204)));
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody("")
+            .withStatus(204)));
+    
+    stubFor(get(urlMatching("/1/courses/courses/.*/students?filterArchived=false"))
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody("")
+            .withStatus(204)));
+    
+    stubFor(get(urlMatching("/1/courses/courses/.*/staffMembers"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody("")
+          .withStatus(204)));
   }
 
 //  @Test
@@ -88,19 +119,16 @@ public class IndexPageTestsBase extends AbstractUITest {
   
   @Test
   @SqlBefore("sql/loginSetup.sql")
-//  @SqlAfter("sql/loginTeardown.sql")
+  @SqlAfter("sql/loginTeardown.sql")
   public void loginTest() throws IOException {
-//    getWebDriver().get(getAppUrl());
+    getWebDriver().get(getAppUrl());
 //    waitForElementToBeClickable(By.className("bt-login"));
 //    getWebDriver().findElement(By.className("bt-login")).click();
-    getWebDriver().get("http://dev.muikku.fi:8080/login?authSourceId=1");  
-//    sleep(2000);
-//    waitForElementToBePresent(By.className("index"));
+    getWebDriver().get("http://dev.muikku.fi:8080/login?authSourceId=1");
+    sleep(500);
+////    waitForElementToBePresent(By.className("index"));
     takeScreenshot();
-    getWebDriver().get(getAppUrl());
-//    getWebDriver().get("http://0.0.0.0:8089/1/students/students/1");
-//    sleep(1000);
-    takeScreenshot();
+//    getWebDriver().get(getAppUrl());
 //    getWebDriver().get("http://0.0.0.0:8089/1/students/students?email=testuser@made.up");
 //    sleep(1000);
 //    takeScreenshot();
--- muikku-atests/src/test/resources/sql/loginSetup.sql
+++ muikku-atests/src/test/resources/sql/loginSetup.sql
@@ -19,10 +19,11 @@ insert into EnvironmentRoleEntity (id, archetype) values
 insert into WorkspaceRoleEntity (id, archetype) values (6, 'STUDENT'), (4, 'TEACHER'); 
 
 insert into UserEntity (id, archived, defaultIdentifier, defaultSchoolDataSource_id) values (1, false, 'STUDENT-1', (select id from SchoolDataSource where identifier = 'PYRAMUS'));
-insert into EnvironmentUser (id, archived, role_id, user_id) values (1, false, 5, 1);
+insert into EnvironmentUser (id, archived, role_id, user_id) values (1, 0, 5, 1);
 --insert into InternalAuth (id, password, userEntityId) values (1, '3675ac5c859c806b26e02e6f9fd62192', 1);
 insert into UserSchoolDataIdentifier (identifier, dataSource_id, userEntity_id) values ('1', (select id from SchoolDataSource where identifier = 'PYRAMUS'), 1);
 -- role id 9 ? --
-insert into UserGroupUser (id, userGroup_id, user_id, role_id) values (1, 3, 1, 9);
+--insert into UserGroupUser (id, userGroup_id, user_id, role_id) values (1, 3, 1, 9);
 insert into UserEmailEntity (id,user_id,address) values (1, 1, 'testuser@made.up');
+insert into UserIdentification (id, externalId, authSource_id, user_id) values (1, 1, 1, 1);
 SET REFERENTIAL_INTEGRITY TRUE;
