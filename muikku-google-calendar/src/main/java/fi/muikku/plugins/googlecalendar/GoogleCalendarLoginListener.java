ba91cf061dc396f2159b6927096cd3a9ef1f7a6c
Antti Leppä <antti.leppa@foyt.fi>
2015-03-11 08:27:57 +0200

    ** Removed google login dependency from GoogleCalendarLoginListener

---
+++ muikku-google-calendar/src/main/java/fi/muikku/plugins/googlecalendar/GoogleCalendarLoginListener.java
@@ -0,0 +1,68 @@
+package fi.muikku.plugins.googlecalendar;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.Stateless;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import fi.muikku.calendar.Calendar;
+import fi.muikku.calendar.CalendarServiceException;
+import fi.muikku.events.LoginEvent;
+import fi.muikku.model.users.UserEntity;
+import fi.muikku.plugins.calendar.CalendarController;
+import fi.muikku.plugins.calendar.model.UserCalendar;
+import fi.muikku.users.UserEmailEntityController;
+import fi.muikku.users.UserEntityController;
+
+@Stateless
+public class GoogleCalendarLoginListener {
+  
+  // TODO: Localize
+  private static final String CALENDAR_SUMMARY = "Muikku";
+  private static final String CALENDAR_DESCRIPTION = "Muikku";
+  
+  @Inject
+  private Logger logger;
+  
+  @Inject
+  private UserEntityController userEntityController;
+  
+  @Inject
+  private UserEmailEntityController userEmailEntityController;
+
+  @Inject
+  private CalendarController calendarController;
+
+  @Inject
+  private GoogleCalendarClient googleCalendarClient;
+
+  public void onLogin(@Observes LoginEvent event) {
+    UserEntity userEntity = userEntityController.findUserEntityById(event.getUserEntityId());
+    if (userEntity != null) {
+      UserCalendar userCalendar = calendarController.findUserCalendarByUserAndProvider(userEntity, "google");
+      if (userCalendar == null) {
+        logger.info("User does not have a calendar, creating one");
+        Calendar calendar = null;
+        try {
+          userCalendar = calendarController.createCalendar(userEntity, "google", CALENDAR_SUMMARY, CALENDAR_DESCRIPTION, Boolean.TRUE);
+          calendar = calendarController.loadCalendar(userCalendar);
+        } catch (CalendarServiceException e) {
+          logger.log(Level.SEVERE, "Failed to create new Google calendar", e);
+        }
+        
+        for (String email : userEmailEntityController.listAddressesByUserEntity(userEntity)) {
+          try {
+            logger.info(String.format("Sharing Google calendar with %s", email));
+            googleCalendarClient.insertCalendarUserAclRule(calendar.getId(), email, "owner");
+          } catch (CalendarServiceException e) {
+            logger.log(Level.WARNING, String.format("Could not share calendar with %s", email), e);
+          }
+        }
+      }
+    }
+    
+  }
+  
+}
