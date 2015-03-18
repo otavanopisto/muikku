2f5130f1a385d99ffbfefc94a167bf33712eaf8d
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2015-03-18 11:14:12 +0200

    - Added Forum JS locale

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/forum/ForumPluginDescriptor.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/forum/ForumPluginDescriptor.java
@@ -1,8 +1,17 @@
 package fi.muikku.plugins.forum;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.apache.commons.lang3.LocaleUtils;
+
+import fi.muikku.i18n.LocaleBundle;
+import fi.muikku.i18n.LocaleLocation;
+import fi.muikku.plugin.LocalizedPluginDescriptor;
 import fi.muikku.plugin.PluginDescriptor;
 
-public class ForumPluginDescriptor implements PluginDescriptor {
+public class ForumPluginDescriptor implements PluginDescriptor, LocalizedPluginDescriptor {
 
   @Override
   public void init() {
@@ -13,4 +22,15 @@ public class ForumPluginDescriptor implements PluginDescriptor {
 		return "forum";
 	}
 	
+  @Override
+  public List<LocaleBundle> getLocaleBundles() {
+    List<LocaleBundle> bundles = new ArrayList<LocaleBundle>();
+//    bundles.add(new LocaleBundle(LocaleLocation.APPLICATION, ResourceBundle.getBundle("fi.muikku.plugins.communicator.ForumPluginMessages", LocaleUtils.toLocale("fi"))));
+//    bundles.add(new LocaleBundle(LocaleLocation.APPLICATION, ResourceBundle.getBundle("fi.muikku.plugins.communicator.ForumPluginMessages", LocaleUtils.toLocale("en"))));
+
+    bundles.add(new LocaleBundle(LocaleLocation.JAVASCRIPT, ResourceBundle.getBundle("fi.muikku.plugins.communicator.ForumJsPluginMessages", LocaleUtils.toLocale("fi"))));
+    bundles.add(new LocaleBundle(LocaleLocation.JAVASCRIPT, ResourceBundle.getBundle("fi.muikku.plugins.communicator.ForumJsPluginMessages", LocaleUtils.toLocale("en"))));
+    return bundles;
+  }
+
 }
