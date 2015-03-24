1c9b1a8beafb182f8808313de667e265f2f8eccf
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2015-03-24 13:55:50 +0200

    Guider dust templates & java functionlaties - Some rework at _mainfunctions.scss

--- muikku/src/main/webapp/resources/scripts/gui/guider.js
+++ muikku/src/main/webapp/resources/scripts/gui/guider.js
@@ -1,44 +1,30 @@
 $(document).ready(function(){
 	
-    DiscImpl = $.klass({
+    GuideImpl = $.klass({
 
     	init : function(){
     		// todo: parse url
-            this.refreshLatest();	
-            this.refreshAreas();	
-    	    $(DiscImpl.msgContainer).on("click", '.di-message:not(.open)', $.proxy(this.loadThread,this));  
-            $(DiscImpl.msgContainer).on("click", '.icon-goback', $.proxy(this.refreshLatest,this));
-    	    $(DiscImpl.msgContainer).on("click", '.di-message-reply-link', $.proxy(this.replyThread,this));		    
-    
+            this.refreshUser();	
+//            this.refreshAreas();	
+    	    $(GuideImpl.guideContainer).on("click", '.gt-user:not(.open)', $.proxy(this.viewUser,this));  
+
     	},
     	
     	
-    	refreshLatest : function(){
+    	refreshUser : function(){
 
-            this.clearMessages();
+            this.clearUsers();
 ;  
     	    
-    	    mApi().forum.latest.read().on('$', function(msgs, msgsCallback){
-    	          mApi().forum.areas.read(msgs.forumAreaId).callback(function(err, area){
-    	            msgs.areaName = area.name;	
-    	             	
-    	          });
-    	          
-    	          var d = new Date(msgs.created);
- 
-    	          msgs.prettyDate = d.toLocaleString();
-    	          
-    	          msgsCallback();
-    	    	
-    	    })
-    	    .callback(function (err, threads) {
+    	    mApi().user.users.read()
+    	    .callback(function (err, users) {
     	  	  
     		    if( err ){
-    		          $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.nothreads', err));
+    		          $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.guider.errormessage.nousers', err));
     		  	}else{    	  
 
-    		  	 renderDustTemplate('/discussion/discussion_items.dust', threads, function(text) {
-    		 		$(DiscImpl.msgContainer).append($.parseHTML(text));
+    		  	 renderDustTemplate('/guider/guider_items.dust', users, function(text) {
+    		 		$(GuideImpl.guideContainer).append($.parseHTML(text));
     		 		
     		  	});
     		  	}
@@ -46,264 +32,253 @@ $(document).ready(function(){
     		
     	},
     	
-    	refreshAreas : function(){   
-
-    	 mApi().forum.areas.read()
-        .callback(function (err, areas) {
-        	  
-    	    if( err ){
-    	          $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.selectarea.empty', err));
-    	  	}else{    	  
-
-	  			 var select = $("#discussionAreaSelect");
-
-
-	           $(select).empty();	  			 
-	  		   if(areas.length != 0){
-	  			 var allAreas = $("<option value='all'>" + getLocaleText('plugin.discussion.browseareas.all', err) + "</option>");
-	  			 allAreas.appendTo(select);
-	  			 for(var i = 0; i < areas.length; i++ ){
-	  				var name = areas[i].name;
-	  				var id = areas[i].id;
-	  				var groupId = areas[i].groupId;
-	  		
-	  				if(groupId == null){
-	  	  				$("<option value='" + id + "'>" + name + "</option>").appendTo(select);
-	  				}
-		  				
-		  		} 			   
-	  		    }else{
-	  		    	$("<option>" + getLocaleText('plugin.discussion.selectarea.empty') + "</option>").appendTo(select);
-	  			   
-	  		    }
-	            
-
-		  			 
-    	  		
-
-    	  	}
-        });
-    
-    
-    	},
+//    	refreshAreas : function(){   
+//
+//    	 mApi().forum.areas.read()
+//        .callback(function (err, areas) {
+//        	  
+//    	    if( err ){
+//    	          $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.selectarea.empty', err));
+//    	  	}else{    	  
+//
+//	  			 var select = $("#discussionAreaSelect");
+//
+//
+//	           $(select).empty();	  			 
+//	  		   if(areas.length != 0){
+//	  			 var allAreas = $("<option value='all'>" + getLocaleText('plugin.discussion.browseareas.all', err) + "</option>");
+//	  			 allAreas.appendTo(select);
+//	  			 for(var i = 0; i < areas.length; i++ ){
+//	  				var name = areas[i].name;
+//	  				var id = areas[i].id;
+//	  				var groupId = areas[i].groupId;
+//	  		
+//	  				if(groupId == null){
+//	  	  				$("<option value='" + id + "'>" + name + "</option>").appendTo(select);
+//	  				}
+//		  				
+//		  		} 			   
+//	  		    }else{
+//	  		    	$("<option>" + getLocaleText('plugin.discussion.selectarea.empty') + "</option>").appendTo(select);
+//	  			   
+//	  		    }
+//	            
+//
+//		  			 
+//    	  		
+//
+//    	  	}
+//        });
+//    
+//    
+//    	},
     	
-    	refreshThread : function(aId,tId){
-
-            this.clearMessages();
-            
-
-    	    
-    	    mApi().forum.areas.threads.read(aId,tId).on('$', function(thread, threadCallback){
-    	          mApi().forum.areas.read(thread.forumAreaId).callback(function(err, area){
-    	            thread.areaName = area.name;	
-    	             	
-    	          });
-
-    	          
-    	          mApi().user.users.read(thread.creator).callback(function(err, user){
-      	            thread.creatorFullName = user.firstName + ' ' +  user.lastName;	
-      	             	
-      	          });    	          
-    	          
-       	          var d = new Date(thread.created);
-          	       
-    	          thread.prettyDate = d.toLocaleString();  	      	          
-    	          
-    	          threadCallback();
-    	    	
-    	    })
-    	    .callback(function (err, threads) {
-    	  	  
-    		    if( err ){
-    		        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.nothreads', err));
-    		  	}else{    	  
-
-    		  	 renderDustTemplate('/discussion/discussion_items_open.dust', threads, function(text) {
-    		 		$(DiscImpl.msgContainer).append($.parseHTML(text));
-    		 		
-    		  	});
-    		  	}
-    	    });		
-    	    
-	  		this.loadThreadReplies(aId, tId);     		
-    	},    	
+//    	refreshThread : function(aId,tId){
+//
+//            this.clearMessages();
+//            
+//
+//    	    
+//    	    mApi().forum.areas.threads.read(aId,tId).on('$', function(thread, threadCallback){
+//    	          mApi().forum.areas.read(thread.forumAreaId).callback(function(err, area){
+//    	            thread.areaName = area.name;	
+//    	             	
+//    	          });
+//
+//    	          
+//    	          mApi().user.users.read(thread.creator).callback(function(err, user){
+//      	            thread.creatorFullName = user.firstName + ' ' +  user.lastName;	
+//      	             	
+//      	          });    	          
+//    	          
+//       	          var d = new Date(thread.created);
+//          	       
+//    	          thread.prettyDate = d.toLocaleString();  	      	          
+//    	          
+//    	          threadCallback();
+//    	    	
+//    	    })
+//    	    .callback(function (err, threads) {
+//    	  	  
+//    		    if( err ){
+//    		        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.nothreads', err));
+//    		  	}else{    	  
+//
+//    		  	 renderDustTemplate('/discussion/discussion_items_open.dust', threads, function(text) {
+//    		 		$(DiscImpl.msgContainer).append($.parseHTML(text));
+//    		 		
+//    		  	});
+//    		  	}
+//    	    });		
+//    	    
+//	  		this.loadThreadReplies(aId, tId);     		
+//    	},    	
     	
-	    filterMessagesByArea : function(sel){
-            var aId = sel.value;
+//	    filterMessagesByArea : function(sel){
+//            var aId = sel.value;
+//
+//	        
+//		    this.clearMessages();	    
+//    	    if (aId == 'all'){
+//    	    	this.refreshLatest();
+//    	    }else{
+//			    mApi().forum.areas.threads.read(aId).on('$', function(thread, threadCallback){
+//	
+//	  	          mApi().forum.areas.read(thread.forumAreaId).callback(function(err, area){
+//	  	            thread.areaName = area.name;	
+//	  	             	
+//	  	          });
+//	
+//		          mApi().user.users.read(thread.creator).callback(function(err, user){
+//	    	            thread.creatorFullName = user.firstName + ' ' +  user.lastName;	
+//	    	             	
+//	    	      });  	          
+//	  	          
+//	   	          var d = new Date(thread.created);
+//	   	       
+//		          thread.prettyDate = d.toLocaleString();  	          
+//	  	          
+//			      threadCallback();
+//			    })
+//		    	.callback(function(err, threads){
+//				    if( err ){
+//				        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.nothreads', err));
+//				  	}else{    	  
+//	
+//					  	renderDustTemplate('/discussion/discussion_items.dust', threads, function(text) {
+//		
+//					  		$(DiscImpl.msgContainer).append($.parseHTML(text));
+//		
+//		
+//					  		
+//					  	});
+//				  	}    
+//				    
+//	
+//		    	});
+//
+//    	    }
+//	    },
+    
+//	    loadThread : function(event){
+//
 //	    	var element = $(event.target); 
 //	        element = element.parents(".di-message");
+//	        var tId = $(element).attr("id");
 //	        var aId = $(element).find("input[name='areaId']").attr('value') ;
-	        
-		    this.clearMessages();	    
-    	    if (aId == 'all'){
-    	    	this.refreshLatest();
-    	    }else{
-			    mApi().forum.areas.threads.read(aId).on('$', function(thread, threadCallback){
-	
-	  	          mApi().forum.areas.read(thread.forumAreaId).callback(function(err, area){
-	  	            thread.areaName = area.name;	
-	  	             	
-	  	          });
-	
-		          mApi().user.users.read(thread.creator).callback(function(err, user){
-	    	            thread.creatorFullName = user.firstName + ' ' +  user.lastName;	
-	    	             	
-	    	      });  	          
-	  	          
-	   	          var d = new Date(thread.created);
-	   	       
-		          thread.prettyDate = d.toLocaleString();  	          
-	  	          
-			      threadCallback();
-			    })
-		    	.callback(function(err, threads){
+//	        
+//		    this.clearMessages();	    
+//    	    
+//		    mApi().forum.areas.threads.read(aId,tId).on('$', function(thread, threadCallback){
+//
+//  	          mApi().forum.areas.read(thread.forumAreaId).callback(function(err, area){
+//  	            thread.areaName = area.name;	
+//  	             	
+//  	          });
+//
+//	          mApi().user.users.read(thread.creator).callback(function(err, user){
+//    	            thread.creatorFullName = user.firstName + ' ' +  user.lastName;	
+//    	             	
+//    	      });  	          
+//  	          
+//   	          var d = new Date(thread.created);
+//   	       
+//	          thread.prettyDate = d.toLocaleString();  	          
+//  	          
+//		      threadCallback();
+//		    })
+//	    	.callback(function(err, threads){
+//			    if( err ){
+//			        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.nothreads', err));
+//			  	}else{    	  
+//
+//				  	renderDustTemplate('/discussion/discussion_items_open.dust', threads, function(text) {
+//	
+//				  		$(DiscImpl.msgContainer).append($.parseHTML(text));
+//	
+//	
+//				  		
+//				  	});
+//			  	}    
+//			    
+//
+//	    	});
+//
+//		    this.loadThreadReplies(aId, tId);   		    
+//		    
+//	    },
+
+//	    loadThreadReplies : function(areaId,threadId){
+//
+//            this.clearReplies();
+//            
+//		    mApi().forum.areas.threads.replies.read(areaId,threadId).on('$', function(replies, repliesCallback){
+//
+//  	          mApi().forum.areas.read(replies.forumAreaId).callback(function(err, area){
+//  	            replies.areaName = area.name;	
+//  	             	
+//  	          });
+//  	          
+//	          mApi().user.users.read(replies.creator).callback(function(err, user){
+//    	            replies.creatorFullName = user.firstName + ' ' +  user.lastName;	
+//    	             	
+//    	      }); 
+//   	          var d = new Date(replies.created);
+//   	       
+//	          replies.prettyDate = d.toLocaleString();  	          
+//		      repliesCallback();
+//		    })
+//	    	.callback(function(err, replies){
+//			    if( err ){
+//			        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.noreplies', err));
+//			  	}else{    	  
+//
+//				  	renderDustTemplate('/discussion/discussion_subitems.dust', replies, function(text) {
+//	
+//				  		$(DiscImpl.msgContainer).append($.parseHTML(text));
+//	
+//				  	});
+//			  	}    		
+//	    	});
+//	    },
+//	    
+//	    
+	    viewUser : function(event){
+
+	    	var element = $(event.target); 
+	        element = element.parents(".gt-user");
+	        var uId = $(element).attr("id");
+		    var cont = $(".gt-user-meta-content"); 
+
+	    	$(element).removeClass("closed");
+	    	$(element).addClass("open");
+	    				
+
+		    mApi().user.users.read(uId).callback(function(err, user){
 				    if( err ){
-				        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.nothreads', err));
+				        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.guider.errormessage.nouser', err));
 				  	}else{    	  
-	
-					  	renderDustTemplate('/discussion/discussion_items.dust', threads, function(text) {
-		
-					  		$(DiscImpl.msgContainer).append($.parseHTML(text));
-		
-		
-					  		
+					  	renderDustTemplate('/discussion/discussion_subitems.dust', user, function(text) {				  		
+					  		$(cont).append($.parseHTML(text));
+						  
 					  	});
-				  	}    
-				    
-	
-		    	});
-
-    	    }
-	    },
-    
-	    loadThread : function(event){
-
-	    	var element = $(event.target); 
-	        element = element.parents(".di-message");
-	        var tId = $(element).attr("id");
-	        var aId = $(element).find("input[name='areaId']").attr('value') ;
-	        
-		    this.clearMessages();	    
-    	    
-		    mApi().forum.areas.threads.read(aId,tId).on('$', function(thread, threadCallback){
-
-  	          mApi().forum.areas.read(thread.forumAreaId).callback(function(err, area){
-  	            thread.areaName = area.name;	
-  	             	
-  	          });
-
-	          mApi().user.users.read(thread.creator).callback(function(err, user){
-    	            thread.creatorFullName = user.firstName + ' ' +  user.lastName;	
-    	             	
-    	      });  	          
-  	          
-   	          var d = new Date(thread.created);
-   	       
-	          thread.prettyDate = d.toLocaleString();  	          
-  	          
-		      threadCallback();
-		    })
-	    	.callback(function(err, threads){
-			    if( err ){
-			        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.nothreads', err));
-			  	}else{    	  
-
-				  	renderDustTemplate('/discussion/discussion_items_open.dust', threads, function(text) {
-	
-				  		$(DiscImpl.msgContainer).append($.parseHTML(text));
-	
-	
-				  		
-				  	});
-			  	}    
-			    
-
-	    	});
-
-		    this.loadThreadReplies(aId, tId);   		    
-		    
-	    },
-
-	    loadThreadReplies : function(areaId,threadId){
-
-            this.clearReplies();
-            
-		    mApi().forum.areas.threads.replies.read(areaId,threadId).on('$', function(replies, repliesCallback){
-
-  	          mApi().forum.areas.read(replies.forumAreaId).callback(function(err, area){
-  	            replies.areaName = area.name;	
-  	             	
-  	          });
-  	          
-	          mApi().user.users.read(replies.creator).callback(function(err, user){
-    	            replies.creatorFullName = user.firstName + ' ' +  user.lastName;	
-    	             	
-    	      }); 
-   	          var d = new Date(replies.created);
-   	       
-	          replies.prettyDate = d.toLocaleString();  	          
-		      repliesCallback();
-		    })
-	    	.callback(function(err, replies){
-			    if( err ){
-			        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.noreplies', err));
-			  	}else{    	  
-
-				  	renderDustTemplate('/discussion/discussion_subitems.dust', replies, function(text) {
-	
-				  		$(DiscImpl.msgContainer).append($.parseHTML(text));
-	
-				  	});
-			  	}    		
-	    	});
-	    },
-	    
-	    
-	    replyThread : function(event){
-
-	    	var element = $(event.target); 
-	        element = element.parents(".di-message");
-	        var tId = $(element).attr("id");
-	        var aId = $(element).find("input[name='areaId']").attr('value');	    	
-	    	
-			var sendReply = function(values){
-				mApi().forum.areas.threads.replies.create(aId, tId, values).callback(function(err, result) {
-				});			
+				  	}	
+		    });  		
 				
-				window.discussion.refreshThread(aId,tId);
-	
-				
-			}			
-
-		    mApi().forum.areas.threads.read(aId,tId).on('$', function(thread, threadCallback){
-
-	  	          mApi().forum.areas.read(thread.forumAreaId).callback(function(err, area){
-	  	            thread.areaName = area.name;	
-	  	             	
-	  	          });
-			      threadCallback();
-			    })
-		    	.callback(function(err, thread){
-				    if( err ){
-				        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.discussion.errormessage.nothreads', err));
-				  	}else{    	  
-						  openInSN('/discussion/discussion_create_reply.dust', thread, sendReply);		
-					  	}
-				  	});  		
 	    },
 
-	    clearMessages : function(){
-	    	$(DiscImpl.msgContainer).empty();
+	    clearUsers : function(){
+	    	$(GuideImpl.guideContainer).empty();
 	    },	    
 	    
-	    clearReplies : function(){
-	    	$(DiscImpl.subContainer).empty();
-		 },	  	    
-		 
+//	    clearReplies : function(){
+//	    	$(DiscImpl.subContainer).empty();
+//		 },	  	    
+//		 
 	    _klass : {
 	    	// Variables for the class
-		  msgContainer : ".di-messages-container",
-	      subContainer : ".di-sumbessages-container",
+		  guideContainer : ".gt-students-container",
+
 	    	  
 	    	  
 	    }
@@ -312,7 +287,7 @@ $(document).ready(function(){
     }); 
 	
 
-   window.discussion = new DiscImpl();
+   window.guider = new GuideImpl();
   
         
 	$(".di-new-message-button").click(function(){
