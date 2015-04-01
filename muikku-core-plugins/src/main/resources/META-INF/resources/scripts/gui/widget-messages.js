2e32432d2d21a6a64553d5bf6395e360f618e5f1
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2015-04-01 10:28:37 +0300

    ** Added preview generation of messages to the frontpage

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/widget-messages.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/widget-messages.js
@@ -1,13 +1,33 @@
  
 $(document).ready(function(){
 
+    var WORD_COUNT = 50;
+  
+    function generatePreview(html){
+      var text = html.replace(/<\/?[^>]+(>|$)/g, "");
+      var words = text.split(' ');
+      if (words.length < WORD_COUNT) {
+        return text;
+      } else {
+        text = "";
+        for (var i = 0; i < WORD_COUNT; i++) {
+          text += ' ' + words[i];
+        }
+        return text + '...';
+      }
+    }
+  
 	
     mApi().communicator.items.read()
       .callback(function (err, messages) {
     	  
-      	if( err ){
+      if( err ){
             $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('TODO: Virheilmoitus', err));
-    	}else{    	  
+    	}else{
+    	  for(var i = 0, j = messages.length; i < j; i++){
+    	    messages[i].content = generatePreview(messages[i].content);
+    	  }
+
         renderDustTemplate('frontpage/widget_messages.dust', 
     		{
     	   	 messages : messages
@@ -17,7 +37,7 @@ $(document).ready(function(){
           
         });
     	}
-      });
+    });
       
 	});
  	
