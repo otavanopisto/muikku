6e5d690fd31fef2a97215ae510c5b608af4fe68a
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2015-04-14 09:17:01 +0300

    Toolbar CKEDITOR tools

--- muikku/src/main/webapp/resources/scripts/gui/socialnavigation.js
+++ muikku/src/main/webapp/resources/scripts/gui/socialnavigation.js
@@ -24,15 +24,14 @@ function openInSN(template, result, formFunction) {
       
       CKEDITOR.replace(textarea, {
         height : '100px',
-
-          toolbar: [
-                    { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
-                    { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },                    
-                    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
-                    { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-                    { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
-       
-                  ]          
+        toolbar: [
+                  { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+                  { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },                    
+                  { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+                  { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+                  { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+     
+                ]          
           
       });
       
