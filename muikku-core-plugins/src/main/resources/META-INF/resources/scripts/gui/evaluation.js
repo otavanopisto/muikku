72edb9f47eb4f3a05b0689d183b09045c6b91b43
jjlankinen <jari.lankinen@otavanopisto.fi>
2015-04-22 08:37:05 +0300

    * Added script to prevent page scrolling when user scrolls evaluation forms content area

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/evaluation.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/evaluation.js
@@ -50,6 +50,35 @@
           return prevent();
         }
       });
+      
+      $('.evaluation-content-evaluateForm-content').on('DOMMouseScroll mousewheel', function(ev) {
+        var $this = $(this),
+          scrollTop = this.scrollTop,
+          scrollHeight = this.scrollHeight,
+          height = $this.height(),
+          delta = (ev.type == 'DOMMouseScroll' ?
+            ev.originalEvent.detail * -40 :
+            ev.originalEvent.wheelDelta),
+          up = delta > 0;
+
+        var prevent = function() {
+          ev.stopPropagation();
+          ev.preventDefault();
+          ev.returnValue = false;
+          return false;
+        }
+
+        if (!up && -delta > scrollHeight - height - scrollTop) {
+          // Scrolling down, but this will take us past the bottom.
+          $this.scrollTop(scrollHeight);
+
+          return prevent();
+        } else if (up && delta > scrollTop) {
+          // Scrolling up, but this will take us past the top.
+          $this.scrollTop(0);
+          return prevent();
+        }
+      });
 
     }
     
