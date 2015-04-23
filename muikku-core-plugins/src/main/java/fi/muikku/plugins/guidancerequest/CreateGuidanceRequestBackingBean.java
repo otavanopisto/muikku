d3ae7858b40a384ffed7165cd8ccd8623e3ef38d
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2015-04-23 11:13:26 +0300

    - Commented unused backing beans to reduce security risks

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/guidancerequest/CreateGuidanceRequestBackingBean.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/guidancerequest/CreateGuidanceRequestBackingBean.java
@@ -1,37 +1,29 @@
 package fi.muikku.plugins.guidancerequest;
 
-import java.util.Date;
-
-import javax.ejb.Stateful;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.inject.Named;
-
-import fi.muikku.session.SessionController;
 
 
-@Named
-@Stateful
-@RequestScoped
+//@Named
+//@Stateful
+//@RequestScoped
 public class CreateGuidanceRequestBackingBean {
   
-  @Inject
-  private GuidanceRequestController guidanceRequestController;
-  
-  @Inject
-  private SessionController sessionController;
-  
-  public GuidanceRequest createGuidanceRequest() {
-    return guidanceRequestController.createGuidanceRequest(sessionController.getLoggedUserEntity(), new Date(), getNewGuidanceRequestMessage());
-  }
-
-  public String getNewGuidanceRequestMessage() {
-    return newGuidanceRequestMessage;
-  }
-
-  public void setNewGuidanceRequestMessage(String newGuidanceRequestMessage) {
-    this.newGuidanceRequestMessage = newGuidanceRequestMessage;
-  }
-
-  private String newGuidanceRequestMessage;
+//  @Inject
+//  private GuidanceRequestController guidanceRequestController;
+//  
+//  @Inject
+//  private SessionController sessionController;
+//  
+//  public GuidanceRequest createGuidanceRequest() {
+//    return guidanceRequestController.createGuidanceRequest(sessionController.getLoggedUserEntity(), new Date(), getNewGuidanceRequestMessage());
+//  }
+//
+//  public String getNewGuidanceRequestMessage() {
+//    return newGuidanceRequestMessage;
+//  }
+//
+//  public void setNewGuidanceRequestMessage(String newGuidanceRequestMessage) {
+//    this.newGuidanceRequestMessage = newGuidanceRequestMessage;
+//  }
+//
+//  private String newGuidanceRequestMessage;
 }
