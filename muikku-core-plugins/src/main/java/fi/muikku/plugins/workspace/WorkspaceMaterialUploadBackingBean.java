429e72dbccf3d091ceacce6d03522d626ce0e1f9
ilmo.euro <ilmo.euro@gmail.com>
2015-04-29 09:31:04 +0300

    ** Workspace material uploading only allowed for teachers

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialUploadBackingBean.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/WorkspaceMaterialUploadBackingBean.java
@@ -16,7 +16,10 @@ import org.ocpsoft.rewrite.annotation.Parameter;
 import org.ocpsoft.rewrite.annotation.RequestAction;
 
 import fi.muikku.files.TempFileUtils;
+import fi.muikku.model.users.UserEntity;
 import fi.muikku.model.workspace.WorkspaceEntity;
+import fi.muikku.model.workspace.WorkspaceRoleArchetype;
+import fi.muikku.model.workspace.WorkspaceUserEntity;
 import fi.muikku.plugins.material.BinaryMaterialController;
 import fi.muikku.plugins.material.model.BinaryMaterial;
 import fi.muikku.plugins.workspace.model.WorkspaceFolder;
@@ -25,19 +28,21 @@ import fi.muikku.plugins.workspace.model.WorkspaceRootFolder;
 import fi.muikku.schooldata.WorkspaceController;
 import fi.muikku.schooldata.WorkspaceEntityController;
 import fi.muikku.schooldata.entity.Workspace;
+import fi.muikku.session.SessionController;
+import fi.muikku.users.WorkspaceUserEntityController;
 
 @Named
 @Stateful
 @RequestScoped
-@Join (path = "/workspace/{workspaceUrlName}/material-upload", to = "/jsf/workspace/material-upload.jsf")
+@Join(path = "/workspace/{workspaceUrlName}/material-upload", to = "/jsf/workspace/material-upload.jsf")
 public class WorkspaceMaterialUploadBackingBean {
 
   @Parameter
   private String workspaceUrlName;
-  
+
   @Parameter
   private Long folderId;
-  
+
   @Inject
   private Logger logger;
 
@@ -45,94 +50,113 @@ public class WorkspaceMaterialUploadBackingBean {
   private BinaryMaterialController binaryMaterialController;
 
   @Inject
-	private WorkspaceController workspaceController;
+  private WorkspaceController workspaceController;
 
-  @Inject
-  private WorkspaceEntityController workspaceEntityController;
-  
   @Inject
   private WorkspaceMaterialController workspaceMaterialController;
-  
-	@Inject
+
+  @Inject
   @Named
   private WorkspaceBackingBean workspaceBackingBean;
+  
+  @Inject
+  private SessionController sessionController;
+  
+  @Inject
+  private WorkspaceUserEntityController workspaceUserEntityController;
 
-	@RequestAction
-	public String init() {
-	  String urlName = getWorkspaceUrlName();
-	  
-		if (StringUtils.isBlank(urlName)) {
-			return "/error/not-found.jsf";
-		}
-		
-		WorkspaceEntity workspaceEntity = workspaceController.findWorkspaceEntityByUrlName(urlName);
-		if (workspaceEntity == null) {
+  @RequestAction
+  public String init() {
+    String urlName = getWorkspaceUrlName();
+
+    if (StringUtils.isBlank(urlName)) {
       return "/error/not-found.jsf";
-		}
-		
-		workspaceBackingBean.setWorkspaceUrlName(urlName);
-		workspaceEntityId = workspaceEntity.getId();
+    }
+
+    WorkspaceEntity workspaceEntity = workspaceController.findWorkspaceEntityByUrlName(urlName);
+    if (workspaceEntity == null) {
+      return "/error/not-found.jsf";
+    }
+
+    workspaceBackingBean.setWorkspaceUrlName(urlName);
+    workspaceEntityId = workspaceEntity.getId();
     Workspace workspace = workspaceController.findWorkspace(workspaceEntity);
     workspaceName = workspace.getName();
-    
+
     return null;
-	}
+  }
 
-	public String getWorkspaceUrlName() {
-		return workspaceUrlName;
-	}
+  public String getWorkspaceUrlName() {
+    return workspaceUrlName;
+  }
 
-	public void setWorkspaceUrlName(String workspaceUrlName) {
-		this.workspaceUrlName = workspaceUrlName;
-	}
+  public void setWorkspaceUrlName(String workspaceUrlName) {
+    this.workspaceUrlName = workspaceUrlName;
+  }
 
   public String getWorkspaceName() {
     return workspaceName;
   }
-  
+
   public String getUploadMeta() {
-    return uploadMeta; 
+    return uploadMeta;
   }
-  
+
   public void setUploadMeta(String uploadMeta) {
     this.uploadMeta = uploadMeta;
   }
-  
+
   public Long getFolderId() {
     return folderId;
   }
-  
+
   public void setFolderId(Long folderId) {
     this.folderId = folderId;
   }
-  
+
   public String upload() {
     ObjectMapper objectMapper = new ObjectMapper();
-    
-    WorkspaceEntity workspaceEntity = workspaceEntityController.findWorkspaceByUrlName(getWorkspaceUrlName());
+
+    if (!sessionController.isLoggedIn()) {
+      return "/error/internal-error.jsf";
+    }
+
+    // Workspace
+    WorkspaceEntity workspaceEntity = workspaceController.findWorkspaceEntityById(workspaceEntityId);
     if (workspaceEntity == null) {
       return "/error/internal-error.jsf";
     }
-    
+
+    UserEntity loggedInUserEntity = sessionController.getLoggedUserEntity();
+    WorkspaceUserEntity workspaceUserEntity = workspaceUserEntityController
+        .findWorkspaceUserEntityByWorkspaceAndIdentifier(workspaceEntity, loggedInUserEntity.getDefaultIdentifier());
+
+    if (workspaceUserEntity == null || workspaceUserEntity.getWorkspaceUserRole() == null
+        || workspaceUserEntity.getWorkspaceUserRole().getArchetype() != WorkspaceRoleArchetype.TEACHER) {
+
+      return "/error/internal-error.jsf";
+    }
+
     WorkspaceNode parent = null;
     if (getFolderId() != null) {
       WorkspaceFolder workspaceFolder = workspaceMaterialController.findWorkspaceFolderById(getFolderId());
       if (workspaceFolder == null) {
         return "/error/internal-error.jsf";
       }
-      
-      WorkspaceRootFolder workspaceRootFolder = workspaceMaterialController.findWorkspaceRootFolderByWorkspaceNode(workspaceFolder);
+
+      WorkspaceRootFolder workspaceRootFolder = workspaceMaterialController
+          .findWorkspaceRootFolderByWorkspaceNode(workspaceFolder);
       if (workspaceRootFolder == null) {
         return "/error/internal-error.jsf";
       }
-      
+
       if (!workspaceRootFolder.getWorkspaceEntityId().equals(workspaceEntityId)) {
         return "/error/internal-error.jsf";
       }
     } else {
       parent = workspaceMaterialController.findWorkspaceRootFolderByWorkspaceEntity(workspaceEntity);
     }
-    
+
     try {
       FileMeta[] fileMetas = objectMapper.readValue(getUploadMeta(), FileMeta[].class);
       for (FileMeta fileMeta : fileMetas) {
@@ -141,8 +165,9 @@ public class WorkspaceMaterialUploadBackingBean {
           String contentType = fileMeta.getContentType();
           String fileName = fileMeta.getName();
           byte[] fileData = TempFileUtils.getTempFileData(fileId);
-          
-          BinaryMaterial binaryMaterial = binaryMaterialController.createBinaryMaterial(fileName, contentType, fileData);
+
+          BinaryMaterial binaryMaterial = binaryMaterialController
+              .createBinaryMaterial(fileName, contentType, fileData);
           workspaceMaterialController.createWorkspaceMaterial(parent, binaryMaterial);
         } finally {
           TempFileUtils.deleteTempFile(fileId);
@@ -152,41 +177,41 @@ public class WorkspaceMaterialUploadBackingBean {
       logger.log(Level.SEVERE, "File uploading filed", e);
       return "/error/internal-error.jsf";
     }
-    
+
     return null;
   }
-  
+
   private String uploadMeta;
   private Long workspaceEntityId;
   private String workspaceName;
-  
+
   @SuppressWarnings("unused")
   private static class FileMeta {
-    
+
     public String getId() {
       return id;
     }
-    
+
     public void setId(String id) {
       this.id = id;
     }
-    
+
     public String getContentType() {
       return contentType;
     }
-    
+
     public void setContentType(String contentType) {
       this.contentType = contentType;
     }
-    
+
     public String getName() {
       return name;
     }
-    
+
     public void setName(String name) {
       this.name = name;
     }
-    
+
     private String id;
     private String contentType;
     private String name;
