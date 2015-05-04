9fb5d88b2d2030c562e80a9511369f7c7fbf48e1
Antti Leppä <antti.leppa@foyt.fi>
2015-05-04 12:56:27 +0300

    ** Fixes issue #184

--- muikku-core/src/main/java/fi/muikku/schooldata/events/SchoolDataWorkspaceUpdatedEvent.java
+++ muikku-core/src/main/java/fi/muikku/schooldata/events/SchoolDataWorkspaceUpdatedEvent.java
@@ -1,12 +1,15 @@
 package fi.muikku.schooldata.events;
 
+import java.util.Map;
+
 public class SchoolDataWorkspaceUpdatedEvent {
 
-  public SchoolDataWorkspaceUpdatedEvent(String dataSource, String identifier, String name) {
+  public SchoolDataWorkspaceUpdatedEvent(String dataSource, String identifier, String name, Map<String, Object> extra) {
     super();
     this.dataSource = dataSource;
     this.identifier = identifier;
     this.name = name;
+    this.extra = extra;
   }
 
   public String getDataSource() {
@@ -20,8 +23,13 @@ public class SchoolDataWorkspaceUpdatedEvent {
   public String getName() {
     return name;
   }
+  
+  public Map<String, Object> getExtra() {
+    return extra;
+  }
 
   private String dataSource;
   private String identifier;
   private String name;
+  private Map<String, Object> extra;
 }
