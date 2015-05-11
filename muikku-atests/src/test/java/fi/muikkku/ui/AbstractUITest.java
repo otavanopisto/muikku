28912a99f85efdd84f93c8299c6db4c08e1e5f42
petja-laitila <petja.laitila@otavanopisto.fi>
2015-05-11 13:53:13 +0300

    Pyramus mocks for login

--- muikku-atests/src/test/java/fi/muikkku/ui/AbstractUITest.java
+++ muikku-atests/src/test/java/fi/muikkku/ui/AbstractUITest.java
@@ -1,13 +1,24 @@
 package fi.muikkku.ui;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.matching;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
+import org.joda.time.DateTime;
 import org.junit.Rule;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -18,9 +29,18 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 import fi.muikku.AbstractIntegrationTest;
+import fi.pyramus.rest.model.Email;
+import fi.pyramus.rest.model.Person;
+import fi.pyramus.rest.model.StaffMember;
+import fi.pyramus.rest.model.Student;
+import fi.pyramus.rest.model.WhoAmI;
 
 public class AbstractUITest extends AbstractIntegrationTest {
 
@@ -103,5 +123,119 @@ public class AbstractUITest extends AbstractIntegrationTest {
     }
   }
 
+  protected void initializePyramusLoginMocks() throws JsonProcessingException {
+    stubFor(get(urlMatching("/dnm"))
+      .willReturn(
+        aResponse()
+        .withHeader("Content-Type", "application/json")
+        .withBody("")
+        .withStatus(204)));
+    
+    stubFor(get(urlMatching("/users/authorize.*"))
+      .willReturn(
+        aResponse()
+          .withStatus(302)
+          .withHeader("Location", "http://dev.muikku.fi:8080/login?_stg=rsp&code=1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")));
+   
+    stubFor(post(urlMatching("/1/oauth/token")).willReturn(
+      aResponse().withHeader("Content-Type", "application/json")
+        .withBody("{\"expires_in\":3600,\"refresh_token\":\"12312ewsdf34fsd234r43rfsw32rf33e\",\"access_token\":\"ur84ur839843ruwf39843ru39ru37y2e\"}")
+        .withStatus(200)));
+    
+    List<String> emails = new ArrayList<String>();
+    emails.add("testuser@made.up");
+    WhoAmI whoAmI = new WhoAmI((long)1, "Test", "User", emails);
+
+    ObjectMapper objectMapper = new ObjectMapper().registerModule(new JodaModule()).enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    
+    String whoAmIJson = objectMapper.writeValueAsString(whoAmI);
+    
+    stubFor(get(urlMatching("/1/system/whoami")).willReturn(
+      aResponse().withHeader("Content-Type", "application/json")
+        .withBody(whoAmIJson)
+        .withStatus(200)));
+
+    Map<String, String> variables = null;
+    List<String> tags = null;
+    Student student = new Student((long) 1, (long) 1, "Test", "User", null, null, null, null, null, null, null, null, null, null, null,
+      (long) 1, null, null, false, null, null, null, null, variables, tags, false);
+    String studentJson = objectMapper.writeValueAsString(student);
+    
+    stubFor(get(urlMatching("/1/students/students/.*"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody(studentJson)
+          .withStatus(200)));
+
+    Email email = new Email((long) 1, (long) 2, true, "testuser@made.up"); 
+    String emailJson = objectMapper.writeValueAsString(email);
+    stubFor(get(urlMatching("/1/students/students/.*/emails")).willReturn(
+      aResponse().withHeader("Content-Type", "application/json")
+        .withBody(emailJson)
+        .withStatus(200)));
+    
+    Student[] studentArray = {student};
+    String studentArrayJson = objectMapper.writeValueAsString(studentArray);
+    stubFor(get(urlEqualTo("/1/students/students?email=testuser@made.up"))
+    // .withQueryParam("email", matching(".*"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody(studentArrayJson)
+          .withStatus(200)));
+
+    DateTime birthday = new DateTime(1990, 2, 2, 0, 0, 0, 0);
+
+    Person person = new Person((long) 1, birthday, "345345-3453", fi.pyramus.rest.model.Sex.MALE, false, "empty", (long) 1);
+    String personJson = objectMapper.writeValueAsString(person);
+    stubFor(get(urlMatching("/1/persons/persons/.*"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody(personJson)
+          .withStatus(200)));
+
+    stubFor(get(urlEqualTo("/1/students/students?filterArchived=false&firstResult=0&maxResults=100"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody(studentArrayJson)
+          .withStatus(200)));
+    
+    StaffMember staffMember = new StaffMember((long) 5, (long) 5, null, "Test", "Staffmember", null, fi.pyramus.rest.model.UserRole.ADMINISTRATOR, tags, variables);
+    StaffMember[] staffArray = {staffMember};
+    String staffArrayJson = objectMapper.writeValueAsString(staffArray);
+    
+    stubFor(get(urlMatching("/1/staff/members"))
+      .withQueryParam("email", matching("staff@made.up"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody(staffArrayJson)
+          .withStatus(200)));
+    
+    stubFor(get(urlMatching("/1/courses/staffMemberRoles"))
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody("")
+            .withStatus(204)));
+    
+    stubFor(get(urlMatching("/1/courses/courses/.*/students?filterArchived=false"))
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody("")
+            .withStatus(204)));
+    
+    stubFor(get(urlMatching("/1/courses/courses/.*/staffMembers"))
+      .willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody("")
+          .withStatus(204)));
+    }
+  
   private RemoteWebDriver webDriver;
 }
