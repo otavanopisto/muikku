e73bfeaa4c473298802da83515434ec7f8e42778
petja-laitila <petja.laitila@otavanopisto.fi>
2015-05-18 09:21:40 +0300

    fd

--- muikku-atests/src/test/java/fi/muikku/ui/base/CoursePickerTestsBase.java
+++ muikku-atests/src/test/java/fi/muikku/ui/base/CoursePickerTestsBase.java
@@ -14,13 +14,17 @@ import fi.muikku.SqlBefore;
 public class CoursePickerTestsBase extends AbstractUITest {
   
   @Test
+  @SqlBefore("sql/workspace1Setup.sql")
+  @SqlAfter("sql/workspace1Delete.sql")
   public void coursePickerExistsTest() throws IOException {
     initializePyramusLoginMocks();
     getWebDriver().get(getAppUrl() + "/login?authSourceId=1");
     waitForElementToBePresent(By.className("index"));
     getWebDriver().get(getAppUrl() + "/coursepicker");
+    waitForElementToBePresent(By.className("bt-mainFunction-content"));
     takeScreenshot();
     boolean elementExists = getWebDriver().findElements(By.className("coursePicker")).size() > 0;
     assertTrue(elementExists);
   }
+  
 }
