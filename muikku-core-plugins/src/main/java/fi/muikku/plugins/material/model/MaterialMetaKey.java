2c947f14835a794eea165e6579f389293e77cd39
Antti Leppä <antti.leppa@foyt.fi>
2015-05-20 11:53:17 +0300

    ** Changed MaterialMetaKey name to be unique

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/material/model/MaterialMetaKey.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/material/model/MaterialMetaKey.java
@@ -37,7 +37,7 @@ public class MaterialMetaKey {
   @GeneratedValue (strategy = GenerationType.IDENTITY)
   private Long id;
   
-  @Column (nullable = false)
+  @Column (nullable = false, unique = true)
   @NotNull
   @NotEmpty
   private String name;
