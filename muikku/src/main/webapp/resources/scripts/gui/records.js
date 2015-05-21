efd89157ecb8d41eac4fff1756643b67f648e7de
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2015-05-21 15:03:41 +0300

    Moves Coursepicker and guider javascripts under core-plugins Creates dust files for transcript of records tool Creates javascript file for records tool

---
+++ muikku/src/main/webapp/resources/scripts/gui/records.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+	
+    RecordsImpl = $.klass({
+
+    	init : function(){
+    		// todo: parse url
+          this.loadWorkspaces();	
+    	    $(RecordsImpl.recordsContainer).on("click", '.tr-item)', $.proxy(this.loadWorkspace,this));  
+
+    	},
+
+  
+
+
+       _loadWorkspace : function(event){
+      
+          this.clearContainer();
+          alert("loadWorkspace!");
+            
+//            if( err ){
+//             $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.tasktool.errormessage.notasks', err));
+//            }else{        
+//             renderDustTemplate('/records/record_item_open.dust', asreq, function(text) {
+//               $(TaskImpl.taskContainer).append($.parseHTML(text));
+//            
+//             });
+//            }
+       
+      },
+
+  
+
+      
+    	_loadWorkspaces : function(){
+
+            this.clearContainer();
+
+              mApi().workspace.workspaces
+                .read({ userId: MUIKKU_LOGGED_USER_ID })
+                .callback( function (err, workspaces) {
+                  if( err ){
+                    $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.records.errormessage.noworkspaces', err));
+                  }else{                    
+                    renderDustTemplate('/records/records_items.dust', workspaces, function(text) {
+                      $(RecordsImpl.recordsContainer).append($.parseHTML(text));
+                   
+                    });
+                  }
+                });
+      
+
+    	},
+    	  	
+
+
+     clearContainer : function(){
+       $(RecordsImpl.recordsContainer).empty();
+     },      
+
+	    _klass : {
+	    	// Variables for the class
+		  recordsContainer : ".tr-records-view-container",
+	    }
+    
+    
+    }); 
+	
+
+   window.records = new RecordsImpl();
+  
+        
+
+	
+	
+});
