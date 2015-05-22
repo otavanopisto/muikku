75a2b49f3e57b1a4946905a07e306f002bceb629
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2015-05-22 10:09:43 +0300

    Transcript of records locale changes

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/transcriptofrecords/TranscriptofRecordsBackingBean.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/transcriptofrecords/TranscriptofRecordsBackingBean.java
@@ -1,5 +1,16 @@
 package fi.muikku.plugins.transcriptofrecords;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.ocpsoft.rewrite.annotation.Join;
+import fi.otavanopisto.security.LoggedIn;
+
+@Named
+@RequestScoped
+@Join(path = "/records/", to = "/jsf/records/index.jsf")
+@LoggedIn
+
+
 public class TranscriptofRecordsBackingBean {
 
 }
