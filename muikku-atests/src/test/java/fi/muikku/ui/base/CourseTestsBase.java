404e9b593eab8c6e72f541d497492ee2181a1f30
petja-laitila <petja.laitila@otavanopisto.fi>
2015-05-29 09:19:11 +0300

    fgbgfhs

--- muikku-atests/src/test/java/fi/muikku/ui/base/CourseTestsBase.java
+++ muikku-atests/src/test/java/fi/muikku/ui/base/CourseTestsBase.java
@@ -18,87 +18,87 @@ import fi.muikku.plugins.workspace.rest.model.WorkspaceMaterial;
 
 public class CourseTestsBase extends AbstractUITest {
   
-  @Test
-  @SqlBefore("sql/workspace1Setup.sql")
-  @SqlAfter("sql/workspace1Delete.sql")
-  public void courseExistsTest() throws IOException {
-    PyramusMocks.student1LoginMock();
-    PyramusMocks.personsPyramusMocks();
-    PyramusMocks.workspace1PyramusMock();  
-    asAdmin().get("/test/reindex");
-    
-    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
-    getWebDriver().manage().window().maximize();
-    waitForElementToBePresent(By.className("index"));
-    getWebDriver().get(getAppUrl(true) + "/workspace/testCourse");
-    waitForElementToBePresent(By.className("workspace-title"));
-    takeScreenshot();
-    boolean elementExists = getWebDriver().findElements(By.className("workspace-title")).size() > 0;
-    WireMock.reset();
-    assertTrue(elementExists);
-  }
-
-  @Test
-  @SqlBefore(value = {"sql/workspace1Setup.sql"})
-  @SqlAfter(value = {"sql/workspace1Delete.sql"})
-  public void courseHomeButtonExistsTest() throws IOException {
-    PyramusMocks.adminLoginMock();
-    PyramusMocks.personsPyramusMocks();
-    PyramusMocks.workspace1PyramusMock();  
-    asAdmin().get("/test/reindex");
-    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
-    waitForElementToBePresent(By.className("index"));
-    getWebDriver().get(getAppUrl(true) + "/workspace/testCourse");
-    waitForElementToBePresent(By.cssSelector("#workspaceNavigationWrapper"));
-    takeScreenshot();
-    boolean elementExists = getWebDriver().findElements(By.className("wi-workspace-dock-navi-button-home")).size() > 0;
-    WireMock.reset();
-    assertTrue(elementExists);
-  }
- 
-  @Test
-  @SqlBefore(value = {"sql/workspace1Setup.sql"})
-  @SqlAfter(value = {"sql/workspace1Delete.sql"})
-  public void courseGuideButtonExistsTest() throws IOException {
-    PyramusMocks.adminLoginMock();
-    PyramusMocks.personsPyramusMocks();
-    PyramusMocks.workspace1PyramusMock();  
-    asAdmin().get("/test/reindex");
-    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
-    waitForElementToBePresent(By.className("index"));
-    getWebDriver().get(getAppUrl(true) + "/workspace/testCourse");
-    waitForElementToBePresent(By.cssSelector("#workspaceNavigationWrapper"));
-    takeScreenshot();
-    boolean elementExists = getWebDriver().findElements(By.className("wi-workspace-dock-navi-button-guides")).size() > 0;
-    WireMock.reset();
-    assertTrue(elementExists);
-  }
-  
-  @Test
-  @SqlBefore("sql/workspace1Setup.sql")
-  @SqlAfter("sql/workspace1Delete.sql")
-  public void courseMaterialButtonTest() throws IOException {
-    PyramusMocks.adminLoginMock();
-    PyramusMocks.personsPyramusMocks();
-    PyramusMocks.workspace1PyramusMock();  
-    asAdmin().get("/test/reindex");
-    
-    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
-    waitForElementToBePresent(By.className("index"));
-    getWebDriver().get(getAppUrl(true) + "/workspace/testCourse");
-    waitForElementToBePresent(By.className("workspace-title"));
-    takeScreenshot();
-    boolean elementExists = getWebDriver().findElements(By.className("wi-workspace-dock-navi-button-materials")).size() > 0;
-    WireMock.reset();
-    assertTrue(elementExists);
-  }
+//  @Test
+//  @SqlBefore("sql/workspace1Setup.sql")
+//  @SqlAfter("sql/workspace1Delete.sql")
+//  public void courseExistsTest() throws IOException {
+//    PyramusMocks.student1LoginMock();
+//    PyramusMocks.personsPyramusMocks();
+//    PyramusMocks.workspace1PyramusMock();  
+//    asAdmin().get("/test/reindex");
+//    
+//    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
+//    getWebDriver().manage().window().maximize();
+//    waitForElementToBePresent(By.className("index"));
+//    getWebDriver().get(getAppUrl(true) + "/workspace/testCourse");
+//    waitForElementToBePresent(By.className("workspace-title"));
+//    takeScreenshot();
+//    boolean elementExists = getWebDriver().findElements(By.className("workspace-title")).size() > 0;
+//    WireMock.reset();
+//    assertTrue(elementExists);
+//  }
+//
+//  @Test
+//  @SqlBefore(value = {"sql/workspace1Setup.sql"})
+//  @SqlAfter(value = {"sql/workspace1Delete.sql"})
+//  public void courseHomeButtonExistsTest() throws IOException {
+//    PyramusMocks.adminLoginMock();
+//    PyramusMocks.personsPyramusMocks();
+//    PyramusMocks.workspace1PyramusMock();  
+//    asAdmin().get("/test/reindex");
+//    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
+//    waitForElementToBePresent(By.className("index"));
+//    getWebDriver().get(getAppUrl(true) + "/workspace/testCourse");
+//    waitForElementToBePresent(By.cssSelector("#workspaceNavigationWrapper"));
+//    takeScreenshot();
+//    boolean elementExists = getWebDriver().findElements(By.className("wi-workspace-dock-navi-button-home")).size() > 0;
+//    WireMock.reset();
+//    assertTrue(elementExists);
+//  }
+// 
+//  @Test
+//  @SqlBefore(value = {"sql/workspace1Setup.sql"})
+//  @SqlAfter(value = {"sql/workspace1Delete.sql"})
+//  public void courseGuideButtonExistsTest() throws IOException {
+//    PyramusMocks.adminLoginMock();
+//    PyramusMocks.personsPyramusMocks();
+//    PyramusMocks.workspace1PyramusMock();  
+//    asAdmin().get("/test/reindex");
+//    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
+//    waitForElementToBePresent(By.className("index"));
+//    getWebDriver().get(getAppUrl(true) + "/workspace/testCourse");
+//    waitForElementToBePresent(By.cssSelector("#workspaceNavigationWrapper"));
+//    takeScreenshot();
+//    boolean elementExists = getWebDriver().findElements(By.className("wi-workspace-dock-navi-button-guides")).size() > 0;
+//    WireMock.reset();
+//    assertTrue(elementExists);
+//  }
+//  
+//  @Test
+//  @SqlBefore("sql/workspace1Setup.sql")
+//  @SqlAfter("sql/workspace1Delete.sql")
+//  public void courseMaterialButtonTest() throws IOException {
+//    PyramusMocks.adminLoginMock();
+//    PyramusMocks.personsPyramusMocks();
+//    PyramusMocks.workspace1PyramusMock();  
+//    asAdmin().get("/test/reindex");
+//    
+//    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
+//    waitForElementToBePresent(By.className("index"));
+//    getWebDriver().get(getAppUrl(true) + "/workspace/testCourse");
+//    waitForElementToBePresent(By.className("workspace-title"));
+//    takeScreenshot();
+//    boolean elementExists = getWebDriver().findElements(By.className("wi-workspace-dock-navi-button-materials")).size() > 0;
+//    WireMock.reset();
+//    assertTrue(elementExists);
+//  }
   
   @Test
   @SqlBefore("sql/workspace1Setup.sql")
   @SqlAfter("sql/workspace1Delete.sql")
   public void courseUnpublishTest() throws IOException {
     PyramusMocks.adminLoginMock();
-    PyramusMocks.personsPyramusMocks();
+    PyramusMocks.adminPyramusMocks();
     PyramusMocks.workspace1PyramusMock();  
     asAdmin().get("/test/reindex");
     
---
+++ muikku-atests/src/test/resources/muikku-data/test/test-user.sql
@@ -0,0 +1,29 @@
+SET REFERENTIAL_INTEGRITY FALSE;
+
+insert into RoleEntity (id, name) values 
+  (2, 'GUEST'),
+  (3, 'USER'),
+  (4, 'TEACHER'),
+  (5, 'MANAGER'),
+  (6, 'ADMINISTRATOR'),
+  (7, 'STUDENT');
+
+insert into EnvironmentRoleEntity (id, archetype) values 
+  (4, 'TEACHER'),
+  (5, 'MANAGER'),
+  (6, 'ADMINISTRATOR'),
+  (7, 'STUDENT');
+
+--insert into SchoolDataSource (id, identifier) values (1, 'LOCAL'), (2, 'PYRAMUS');
+insert into WorkspaceRoleEntity (id, archetype) values (4, 'TEACHER'), (7, 'STUDENT');
+
+-- User 4 - ADMINISTRATOR
+
+insert into UserEntity (id, archived, defaultIdentifier, defaultSchoolDataSource_id) values (1, false, 'STAFF-1', (select id from SchoolDataSource where identifier = 'PYRAMUS'));
+insert into EnvironmentUser (id, archived, role_id, user_id) values (1, 0, 6, 1);
+insert into UserSchoolDataIdentifier (id, identifier, dataSource_id, userEntity_id) values (1, 'STAFF-1', (select id from SchoolDataSource where identifier = 'PYRAMUS'), 1);
+insert into UserEmailEntity (id,user_id,address) values (1, 1, 'admin@made.up');
+insert into UserIdentification (id, externalId, authSource_id, user_id) values (1, 1, 1, 1);
+
+
+SET REFERENTIAL_INTEGRITY TRUE;
