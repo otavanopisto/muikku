71b6d9f46267f8fcbc33527077b36870f1cf20a1
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2015-05-29 10:44:13 +0300

    Transcript of records evaluation date to dateFormat Changes dock icon for transcript of records Cleans up ToR javascript

--- muikku/src/main/webapp/resources/scripts/gui/records.js
+++ muikku/src/main/webapp/resources/scripts/gui/records.js
@@ -27,8 +27,8 @@
               var assessment = assessments && assessments.length == 1 ? assessments[0] : null;
               if (assessment) {
                 var grade = this._grades[[assessment.gradingScaleSchoolDataSource, assessment.gradingScaleIdentifier, assessment.gradeSchoolDataSource, assessment.gradeIdentifier].join('-')];
-                var dt = new Date(assessment.evaluated);
-                workspaceEntity.evaluated = dt.toLocaleDateString();
+//                var dt = new Date(assessment.evaluated);
+                workspaceEntity.evaluated = formatDate(new Date(assessment.evaluated));
                 workspaceEntity.verbalAssessment = assessment.verbalAssessment;
                 workspaceEntity.grade = grade.grade;
                 workspaceEntity.gradingScale = grade.scale;
@@ -144,84 +144,7 @@
     });
   });
   
-  
- /**
-
-$(document).ready(function(){
-	
-    RecordsImpl = $.klass({
-
-    	init : function(){
-    		// todo: parse url
-          this._loadWorkspaces();	
-    	    $(RecordsImpl.recordsContainer).on("click", '.tr-item)', $.proxy(this.loadWorkspace,this));  
-
-    	},
-
-  
-
-
-       _loadWorkspace : function(event){
-      
-          this.clearContainer();
-          alert("loadWorkspace!");
-            
-//            if( err ){
-//             $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.tasktool.errormessage.notasks', err));
-//            }else{        
-//             renderDustTemplate('/records/record_item_open.dust', asreq, function(text) {
-//               $(TaskImpl.taskContainer).append($.parseHTML(text));
-//            
-//             });
-//            }
-       
-      },
-
-  
-
-      
-    	_loadWorkspaces : function(){
-
-            this.clearContainer();
-
-              mApi().workspace.workspaces
-                .read({ userId: MUIKKU_LOGGED_USER_ID })
-                .callback( function (err, workspaces) {
-                  if( err ){
-                    $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('plugin.records.errormessage.noworkspaces', err));
-                  }else{                    
-                    renderDustTemplate('/records/records_items.dust', workspaces, function(text) {
-                      $(RecordsImpl.recordsContainer).append($.parseHTML(text));
-                   
-                    });
-                  }
-                });
-      
-
-    	},
-    	  	
-
-
-     clearContainer : function(){
-       $(RecordsImpl.recordsContainer).empty();
-     },      
-
-	    _klass : {
-	    	// Variables for the class
-		  recordsContainer : ".tr-records-view-container"
-	    }
-    
-    
-    }); 
-	
-
-   window.records = new RecordsImpl();
-  
-        
-
-	
-	
-}); **/
+ 
 
 }).call(this);
 
