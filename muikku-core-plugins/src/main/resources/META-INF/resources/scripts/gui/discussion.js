23b1093cd0cd6c35fdef84a7ef902d3b8fd8b285
jjlankinen <jari.lankinen@otavanopisto.fi>
2015-06-05 14:41:16 +0300

    * Rather major overhaul for Discussion (both enviroment and workspace) visual styles * Changed new area icon

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/discussion.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/discussion.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
       // todo: parse url
       this.refreshLatest();
       this.refreshAreas();
-      $(DiscImpl.msgContainer).on("click", '.di-message:not(.open)', $.proxy(this.loadThread, this));
+      $(DiscImpl.msgContainer).on("click", '.di-message:not(.open) .di-message-meta-topic span', $.proxy(this.loadThread, this));
       $(DiscImpl.msgContainer).on("click", '.icon-goback', $.proxy(this.refreshLatest, this));
       $(DiscImpl.msgContainer).on("click", '.di-message-reply-link', $.proxy(this.replyThread, this));
       $(DiscImpl.msgContainer).on("click", '.di-remove-thread-link', $.proxy(this._onRemoveThreadClick, this));
