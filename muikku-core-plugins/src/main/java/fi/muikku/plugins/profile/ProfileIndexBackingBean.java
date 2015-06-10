a2ab2b49cfbc6b2643d0eac6e1aaecd70629a11b
jjlankinen <jari.lankinen@otavanopisto.fi>
2015-06-10 12:35:57 +0300

    * Backingbean for profile and some minor changed

---
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/profile/ProfileIndexBackingBean.java
@@ -0,0 +1,29 @@
+package fi.muikku.plugins.profile;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.annotation.Parameter;
+import org.ocpsoft.rewrite.annotation.RequestAction;
+
+import fi.muikku.jsf.NavigationRules;
+import fi.muikku.model.users.UserEntity;
+import fi.muikku.schooldata.SchoolDataBridgeSessionController;
+import fi.muikku.schooldata.entity.User;
+import fi.muikku.session.SessionController;
+import fi.muikku.users.UserController;
+import fi.muikku.users.UserEntityController;
+import fi.otavanopisto.security.LoggedIn;
+
+@Named
+@LoggedIn
+@Stateful
+@RequestScoped
+@Join ( path = "/profile/{userId}", to = "/jsf/profile/profile.jsf")
+public class ProfileIndexBackingBean {
+
+  
+}
