7e0d672449ec14a532df75e464ad4b2c61016eff
jjlankinen <jari.lankinen@otavanopisto.fi>
2015-06-11 12:06:39 +0300

    * Added click functions to change password dialog etc.

--- muikku/src/main/webapp/resources/scripts/gui/profile.js
+++ muikku/src/main/webapp/resources/scripts/gui/profile.js
@@ -1,5 +1,33 @@
 (function() {
   
+  function changePassword() {
+    renderDustTemplate('profile/profile-change-password.dust', { }, $.proxy(function (text) {
+      var dialog = $(text);
+      $(text).dialog({
+        modal: true, 
+        resizable: false,
+        width: 400,
+        dialogClass: "profile-change-password-dialog",
+        buttons: [{
+          'text': dialog.data('button-send-text'),
+          'class': 'send-button',
+          'click': function(event) {
+            $(this).dialog("close");
+            confirmCallback();
+          }
+        }, {
+          'text': dialog.data('button-cancel-text'),
+          'class': 'cancel-button',
+          'click': function(event) {
+            $(this).dialog("close");
+          }
+        }]
+      });
+    }, this));
+  }
   
+  $(document).on('click', '.profile-change-password', function (event, data) {
+    changePassword();
+  });
   
 }).call(this);
