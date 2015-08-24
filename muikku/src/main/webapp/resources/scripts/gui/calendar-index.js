019b1ed285afde248ac0d2a74a6d47558b80f518
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2015-08-24 12:30:14 +0300

    Calendar update css and bug fixes

--- muikku/src/main/webapp/resources/scripts/gui/calendar-index.js
+++ muikku/src/main/webapp/resources/scripts/gui/calendar-index.js
@@ -2,8 +2,13 @@
   $(document).ready(function(){
     $('#fpDayEvents').fullCalendar({
       defaultView : 'basicDay',
+      timeFormat : 'HH', 
       firstDay : 1,
       header: false,
+      lang : _MUIKKU_LOCALE,
+      viewRender: function(view, element) {
+        element.find('.fc-day-header').html('');
+       }
     });
     
     $('#fpWeekView').fullCalendar({
@@ -13,7 +18,8 @@
       columnFormat : {
         week : 'ddd',
       },
-      height : 500
+      height : 500,
+      lang : _MUIKKU_LOCALE,
     });
   
     loadFullCalendarEvents($('#fpDayEvents'));
