fa97504729e1f6260a011f65ace4ecd89e711d03
Antti Leppä <antti.leppa@foyt.fi>
2015-09-10 10:37:03 +0300

    ** Added more identification properties into SaveFieldErrorWebSocketMessage

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/SaveFieldErrorWebSocketMessage.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/workspace/SaveFieldErrorWebSocketMessage.java
@@ -10,6 +10,50 @@ public class SaveFieldErrorWebSocketMessage {
     this.error = error;
   }
 
+  public String getEmbedId() {
+    return embedId;
+  }
+
+  public void setEmbedId(String embedId) {
+    this.embedId = embedId;
+  }
+
+  public Long getMaterialId() {
+    return materialId;
+  }
+
+  public void setMaterialId(Long materialId) {
+    this.materialId = materialId;
+  }
+
+  public String getFieldName() {
+    return fieldName;
+  }
+
+  public void setFieldName(String fieldName) {
+    this.fieldName = fieldName;
+  }
+
+  public Long getWorkspaceMaterialId() {
+    return workspaceMaterialId;
+  }
+
+  public void setWorkspaceMaterialId(Long workspaceMaterialId) {
+    this.workspaceMaterialId = workspaceMaterialId;
+  }
+
+  public Long getWorkspaceEntityId() {
+    return workspaceEntityId;
+  }
+
+  public void setWorkspaceEntityId(Long workspaceEntityId) {
+    this.workspaceEntityId = workspaceEntityId;
+  }
+
   private String error;
-  
+  private String embedId;
+  private Long materialId;
+  private String fieldName;
+  private Long workspaceMaterialId;
+  private Long workspaceEntityId;
 }
