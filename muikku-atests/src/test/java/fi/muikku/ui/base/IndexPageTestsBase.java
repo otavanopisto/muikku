90fed23f21994d613175d68f6670d8cf2cc2418c
Antti Leppä <antti.leppa@foyt.fi>
2015-09-23 13:24:30 +0300

    ** Enabled rest of ui tests in sauce profile

--- muikku-atests/src/test/java/fi/muikku/ui/base/IndexPageTestsBase.java
+++ muikku-atests/src/test/java/fi/muikku/ui/base/IndexPageTestsBase.java
@@ -1,5 +1,9 @@
 package fi.muikku.ui.base;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -20,7 +24,7 @@ import fi.pyramus.webhooks.WebhookStudentCreatePayload;
 public class IndexPageTestsBase extends AbstractUITest {
 
   @Test
-  public void IndexPageTest() throws IOException {
+  public void indexPageTest() throws IOException {
     getWebDriver().get(getAppUrl());
     boolean elementExists = getWebDriver().findElements(By.className("index")).size() > 0;;
     assertTrue(elementExists);
@@ -33,6 +37,10 @@ public class IndexPageTestsBase extends AbstractUITest {
     ObjectMapper objectMapper = new ObjectMapper().registerModule(new JodaModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     String payload = objectMapper.writeValueAsString(new WebhookStudentCreatePayload((long) 1));
     webhookCall("http://dev.muikku.fi:8080/pyramus/webhook", payload);
+
+    verify(1, getRequestedFor(urlEqualTo("/users/authorize.*")));
+    verify(1, postRequestedFor(urlEqualTo("/1/oauth/token")));
+    verify(1, getRequestedFor(urlEqualTo("/1/system/whoami")));
     
     getWebDriver().get(getAppUrl() + "/login?authSourceId=1");
     boolean elementExists = getWebDriver().findElements(By.className("index")).size() > 0;
