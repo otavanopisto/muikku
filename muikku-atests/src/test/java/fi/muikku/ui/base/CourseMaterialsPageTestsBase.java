44a22e6f767fd4b1d2436aa732521d3c50cab1ef
Antti Leppä <antti.leppa@foyt.fi>
2015-09-28 10:25:56 +0300

    ** courseMaterialExistsTest and courseFullscreenReadingButtonExistsTest in CourseMaterialsPageTestsBase to use atest rest service for data setup

--- muikku-atests/src/test/java/fi/muikku/ui/base/CourseMaterialsPageTestsBase.java
+++ muikku-atests/src/test/java/fi/muikku/ui/base/CourseMaterialsPageTestsBase.java
@@ -1,7 +1,6 @@
 package fi.muikku.ui.base;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -24,65 +23,60 @@ import fi.pyramus.webhooks.WebhookStaffMemberCreatePayload;
 public class CourseMaterialsPageTestsBase extends AbstractUITest {
 
   @Test
-  @SqlBefore(value = {"sql/workspace1Setup.sql", "sql/workspace1MaterialSetup.sql"})
-  @SqlAfter(value = {"sql/workspace1Delete.sql", "sql/workspace1MaterialDelete.sql"})
   public void courseMaterialExistsTest() throws Exception {
-    PyramusMocks.adminLoginMock();
-    PyramusMocks.personsPyramusMocks();
-    PyramusMocks.workspace1PyramusMock();
-    
-    ObjectMapper objectMapper = new ObjectMapper().registerModule(new JodaModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    String payload = objectMapper.writeValueAsString(new WebhookStaffMemberCreatePayload((long) 4));
-    webhookCall("http://dev.muikku.fi:8080/pyramus/webhook", payload);
-    payload = objectMapper.writeValueAsString(new WebhookPersonCreatePayload((long) 4));
-    webhookCall("http://dev.muikku.fi:8080/pyramus/webhook", payload);
-    
-    asAdmin().get("/test/reindex");
-    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
-    waitForElementToBePresent(By.className("index"));
-    getWebDriver().get(getAppUrl(true) + "/workspace/testcourse/materials");
-    waitForElementToBePresent(By.cssSelector(".material-view"));
-//    takeScreenshot();
-    boolean elementExists = getWebDriver().findElements(By.cssSelector("article p")).size() > 0;
-    WireMock.reset();
-    assertTrue(elementExists);
+    loginAdmin();
+    Workspace workspace = createWorkspace("testcourse", "1", Boolean.TRUE);
+    try {
+      WorkspaceFolder workspaceFolder1 = createWorkspaceFolder(workspace.getId(), null, Boolean.FALSE, 1, "Test Course material folder", "DEFAULT");
+      
+      WorkspaceHtmlMaterial htmlMaterial1 = createWorkspaceHtmlMaterial(workspace.getId(), workspaceFolder1.getId(), 
+          "1.0 Testimateriaali", "text/html;editor=CKEditor", 
+          "<html><body><p>Testi materiaalia:  Lorem ipsum dolor sit amet </p><p>Proin suscipit luctus orci placerat fringilla. Donec hendrerit laoreet risus eget adipiscing. Suspendisse in urna ligula, a volutpat mauris. Sed enim mi, bibendum eu pulvinar vel, sodales vitae dui. Pellentesque sed sapien lorem, at lacinia urna. In hac habitasse platea dictumst. Vivamus vel justo in leo laoreet ullamcorper non vitae lorem</p></body></html>", 1l, 
+          "EXERCISE");
+      
+      navigate(String.format("/workspace/%s/materials", workspace.getUrlName()), true);
+      waitForPresent(".material-view");
+      assertVisible("article p");
+
+      deleteWorkspaceHtmlMaterial(workspace.getId(), htmlMaterial1.getId());
+    } finally {
+      deleteWorkspace(workspace.getId());
+    }
   }
   
   @Test
-  @SqlBefore(value = {"sql/workspace1Setup.sql", "sql/workspace1MaterialSetup.sql"})
-  @SqlAfter(value = {"sql/workspace1Delete.sql", "sql/workspace1MaterialDelete.sql"})
   public void courseFullscreenReadingButtonExistsTest() throws Exception {
-    PyramusMocks.adminLoginMock();
-    PyramusMocks.personsPyramusMocks();
-    PyramusMocks.workspace1PyramusMock();
-    
-    ObjectMapper objectMapper = new ObjectMapper().registerModule(new JodaModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    
-    String payload = objectMapper.writeValueAsString(new WebhookStaffMemberCreatePayload((long) 4));
-    webhookCall("http://dev.muikku.fi:8080/pyramus/webhook", payload);
-    
-    payload = objectMapper.writeValueAsString(new WebhookPersonCreatePayload((long) 4));
-    webhookCall("http://dev.muikku.fi:8080/pyramus/webhook", payload);
-    
-    asAdmin().get("/test/reindex");
-    getWebDriver().get(getAppUrl(true) + "/login?authSourceId=1");
-    waitForElementToBePresent(By.className("index"));
-    getWebDriver().get(getAppUrl(true) + "/workspace/testcourse/materials");
-    waitForElementToBePresent(By.cssSelector("#workspaceNavigationWrapper"));
-//    takeScreenshot();
-    boolean elementExists = getWebDriver().findElements(By.className("wi-workspace-dock-navi-button-materials-reading")).size() > 0;
-    WireMock.reset();
-    assertTrue(elementExists);
+    loginAdmin();
+    Workspace workspace = createWorkspace("testcourse", "1", Boolean.TRUE);
+    try {
+      WorkspaceFolder workspaceFolder1 = createWorkspaceFolder(workspace.getId(), null, Boolean.FALSE, 1, "Test Course material folder", "DEFAULT");
+      
+      WorkspaceHtmlMaterial htmlMaterial1 = createWorkspaceHtmlMaterial(workspace.getId(), workspaceFolder1.getId(), 
+          "1.0 Testimateriaali", "text/html;editor=CKEditor", 
+          "<html><body><p>Testi materiaalia:  Lorem ipsum dolor sit amet </p><p>Proin suscipit luctus orci placerat fringilla. Donec hendrerit laoreet risus eget adipiscing. Suspendisse in urna ligula, a volutpat mauris. Sed enim mi, bibendum eu pulvinar vel, sodales vitae dui. Pellentesque sed sapien lorem, at lacinia urna. In hac habitasse platea dictumst. Vivamus vel justo in leo laoreet ullamcorper non vitae lorem</p></body></html>", 1l, 
+          "EXERCISE");
+      
+      navigate(String.format("/workspace/%s/materials", workspace.getUrlName()), true);
+      waitForPresent(".wi-workspace-dock-navi-button-materials-reading");
+      assertVisible(".wi-workspace-dock-navi-button-materials-reading");
+
+      deleteWorkspaceHtmlMaterial(workspace.getId(), htmlMaterial1.getId());
+    } finally {
+      deleteWorkspace(workspace.getId());
+    }
   }
 
   @Test
   public void courseMaterialManagementButtonExistsTest() throws Exception {
     loginAdmin();
     Workspace workspace = createWorkspace("testcourse", "1", Boolean.TRUE);
-    navigate(String.format("/workspace/%s/materials", workspace.getUrlName()), true);
-    waitForPresent("#contentWorkspaceMaterials");
-    assertPresent(".wi-workspace-dock-navi-button-materials-management");
-    deleteWorkspace(workspace.getId());
+    try {
+      navigate(String.format("/workspace/%s/materials", workspace.getUrlName()), true);
+      waitForPresent("#contentWorkspaceMaterials");
+      assertPresent(".wi-workspace-dock-navi-button-materials-management");
+    } finally {
+      deleteWorkspace(workspace.getId());
+    }
   }
 
   @Test
