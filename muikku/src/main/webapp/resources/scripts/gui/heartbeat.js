7395eafe433f34ac0b378c7ab69f9c82b891e059
ilmo.euro <ilmo.euro@gmail.com>
2015-10-05 13:34:05 +0300

    ** Failsafe heartbeat

--- muikku/src/main/webapp/resources/scripts/gui/heartbeat.js
+++ muikku/src/main/webapp/resources/scripts/gui/heartbeat.js
@@ -1,9 +1,22 @@
 $(function() {
-  if (mApi) {
-      setInterval(function() {
 
-        mApi().system.ping.read().callback(function() {});
-        
-      }, 15*60*1000);
+  function isObject(obj) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+    return obj === Object(obj)
+  }
+
+  if (mApi) {
+    setInterval(function() {
+
+      try {
+        mApi().system.ping.read().callback(function() {
+        });
+      } catch (ex) {
+        if (isObject(console) && console.log) {
+          console.log("Ping failed");
+        }
+      }
+
+    }, 15 * 60 * 1000);
   }
 });
