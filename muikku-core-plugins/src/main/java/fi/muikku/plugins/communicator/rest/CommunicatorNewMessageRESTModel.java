e75d2045072a55e9e2d6544f6d8ba646993d1ca9
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2015-10-15 09:52:53 +0300

    - Changed new message REST model to inherit from common base class  - Cleaned unused Injects

--- muikku-core-plugins/src/main/java/fi/muikku/plugins/communicator/rest/CommunicatorNewMessageRESTModel.java
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/communicator/rest/CommunicatorNewMessageRESTModel.java
@@ -2,103 +2,22 @@ package fi.muikku.plugins.communicator.rest;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class CommunicatorNewMessageRESTModel {
+public class CommunicatorNewMessageRESTModel extends CommunicatorMessageRESTModel {
 
   public CommunicatorNewMessageRESTModel() {
-    
   }
   
   public CommunicatorNewMessageRESTModel(Long id, Long communicatorMessageId, Long senderId, String categoryName, String caption, 
       String content, Date created, Set<String> tags, List<Long> recipientIds, List<Long> recipientGroupIds, 
       List<Long> recipientTeachersWorkspaceIds, List<Long> recipientStudentsWorkspaceIds) {
-    this.id = id;
-    this.communicatorMessageId = communicatorMessageId;
-    this.senderId = senderId;
-    this.categoryName = categoryName;
-    this.caption = caption;
-    this.content = content;
-    this.created = created;
-    this.tags = tags;
-    this.recipientIds = recipientIds;
+    super(id, communicatorMessageId, senderId, categoryName, caption, content, created, tags, recipientIds);
+
     this.recipientGroupIds = recipientGroupIds;
     this.recipientStudentsWorkspaceIds = recipientStudentsWorkspaceIds;
     this.recipientTeachersWorkspaceIds = recipientTeachersWorkspaceIds;
-  }
-  
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public Long getCommunicatorMessageId() {
-    return communicatorMessageId;
-  }
-
-  public void setCommunicatorMessageId(Long communicatorMessageId) {
-    this.communicatorMessageId = communicatorMessageId;
-  }
-
-  public Long getSenderId() {
-    return senderId;
-  }
-
-  public void setSenderId(Long senderId) {
-    this.senderId = senderId;
-  }
-
-  public String getCategoryName() {
-    return categoryName;
-  }
-
-  public void setCategoryName(String categoryName) {
-    this.categoryName = categoryName;
-  }
-
-  public String getCaption() {
-    return caption;
-  }
-
-  public void setCaption(String caption) {
-    this.caption = caption;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
-  }
-
-  public Date getCreated() {
-    return created;
-  }
-
-  public void setCreated(Date created) {
-    this.created = created;
-  }
-
-  public Set<String> getTags() {
-    return tags;
-  }
-
-  public void setTags(Set<String> tags) {
-    this.tags = tags;
-  }
-
-  public List<Long> getRecipientIds() {
-    return recipientIds;
-  }
-
-  public void setRecipientIds(List<Long> recipientIds) {
-    this.recipientIds = recipientIds;
   }
 
   public List<Long> getRecipientGroupIds() {
@@ -124,24 +43,6 @@ public class CommunicatorNewMessageRESTModel {
   public void setRecipientTeachersWorkspaceIds(List<Long> recipientTeachersWorkspaceIds) {
     this.recipientTeachersWorkspaceIds = recipientTeachersWorkspaceIds;
   }
-
-  private Long id;
-
-  private Long communicatorMessageId;
-
-  private Long senderId;
-  
-  private String categoryName;
-  
-  private String caption;
-
-  private String content;
-
-  private Date created;
-  
-  private Set<String> tags = new HashSet<String>();
-  
-  private List<Long> recipientIds = new ArrayList<Long>();
 
   private List<Long> recipientGroupIds = new ArrayList<Long>();
   
