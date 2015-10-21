b3df9fd6c0179d211789a0488632eae0d84d45d7
jjlankinen <jari.lankinen@otavanopisto.fi>
2015-10-21 12:38:57 +0300

    * Removed _icons.scss from screen.scss and added it as an individual file to be loaded directly. * Added some resource rewrites for fonts

--- muikku/src/main/java/fi/muikku/jsf/JsfResourceRewriteRules.java
+++ muikku/src/main/java/fi/muikku/jsf/JsfResourceRewriteRules.java
@@ -19,6 +19,7 @@ public class JsfResourceRewriteRules extends HttpConfigurationProvider {
     configuration.addRule(Join.path("/scripts/{file}").to("/faces/javax.faces.resource/scripts/{file}"))
                     .where("file").matches("[a-zA-Z0-9/_.\\-]*");
     configuration.addRule(Join.path("/css/{file}").to("/javax.faces.resource/css/{file}.jsf?ln=theme-muikku"));
+    configuration.addRule(Join.path("/fonts/{file}").to("/javax.faces.resource/fonts/{file}.jsf?ln=theme-muikku"));
     
     return configuration;
   }
