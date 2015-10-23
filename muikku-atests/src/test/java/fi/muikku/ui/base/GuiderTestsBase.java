44bc363c2bcbe11ab9c4413f22b2d3bbe7fa605d
petja-laitila <petja.laitila@otavanopisto.fi>
2015-10-23 12:24:00 +0300

    ** filtering by workspace

--- muikku-atests/src/test/java/fi/muikku/ui/base/GuiderTestsBase.java
+++ muikku-atests/src/test/java/fi/muikku/ui/base/GuiderTestsBase.java
@@ -12,7 +12,7 @@ public class GuiderTestsBase extends AbstractUITest {
   public void filterByNameTest() throws Exception {
     loginAdmin();
     Workspace workspace = createWorkspace("testcourse", "test course for testing", "1", Boolean.TRUE);
-    PyramusMocks.guiderTestMock();
+    PyramusMocks.personsPyramusMocks();
     try {
       navigate("/guider", true);
       sendKeys(".gt-search .search", "Second User");
@@ -26,13 +26,14 @@ public class GuiderTestsBase extends AbstractUITest {
   public void filterByWorkspaceTest() throws Exception {
     loginAdmin();
     Workspace workspace = createWorkspace("testcourse", "test course for testing", "1", Boolean.TRUE);
-    createWorkspace("diffentcourse", "Second test course", "2", Boolean.TRUE);
-    PyramusMocks.guiderTestMock();
-    try {  
+    Workspace workspace2 = createWorkspace("diffentcourse", "Second test course", "2", Boolean.TRUE);
+    PyramusMocks.personsPyramusMocks();
+    try {
       navigate("/guider", true);
-      
-      assertText(".gt-user .gt-user-meta-topic>span", "Second User");
+      waitAndClick(String.format("#workspace-%d>a", workspace.getId()));
+      assertText(".gt-user .gt-user-meta-topic>span", "Test User");
     } finally {
+      deleteWorkspace(workspace2.getId());
       deleteWorkspace(workspace.getId());
     }
   }
