ef8779ecb62616f8275740b3ebec01324efa58a8
petja-laitila <petja.laitila@otavanopisto.fi>
2015-11-05 14:32:21 +0200

    ** More communocator tests

--- muikku-atests/src/test/java/fi/muikku/ui/base/communicator/CommunicatorTestsBase.java
+++ muikku-atests/src/test/java/fi/muikku/ui/base/communicator/CommunicatorTestsBase.java
@@ -1,54 +1,115 @@
 package fi.muikku.ui.base.communicator;
 
-import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
 import fi.muikku.ui.AbstractUITest;
-import fi.muikku.ui.PyramusMocks;
+
 
 public class CommunicatorTestsBase extends AbstractUITest {
   
   @Test
   public void communicatorSendMessageTest() throws Exception {
-    loginAdmin();
-    PyramusMocks.personsPyramusMocks();
-    navigate("/communicator", true);
-    waitAndClick(".bt-mainFunction-content span");
-    waitForPresent("#recipientContent");
-    sendKeys("#recipientContent", "Test User");
-    waitAndClick(".ui-autocomplete li.ui-menu-item");
-    waitForPresent(".mf-textfield-subject");
-    sendKeys(".mf-textfield-subject", "Test");    
-    waitAndClick("#cke_1_contents");
-    getWebDriver().switchTo().activeElement().sendKeys("Communicator test");
-    click("*[name='send']");
-    logout();
-    loginStudent1();
-    navigate("/communicator", true);
-    waitForPresent("div.cm-message-header-content-secondary");
-    assertText("div.cm-message-header-content-secondary", "Test");
-//    viestien poisto
+    try{
+      loginAdmin();
+      navigate("/communicator", true);
+      waitAndClick(".bt-mainFunction-content span");
+      waitForPresent("#recipientContent");
+      sendKeys("#recipientContent", "Test User");
+      waitAndClick(".ui-autocomplete li.ui-menu-item");
+      waitForPresent(".mf-textfield-subject");
+      sendKeys(".mf-textfield-subject", "Test");    
+      waitAndClick("#cke_1_contents");
+      getWebDriver().switchTo().activeElement().sendKeys("Communicator test");
+      click("*[name='send']");
+      logout();
+      loginStudent1();
+      navigate("/communicator", true);
+      waitForPresent("div.cm-message-header-content-secondary");
+      assertText("div.cm-message-header-content-secondary", "Test");  
+    }finally{
+      deleteCommunicatorMessages(); 
+    }
   }
   
   @Test
   public void communicatorSentMessagesTest() throws Exception {
-    loginAdmin();
-    PyramusMocks.personsPyramusMocks();
-    navigate("/communicator", true);
-    waitAndClick(".bt-mainFunction-content span");
-    waitForPresent("#recipientContent");
-    sendKeys("#recipientContent", "Test User");
-    waitAndClick(".ui-autocomplete li.ui-menu-item");
-    waitForPresent(".mf-textfield-subject");
-    sendKeys(".mf-textfield-subject", "Test");    
-    waitAndClick("#cke_1_contents");
-    getWebDriver().switchTo().activeElement().sendKeys("Communicator test");
-    click("*[name='send']");
-    waitForPresentVisible(".notification-queue-item-success span");
-    navigate("/communicator#sent", true);
-    waitForPresent("div.cm-message-header-content-secondary");
-    assertText("div.cm-message-header-content-secondary", "Test");
-//    test cleanup
+    try{
+      loginAdmin();
+      navigate("/communicator", true);
+      waitAndClick(".bt-mainFunction-content span");
+      waitForPresent("#recipientContent");
+      sendKeys("#recipientContent", "Test User");
+      waitAndClick(".ui-autocomplete li.ui-menu-item");
+      waitForPresent(".mf-textfield-subject");
+      sendKeys(".mf-textfield-subject", "Test");    
+      waitAndClick("#cke_1_contents");
+      getWebDriver().switchTo().activeElement().sendKeys("Communicator test");
+      click("*[name='send']");
+      waitForPresentVisible(".notification-queue-item-success span");
+      navigate("/communicator#sent", true);
+      waitForPresent("div.cm-message-header-content-secondary");
+      assertText("div.cm-message-header-content-secondary", "Test");
+    }finally{
+      deleteCommunicatorMessages(); 
+    }
   }
-
+  
+  @Test
+  public void communicatorPagingTest() throws Exception {
+    try{
+      loginAdmin();
+      long sender = getUserIdByEmail("admin@made.up");
+      long recipient = getUserIdByEmail("testuser@made.up");
+      for(int i = 0; i < 30; i++)
+        createCommunicatorMesssage("Test " + i, "Test content " + i, sender, recipient);
+      logout();
+      loginStudent1();
+      navigate("/communicator", true);
+      waitAndClick(".cm-page-link-load-more");
+//      TODO: Find a way to wait by condition rather than sleep.
+      sleep(5000);
+      int elementCount = countElements("div.cm-message-header-content-secondary");
+      assertTrue(String.format("Elements list is not greater than %d in size", 10), elementCount > 10); 
+    }finally{
+      deleteCommunicatorMessages(); 
+    }
+  }
+    
+  @Test
+  public void communicatorReadMessageTest() throws Exception {
+    try{
+      loginAdmin();
+      long sender = getUserIdByEmail("admin@made.up");
+      long recipient = getUserIdByEmail("testuser@made.up");
+      createCommunicatorMesssage("Test caption", "Test content.", sender, recipient);
+      logout();
+      loginStudent1();
+      navigate("/communicator", true);
+      waitAndClick("div.cm-message-header-content-secondary");
+      waitForPresent(".cm-message-content-text");
+      assertText("div.cm-message-content-text", "Test content.");
+    }finally{
+      deleteCommunicatorMessages(); 
+    }
+  }
+  
+  @Test
+  public void communicatorDeleteInboxMessageTest() throws Exception {
+    try{
+      loginAdmin();
+      long sender = getUserIdByEmail("admin@made.up");
+      long recipient = getUserIdByEmail("testuser@made.up");
+      createCommunicatorMesssage("Test caption", "Test content.", sender, recipient);
+      logout();
+      loginStudent1();
+      navigate("/communicator", true);
+      waitAndClick("div.mf-item-select input[type=\"checkbox\"]");
+      waitAndClick("div.icon-delete");
+      assertTrue("Element found even though it shouldn't be there", isElementPresent("div.mf-item-select input[type=\"checkbox\"]") == false);
+    }finally{
+      deleteCommunicatorMessages(); 
+    }
+  }
 
 }
