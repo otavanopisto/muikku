a9a9e14d2990ddcf5c2d910e7c2d3433959a6857
jjlankinen <jari.lankinen@otavanopisto.fi>
2015-11-11 08:51:49 +0200

    * Added initial styles to users listing in workspace

--- muikku/src/main/webapp/resources/scripts/gui/workspace-users.js
+++ muikku/src/main/webapp/resources/scripts/gui/workspace-users.js
@@ -1,7 +1,64 @@
 (function() {
+  
+  function confirmUserDelete(id) {
+    renderDustTemplate('workspace/workspace-users-delete-request-confirm.dust', {id:id}, $.proxy(
+      function(text) {
+        var dialog = $(text);
+        $(text).dialog({
+          modal : true,
+          minHeight : 200,
+          maxHeight : $(window).height() - 50,
+          resizable : false,
+          width : 560,
+          dialogClass : "workspace-user-confirm-dialog",
+          buttons : [ {
+            'text' : dialog.data('button-delete-text'),
+            'class' : 'delete-button',
+            'click' : function(event) {
+              $(this).dialog("destroy").remove();
+            }
+          }, {
+            'text' : dialog.data('button-cancel-text'),
+            'class' : 'cancel-button',
+            'click' : function(event) {
+              $(this).dialog("destroy").remove();
+            }
+          } ]
+        });
+      }, this));
+  }
+  
+  function confirmUserArchive(id) {
+    renderDustTemplate('workspace/workspace-users-archive-request-confirm.dust', {id:id}, $.proxy(
+      function(text) {
+        var dialog = $(text);
+        $(text).dialog({
+          modal : true,
+          minHeight : 200,
+          maxHeight : $(window).height() - 50,
+          resizable : false,
+          width : 560,
+          dialogClass : "workspace-user-confirm-dialog",
+          buttons : [ {
+            'text' : dialog.data('button-archive-text'),
+            'class' : 'archive-button',
+            'click' : function(event) {
+              $(this).dialog("destroy").remove();
+            }
+          }, {
+            'text' : dialog.data('button-cancel-text'),
+            'class' : 'cancel-button',
+            'click' : function(event) {
+              $(this).dialog("destroy").remove();
+            }
+          } ]
+        });
+      }, this));
+  }
 
   $(document).ready(function() {
     
+    /* Get Workspace Teachers */
     mApi().user.users.read()
     .callback(function (err, teachers) {
 
@@ -15,6 +72,7 @@
       
     });  
     
+    /* Get Workspace Students */
     mApi().user.users.read()
     .callback(function (err, students) {
 
@@ -28,6 +86,16 @@
       
     }); 
 
+  });
+  
+  $(document).on('click', '.workspace-users-archive', function(event) {
+    var id = $(this).closest(".workspace-users").attr("data-user-id");
+    confirmUserArchive(id);
+  });
+  
+  $(document).on('click', '.workspace-users-delete', function(event) {
+    var id = $(this).closest(".workspace-users").attr("data-user-id");
+    confirmUserDelete(id);
   });
   
 }).call(this);
