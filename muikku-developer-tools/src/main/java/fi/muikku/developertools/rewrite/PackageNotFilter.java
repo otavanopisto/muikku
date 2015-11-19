08d7d434acccd0a7c353af28007af692be251a4c
Antti Leppä <antti.leppa@foyt.fi>
2015-11-19 10:01:16 +0200

    ** Added a developer tools project ** Added rewrite finder that scans exploded jar files

---
+++ muikku-developer-tools/src/main/java/fi/muikku/developertools/rewrite/PackageNotFilter.java
@@ -0,0 +1,25 @@
+package fi.muikku.developertools.rewrite;
+
+import java.util.List;
+
+import org.ocpsoft.rewrite.annotation.scan.PackageFilter;
+
+public class PackageNotFilter extends PackageFilter {
+
+  public PackageNotFilter(String config, List<String> notPackages) {
+    super(config);
+    this.notPackages = notPackages;
+  }  
+
+  @Override
+  public boolean isAllowedPackage(String packageName) {
+    if (notPackages.contains(packageName)) {
+      return false;
+    }
+    
+    return super.isAllowedPackage(packageName);
+  }
+  
+  private List<String> notPackages;
+}
+
