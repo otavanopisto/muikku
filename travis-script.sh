c1955a767319866481e2cf734fa96d39a9821c02
petja-laitila <petja.laitila@otavanopisto.fi>
2015-11-30 12:26:37 +0200

    ** send error mesg if verify fails

--- travis-script.sh
+++ travis-script.sh
@@ -2,12 +2,8 @@
 if [[ $TRAVIS_BRANCH != 'master' ]] && [[ $TRAVIS_PULL_REQUEST != false ]]; then
   pushd .;
   cd muikku-atests;
-  mvn clean verify -Dmaven.javadoc.skip=true -Dsource.skip=true -Dit.sauce.browser="$browser" -Dit.sauce.browser.version="$browser_version" -Dit.sauce.browser.resolution="$browser_resolution" -Dit.sauce.platform="$platform" -Dit.package="$package" -P$it_profile & wait;
-  if [[ "$?" = "0" ]]; then
-    exit 0;
-  else
-    echo "There were tests in error";
-    exit 1;
-  fi;
+  set -e; 
+  mvn clean verify -Dmaven.javadoc.skip=true -Dsource.skip=true -Dit.sauce.browser="$browser" -Dit.sauce.browser.version="$browser_version" -Dit.sauce.browser.resolution="$browser_resolution" -Dit.sauce.platform="$platform" -Dit.package="$package" -P$it_profile;
+  set +e;
   popd;
 fi;
