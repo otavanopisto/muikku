8b9c7017ba2460176476e94f1d3bbcb8cce844e6
petja-laitila <petja.laitila@otavanopisto.fi>
2015-11-30 10:41:39 +0200

    ** travis-script

---
+++ travis-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [[ $TRAVIS_BRANCH != 'master' ]] && [[ $TRAVIS_BRANCH != 'devel' ]]; then
+  pushd .;
+  cd muikku-atests;
+  mvn clean verify 
+    -Dmaven.javadoc.skip=true
+    -Dsource.skip=true
+    -Dit.sauce.browser="$browser"
+    -Dit.sauce.browser.version="$browser_version"
+    -Dit.sauce.browser.resolution="$browser_resolution"
+    -Dit.sauce.platform="$platform"
+    -Dit.package="$package"
+    -P$it_profile &;
+  popd;
+  if [[ "$?" = "0" ]]; then
+    exit 0;
+  else
+    echo "There were tests in error";
+    exit 1;
+  fi;
+fi;
