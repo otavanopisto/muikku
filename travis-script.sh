f7943044c5b677a750e2805cf77be0edf7a3c8d9
petja-laitila <petja.laitila@otavanopisto.fi>
2015-11-30 11:20:41 +0200

    ** mvn verify

--- travis-script.sh
+++ travis-script.sh
@@ -2,15 +2,7 @@
 if [[ $TRAVIS_BRANCH != 'master' ]] && [[ $TRAVIS_PULL_REQUEST != false ]]; then
   pushd .;
   cd muikku-atests;
-  mvn clean verify 
-    -Dmaven.javadoc.skip=true
-    -Dsource.skip=true
-    -Dit.sauce.browser="$browser"
-    -Dit.sauce.browser.version="$browser_version"
-    -Dit.sauce.browser.resolution="$browser_resolution"
-    -Dit.sauce.platform="$platform"
-    -Dit.package="$package"
-    -P$it_profile & wait;
+  mvn clean verify -Dmaven.javadoc.skip=true -Dsource.skip=true -Dit.sauce.browser="$browser" -Dit.sauce.browser.version="$browser_version" -Dit.sauce.browser.resolution="$browser_resolution" -Dit.sauce.platform="$platform" -Dit.package="$package" -P$it_profile & wait;
   popd;
   if [[ "$?" = "0" ]]; then
     exit 0;
