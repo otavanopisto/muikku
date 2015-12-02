52999955fd02786c2b17808a7e0ab896cb8476be
ilmo.euro <ilmo.euro@gmail.com>
2015-12-02 14:38:29 +0200

    ** Announcement data model

---
+++ muikku-core-plugins-persistence/src/main/java/fi/muikku/plugins/announcer/model/Announcement.java
@@ -0,0 +1,83 @@
+package fi.muikku.plugins.announcer.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import fi.otavanopisto.security.ContextReference;
+
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
+public class Announcement implements ContextReference {
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getCaption() {
+    return caption;
+  }
+
+  public void setCaption(String caption) {
+    this.caption = caption;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  public void setCreated(Date created) {
+    this.created = created;
+  }
+
+  public Long getPublisherUserEntityId() {
+    return publisherUserEntityId;
+  }
+
+  public void setPublisherUserEntityId(Long sender) {
+    this.publisherUserEntityId = sender;
+  }
+  
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column (name = "publisherUserEntity_id")
+  private Long publisherUserEntityId;
+  
+  @NotNull
+  @NotEmpty
+  @Column (nullable = false, length = 1024)
+  private String caption;
+
+  @NotNull
+  @NotEmpty
+  @Column (nullable = false)
+  @Lob
+  private String content;
+
+  @NotNull
+  @Column (updatable=false, nullable=false)
+  @Temporal (value=TemporalType.TIMESTAMP)
+  private Date created;
+}
