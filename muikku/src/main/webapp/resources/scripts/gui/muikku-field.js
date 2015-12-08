c42840dccc819bb4db7616a2c72467c767247190
jjlankinen <jari.lankinen@otavanopisto.fi>
2015-12-08 08:58:26 +0200

    * Adjusted exercise's show/hide correct answers buttons state changes

--- muikku/src/main/webapp/resources/scripts/gui/muikku-field.js
+++ muikku/src/main/webapp/resources/scripts/gui/muikku-field.js
@@ -199,17 +199,14 @@
       var showAnswersButton = stateOptions['show-answers-button-visible'] && this.correctAnswers() == 'ON_REQUEST' && this._hasDisplayableAnswers();
       if (showAnswersButton) {
         
-        var correctAnswersButton = this.element.find('.muikku-hide-correct-answers-button');  
-        if (correctAnswersButton) {
-          $(correctAnswersButton)
+        this.element.find('.muikku-hide-correct-answers-button, .muikku-show-correct-answers-button')  
           .removeClass("muikku-hide-correct-answers-button")
           .addClass("muikku-show-correct-answers-button")
           .text(getLocaleText('plugin.workspace.materialsLoader.showAnswers'));
-        }
-          
+        
         this.element.find('.muikku-show-correct-answers-button').show();
       } else {
-        this.element.find('.muikku-show-correct-answers-button').hide();
+        this.element.find('.muikku-hide-correct-answers-button, .muikku-show-correct-answers-button').hide();
       }
       
       this.workspaceMaterialState(state);
@@ -599,14 +596,14 @@
 
       $(document).connectionLostNotifier("notifyReconnected");
       
-      var page = $(this.element).closest('.workspace-materials-view-page');
-      var hideAnswersButton = $(page).find('.muikku-hide-correct-answers-button');
-      if ($(hideAnswersButton).length > 0) {
-        $(hideAnswersButton)
-          .addClass("muikku-show-correct-answers-button")
-          .removeClass("muikku-hide-correct-answers-button")
-          .text(getLocaleText('plugin.workspace.materialsLoader.showAnswers'));
-      }
+//      var page = $(this.element).closest('.workspace-materials-view-page');
+//      var hideAnswersButton = $(page).find('.muikku-hide-correct-answers-button');
+//      if ($(hideAnswersButton).length > 0) {
+//        $(hideAnswersButton)
+//          .addClass("muikku-show-correct-answers-button")
+//          .removeClass("muikku-hide-correct-answers-button")
+//          .text(getLocaleText('plugin.workspace.materialsLoader.showAnswers'));
+//      }
 
       // TODO: Shouldn't this be workspaceMaterialId insteadOf materialId?
       if ((message.embedId == this.embedId()) && (message.materialId == this.materialId()) && (message.fieldName == this.fieldName())) {
