c86d9e9e5a1f7771f83f4a974f311409bbeafff3
ilmo.euro <ilmo.euro@gmail.com>
2015-12-15 09:42:47 +0200

    ** Entity+DAO for announcement user group

---
+++ muikku-core-plugins/src/main/java/fi/muikku/plugins/announcer/dao/AnnouncementUserGroupDAO.java
@@ -0,0 +1,50 @@
+package fi.muikku.plugins.announcer.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import fi.muikku.plugins.CorePluginsDAO;
+import fi.muikku.plugins.announcer.model.Announcement;
+import fi.muikku.plugins.announcer.model.AnnouncementUserGroup;
+import fi.muikku.plugins.announcer.model.AnnouncementUserGroup_;
+
+public class AnnouncementUserGroupDAO extends CorePluginsDAO<AnnouncementUserGroup> {
+	
+  private static final long serialVersionUID = -8721990589622544635L;
+  
+  public AnnouncementUserGroup create(
+      Announcement announcement,
+      Long userGroupEntityId,
+      boolean archived
+  ) {
+    AnnouncementUserGroup announcementUserGroup = new AnnouncementUserGroup();
+    announcementUserGroup.setAnnouncement(announcement);
+    announcementUserGroup.setUserGroupEntityId(userGroupEntityId);
+    announcementUserGroup.setArchived(true);
+    
+    return persist(announcementUserGroup);
+ }
+  
+  public void archive(AnnouncementUserGroup announcementUserGroup) {
+    if(announcementUserGroup != null){
+      announcementUserGroup.setArchived(true);
+      getEntityManager().persist(announcementUserGroup);
+    }
+  }
+  
+  public List<AnnouncementUserGroup> listUnarchivedByAnnouncement(boolean archived){
+    EntityManager entityManager = getEntityManager();
+    
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<AnnouncementUserGroup> criteria = criteriaBuilder.createQuery(AnnouncementUserGroup.class);
+    Root<AnnouncementUserGroup> root = criteria.from(AnnouncementUserGroup.class);
+    criteria.select(root);
+    criteria.where(criteriaBuilder.equal(root.get(AnnouncementUserGroup_.archived), archived));
+    
+    return entityManager.createQuery(criteria).getResultList();
+  }
+}
