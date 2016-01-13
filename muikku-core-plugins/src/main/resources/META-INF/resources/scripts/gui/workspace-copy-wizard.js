e139148d205a7b0bd78de285d9757a5ae9007e70
Antti Leppä <antti.leppa@foyt.fi>
2016-01-13 10:01:28 +0200

    ** Moved workspace copy wizard into course picker ** Added initial support for copying workspaces into workspace copy wizard

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-copy-wizard.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-copy-wizard.js
@@ -83,13 +83,17 @@
       }, this));
     },
     
+    _getPage: function (pageId) {
+      return this._dialog
+        .find('.wizard-page[data-page-id="' + pageId + '"]')
+    },
+    
     _showPage: function (pageId) {
       this._dialog
         .find('.progress-pages').find('li[data-page-id="' + pageId + '"]')
         .show();
       
-      this._dialog
-        .find('.wizard-page[data-page-id="' + pageId + '"]')
+      this._getPage(pageId)
         .addClass('wizard-page-visible');
       
       this._updateButtons();
@@ -224,7 +228,7 @@
             .find('.summary li[data-id="' + stepId + '"]')
             .addClass('inProgress');
           
-          this.options.steps[stepId]($.proxy(function (err, result) {
+          this.options.steps[stepId].call(this, $.proxy(function (err, result) {
             $(this._dialog)
               .find('.summary li[data-id="' + stepId + '"]')
               .removeClass('inProgress')
@@ -243,23 +247,6 @@
       }, this));
     }
     
-  });
-  
-  $(document).ready(function () {
-    $('<div>').workspaceCopyWizard({
-      steps: {
-        'copy-course': function (callback) {
-          setTimeout(function () {
-            callback(null, {});
-          }, 3000);
-        },
-        'copy-materials': function (callback) {
-          setTimeout(function () {
-            callback('Errori', {});
-          }, 1000);
-        }
-      }
-    });
   });
   
 }).call(this);
