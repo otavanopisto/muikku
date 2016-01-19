46bc4f8bf9a3e8444cd11cc0fb665097d7631863
ilmo.euro <ilmo.euro@gmail.com>
2016-01-19 08:24:57 +0200

    ** Mock course education types in tests

--- muikku-atests/src/test/java/fi/muikku/mock/PyramusMock.java
+++ muikku-atests/src/test/java/fi/muikku/mock/PyramusMock.java
@@ -308,6 +308,22 @@ public class PyramusMock {
         return this;
       }
       
+      public Builder mockCourseEducationTypes() throws JsonProcessingException, Exception {
+        
+        stubFor(get(urlMatching("/1/courses/\\d+/educationTypes"))
+          .willReturn(aResponse()
+              .withHeader("Content-Type", "application/json")
+              .withStatus(404)));
+
+        
+        stubFor(get(urlMatching("/1/courses/\\d+/educationTypes/\\d+/subtypes"))
+          .willReturn(aResponse()
+              .withHeader("Content-Type", "application/json")
+              .withStatus(404)));
+        
+        return this;
+      }
+      
       public Builder mockCourseTypes() throws JsonProcessingException {
         stubFor(get(urlEqualTo("/1/courses/courseTypes"))
           .willReturn(aResponse()
@@ -662,6 +678,7 @@ public class PyramusMock {
         mockEducationalTimeUnits();
         mockEducationTypes();
         mockSubjects();
+        mockCourseEducationTypes();
         mockCourseTypes();
         mockCourseStaffMembers();
         mockCourseStudents();
