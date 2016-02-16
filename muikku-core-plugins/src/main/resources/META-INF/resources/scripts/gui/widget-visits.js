cad66ace0ce74b75dd4d40b24774970d4a8ca764
jjlankinen <jari.lankinen@otavanopisto.fi>
2016-02-16 13:07:11 +0200

    * Added nameExgtension to frontpage visited widget * Reformatted widget-visits.js file as it was abysmal to read!

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/widget-visits.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/widget-visits.js
@@ -1,104 +1,95 @@
- 
-$(document).ready(function(){
-    
+$(document).ready(function() {
 
-	
-	
-    mApi({async: false}).workspace.workspaces
-    .read({ minVisits: 1, orderBy: "lastVisit" }).on('$', function(workspace, workspaceCallback){
+  mApi({
+    async : false
+  }).workspace.workspaces.read({
+    minVisits : 1,
+    orderBy : "lastVisit"
+  }).on('$', function(workspace, workspaceCallback) {
 
-    	 var td = new Date();
-    	 
-    	 // 1 h == 3600 seconds 
-    	  
-    	 var dayScs =  24 * 3600;
-    	 var weekScs = (24 * 3600) * 7;
- 
-    	 
-    	// Turning timestamps (visited, now) from milliseconds to seconds 
-    	 
-       	 var visitScs = eval(workspace.lastVisit / 1000); 
-    	 var nowScs = eval(td.getTime() / 1000);
-       
-        // Defining the last day and last week startpoints	 
-    	 
-    	 var lDaySec = eval(nowScs - dayScs);
-    	 var lWeekSec = eval(nowScs - weekScs);
-    	 
-       // Visit day daystamp to date
-    	 
-    	 var vdt = new Date(visitScs);
-       
-    	 
-      // Comparing visitday to last day and week startpoints 
-    	 
-    	 if(visitScs > lDaySec){
-   		  workspace.lastDay = true;		 
-    	 }else if(visitScs > lWeekSec){
-      	  workspace.lastWeek = true;    		 
-       	 }
-    	 
-     // Setting date to String 
-    	 
- 		  workspace.trueDat = vdt.toDateString();
- 		  workspaceCallback();
-    	
- 		  
- 		 
-    })
-    .callback( function (err, workspaces) {
-    	
-    	if( err ){
-            $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('TODO: Virheilmoitus', err));
-    	}else{
-    		
-     // Empty arrays foe visiting time categories to present to the dust templates
-    		
-		var lastDay = [];
-		var lastWeek = [];
-		var older = []	
+    var td = new Date();
 
-		
-		if(workspaces != null){
-    		
-	     	for (var i = 0, l = workspaces.length; i < l; i++) {
-	     	  var ld = workspaces[i].lastDay;
-	     	  var lw = workspaces[i].lastWeek;
-	     	  
-	    // sorting the given workspaces into said categories     
-	     	  
-	
-	     	  
-	     	   if( ld == true ){
-	     		var ldl = lastDay.length; 
-	     		lastDay[ldl] = workspaces[i];
-	     	   }else if( lw == true ){
-	     	     var lwl = lastWeek.length;
-	        	 lastWeek[lwl] = workspaces[i];     			 
-	     	   }else{
-	     		 var ol = older.length;
-	        	 older[ol] = workspaces[i];     			 
-	        	 }
-	     	   
-	          
-	     	}    		
-     	
-		}
+    // 1 h == 3600 seconds 
 
-    	renderDustTemplate('frontpage/widget_visits.dust', {
-        	
-        	workspaces :  workspaces,
-          lastDay : lastDay,
-        	lastWeek : lastWeek,
-        	older : older
-             
-        }, function (text) {
-            $('#widgetVisits').append($.parseHTML(text));
-            $('#widgetVisits').perfectScrollbar({"suppressScrollX" : true});
-          });
-        
-    	}
-    });
-    
-	});
+    var dayScs = 24 * 3600;
+    var weekScs = (24 * 3600) * 7;
 
+    // Turning timestamps (visited, now) from milliseconds to seconds 
+
+    var visitScs = eval(workspace.lastVisit / 1000);
+    var nowScs = eval(td.getTime() / 1000);
+
+    // Defining the last day and last week startpoints	 
+
+    var lDaySec = eval(nowScs - dayScs);
+    var lWeekSec = eval(nowScs - weekScs);
+
+    // Visit day daystamp to date
+
+    var vdt = new Date(visitScs);
+
+    // Comparing visitday to last day and week startpoints 
+
+    if (visitScs > lDaySec) {
+      workspace.lastDay = true;
+    } else if (visitScs > lWeekSec) {
+      workspace.lastWeek = true;
+    }
+
+    // Setting date to String 
+
+    workspace.trueDat = vdt.toDateString();
+    workspaceCallback();
+
+  }).callback(function(err, workspaces) {
+
+    if (err) {
+      $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('TODO: Virheilmoitus', err));
+    } else {
+
+      // Empty arrays foe visiting time categories to present to the dust templates
+
+      var lastDay = [];
+      var lastWeek = [];
+      var older = []
+
+      if (workspaces != null) {
+
+        for (var i = 0, l = workspaces.length; i < l; i++) {
+          var ld = workspaces[i].lastDay;
+          var lw = workspaces[i].lastWeek;
+
+          // sorting the given workspaces into said categories     
+
+          if (ld == true) {
+            var ldl = lastDay.length;
+            lastDay[ldl] = workspaces[i];
+          } else if (lw == true) {
+            var lwl = lastWeek.length;
+            lastWeek[lwl] = workspaces[i];
+          } else {
+            var ol = older.length;
+            older[ol] = workspaces[i];
+          }
+
+        }
+
+      }
+
+      renderDustTemplate('frontpage/widget_visits.dust', {
+        workspaces : workspaces,
+        lastDay : lastDay,
+        lastWeek : lastWeek,
+        older : older
+
+      }, function(text) {
+        $('#widgetVisits').append($.parseHTML(text));
+        $('#widgetVisits').perfectScrollbar({
+          "suppressScrollX" : true
+        });
+      });
+
+    }
+  });
+
+});
