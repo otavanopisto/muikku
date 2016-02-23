e31b99eb798f2ceef531925e236a136dfa2d24ab
ilmo.euro <ilmo.euro@gmail.com>
2016-02-23 09:56:10 +0200

    ** A solution that works for different database names

--- updates/1.0100/20160223_communicator_index.sql
+++ updates/1.0100/20160223_communicator_index.sql
@@ -1,12 +1,2 @@
--- http://dba.stackexchange.com/a/62997
-select if (
-    exists(
-        select distinct index_name from information_schema.statistics 
-        where table_schema = 'muikku'
-        and table_name = 'communicatormessage' and index_name like 'sender_id_idx'
-    )
-    ,'select ''index sender_id_idx exists'' _______;'
-    ,'create index sender_id_idx on communicatormessage(sender_id)') into @a;
-PREPARE stmt1 FROM @a;
-EXECUTE stmt1;
-DEALLOCATE PREPARE stmt1;
+DROP INDEX IF EXISTS sender_id_idx ON communicatormessage;
+CREATE INDEX sender_id_idx ON communicatormessage (sender_id);
