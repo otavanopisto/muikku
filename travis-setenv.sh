8941ab6c77e382f98059a7b787dad09e7e0a36f5
petja-laitila <petja.laitila@otavanopisto.fi>
2016-03-15 12:43:24 +0200

    ** Environment setup

--- travis-setenv.sh
+++ travis-setenv.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
-export run_tests=true
+export run_tests=false
 export start_sc_tunnel=false
-#export deploy=false
+export deploy=false
 export rc=false
-export devel_merge=false
-#export release=false
+export release=false
+export test_suite=false
 
-#if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
-#  export run_tests=true
-#  if [[ $TRAVIS_BRANCH == "master" ]]; then
-#    export rc=true
-#  fi;
-#else
-#  if [[ $TRAVIS_BRANCH == "master" ]]; then
-#    export release=true
-#  fi;
-#fi;
-
-if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == 'devel' ]]; then
-  export devel_merge=true
-  export start_sc=true
+if [[ $TRAVIS_PULL_REQUEST != "false" ]] && [[ $TRAVIS_BRANCH == "devel" ]] then;
+  export $test_suite="phantom"
+  export $run_tests=true
 fi;
 
-if [[ $use_sc == "true" ]] && [[ $run_tests == "true" ]]; then
-  export start_sc_tunnel=true
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "devel" ]] then;
+  export $test_suite="phantom"
+  export $run_tests=true
 fi;
 
-#if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "devel" ]]; then
-#  export deploy=true
-#fi;
+if [[ $TRAVIS_PULL_REQUEST != "false" ]] && [[ $TRAVIS_BRANCH == "master" ]] then;
+  export $test_suite="full"
+  export $run_tests=true
+  export $rc=true
+  export $start_sc_tunnel=true
+  export deploy=true
+fi;
 
-echo "Test setup: run tests: $run_tests, start sauce tunnel: $start_sc_tunnel, deploy: $deploy, rc: $rc, release: $release"
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]] then;
+  commitmessage=`git log --pretty=format:"%s" -1`;
+  if [[ ($commitmessage == *"Merge pull request"*) && ($commitmessage == *"from muikku/devel"*) ]]; 
+    export release=true
+fi;
+
+echo "Test setup: run tests: $run_tests, test suite: $test_suite, start sauce tunnel: $start_sc_tunnel, deploy: $deploy, rc: $rc, release: $release"
+
