c37ce39781aeffbfceb8136727db55f6c6499d97
petja-laitila <petja.laitila@otavanopisto.fi>
2016-03-15 14:30:29 +0200

    ** Moving build process along

--- travis-install.sh
+++ travis-install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Staging build and PRs
 # is devel branch and is PR or rest-it profile
-if [[ ( "$TRAVIS_BRANCH" == "devel" ) && ( "$TRAVIS_PULL_REQUEST" != "false" || "$it_profile" == "rest-it" ) ]]; then
-  mvn install -Pmongo-log-plugin,jndi-mail-plugin,pyramus-plugins,elastic-search-plugin,atests-plugin,evaluation-plugin -Dfindbugs.skip=$findbugs_skip -Dmaven.javadoc.skip=true -Dsource.skip=true
+if [[ "$TRAVIS_BRANCH" == "devel" ]] && [[ "$TRAVIS_PULL_REQUEST" != "false" || "$it_profile" == "rest-it" ]]; then
+ echo "Installoi testiä varten"
+ # mvn install -Pmongo-log-plugin,jndi-mail-plugin,pyramus-plugins,elastic-search-plugin,atests-plugin,evaluation-plugin -Dfindbugs.skip=$findbugs_skip -Dmaven.javadoc.skip=true -Dsource.skip=true
+fi;
+if [[ $TRAVIS_BRANCH = "master" && TRAVIS_PULL_REQUEST != "false" && $it_profile == "rest-it"  ]]; then
+  echo "Installoi RC testiä varten"
 fi;
 # Master build
 # Master branch, not PR and is rest profile
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" && "$it_profile" == "rest-it" ]]; then
-  mvn install -Dfindbugs.skip=$findbugs_skip -Dmaven.javadoc.skip=true -Dsource.skip=true
+ echo "Installoi master asetuksilla"
+ #mvn install -Dfindbugs.skip=$findbugs_skip -Dmaven.javadoc.skip=true -Dsource.skip=true
 fi;
--- travis-script.sh
+++ travis-script.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-if [[ "$TRAVIS_BRANCH" != "devel" ]]; then
-  echo 'Branch is not devel, exiting.'
-  exit 0
-fi;
+mvn clean
 if [[ "$TRAVIS_PULL_REQUEST" != "false" || "$it_profile" == "rest-it" ]]; then
   echo PR : $TRAVIS_PULL_REQUEST , it_profile: $it_profile
   pushd .;
   cd muikku-atests;
   set -e; 
-  mvn clean verify -Dmaven.javadoc.skip=true -Dsource.skip=true -Dit.sauce.browser="$browser" -Dit.sauce.browser.version="$browser_version" -Dit.sauce.browser.resolution="$browser_resolution" -Dit.sauce.platform="$platform" -Dit.package="$package" -P$it_profile;
+  mvn $goals -Dmaven.javadoc.skip=true -Dsource.skip=true -Dit.sauce.browser="$browser" -Dit.sauce.browser.version="$browser_version" -Dit.sauce.browser.resolution="$browser_resolution" -Dit.sauce.platform="$platform" -Dit.package="$package" -P$it_profile;
   set +e;
   popd;
 else
-  echo Skipping it_profile: $it_profile, PR: $TRAVIS_PULL_REQUEST , BRANCH: $TRAVIS_BRANCH
+  echo Skipping testing of it_profile: $it_profile, PR: $TRAVIS_PULL_REQUEST , BRANCH: $TRAVIS_BRANCH
 fi;
