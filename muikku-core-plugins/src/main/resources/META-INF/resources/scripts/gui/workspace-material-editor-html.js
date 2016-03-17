2e1c862893d6fff81fb9474cbdbf0f6297f92dd1
jjlankinen <jari.lankinen@otavanopisto.fi>
2016-03-17 13:20:01 +0200

    * Added create div button to ckeditor so content producers can create more immersive custom styled boxes * Adjusted custom styled boxes so they can be nested properly if needed * Adjusted block level elements so they can be nested properly inside custom styled elements

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-material-editor-html.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-material-editor-html.js
@@ -144,7 +144,7 @@
             { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
             '/',
             { name: 'styles', items : [ 'Styles','Format' ] },
-            { name: 'insert', items : [ 'Image','oembed','Mathjax','Table','Iframe','SpecialChar' ] },
+            { name: 'insert', items : [ 'Image','oembed','Mathjax','Table','Iframe','SpecialChar', 'CreateDiv' ] },
             { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
             { name: 'colors', items : [ 'TextColor','BGColor' ] },
             '/',
