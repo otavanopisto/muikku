b44ca764a928bc99b158ef41c637eadf4ac2cbea
petja-laitila <petja.laitila@otavanopisto.fi>
2016-03-21 10:01:16 +0200

    ew

--- travis-install.sh
+++ travis-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 if [[ $run_tests = "true" ]]; then
+  mvn clean
   mvn install -Pmongo-log-plugin,jndi-mail-plugin,pyramus-plugins,elastic-search-plugin,atests-plugin,evaluation-plugin -Dfindbugs.skip=$findbugs_skip -Dmaven.javadoc.skip=true -Dsource.skip=true
 fi;
