cbad9ba5fc88b4864ee41bfdf4c74879471b04a6
petja-laitila <petja.laitila@otavanopisto.fi>
2016-03-21 10:44:58 +0200

    ** Streamlining install

--- travis-install.sh
+++ travis-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ $run_tests = "true" ]]; then
+if [[ $run_tests = "true" && $test_suite = $suite || $run_tests = "true" && $it_profile = "rest-it" ]]; then
   mvn clean
   mvn install -Pmongo-log-plugin,jndi-mail-plugin,pyramus-plugins,elastic-search-plugin,atests-plugin,evaluation-plugin -Dfindbugs.skip=$findbugs_skip -Dmaven.javadoc.skip=true -Dsource.skip=true
 fi;
