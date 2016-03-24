412222c6df7fa629ee6805b5fbb4a601e6b388c2
petja-laitila <petja.laitila@otavanopisto.fi>
2016-03-24 13:45:15 +0200

    ** Added release_deploy to properly release different versions

--- travis-setenv.sh
+++ travis-setenv.sh
@@ -4,6 +4,7 @@ export start_sc_tunnel=false
 export release=false
 export test_suite=false
 export deploy_snapshot=false
+export deploy_release=false
 
 if [[ $TRAVIS_PULL_REQUEST != "false" && $TRAVIS_BRANCH == "devel" ]]; then
   export test_suite="phantom"
@@ -26,7 +27,14 @@ if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" && $it_profi
 fi;
 
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "devel" && $it_profile = "rest-it" ]]; then
-  export deploy_snapshot=true
+  export deploy_snapshot="true"
+fi;
+
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" && $it_profile = "rest-it" ]]; then
+  commitmessage=`git log --pretty=format:"%s" -1`;
+  if [[ ($commitmessage == *"[maven-release-plugin] prepare release"*) ]]; then 
+    export deploy_release="true"
+  fi;
 fi;
 
 echo "Test setup: run tests: $run_tests, test suite: $test_suite, start sauce tunnel: $start_sc_tunnel, release: $release"
