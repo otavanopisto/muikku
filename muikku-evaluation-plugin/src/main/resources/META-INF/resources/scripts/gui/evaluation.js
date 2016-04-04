b402ad7f762d071ae441f1824657a42ded668dd6
jjlankinen <jari.lankinen@otavanopisto.fi>
2016-04-04 13:31:20 +0300

    * Removed clickarea permanetly, student name has click event now

--- muikku-evaluation-plugin/src/main/resources/META-INF/resources/scripts/gui/evaluation.js
+++ muikku-evaluation-plugin/src/main/resources/META-INF/resources/scripts/gui/evaluation.js
@@ -1129,7 +1129,6 @@
       this.element.addClass('evaluation-student-wrapper');
       this.element.append($('<div>').addClass('evaluation-student-picture'));
       this.element.append($('<div>').addClass('evaluation-student-name').text(this._displayName).append($('<span>').text(this._studyProgrammeName)));
-//      this.element.append($('<div>').addClass('evaluation-student-clickarea'));
       this.element.prepend($('<div>').addClass('workspace-student-joined-date').text(this.options.enrollmentTime).attr("title", getLocaleText("plugin.evaluation.studentGrid.joined.label")));
       
       if (this.options.assessment) {
