121db00a900fbb88702a1459a2fdec209f85da99
jjlankinen <jari.lankinen@otavanopisto.fi>
2016-04-11 12:21:00 +0300

    * Added autosize to every textarea in materials, materials-reading and materials-management views

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/muikku-material-loader.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/muikku-material-loader.js
@@ -935,6 +935,9 @@
     $(data.pageElement).find('.js-lazyyt').lazyYT();
     
     $(data.pageElement).find('.ckeditor-field').muikkuRichMemoField();
+    
+    /* Add autosize to textarea */
+    autosize($('textarea'));
 
     var maxFileSize = null;
     if ($("input[name='max-file-size']").length) {
