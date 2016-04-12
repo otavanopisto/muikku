2f45f363397b37d66804a9ca9229b3f5dd1da846
jjlankinen <jari.lankinen@otavanopisto.fi>
2016-04-12 14:19:37 +0300

    * Removed unnecessary autosize script * Added check if autosize function is present

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/muikku-material-loader.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/muikku-material-loader.js
@@ -937,7 +937,9 @@
     $(data.pageElement).find('.ckeditor-field').muikkuRichMemoField();
     
     /* Add autosize to textarea */
-    autosize($('textarea'));
+    if ((typeof autosize) == 'function') {
+      autosize($('textarea'));
+    }
 
     var maxFileSize = null;
     if ($("input[name='max-file-size']").length) {
