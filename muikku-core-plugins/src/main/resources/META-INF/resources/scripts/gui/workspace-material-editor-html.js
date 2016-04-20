ec530d716292bc4c4b077668cdc615bb06e84fdc
Antti Leppä <antti.leppa@foyt.fi>
2016-04-20 14:02:53 +0300

    Added CKEditor plugin for changing audio plugin defaults

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-material-editor-html.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-material-editor-html.js
@@ -116,7 +116,8 @@
           'muikku-filefield': CONTEXTPATH + '/scripts/ckplugins/muikku-filefield/',
           'muikku-connectfield': CONTEXTPATH + '/scripts/ckplugins/muikku-connectfield/',
           'muikku-embedded': CONTEXTPATH + '/scripts/ckplugins/muikku-embedded/',
-          'muikku-image-details': CONTEXTPATH + '/scripts/ckplugins/muikku-image-details/'
+          'muikku-image-details': CONTEXTPATH + '/scripts/ckplugins/muikku-image-details/',
+          'muikku-audio-defaults': CONTEXTPATH + '/scripts/ckplugins/muikku-audio-defaults/'
         },
         extraPlugins : [
                        'oembed',
@@ -133,6 +134,7 @@
                        'muikku-connectfield',
                        'muikku-embedded',
                        'muikku-image-details',
+                       'muikku-audio-defaults',
                        'mathjax',
                        'autogrow',
                        'uploadimage'],
