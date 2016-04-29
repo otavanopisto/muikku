2c341681eaabd8ce8ad1b07a59400fd7b12a8419
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2016-04-29 14:59:28 +0300

    Message edited info to messages & replies Some style tuning

--- muikku/src/main/webapp/resources/scripts/gui/discussion.js
+++ muikku/src/main/webapp/resources/scripts/gui/discussion.js
@@ -131,10 +131,11 @@ $(document).ready(function() {
           thread.fromView = fromView;
           mApi({async: false}).user.users.basicinfo.read(thread.creator).callback($.proxy(function(err, user) {
             thread.creatorFullName = user.firstName + ' ' + user.lastName;
+            thread.isEdited = thread.lastModified == thread.created ? false : true;
             var d = new Date(thread.created);
-            var ud = new Date(thread.updated);          
+            var ld = new Date(thread.lastModified);          
             thread.prettyDate = formatDate(d) + ' ' + formatTime(d);
-            thread.prettyDateUpdated = formatDate(ud) + ' ' + formatTime(ud);
+            thread.prettyDateModified = formatDate(ld) + ' ' + formatTime(ld);
             thread.canEdit = thread.creator === MUIKKU_LOGGED_USER_ID ? true : false;
             threadCallback();          
           }, this));
@@ -303,8 +304,11 @@ $(document).ready(function() {
           mApi({async: false}).user.users.basicinfo.read(replies.creator).callback($.proxy(function(err, user) {
               replies.creatorFullName = user.firstName + ' ' + user.lastName;
               var d = new Date(replies.created);
-              replyCreatedMap[replies.id] = d;
+              var ld = new Date(replies.lastModified);                    
+              replyCreatedMap[replies.id] = d;                  
               replies.prettyDate = formatDate(d) + ' ' + formatTime(d);
+              replies.prettyDateModified = formatDate(ld) + ' ' + formatTime(ld);              
+              replies.isEdited = replies.lastModified == replies.created ? false : true;
               replies.canEdit = replies.creator === MUIKKU_LOGGED_USER_ID ? true : false;
               replies.userRandomNo = Math.floor(Math.random() * 6) + 1;
               replies.nameLetter = user.firstName.substring(0,1);
@@ -340,9 +344,12 @@ $(document).ready(function() {
           thread.fromView = from;
           mApi({async: false}).user.users.basicinfo.read(thread.creator).callback($.proxy(function(err, user) {
             thread.creatorFullName = user.firstName + ' ' + user.lastName;
-            thread.canEdit = thread.creator === MUIKKU_LOGGED_USER_ID ? true : false;
+            thread.isEdited = thread.lastModified == thread.created ? false : true;
             var d = new Date(thread.created);
+            var ld = new Date(thread.lastModified);          
             thread.prettyDate = formatDate(d) + ' ' + formatTime(d);
+            thread.prettyDateModified = formatDate(ld) + ' ' + formatTime(ld);
+            thread.canEdit = thread.creator === MUIKKU_LOGGED_USER_ID ? true : false;            
             thread.userRandomNo = Math.floor(Math.random() * 6) + 1;
             thread.nameLetter = user.firstName.substring(0,1);
             threadCallback();
@@ -376,10 +383,13 @@ $(document).ready(function() {
           replies.areaName = area.name;
           mApi({async: false}).user.users.basicinfo.read(replies.creator).callback($.proxy(function(err, user) {
             replies.creatorFullName = user.firstName + ' ' + user.lastName;
+            replies.isEdited = replies.lastModified == replies.created ? false : true;            
             replies.canEdit = replies.creator === MUIKKU_LOGGED_USER_ID ? true : false;
             var d = new Date(replies.created);
-            replyCreatedMap[replies.id] = d;
+            var ld = new Date(replies.lastModified);                    
+            replyCreatedMap[replies.id] = d;                  
             replies.prettyDate = formatDate(d) + ' ' + formatTime(d);
+            replies.prettyDateModified = formatDate(ld) + ' ' + formatTime(ld);                    
             replies.threadId = threadId;
             replies.userRandomNo = Math.floor(Math.random() * 6) + 1;
             replies.nameLetter = user.firstName.substring(0,1);
