c551012dd10bfa2acc5cc6f13f49ea41e991ee1b
pkukkon <pasi.kukkonen@otavanopisto.fi>
2016-04-29 08:14:45 +0300

    - journal test fix

--- muikku-atests/src/test/java/fi/otavanopisto/muikku/ui/base/course/journal/CourseJournalPageTestsBase.java
+++ muikku-atests/src/test/java/fi/otavanopisto/muikku/ui/base/course/journal/CourseJournalPageTestsBase.java
@@ -91,7 +91,7 @@ public class CourseJournalPageTestsBase extends AbstractUITest {
         click(".workspace-journal-new-entry-button");
         addTextToCKEditor("content");
         sendKeys(".mf-textfield-subject", "title");
-        click("input[type='button'][value='Create']");
+        click("input[type='button'][name='send']");
         waitForPresent("#content");
         assertText(".workspace-journal-title", "title");
         assertText(".workspace-journal-content", "content");
