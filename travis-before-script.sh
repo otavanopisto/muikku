01f7926dfadbdf1abb2549660af25efa09fd79f1
petja-laitila <petja.laitila@otavanopisto.fi>
2016-05-09 11:52:33 +0300

    ** Use java8 on tests ** Temporary echo for phantomjs download

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -15,11 +15,14 @@ if [[ $run_tests == "true" ]]; then
     done;
   fi;
   if [[ $browser == "phantomjs" && $test_suite == "phantom" ]]; then
-    if [[ ! -f /home/travis/build/otavanopisto/muikku/muikku-atests/.phantomjs/bin/phantomjs ]]; then
+    if [[ ! -f muikku-atests/.phantomjs/bin/phantomjs ]]; then
+      echo "PhantomJS not found. Downloading..."
       rm -fR muikku-atests/.phantomjs
       wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2 & wait
       tar -jxf phantomjs-1.9.8-linux-x86_64.tar.bz2 & wait
       mv phantomjs-1.9.8-linux-x86_64 muikku-atests/.phantomjs
+    else
+      echo "Found PhantomJS"
     fi;
   fi;
 fi;
