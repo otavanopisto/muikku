5f2d1820f7c441deda6bfd949d67a563e3d875ec
petja-laitila <petja.laitila@otavanopisto.fi>
2016-05-12 13:29:48 +0300

    ** Forces tests to fail if no tests are found

--- travis-script.sh
+++ travis-script.sh
@@ -3,7 +3,7 @@ if [[ $run_tests = "true" && $test_suite = $suite || $run_tests = "true" && $it_
   pushd .;
   cd muikku-atests;
   set -e;
-  mvn $goals -Dmaven.javadoc.skip=true -Dsource.skip=true -Dit.browser="$browser" -Dit.browser.version="$browser_version" -Dit.browser.resolution="$browser_resolution" -Dit.platform="$platform" -Dit.package="$package" -P$it_profile;
+  mvn $goals -Dmaven.javadoc.skip=true -DfailIfNoTests=True -Dsource.skip=true -Dit.browser="$browser" -Dit.browser.version="$browser_version" -Dit.browser.resolution="$browser_resolution" -Dit.platform="$platform" -Dit.package="$package" -P$it_profile;
   set +e;
   popd;
 else
