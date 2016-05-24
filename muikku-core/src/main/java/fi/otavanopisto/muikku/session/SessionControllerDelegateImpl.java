6bb8333e20c246feaed9e60cab9d60e0a39e7685
Antti Leppä <antti.leppa@foyt.fi>
2016-05-24 12:02:17 +0300

    muikkuSession -> sessionBackingBean

--- muikku-core/src/main/java/fi/otavanopisto/muikku/session/SessionControllerDelegateImpl.java
+++ muikku-core/src/main/java/fi/otavanopisto/muikku/session/SessionControllerDelegateImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Locale;
 
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
 
 import fi.otavanopisto.muikku.model.users.UserEntity;
 import fi.otavanopisto.muikku.model.util.ResourceEntity;
@@ -14,7 +13,6 @@ import fi.otavanopisto.muikku.schooldata.SchoolDataIdentifier;
 import fi.otavanopisto.security.ContextReference;
 
 @RequestScoped 
-@Named ("muikkuSession")
 public class SessionControllerDelegateImpl implements SessionControllerDelegate {
 
   @Override
