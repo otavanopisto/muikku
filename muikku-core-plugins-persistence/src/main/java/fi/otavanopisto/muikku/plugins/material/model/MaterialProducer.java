53d117d6bf5c58f324ea2cf5b94ba42483998b67
Antti Leppä <antti.leppa@foyt.fi>
2016-06-08 07:16:00 +0300

    Added persistent entities for material producers

---
+++ muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/material/model/MaterialProducer.java
@@ -0,0 +1,49 @@
+package fi.otavanopisto.muikku.plugins.material.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class MaterialProducer {
+  
+  public Long getId() {
+    return id;
+  }
+  
+  public void setId(Long id) {
+    this.id = id;
+  }
+  
+  public Material getMaterial() {
+    return material;
+  }
+  
+  public void setMaterial(Material material) {
+    this.material = material;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  @ManyToOne
+  private Material material;
+
+  @NotNull
+  @NotEmpty
+  private String name;
+}
