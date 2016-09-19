ce8f6795a9800b87fde2ff3124195cfab19bfa1e
pkukkon <pasi.kukkonen@otavanopisto.fi>
2016-09-19 13:10:09 +0300

    - interceptor changes

--- muikku-core/src/main/java/fi/otavanopisto/muikku/schooldata/RestCatchSchoolDataExceptionsInterceptor.java
+++ muikku-core/src/main/java/fi/otavanopisto/muikku/schooldata/RestCatchSchoolDataExceptionsInterceptor.java
@@ -12,18 +12,16 @@ import javax.ws.rs.core.Response.Status;
 @RestCatchSchoolDataExceptions
 public class RestCatchSchoolDataExceptionsInterceptor implements Serializable {
   
-  private static final long serialVersionUID = 1L;
-
+  private static final long serialVersionUID = 7030618913912374869L;
+  
   @AroundInvoke
-  public Object aroundInvoke(
-      InvocationContext invocationContext
-  ) throws Exception {
+  public Object aroundInvoke(InvocationContext invocationContext) throws Exception {
     try {
       return invocationContext.proceed();
     } catch (SchoolDataBridgeUnauthorizedException ex) {
-      return Response.status(Status.FORBIDDEN).entity(ex.getMessage()).build();
+      return Response.status(Status.FORBIDDEN).build();
     } catch (SchoolDataBridgeException ex) {
-      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+      return Response.status(Status.INTERNAL_SERVER_ERROR).build();
     } 
   }
 }
