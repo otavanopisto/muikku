9c720e51821f0dcfe1263760bccd2066757d6d6e
pkukkon <pasi.kukkonen@otavanopisto.fi>
2016-09-21 10:53:18 +0300

    - optimize imports

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/websocket/WebSocketMessenger.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/websocket/WebSocketMessenger.java
@@ -1,7 +1,6 @@
 package fi.otavanopisto.muikku.plugins.websocket;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
