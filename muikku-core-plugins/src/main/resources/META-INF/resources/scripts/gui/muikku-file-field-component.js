5d6c2a7a1103bd4d01bc2c2c81fe0edd4e697525
lankinen <lankinen@derangedpineapple.oopisto.local>
2016-09-29 14:05:07 +0300

    ** Added disabvled styles to connect, organizer and sorter fields

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/muikku-file-field-component.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/muikku-file-field-component.js
@@ -268,6 +268,7 @@
       }
       else {
         $(this._uploader).removeAttr("disabled");
+        $(this._uploader).closest(".muikku-file-input-field-file-uploader-container").removeAttr("data-readonly");
         $(this._uploaderContainer).find('.muikku-file-input-field-description').html(getLocaleText('plugin.workspace.fileField.fieldHint'))
         $(this._uploaderContainer).find('.muikku-file-input-field-description').show();
         $(this._uploaderContainer).find('.muikku-file-input-field-file-remove').show();
