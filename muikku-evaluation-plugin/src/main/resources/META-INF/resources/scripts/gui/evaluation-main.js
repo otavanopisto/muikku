6264c8dc0f801c4d2d4852b7b3997bde36848922
pkukkon <pasi.kukkonen@otavanopisto.fi>
2016-10-13 14:35:44 +0300

    - sorting

--- muikku-evaluation-plugin/src/main/resources/META-INF/resources/scripts/gui/evaluation-main.js
+++ muikku-evaluation-plugin/src/main/resources/META-INF/resources/scripts/gui/evaluation-main.js
@@ -20,6 +20,10 @@
             $('.notification-queue').notificationQueue('notification', 'error', err);
           }
           else {
+            // Default sort by lowest assessment request date first
+            assessmentRequests.sort(function (a, b) {
+              return Date.parse(a.assessmentRequestDate) > Date.parse(b.assessmentRequestDate);
+            });
             for (var i = 0; i < assessmentRequests.length; i++) {
               renderDustTemplate("evaluation/evaluation-request-card.dust", assessmentRequests[i], $.proxy(function (html) {
                 $(requestContainer).append(html);
@@ -37,6 +41,46 @@
   $(document).on('click', '.evaluate-button', function (event) {
     var requestCard = event.target.closest('.evaluation-request');
     $(document).evaluationModal('open', requestCard);
+  });
+  
+  // Sort by assessment request date, ascending
+  $(document).on('click', '.icon-sort-amount-asc', function (event) {
+    $('.eval-sorting').removeClass('selected');
+    $(event.target).addClass('selected');
+    var cards = $('.evaluation-request').sort(function (a,b) {
+      return Date.parse($(a).attr('data-assessment-request-date')) > Date.parse($(b).attr('data-assessment-request-date')); 
+    });
+    $('.evaluation-requests-container').html(cards);
+  });
+
+  // Sort by assessment request date, descending
+  $(document).on('click', '.icon-sort-amount-desc', function (event) {
+    $('.eval-sorting').removeClass('selected');
+    $(event.target).addClass('selected');
+    var cards = $('.evaluation-request').sort(function (a,b) {
+      return Date.parse($(a).attr('data-assessment-request-date')) < Date.parse($(b).attr('data-assessment-request-date')); 
+    });
+    $('.evaluation-requests-container').html(cards);
+  });
+
+  // Sort by student name, ascending
+  $(document).on('click', '.icon-sort-alpha-asc', function (event) {
+    $('.eval-sorting').removeClass('selected');
+    $(event.target).addClass('selected');
+    var cards = $('.evaluation-request').sort(function (a,b) {
+      return $(a).find('.evaluation-request-student').text() > $(b).find('.evaluation-request-student').text(); 
+    });
+    $('.evaluation-requests-container').html(cards);
+  });
+
+  // Sort by student name, descending
+  $(document).on('click', '.icon-sort-alpha-desc', function (event) {
+    $('.eval-sorting').removeClass('selected');
+    $(event.target).addClass('selected');
+    var cards = $('.evaluation-request').sort(function (a,b) {
+      return $(a).find('.evaluation-request-student').text() < $(b).find('.evaluation-request-student').text(); 
+    });
+    $('.evaluation-requests-container').html(cards);
   });
   
 }).call(this);
