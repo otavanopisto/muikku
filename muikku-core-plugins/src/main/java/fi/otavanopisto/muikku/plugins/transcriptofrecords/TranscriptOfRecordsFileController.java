1dfc007b30f28f7113fb36a534fb97877ace4846
ilmo.euro <ilmo.euro@gmail.com>
2016-10-18 15:28:32 +0300

    ** Attachment files json data attribute in ToR

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/transcriptofrecords/TranscriptOfRecordsFileController.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/transcriptofrecords/TranscriptOfRecordsFileController.java
@@ -53,7 +53,7 @@ public class TranscriptOfRecordsFileController {
     
     transcriptOfRecordsFileDAO.create(
       student,
-      file.getAbsolutePath(),
+      fileUuid,
       contentType,
       title,
       description
