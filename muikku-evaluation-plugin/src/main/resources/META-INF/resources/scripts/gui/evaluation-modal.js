443c4b39be77b702e0508bc6e485b76fd1e4c745
pkukkon <pasi.kukkonen@otavanopisto.fi>
2016-10-25 08:38:57 +0300

    - ui tidying

--- muikku-evaluation-plugin/src/main/resources/META-INF/resources/scripts/gui/evaluation-modal.js
+++ muikku-evaluation-plugin/src/main/resources/META-INF/resources/scripts/gui/evaluation-modal.js
@@ -27,24 +27,30 @@
         }
       }
     },
+    
     _create : function() {
+      
       // CKEditor
+      
       var extraPlugins = [];
       $.each($.extend(this.options.ckeditor.extraPlugins, {}, true), $.proxy(function (plugin, url) {
         CKEDITOR.plugins.addExternal(plugin, url);
         extraPlugins.push(plugin);
       }, this));
-      this._gradingScales = null;
       this.options.ckeditor.extraPlugins = extraPlugins.join(',');
+      
+      // Init grading scales
+      
+      this._gradingScales = null;
     },
-    setGradingScales: function(gradingScales) {
-      this._gradingScales = gradingScales;
-    },
+    
     open: function(requestCard) {
       this._evaluationModal = $('<div>')
         .addClass('eval-modal')
         .appendTo('body');
       $('body').addClass('no-scroll');
+      
+      // Load assessment
       
       if ($(requestCard).attr('data-evaluated') == 'true') {
         var courseStudentIdentifier = $(requestCard).attr('data-course-student-identifier');
@@ -63,7 +69,8 @@
         this._assessment = null;
       }
       
-      // Assessors
+      // Load assessors
+      
       var workspaceEntityId = $(requestCard).attr('data-workspace-entity-id');
       mApi().workspace.workspaces.staffMembers
         .read(workspaceEntityId, {orderBy: 'name'})
@@ -72,20 +79,30 @@
             $('.notification-queue').notificationQueue('notification', 'error', err);
           }
           else {
-            // Modal itself
+            
+            // Render modal
+            
             renderDustTemplate("evaluation/evaluation-modal-view.dust", {
               studentName: $(requestCard).find('.evaluation-request-student').text(),
               studyProgrammeName: $(requestCard).find('.evaluation-request-study-programme').text(),
               courseName: $(requestCard).find('.workspace-name').text(),
               gradingScales: this._gradingScales||{},
-              assessors: staffMembers
+              assessors: staffMembers,
+              assessmentIdentifier: this._assessment ? this._assessment.identifier : ''
             }, $.proxy(function (html) {
+              
+              // Modal UI
+              
               this._evaluationModal.append(html);
+              
               // Verbal assessment and CKEditor
+              
               var verbalAssessmentEditor = this._evaluationModal.find("#evaluateFormLiteralEvaluation")[0];
               $(verbalAssessmentEditor).val(this._assessment ? this._assessment.verbalAssessment : '');
               CKEDITOR.replace(verbalAssessmentEditor, this.options.ckeditor);
+
               // Assessment date and date picker
+              
               var dateEditor = $(this._evaluationModal).find('input[name="evaluationDate"]'); 
               $(dateEditor)
                 .css({'z-index': 9999, 'position': 'relative'})
@@ -93,22 +110,42 @@
                 .datepicker();
               var evaluationDate = this._assessment ? new Date(moment(this._assessment.assessmentDate)) : new Date();
               $(dateEditor).datepicker('setDate', evaluationDate);
-              // Assessor
+              
+              // Assessor and grade
+              
               if (this._assessment) {
                 $('#assessor').val(this._assessment.assessorIdentifier);
                 $('#grade').val(this._assessment.gradingScaleIdentifier + '@' + this._assessment.gradeIdentifier);
-              } 
+              }
+
+              // Save assessment button
+              
+              $('.button-evaluate-passing').click($.proxy(function(event) {
+                this._saveAssessment();
+              }, this));
+              
               // Close button
+              
               $('.eval-modal-close').click($.proxy(function (event) {
                 this.close();
               }, this));
+            
             }, this));
           }
         }, this));
     },
+    
     close: function() {
       $('body').removeClass('no-scroll');
       this._evaluationModal.remove();
+    },
+
+    setGradingScales: function(gradingScales) {
+      this._gradingScales = gradingScales;
+    },
+    
+    _saveAssessment: function() {
+      alert('let us shave!!');
     }
   });
   
