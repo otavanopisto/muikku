bcd38c88b57c9361261eec899203b4f29fea7a46
pkukkon <pasi.kukkonen@otavanopisto.fi>
2016-11-14 13:21:04 +0200

    - fixes #2548

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/muikku-word-definition.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/muikku-word-definition.js
@@ -7,36 +7,60 @@
     },
     
     _create : function() {
-      if ($('.muikku-word-definition').length == 0) {
-        $('<div>')
-          .addClass('muikku-word-definition')
-          .hide()
-          .appendTo(document.body);
-      }
-
+      this._definition = $('<div>')
+        .addClass('muikku-word-definition')
+        .text(this.element.attr('data-muikku-word-definition'))
+        .hide()
+        .appendTo(document.body)[0];
+      this._definitionVisible = false;
       this.element.on('mousemove', $.proxy(this._onMouseMove, this));
       this.element.on('mouseout', $.proxy(this._onMouseOut, this));
+      this.element.on('touchend', $.proxy(this._onTouchEnd, this));
+      $(this._definition).on('mouseout', $.proxy(this._onMouseOut, this));
+      $(this._definition).on('touchend', $.proxy(this._onTouchEnd, this));
+    },
+    
+    _onTouchEnd: function (event, data) {
+      if (this._definitionVisible) {
+        $('.muikku-word-definition').hide();
+        this._definitionVisible = false;
+      }
+      else {
+        $('.muikku-word-definition')
+          .css({
+            'left': event.pageX + 'px',
+            'top': event.pageY + 'px'
+          })
+          .show();
+        this._definitionVisible = true;
+      }
     },
     
     _onMouseMove: function (event, data) {
       $('.muikku-word-definition')
-        .text(this.element.attr('data-muikku-word-definition'))
         .css({
           'left': event.pageX + 'px',
           'top': event.pageY + 'px'
-        })
-        .show();
+        });
+      if (!this._definitionVisible) {
+        $('.muikku-word-definition').show();
+        this._definitionVisible = true;
+      }
     },
     
     _onMouseOut: function (event) {
-      $('.muikku-word-definition').hide();
+      if (this._definitionVisible) {
+        var target = event.toElement||event.relatedTarget;
+        if (target !== this._definition) {
+          $('.muikku-word-definition').hide();
+          this._definitionVisible = false;
+        }
+      }
     },
     
     _destroy: function () {
-      
     }
   });
-  
   
 
 }).call(this);
