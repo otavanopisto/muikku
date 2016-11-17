945b81415bbdb5abbf2c4957405b279bdd9d5d9c
pkukkon <pasi.kukkonen@otavanopisto.fi>
2016-11-17 10:17:50 +0200

    - resolves #2630

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/records.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/records.js
@@ -87,7 +87,7 @@
                 var assessment = assessments && assessments.length == 1 ? assessments[0] : null;
                 if (assessment) {
                   var grade = this._getGrade(assessment.gradingScaleSchoolDataSource, assessment.gradingScaleIdentifier, assessment.gradeSchoolDataSource, assessment.gradeIdentifier);
-                  workspaceEntity.evaluated = formatDate(new Date(moment(assessment.evaluated)));
+                  workspaceEntity.evaluated = formatDate(moment(assessment.evaluated).toDate());
                   workspaceEntity.verbalAssessment = assessment.verbalAssessment;
                   workspaceEntity.grade = grade.grade;
                   workspaceEntity.gradingScale = grade.scale;
@@ -131,7 +131,7 @@
             if (scaleSchoolDataSource && scaleIdentifier && gradeSchoolDataSource && gradeIdentifier) {
               var grade = this._getGrade(scaleSchoolDataSource, scaleIdentifier, gradeSchoolDataSource, gradeIdentifier);
               return $.extend(transferCredit, {
-                evaluated: formatDate(new Date(moment(transferCredit.date))),
+                evaluated: formatDate(moment(transferCredit.date).toDate()),
                 grade: grade.grade,
                 gradingScale: grade.scale
               });
