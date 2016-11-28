fa9d61168ce905607903ed6dc1b39098debec49c
lankinen <lankinen@192.168.1.3>
2016-11-28 08:42:56 +0200

    * Front page content

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/frontpage.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/frontpage.js
@@ -2,25 +2,26 @@
   
   $(document).ready(function(){
     $('.carousel-videos-wrapper').slick({
+      appendDots: ".carousel-video-controls-wrapper",
       arrows: false,
       dots: true,
       dotsClass: "carousel-video-controls",
       fade: true,
-      appendDots: ".carousel-video-controls-wrapper",
-      speed: 1250
+      speed: 750,
+      waitForAnimate: false
     });
   });
   
   $(document).ready(function(){
     $('.carousel-images-wrapper').slick({
+      appendDots: ".carousel-image-controls-wrapper",
       arrows: false,
       dots: true,
       dotsClass: "carousel-image-controls",
       fade: true,
-      appendDots: ".carousel-image-controls-wrapper",
-      speed: 1250,
-      autoplay: true,
-      autoplaySpeed: 10000
+      speed: 750,
+      swipeToSlide: true,
+      waitForAnimate: false
     });
   });
 
