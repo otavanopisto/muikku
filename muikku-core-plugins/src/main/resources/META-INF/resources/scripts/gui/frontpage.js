9ed7e440bcdf337803b82027c497800f685e3732
lankinen <lankinen@192.168.1.7>
2016-12-13 15:09:52 +0200

    * Added functionality for mobile and narrow navigation so user can access hidden navigation items.

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/frontpage.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/frontpage.js
@@ -42,5 +42,88 @@
                  ]
     });
   });
+  
+  $(document).on('click', '.show-more-nav-desktop', function (event, data) {
+    event.preventDefault();
+    
+    if ($('.of-navigation').attr('data-status') != 'open') {
+      $('.of-navigation').animate({
+        height: "140px"
+        }, 250, function() {
+          $(this).attr('data-status', 'open');
+          
+          $('.of-navigation')
+            .find('.narrow-navigation')
+            .children('.secondary-links')
+            .css({
+              display: 'flex',
+              padding: '0 0 0 50px'
+            }).animate({
+              opacity: 1
+            }, 100);
+          
+      });  
+      
+    } else {
+      $('.of-navigation')
+        .find('.narrow-navigation')
+        .children('.secondary-links')
+        .animate({
+          opacity: 0
+        }, 100, function() {
+          $(this).hide();
+        });
+      
+      $('.of-navigation').animate({
+        height: "70px"
+        }, 250, function() {
+          $(this).removeAttr('data-status');
+      });
+    }
+    
+  });
+  
+  $(document).on('click', '.show-more-nav-mobile', function (event, data) {
+    event.preventDefault();
+    
+    if ($('.of-navigation').attr('data-status') != 'open') {
+      $('.of-navigation').animate({
+        height: "50vh",
+        background: '#fff'
+        }, 250, function() {
+          $(this).attr('data-status', 'open');
+          
+          $('.of-navigation')
+          .find('.mobile-navigation')
+          .children('.secondary-links')
+          .css({
+            display: 'flex',
+            position: 'absolute',
+            top: '70px',
+            'justify-content' : 'space-around',
+            height: 'calc(100% - 70px)'
+          }).animate({
+            opacity: 1
+          }, 100);
+          
+      });  
+    } else {
+      $('.of-navigation')
+        .find('.mobile-navigation')
+        .children('.secondary-links')
+        .animate({
+          opacity: 0
+        }, 100, function() {
+          $(this).hide();
+        });
+      
+      $('.of-navigation').animate({
+        height: "70px"
+        }, 250, function() {
+          $(this).removeAttr('data-status');
+      });
+    }
+    
+  });
 
 }).call(this);
