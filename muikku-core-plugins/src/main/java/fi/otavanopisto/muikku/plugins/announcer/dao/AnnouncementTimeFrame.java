31c7d9a1732d5921a5b503db912af72d5402d46d
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2016-12-21 14:09:30 +0200

    Added enums to help with the listing features

---
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/announcer/dao/AnnouncementTimeFrame.java
@@ -0,0 +1,11 @@
+package fi.otavanopisto.muikku.plugins.announcer.dao;
+
+public enum AnnouncementTimeFrame {
+  ALL,
+  CURRENT,
+  EXPIRED,
+  UPCOMING,
+  
+  CURRENTANDEXPIRED,
+  CURRENTANDUPCOMING
+}
