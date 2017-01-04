a7a209ff463df664c810810911881afe1e1283ca
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2017-01-04 12:52:05 +0200

    Added recipient groups and workspaces to sent items list

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/communicator/rest/CommunicatorSentThreadRESTModel.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/communicator/rest/CommunicatorSentThreadRESTModel.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 import fi.otavanopisto.muikku.rest.model.UserBasicInfo;
+import fi.otavanopisto.muikku.rest.model.UserGroup;
 
 /**
  * REST model for sent messages including the thread information plus recipients.
@@ -15,11 +16,14 @@ public class CommunicatorSentThreadRESTModel extends CommunicatorThreadRESTModel
   public CommunicatorSentThreadRESTModel(Long id, Long communicatorMessageId, Long senderId, UserBasicInfo sender, 
       String categoryName, String caption, Date created, Set<String> tags, boolean unreadMessagesInThread, 
       Date threadLatestMessageDate, Long messageCountInThread, List<CommunicatorMessageIdLabelRESTModel> labels,
-      List<CommunicatorMessageRecipientRESTModel> recipients, Long recipientCount) {
+      List<CommunicatorMessageRecipientRESTModel> recipients, List<UserGroup> userGroupRecipients, 
+      List<CommunicatorMessageRecipientWorkspaceGroupRESTModel> workspaceRecipients, Long recipientCount) {
     super(id, communicatorMessageId, senderId, sender, categoryName, caption, created, tags, unreadMessagesInThread, 
         threadLatestMessageDate, messageCountInThread, labels);
     this.recipients = recipients;
     this.recipientCount = recipientCount;
+    this.userGroupRecipients = userGroupRecipients;
+    this.workspaceRecipients = workspaceRecipients;
   }
 
   public List<CommunicatorMessageRecipientRESTModel> getRecipients() {
@@ -38,6 +42,24 @@ public class CommunicatorSentThreadRESTModel extends CommunicatorThreadRESTModel
     this.recipientCount = recipientCount;
   }
 
+  public List<UserGroup> getUserGroupRecipients() {
+    return userGroupRecipients;
+  }
+
+  public void setUserGroupRecipients(List<UserGroup> userGroupRecipients) {
+    this.userGroupRecipients = userGroupRecipients;
+  }
+
+  public List<CommunicatorMessageRecipientWorkspaceGroupRESTModel> getWorkspaceRecipients() {
+    return workspaceRecipients;
+  }
+
+  public void setWorkspaceRecipients(List<CommunicatorMessageRecipientWorkspaceGroupRESTModel> workspaceRecipients) {
+    this.workspaceRecipients = workspaceRecipients;
+  }
+
   private Long recipientCount;
   private List<CommunicatorMessageRecipientRESTModel> recipients = new ArrayList<CommunicatorMessageRecipientRESTModel>();
+  private List<UserGroup> userGroupRecipients;
+  private List<CommunicatorMessageRecipientWorkspaceGroupRESTModel> workspaceRecipients;
 }
