67f27aa66c7d81cf472aad1a2f53f14048ed4226
pkukkon <pasi.kukkonen@otavanopisto.fi>
2017-03-03 08:21:34 +0200

    Progress HUD changes
    - HUD widget
    - HUD refresh when submitting an assignment
    - locale fixes
    - CC public domain license crash fix (unrelated)

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-activity.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-activity.js
@@ -1,62 +1,53 @@
 (function() {
   'use strict';
   
-  $(document).ready(function () {
-    var workspaceEntityId = $('.workspaceEntityId').val();
-    if (workspaceEntityId) {
+  $.widget("custom.workspaceActivity", {
+    options: {
+      workspaceEntityId: null
+    },
+    _create : function() {
+      this.loadActivity();
+    },
+    loadActivity: function() {
       mApi().guider.workspaces.activity
-        .read(workspaceEntityId)
+        .cacheClear()
+        .read(this.options.workspaceEntityId)
         .callback($.proxy(function (err, activity) {
           if (!err) {
-
-            var progressViewType = $('.workspace-progress-container').length ? 'frontpage' : 'materials';
-            
-            if (progressViewType === 'materials') {
-               progressViewType = $('.workspace-materials-fullscreen-progress-container').length ? 'materials-fullscreen' : 'materials-normal';              
-            }
-            
-            var progressContainer = null;
-            var template = null;  
-            
-            switch (progressViewType) {
-              case 'frontpage':
-                progressContainer = $('.workspace-progress-container');
-                template = 'workspace/workspace_progress_frontpage.dust';
-                break;
-              case 'materials-normal':
-                progressContainer = $('.workspace-materials-progress-container');
-                template = 'workspace/workspace_progress_materials.dust';
-                break;
-              case 'materials-fullscreen':
-                progressContainer = $('.workspace-materials-fullscreen-progress-container');
-                template = 'workspace/workspace_progress_materials_fullscreen.dust';
-                break;
-              
-              default:
-                progressContainer = $('.workspace-materials-progress-container');
-                template = 'workspace/workspace_progress_materials.dust';
-            }
-
             activity.evaluablesDone = activity.evaluablesPassed + activity.evaluablesSubmitted + activity.evaluablesFailed + activity.evaluablesIncomplete;
-            
+            var template = 'workspace/workspace_progress_materials.dust';
+            if ($(this.element).hasClass('workspace-progress-container')) {
+              template = 'workspace/workspace_progress_frontpage.dust';
+            }
+            else if ($(this.element).hasClass('workspace-materials-fullscreen-progress-container')) {
+              template = 'workspace/workspace_progress_materials_fullscreen.dust';
+            }
             renderDustTemplate(template, {progress : activity}, $.proxy(function (text) {
-              progressContainer.html(text);
-              $(progressContainer).on('click', '.c100', function (event) {
+              $(this.element).html(text);
+              $(this.element).on('click', '.c100', function (event) {
                 var activeMenu = $('.workspace-progress-element-menu:visible');
                 var thisMenu = $(event.target).closest('.c100').next('.workspace-progress-element-menu');
-
-                if(thisMenu.attr('id') == activeMenu.attr('id') || activeMenu.length == 0){
+                  if (thisMenu.attr('id') == activeMenu.attr('id') || activeMenu.length == 0) {
                   $(event.target).closest('.c100').next('.workspace-progress-element-menu').toggle();          
-                }else{
+                }
+                else {
                   activeMenu.hide();
                   thisMenu.show();
                 }
-    
               });
               
             }, this));
           }
         }, this));
+    }
+  });
+  
+  $(document).ready(function () {
+    var workspaceEntityId = $('.workspaceEntityId').val();
+    if (workspaceEntityId) {
+      $('.workspace-progress-widget').workspaceActivity({
+        workspaceEntityId: workspaceEntityId
+      });
     }
   });
   
