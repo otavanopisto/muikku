05416171156e2f2cb7136be779b1e4c4c133c37e
Felishia Gonzalez <ejgonzalezviera@gmail.com>
2017-04-10 14:18:55 +0300

    fixed menu clicking issue

--- muikku/src/main/webapp/resources/scripts/interaction/menu.js
+++ muikku/src/main/webapp/resources/scripts/interaction/menu.js
@@ -9,6 +9,7 @@ jQuery(document).ready(function(){
 	});
 	
 	function hideMenu($menu){
+		$(document.body).css({'overflow': ''});
 		$menu.cssAnimate({
 			removeClass: 'menu-_visible_',
 			callback: function(){
@@ -20,15 +21,17 @@ jQuery(document).ready(function(){
 	$('.menu').click(function(e){
 		if(e.target !== e.currentTarget) return;
 		hideMenu($(e.currentTarget));
-		$(document.body).css({'overflow': ''});
 	});
 	
 	$('.menu').bind('touchstart', function(e){
+		var $touchTarget = $(e.target);
+		$touchTarget.addClass("active").parent('.link, .button').addClass("active");
+		
 		$(e.currentTarget).addClass("menu-_dragging_")
-			.data("touchCordX", e.originalEvent.changedTouches[0].pageX);
-		if (e.currentTarget === e.target) {
-			$(e.currentTarget).data("touchTime", (new Date()).getTime());
-		}
+			.data("touchCordX", e.originalEvent.changedTouches[0].pageX)
+			.data("touchMovementX", 0)
+			.data("touchTarget", $touchTarget);
+		
 		e.preventDefault();
 	});
 	
@@ -37,10 +40,15 @@ jQuery(document).ready(function(){
 		var startingPoint = $menu.data("touchCordX");
 		$menuCont = $menu.children('.menu-container');
 		var diff = e.originalEvent.changedTouches[0].pageX - startingPoint;
+		
+		var absoluteDifference = Math.abs(diff - parseInt($menuCont.css('left')));
+		$menu.data("touchMovementX", $menu.data("touchMovementX") + absoluteDifference);
+		
 		if (diff > 0) {
 			diff = 0;
 		}
 		$menuCont.css({'left': diff});
+		
 		e.preventDefault();
 	});
 	
@@ -50,10 +58,23 @@ jQuery(document).ready(function(){
 		$menuCont = $menu.children('.menu-container');
 		var width = $menuCont.width();
 		var diff = parseInt($menuCont.css("left"));
-		var timediff = (new Date()).getTime() - $menu.data("touchTime")
+		var movement = $menu.data("touchMovementX");
+		
+		var $touchTarget = $menu.data("touchTarget");
+		$touchTarget.removeClass("active")
+			.parent('.link, .button').removeClass("active");
+		
 		setTimeout(function(){
 			$menuCont.css({'left': ''});
-			if (Math.abs(diff) >= width*0.33 || timediff <= 300) {
+			if (Math.abs(diff) >= width*0.33 || movement <= 5) {
+				if (movement <= 5){
+					$(e.target).trigger("click");
+					
+					var href = $(e.target).attr("href");
+					if (href){
+						window.location.href = href;
+					}
+				}
 				hideMenu($menu);
 			}
 		},10);
