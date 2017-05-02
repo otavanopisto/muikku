480762bd1cd638c6392d690d26ff34ea48c6d2a7
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2017-05-02 15:06:19 +0300

    Better modularization

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-frontpage.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/workspace-frontpage.js
@@ -4,73 +4,13 @@
 
   $.widget("custom.workspaceTeachers", {
     options: {
+      workspaceEntityId: undefined
     },
     _create : function() {
-      var workspaceEntityId = $('.workspaceEntityId').val();
-
       this.element.on('click', '.workspace-teacher-info.sendmessage', $.proxy(this._sendMessage, this));
       
-      $(document)
-        .muikkuMaterialLoader({
-          workspaceEntityId: workspaceEntityId,
-          baseUrl: $('.materialsBaseUrl').val()
-        })
-        .muikkuMaterialLoader('loadMaterials', $('.workspace-materials-view-page'));
-      
-      mApi().workspace.workspaces.materialProducers
-        .read(workspaceEntityId).callback(function (err, materialProducers) {
-          if (err) {
-            $('.notification-queue').notificationQueue('notification', 'error', err);
-          } else {
-            renderDustTemplate('workspace/workspace-index-material-producers.dust', {
-              materialProducers: materialProducers
-            }, $.proxy(function (text) {
-              $('.workspace-frontpage-footer').append($.parseHTML(text));
-            }, this));
-          }
-        });
-      mApi().workspace.workspaces
-        .read(workspaceEntityId).callback($.proxy(function (err, workspace) {
-          if (err) {
-            $('.notification-queue').notificationQueue('notification', 'error', err);
-          } else {
-            var materialLicenseIcons = [];
-            if (workspace.materialDefaultLicense && workspace.materialDefaultLicense.includes('creativecommons.org')) {
-              materialLicenseIcons.push('cc');
-              if (workspace.materialDefaultLicense.includes('creativecommons.org/licenses/')) {
-                var license = workspace.materialDefaultLicense.match('creativecommons.org/licenses/(.*)/')[1];
-                if (license) {
-                  if (license.includes('by')) {
-                    materialLicenseIcons.push('cc-by');
-                  }
-                  if (license.includes('nc')) {
-                    materialLicenseIcons.push('cc-nc');
-                  }
-                  if (license.includes('nd')) {
-                    materialLicenseIcons.push('cc-nd');
-                  }
-                  if (license.includes('sa')) {
-                    materialLicenseIcons.push('cc-sa');
-                  }
-                }
-              }
-              else if (workspace.materialDefaultLicense.includes('creativecommons.org/publicdomain/')) {
-                materialLicenseIcons.push('cc-zero');
-              }
-            }
-            renderDustTemplate('workspace/workspace-index-material-license.dust', {
-              materialDefaultLicense: workspace.materialDefaultLicense,
-              materialLicenseIcons: materialLicenseIcons
-            }, $.proxy(function (text) {
-              $('.workspace-frontpage-footer').prepend($.parseHTML(text));
-            }, this));
-          }
-        }, this));
-      
-      // #1813: Workspace teachers
-      
       mApi().user.staffMembers.read({
-        workspaceEntityId: workspaceEntityId,
+        workspaceEntityId: this.options.workspaceEntityId,
         properties: 'profile-phone,profile-vacation-start,profile-vacation-end'
       }).callback(function (err, staffMembers) {
         if (!err && staffMembers) {
@@ -109,34 +49,6 @@
           }, this));
         }
       });
-      
-      if ($('.workspace-announcements-container').length > 0) {
-
-        $('.workspace-announcements-container').on('click', '.workspace-single-announcement', function() {
-          var href = $(this).attr('data-href');
-          window.location.assign(href);
-        });
-    
-        mApi().announcer.announcements
-          .read({ hideEnvironmentAnnouncements: true, workspaceEntityId: workspaceEntityId })
-          .callback($.proxy(function(err, result) {
-            if (err) {
-              $(".notification-queue").notificationQueue('notification','error', err);
-            } else {
-              var baseUrl = $('.announcementsBaseUrl').val();
-    
-              for (var i=0; i<result.length; i++) {
-                result[i].link = baseUrl + "?announcementId=" + result[i].id;
-              }
-              
-              renderDustTemplate('workspace/workspace-frontpage-announcements.dust', result, $.proxy(function (text) {
-                var element = $(text);
-                $('.workspace-announcements-container').append(element);
-                $('.workspace-announcements-container').perfectScrollbar({"suppressScrollY" : true});
-              }, this));
-            }
-          }, this));
-      }
     },
     _sendMessage: function (event) {
       var teacherId = $(event.target).closest(".workspace-teacher").attr("data-id");
@@ -173,7 +85,95 @@
   });
 
   $(document).ready(function() {
+    var workspaceEntityId = $('.workspaceEntityId').val();
+
+    $(document)
+      .muikkuMaterialLoader({
+        workspaceEntityId: workspaceEntityId,
+        baseUrl: $('.materialsBaseUrl').val()
+      })
+      .muikkuMaterialLoader('loadMaterials', $('.workspace-materials-view-page'));
+    
+    mApi().workspace.workspaces.materialProducers
+      .read(workspaceEntityId).callback(function (err, materialProducers) {
+        if (err) {
+          $('.notification-queue').notificationQueue('notification', 'error', err);
+        } else {
+          renderDustTemplate('workspace/workspace-index-material-producers.dust', {
+            materialProducers: materialProducers
+          }, $.proxy(function (text) {
+            $('.workspace-frontpage-footer').append($.parseHTML(text));
+          }, this));
+        }
+      });
+    mApi().workspace.workspaces
+      .read(workspaceEntityId).callback($.proxy(function (err, workspace) {
+        if (err) {
+          $('.notification-queue').notificationQueue('notification', 'error', err);
+        } else {
+          var materialLicenseIcons = [];
+          if (workspace.materialDefaultLicense && workspace.materialDefaultLicense.includes('creativecommons.org')) {
+            materialLicenseIcons.push('cc');
+            if (workspace.materialDefaultLicense.includes('creativecommons.org/licenses/')) {
+              var license = workspace.materialDefaultLicense.match('creativecommons.org/licenses/(.*)/')[1];
+              if (license) {
+                if (license.includes('by')) {
+                  materialLicenseIcons.push('cc-by');
+                }
+                if (license.includes('nc')) {
+                  materialLicenseIcons.push('cc-nc');
+                }
+                if (license.includes('nd')) {
+                  materialLicenseIcons.push('cc-nd');
+                }
+                if (license.includes('sa')) {
+                  materialLicenseIcons.push('cc-sa');
+                }
+              }
+            }
+            else if (workspace.materialDefaultLicense.includes('creativecommons.org/publicdomain/')) {
+              materialLicenseIcons.push('cc-zero');
+            }
+          }
+          renderDustTemplate('workspace/workspace-index-material-license.dust', {
+            materialDefaultLicense: workspace.materialDefaultLicense,
+            materialLicenseIcons: materialLicenseIcons
+          }, $.proxy(function (text) {
+            $('.workspace-frontpage-footer').prepend($.parseHTML(text));
+          }, this));
+        }
+      }, this));
+    
+    if ($('.workspace-announcements-container').length > 0) {
+
+      $('.workspace-announcements-container').on('click', '.workspace-single-announcement', function() {
+        var href = $(this).attr('data-href');
+        window.location.assign(href);
+      });
+  
+      mApi().announcer.announcements
+        .read({ hideEnvironmentAnnouncements: true, workspaceEntityId: workspaceEntityId })
+        .callback($.proxy(function(err, result) {
+          if (err) {
+            $(".notification-queue").notificationQueue('notification','error', err);
+          } else {
+            var baseUrl = $('.announcementsBaseUrl').val();
+  
+            for (var i=0; i<result.length; i++) {
+              result[i].link = baseUrl + "?announcementId=" + result[i].id;
+            }
+            
+            renderDustTemplate('workspace/workspace-frontpage-announcements.dust', result, $.proxy(function (text) {
+              var element = $(text);
+              $('.workspace-announcements-container').append(element);
+              $('.workspace-announcements-container').perfectScrollbar({"suppressScrollY" : true});
+            }, this));
+          }
+        }, this));
+    }
+    
     $('.workspace-teachers-container').workspaceTeachers({
+      workspaceEntityId: workspaceEntityId
     });
   });
 
