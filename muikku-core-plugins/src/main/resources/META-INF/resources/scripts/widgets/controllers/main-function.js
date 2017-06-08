29d490e1e3cd20dd969dccfd48859c893a652ec1
Felishia Gonzalez <ejgonzalezviera@gmail.com>
2017-06-08 14:46:52 +0300

    added menu for main function

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/widgets/controllers/main-function.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/widgets/controllers/main-function.js
@@ -7,11 +7,16 @@ module([
     _create: function(){
       var self = this;
       self.element.find(".dropdown").dropdownWidget();
+      self.element.find(".menu").menuWidget();
+      
       self.element.find(".main-function-interact-show-language-picker").click(function(e){
         self.element.find("#language-picker").dropdownWidget('open', e.currentTarget);
       });
       self.element.find(".main-function-interact-show-profile-menu").click(function(e){
         self.element.find("#profile-menu").dropdownWidget('open', e.currentTarget);
+      });
+      self.element.find(".main-function-interact-show-menu").click(function(e){
+        self.element.find("#main-function-menu").menuWidget("open");
       });
     }
   });
