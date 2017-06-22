7f739821169e1658224b66a8f05337925b6c7bd3
onzag <ejgonzalezviera@gmail.com>
2017-06-22 14:23:26 +0300

    working on last messages

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/widgets/controllers/index/panel-last-messages.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/widgets/controllers/index/panel-last-messages.js
@@ -1,10 +1,32 @@
 module([
 ], function(){
   $.widget("custom.panelMessagesControllerWidget", {
-    _create(){
+    options: {
+      onResolved: null,
+      onError: null
+    },
+    _create: function(){
       var self = this;
-      renderDustTemplate('index/panel-last-messages.dust', null, function(text) {
-        self.element.html(text);
+      $(document).muikkuWebSocket("addEventListener", "Communicator:newmessagereceived", function (data) {
+        self._refreshMessagesWidgetMessagesList();
+      });
+      self._refreshMessagesWidgetMessagesList();
+    },
+    _refreshMessagesWidgetMessagesList: function() {
+      var self = this;
+      mApi().communicator.items.read({
+        'firstResult': 0,
+        'maxResults': 6
+      }).callback(function (err, messages) {
+        if( err ){
+          $('.notification-queue').notificationQueue('notification', 'error', err.message);
+          self.options.onError && self.options.onError(self, err);
+        } else {
+          self.options.onResolved && self.options.onResolved(self, messages);
+          renderDustTemplate('index/panel-last-messages.dust', {messages: messages}, function(text) {
+            self.element.html(text);
+          });
+        }
       });
     }
   });
