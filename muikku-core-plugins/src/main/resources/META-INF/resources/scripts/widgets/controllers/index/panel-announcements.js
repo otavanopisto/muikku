0271c01080ea64ec79e464d8bf8f11806f1bf94c
onzag <ejgonzalezviera@gmail.com>
2017-06-27 15:24:55 +0300

    completed announcements implementation

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/widgets/controllers/index/panel-announcements.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/widgets/controllers/index/panel-announcements.js
@@ -1,10 +1,28 @@
 module([
 ], function(){
   $.widget("custom.panelAnnouncementsControllerWidget", {
+    options: {
+      onResolved: null,
+      onError: null
+    },
     _create(){
       var self = this;
-      renderDustTemplate('index/panel-announcements.dust', null, function(text) {
-        self.element.html(text);
+      
+      mApi()
+      .announcer
+      .announcements
+      .read({ hideWorkspaceAnnouncements: "false" })
+      .callback(function(err, announcements) {
+        console.log(announcements);
+        if( err ){
+          $('.notification-queue').notificationQueue('notification', 'error', err.message);
+          self.options.onError && self.options.onError(self, err);
+        } else {
+          self.options.onResolved && self.options.onResolved(self, messages);
+          renderDustTemplate('index/panel-announcements.dust', {announcements: announcements}, function(text) {
+            self.element.html(text);
+          });
+        }
       });
     }
   });
