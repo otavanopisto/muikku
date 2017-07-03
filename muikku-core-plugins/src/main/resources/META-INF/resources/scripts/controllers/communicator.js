279d026f2ecdac5b59d4a1d6fa242e691a4223b2
onzag <ejgonzalezviera@gmail.com>
2017-07-03 12:13:29 +0300

    renamed all wrongly named main-function to index

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/controllers/communicator.js
@@ -0,0 +1,9 @@
+loadModules([
+  CONTEXTPATH + "/javax.faces.resource/scripts/widgets/controllers/base.js.jsf",
+  CONTEXTPATH + "/javax.faces.resource/scripts/widgets/controllers/websocket.js.jsf",
+  CONTEXTPATH + "/javax.faces.resource/scripts/widgets/controllers/generic-environment.js.jsf",
+], function(){
+  $(document).muikkuWebSocket();
+  $(document.body).baseControllerWidget();
+  $.getWidgetContainerFor("generic-environment").genericEvironmentControllerWidget();
+});
