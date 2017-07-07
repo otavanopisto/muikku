fb6c1e1cff89ec8c90a4c20ee98eb34e043def3b
petja-laitila <petja.laitila@otavanopisto.fi>
2017-07-07 15:04:32 +0300

    ** Testing sc 4.4.7 configuration

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [[ $run_tests == "true" ]]; then
   if [[ $start_sc_tunnel == "true" ]]; then 
-    curl -sS https://saucelabs.com/downloads/sc-4.3.12-linux.tar.gz|tar -xzC /tmp/;
-    /tmp/sc-4.3.12-linux/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -i $TRAVIS_JOB_NUMBER -f /tmp/sc-ready -r 10 --pidfile /tmp/sc.pid --vm-version dev-varnish --daemonize;
+    curl -sS https://saucelabs.com/downloads/sc-4.4.7-linux.tar.gz|tar -xzC /tmp/;
+    daemon -- /tmp/sc-4.4.7-linux/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -i $TRAVIS_JOB_NUMBER -f /tmp/sc-ready -r 10 --pidfile /tmp/sc.pid --vm-version dev-varnish --daemonize;
     t=0;
     while [ ! -f /tmp/sc-ready ]; do 
       sleep 0.5; 
       t=$((t+1)); 
       if [[ $t -gt 180 ]]; then 
-        killall sc;
+        kill -9 `cat /tmp/sc.pid`;
         echo "Unable to get Sauce connection within 3 minutes";
         exit 1;
       fi;
