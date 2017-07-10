37979e35c123258ee9430b173f1dbbc932bd8a31
petja-laitila <petja.laitila@otavanopisto.fi>
2017-07-10 10:11:08 +0300

    ** sdjgfu

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -5,11 +5,12 @@ if [[ $run_tests == "true" ]]; then
     daemon -- /tmp/sc-4.4.7-linux/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -i $TRAVIS_JOB_NUMBER -f /tmp/sc-ready -r 10 --pidfile /tmp/sc.pid --vm-version dev-varnish;
     t=0;
     while [ ! -f /tmp/sc-ready ]; do 
-      sleep 0.5; 
+      sleep 1; 
       t=$((t+1)); 
       if [[ $t -gt 180 ]]; then 
         kill -9 `cat /tmp/sc.pid`;
         echo "Unable to get Sauce connection within 3 minutes";
+        wait;
         exit 1;
       fi;
     done;
