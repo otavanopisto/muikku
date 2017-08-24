dc88074672d6857708f14e7b166dbff752c39cf6
pkukkon <pasi.kukkonen@otavanopisto.fi>
2017-08-24 14:06:22 +0300

    - safer elastic search for staff

--- muikku-elastic-search/src/main/java/fi/otavanopisto/muikku/plugins/search/ElasticSearchProvider.java
+++ muikku-elastic-search/src/main/java/fi/otavanopisto/muikku/plugins/search/ElasticSearchProvider.java
@@ -196,7 +196,7 @@ public class ElasticSearchProvider implements SearchProvider {
          * List only active users. 
          * 
          * Active user is
-         * - staff member or
+         * - staff member (teacher, manager, study guider, study programme leader, administrator)
          * - student that has study start date (in the past) and no study end date
          * - student that has study start date (in the past) and study end date in the future
          * - student that has no study start and end date but belongs to an active workspace
@@ -210,8 +210,12 @@ public class ElasticSearchProvider implements SearchProvider {
         
         query.must(
           boolQuery()
-            .should(boolQuery()
-              .mustNot(termQuery("archetype", EnvironmentRoleArchetype.STUDENT.name().toLowerCase()))
+          .should(termsQuery("archetype",
+              EnvironmentRoleArchetype.TEACHER.name().toLowerCase(),
+              EnvironmentRoleArchetype.MANAGER.name().toLowerCase(),
+              EnvironmentRoleArchetype.STUDY_GUIDER.name().toLowerCase(),
+              EnvironmentRoleArchetype.STUDY_PROGRAMME_LEADER.name().toLowerCase(),
+              EnvironmentRoleArchetype.ADMINISTRATOR.name().toLowerCase())
             )
             .should(boolQuery()
               .must(termQuery("archetype", EnvironmentRoleArchetype.STUDENT.name().toLowerCase()))
