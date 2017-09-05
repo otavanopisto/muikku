4c4175dd7ba031a50e735632bad40a8fc42c2b11
Jari Lankinen <Jari Lankinen@10.0.32.96>
2017-09-05 10:41:05 +0300

    * Added initial dummy converse initialization  script

--- muikku/src/main/webapp/resources/scripts/gui/environment.js
+++ muikku/src/main/webapp/resources/scripts/gui/environment.js
@@ -1,6 +1,21 @@
 (function() {
   'use strict';
   
+  /* global converse */
+  /* global CURRENT_USER_AUTHENTICATED */
+  /* global CURRENT_USER_FIRST_NAME */
+  /* global CURRENT_USER_LAST_NAME */
+
+
+  converse.initialize({
+    bosh_service_url: 'https://chatproto.muikkuverkko.fi/http-bind/',
+	show_controlbox_by_default: true,
+	authentication: "login",
+	keepalive: "true",
+	credentials_url: "/credentials",
+	auto_login: true
+  });
+
   function reloadMessageCount() {
     if (MUIKKU_LOGGEDIN) {
       mApi()
