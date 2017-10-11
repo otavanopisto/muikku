963577c9177719f03dedbc8c008f23a57e735572
onzag <ejgonzalezviera@gmail.com>
2017-10-11 13:03:13 +0300

    started discussion

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/containers/discussion.tsx
@@ -0,0 +1,13 @@
+import Notifications from '../components/base/notifications';
+import Body from '../components/discussion/body';
+import * as React from 'react';
+import '~/sass/util/base.scss';
+
+export default class Announcer extends React.Component<{},{}> {
+  render(){
+    return (<div id="root">
+      <Notifications></Notifications>
+      <Body></Body>
+    </div>);
+  }
+}
