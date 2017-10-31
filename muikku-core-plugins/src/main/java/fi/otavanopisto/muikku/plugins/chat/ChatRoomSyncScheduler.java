555b5e311cb5f39fcfc402960a0e3fdd741c0e36
Ilmo Euro <ilmo.euro@gmail.com>
2017-10-31 15:14:55 +0200

    Proper log messages

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/chat/ChatRoomSyncScheduler.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/chat/ChatRoomSyncScheduler.java
@@ -55,23 +55,23 @@ public class ChatRoomSyncScheduler {
 
     String openfireToken = pluginSettingsController.getPluginSetting("chat", "openfireToken");
     if (openfireToken == null) {
-      logger.log(Level.INFO, "No openfire token set, skipping user sync");
+      logger.log(Level.INFO, "No openfire token set, skipping room sync");
       return;
     }
       
     String openfireUrl = pluginSettingsController.getPluginSetting("chat", "openfireUrl");
     if (openfireUrl == null) {
-      logger.log(Level.INFO, "No openfire url set, skipping user sync");
+      logger.log(Level.INFO, "No openfire url set, skipping room sync");
       return;
     }
 
     String openfirePort = pluginSettingsController.getPluginSetting("chat", "openfirePort");
     if (openfirePort == null) {
-      logger.log(Level.INFO, "No openfire port set, skipping user sync");
+      logger.log(Level.INFO, "No openfire port set, skipping room sync");
       return;
     }
     if (!StringUtils.isNumeric(openfirePort)) {
-      logger.log(Level.WARNING, "Invalid openfire port, skipping user sync");
+      logger.log(Level.WARNING, "Invalid openfire port, skipping room sync");
       return;
     }
 
