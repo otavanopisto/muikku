07b1a7056428543e8c4b84e3e7e7f14546edb44b
onzag <ejgonzalezviera@gmail.com>
2017-11-02 14:03:29 +0200

    added pager

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/discussion/body/application/current-thread.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/discussion/body/application/current-thread.tsx
@@ -3,6 +3,11 @@ import * as React from "react";
 import { i18nType } from "~/reducers/base/i18n";
 import { DiscussionType, DiscussionThreadReplyType } from "~/reducers/main-function/discussion/discussion-threads";
 import { Dispatch, connect } from "react-redux";
+import Pager from "~/components/general/pager";
+
+import '~/sass/elements/application-list.scss';
+import '~/sass/elements/text.scss';
+import '~/sass/elements/container.scss';
 
 interface CurrentThreadProps {
   discussionThreads: DiscussionType,
@@ -15,6 +20,14 @@ interface CurrentThreadState {
 }
 
 class CurrentThread extends React.Component<CurrentThreadProps, CurrentThreadState> {
+  getToPage(n: number){
+    if (this.props.discussionThreads.areaId === this.props.discussionThreads.current.forumAreaId){
+      window.location.hash = this.props.discussionThreads.current.forumAreaId + "/" + this.props.discussionThreads.page +
+      "/" + this.props.discussionThreads.current.id + "/" + n;
+    }
+    window.location.hash = this.props.discussionThreads.current.forumAreaId + "/1" +
+      "/" + this.props.discussionThreads.current.id + "/" + n;
+  }
   render(){
     if (!this.props.discussionThreads.current){
       return null;
@@ -23,7 +36,7 @@ class CurrentThread extends React.Component<CurrentThreadProps, CurrentThreadSta
     //Again note that the user might not be ready
     let userCreator = this.props.userIndex[this.props.discussionThreads.current.creator];
     
-    return <div className="application-list application-list__items">
+    return <div><div className="application-list application-list__items">
       <div className="application-list__item application-list__item--discussion-current-thread">
         <div className="application-list__item__header">
           {this.props.discussionThreads.current.title}
@@ -44,7 +57,7 @@ class CurrentThread extends React.Component<CurrentThreadProps, CurrentThreadSta
           </div>
         })
       }
-    </div>
+    </div><Pager onClick={this.getToPage} current={this.props.discussionThreads.page} pages={this.props.discussionThreads.totalPages}/></div>
   }
 }
 
