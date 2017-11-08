793e1d55e8b37ceac459dbec12fe819828aecd73
antti <antti@antti-XPS-15-9560>
2017-11-08 14:53:48 +0200

    Discussion tuning

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/discussion/body/application/current-thread.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/discussion/body/application/current-thread.tsx
@@ -42,6 +42,20 @@ class CurrentThread extends React.Component<CurrentThreadProps, CurrentThreadSta
     
     //Again note that the user might not be ready
     let userCreator = this.props.userIndex[this.props.discussionThreads.current.creator];
+
+    let avatar;
+    if (!userCreator){
+      //This is what it shows when the user is not ready
+      avatar = <div className="application-list__item-content-avatar"></div>;
+    } else {
+      //This is what it shows when the user is ready
+      avatar = <object className="container container--profile-image"
+        data={`/rest/user/files/user/${userCreator.id}/identifier/profile-image-96`}
+        type="image/jpeg">
+          <div className="application-list__item-content-avatar">{userCreator.firstName[0]}</div>
+       </object>;
+    }    
+    
     
     let canRemoveThread = this.props.userId === this.props.discussionThreads.current.creator || areaPermissions.removeThread;
     let canEditThread = this.props.userId === this.props.discussionThreads.current.creator || areaPermissions.editMessage;
@@ -50,7 +64,7 @@ class CurrentThread extends React.Component<CurrentThreadProps, CurrentThreadSta
       <div className="application-list__item--open application-list__item--discussion-current-thread">
         <div className="application-list__item-content-container--avatar">
           <div className="application-list__item-content--avatar-container">
-            <div className="application-list__item-content-avatar">A</div>
+            <div className="application-list__item-content-avatar">{avatar}</div>
           </div>
           <div className="application-list__item-content--content-container">
             <div className="application-list__item__header">
@@ -79,18 +93,41 @@ class CurrentThread extends React.Component<CurrentThreadProps, CurrentThreadSta
           let canRemoveMessage = this.props.userId === reply.creator || areaPermissions.removeThread;
           let canEditMessage = this.props.userId === reply.creator || areaPermissions.editMessages;
           
-          return <div key={reply.id} className={`application-list__item--open application-list__item--discussion-reply ${reply.parentReplyId ? "application-list__item--discussion-reply--of-reply" : "application-list__item--discussion-reply--main"}`}>
-            <div className="application-list__item__body" dangerouslySetInnerHTML={{__html: reply.message}} />
-            <div className="application-list__item__footer">
-              <ReplyThread thread={this.props.discussionThreads.current} reply={reply}>
-                <Link as="span" className="link link--application-list-item-footer">{this.props.i18n.text.get("plugin.discussion.reply.message")}</Link>
-              </ReplyThread>
-              <Link as="span" className="link link--application-list-item-footer">{this.props.i18n.text.get("plugin.discussion.reply.quote")}</Link>
-              {canEditMessage ? <Link as="span" className="link application-list-item-footer">{this.props.i18n.text.get("plugin.discussion.reply.edit")}</Link> : null}
-              {canRemoveMessage ? <Link as="span" className="link application-list-item-footer">{this.props.i18n.text.get("plugin.discussion.reply.delete")}</Link> : null}
+          let avatar;
+          if (!user){
+            //This is what it shows when the user is not ready
+            avatar = <div className="application-list__item-content-avatar"></div>;
+          } else {
+            //This is what it shows when the user is ready
+            avatar = <object className="container container--profile-image"
+              data={`/rest/user/files/user/${user.id}/identifier/profile-image-96`}
+              type="image/jpeg">
+                <div className="application-list__item-content-avatar">{user.firstName[0]}</div>
+             </object>;
+          }          
+          
+          
+          return ( 
+              
+            <div key={reply.id} className={`application-list__item--open application-list__item--discussion-reply ${reply.parentReplyId ? "application-list__item--discussion-reply--of-reply" : "application-list__item--discussion-reply--main"}`}>
+              <div className="application-list__item-content-container--avatar">            
+                <div className="application-list__item-content--avatar-container">
+                  {avatar}
+                </div>            
+                <div className="application-list__item-content--content-container">                        
+                  <div className="application-list__item__body" dangerouslySetInnerHTML={{__html: reply.message}} />
+                  <div className="application-list__item__footer">
+                    <ReplyThread thread={this.props.discussionThreads.current} reply={reply}>
+                      <Link as="span" className="link link--application-list-item-footer">{this.props.i18n.text.get("plugin.discussion.reply.message")}</Link>
+                    </ReplyThread>
+                    <Link as="span" className="link link--application-list-item-footer">{this.props.i18n.text.get("plugin.discussion.reply.quote")}</Link>
+                    {canEditMessage ? <Link as="span" className="link application-list-item-footer">{this.props.i18n.text.get("plugin.discussion.reply.edit")}</Link> : null}
+                    {canRemoveMessage ? <Link as="span" className="link application-list-item-footer">{this.props.i18n.text.get("plugin.discussion.reply.delete")}</Link> : null}
+                  </div>
+                </div>      
+              </div>
             </div>
-          </div>
-        })
+        )})
       }
       <Pager onClick={this.getToPage} current={this.props.discussionThreads.currentPage} pages={this.props.discussionThreads.currentTotalPages}/>
     </div>
