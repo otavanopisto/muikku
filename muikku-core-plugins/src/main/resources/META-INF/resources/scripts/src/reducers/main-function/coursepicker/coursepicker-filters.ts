70c711a8a392f6f97fd381b504c642d090654589
onzag <ejgonzalezviera@gmail.com>
2017-12-07 10:23:08 +0200

    added base for the navigation and filters

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/reducers/main-function/coursepicker/coursepicker-filters.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/reducers/main-function/coursepicker/coursepicker-filters.ts
@@ -19,5 +19,14 @@ export default function areas(state: CoursepickerFilters={
   educationTypes: [],
   curriculums: []
 }, action: ActionType): CoursepickerFilters {
+  if (action.type === "UPDATE_COURSEPICKER_FILTERS_EDUCATION_TYES"){
+    return Object.assign({}, state, {
+      educationTypes: action.payload
+    });
+  } else if (action.type === "UPDATE_COURSEPICKER_FILTERS_CURRICULUMS"){
+    return Object.assign({}, state, {
+      curriculums: action.payload
+    });
+  }
   return state;
-} 
+}
