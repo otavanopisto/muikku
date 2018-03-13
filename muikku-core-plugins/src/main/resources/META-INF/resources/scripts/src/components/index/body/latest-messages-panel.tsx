090da3d0c382187c80b18df0fa059b29f22c196f
petja-laitila <petja.laitila@otavanopisto.fi>
2018-03-13 10:10:14 +0200

    ** Locale change test ** Communicator index widget test ** Caption span for communicator index widget

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/index/body/latest-messages-panel.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/index/body/latest-messages-panel.tsx
@@ -28,7 +28,9 @@ class LastMessagesPanel extends React.Component<LastMessagesPanelProps, LastMess
                       href={`/communicator#inbox/${message.communicatorMessageId}`}>
                 <span className={`item-list__icon item-list__icon--latest-messages icon-envelope${message.unreadMessagesInThread ? "-alt" : ""}`}></span>
                 <span className="text item-list__text-body item-list__text-body--multiline">
-                  {message.caption}
+                  <span className="text item-list__latest-message-caption">
+                    {message.caption}
+                  </span>
                   <span className="text item-list__latest-message-date">
                     {this.props.i18n.time.format(message.created)}
                   </span>
