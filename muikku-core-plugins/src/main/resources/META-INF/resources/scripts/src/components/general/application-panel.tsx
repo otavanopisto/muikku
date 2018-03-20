1ac09acb12a0467a9e8724e09cb6e2a7129bea16
onzag <ejgonzalezviera@gmail.com>
2018-03-20 14:26:12 +0200

    reactivated half of sticky

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/application-panel.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/application-panel.tsx
@@ -19,36 +19,87 @@ interface ApplicationPanelProps {
 
 interface ApplicationPanelState {
   sticky: boolean,
-  remainingHeight: number
+  remainingHeight: number,
+  stickyHeight: number,
+  offsetElementAgainstTop: number,
+  extraPaddingLeft: number,
+  extraPaddingRight: number
 }
 
 export default class ApplicationPanel extends React.Component<ApplicationPanelProps, ApplicationPanelState> {
-  private maxTop:number;
-  private stickyHeight:number;
+  private offsetElementAgainstTop:number;
+  private offsetStickyElementTop: number;
+  private offsetBorderAgainstBottom: number;
+  private stickyHeight: number;
+  private extraPaddingLeft: number;
+  private extraPaddingRight: number;
+  private disabled: boolean;
   
   constructor(props: ApplicationPanelProps){
     super(props);
     
     this.state = {
       sticky: false,
-      remainingHeight: null
+      remainingHeight: null,
+      stickyHeight: null,
+      offsetElementAgainstTop: null,
+      extraPaddingLeft: null,
+      extraPaddingRight: null
     }
     
-    this.maxTop = null;
-    this.stickyHeight = null;
     this.onScroll = this.onScroll.bind(this);
   }
   componentDidMount(){
-    window.addEventListener("scroll", this.onScroll);
-    this.maxTop = (this.refs["top-reference"] as HTMLElement).offsetTop;
+    let computedStyle = document.defaultView.getComputedStyle(this.refs["sticky"] as HTMLElement);
+    if (computedStyle.getPropertyValue("position") === "fixed"){
+      this.disabled = true;
+      return;
+    }
+    this.stickyHeight = parseInt(computedStyle.getPropertyValue("height"));
+    this.setState({
+      stickyHeight: this.stickyHeight
+    });
     
-//    let computedStyle = document.defaultView.getComputedStyle(this.refs["sticky"] as HTMLElement);
-//    this.stickyHeight = (this.refs["sticky"] as HTMLElement).offsetTop +
-//        parseInt(computedStyle.getPropertyValue("border-top")) + parseInt(computedStyle.getPropertyValue("border-top"))
-//    this.setRemainingHeight();
+    this.offsetStickyElementTop = (this.refs["sticky"] as HTMLElement).offsetTop;
+    
+    let element:Element = document.querySelector("#stick");
+    if (!element){
+      this.offsetElementAgainstTop = 0;
+    } else {
+      let stickyElementComputedStyle = document.defaultView.getComputedStyle(element);
+      this.offsetElementAgainstTop = parseInt(stickyElementComputedStyle.getPropertyValue("height"));
+    }
+    
+    this.setState({
+      offsetElementAgainstTop: this.offsetElementAgainstTop
+    })
+    
+    this.extraPaddingLeft = (this.refs["sticky"] as HTMLElement).getBoundingClientRect().left;
+    
+    let root:Element = document.querySelector("#root");
+    if (!element){
+      this.extraPaddingRight = this.extraPaddingLeft;
+    } else {
+      this.extraPaddingRight = root.getBoundingClientRect().width - 
+        ((this.refs["sticky"] as HTMLElement).getBoundingClientRect().width + this.extraPaddingLeft);
+    }
+    
+    this.setState({
+      extraPaddingLeft: this.extraPaddingLeft,
+      extraPaddingRight: this.extraPaddingRight
+    });
+    
+    let panelComputedStyle = document.defaultView.getComputedStyle(this.refs["panel"] as HTMLElement);
+    this.offsetBorderAgainstBottom = parseInt(panelComputedStyle.getPropertyValue("padding-bottom"));
+    
+    window.addEventListener("scroll", this.onScroll);
+    
+    this.setRemainingHeight();
   }
   componentWillUnmount(){
-    window.removeEventListener("scroll", this.onScroll);
+    if (!this.disabled){
+      window.removeEventListener("scroll", this.onScroll);
+    }
   }
   setRemainingHeight(){
 //    if (!this.props.aside){
@@ -70,17 +121,17 @@ export default class ApplicationPanel extends React.Component<ApplicationPanelPr
 //    }
   }
   onScroll(e: Event){
-//    let top = (document.documentElement.scrollTop || document.body.scrollTop);
-//    let diff = this.offsetTop - top;
-//    let nDiff = (diff < 70);
-//    if (nDiff !== this.state.sticky){
-//      this.setState({sticky: nDiff});
-//    }
-//    this.setRemainingHeight();
+    let top = (document.documentElement.scrollTop || document.body.scrollTop);
+    let diff = this.offsetStickyElementTop - top;
+    let isSticky = (diff < this.offsetElementAgainstTop);
+    if (isSticky !== this.state.sticky){
+      this.setState({sticky: isSticky});
+    }
+    this.setRemainingHeight();
   }
   render(){
     return (        
-    <div className={`application-panel application-panel--${this.props.modifier}`} ref="top-reference">
+    <div className={`application-panel application-panel--${this.props.modifier}`} ref="panel">
       <div className="application-panel__container">                
         
         <div className="application-panel__header">
@@ -92,14 +143,20 @@ export default class ApplicationPanel extends React.Component<ApplicationPanelPr
         : null}
         </div>
         <div className="application-panel__body">
-          <div style={{display: this.state.sticky ? "block" : "none"}}></div>          
-         {this.props.toolbar ?
-          <div className="application-panel__actions" ref="sticky">
+         <div style={{display: this.state.sticky ? "block" : "none", height: this.state.stickyHeight}}></div>
+          <div className="application-panel__actions" ref="sticky" style={this.state.sticky ? {
+               position: "fixed",
+               top: this.state.offsetElementAgainstTop,
+               left: this.state.extraPaddingLeft,
+               right: this.state.extraPaddingRight
+             } : null}>
             {this.props.primaryOption ? <div className="application-panel__helper-container application-panel__helper-container--main-action">{this.props.primaryOption}</div> : null}
             <div className="application-panel__main-container application-panel__main-container--actions">{this.props.toolbar}</div>
-          </div> : null}             
-          <div ref="damn" className="application-panel__content">
-            {this.props.asideBefore ? <div className="application-panel__helper-container" style={{height: this.state.remainingHeight}}>{this.props.asideBefore}</div> : null}
+          </div>
+          <div className="application-panel__content">
+            {this.props.asideBefore ? <div className="application-panel__helper-container" style={{
+               height: this.state.remainingHeight
+             }}>{this.props.asideBefore}</div> : null}
             <div className={`application-panel__main-container loader-empty`}>{this.props.children}</div>
             {this.props.asideAfter ? <div className="application-panel__helper-container" style={{height: this.state.remainingHeight}}>{this.props.asideAfter}</div> : null}
           </div>
