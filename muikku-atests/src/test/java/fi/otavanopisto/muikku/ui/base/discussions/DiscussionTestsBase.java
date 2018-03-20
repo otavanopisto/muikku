1b46982fabe03eb80f9cf478bdebfa1aae882ccf
petja-laitila <petja.laitila@otavanopisto.fi>
2018-03-20 13:59:37 +0200

    ** Discussion testing continued

--- muikku-atests/src/test/java/fi/otavanopisto/muikku/ui/base/discussions/DiscussionTestsBase.java
+++ muikku-atests/src/test/java/fi/otavanopisto/muikku/ui/base/discussions/DiscussionTestsBase.java
@@ -2,21 +2,21 @@ package fi.otavanopisto.muikku.ui.base.discussions;
 
 import static fi.otavanopisto.muikku.mock.PyramusMock.mocker;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 import org.junit.Test;
-
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import fi.otavanopisto.muikku.TestEnvironments;
+import fi.otavanopisto.muikku.TestUtilities;
 import fi.otavanopisto.muikku.atests.Discussion;
 import fi.otavanopisto.muikku.atests.DiscussionGroup;
 import fi.otavanopisto.muikku.atests.DiscussionThread;
 import fi.otavanopisto.muikku.mock.PyramusMock.Builder;
 import fi.otavanopisto.muikku.mock.model.MockStaffMember;
+import fi.otavanopisto.muikku.mock.model.MockStudent;
 import fi.otavanopisto.muikku.ui.AbstractUITest;
 import fi.otavanopisto.pyramus.rest.model.Sex;
 import fi.otavanopisto.pyramus.rest.model.UserRole;
@@ -41,19 +41,23 @@ public class DiscussionTestsBase extends AbstractUITest {
     login();
     DiscussionGroup discussionGroup = createDiscussionGroup("test group");
     try {
-      createDiscussion(discussionGroup.getId(), "test discussion");
-      navigate("/discussion", false);
-      waitAndClick(".application-panel__helper-container--main-action .button--primary-function");
-      waitAndClick("input.form-field--new-discussion-thread-title");
-      sendKeys("input.form-field--new-discussion-thread-title", "Test title for discussion");
-      addTextToCKEditor("Test text for discussion.");
-      click("a.button--standard-ok");
-      waitForPresent("h3.text--discussion-current-thread-title");
-      assertText("h3.text--discussion-current-thread-title", "Test title for discussion");
-      waitForPresent(".text--item-article p");
-      assertTextIgnoreCase(".text--item-article p", "Test text for discussion.");
+      Discussion discussion = createDiscussion(discussionGroup.getId(), "test discussion");
+      try {
+        navigate("/discussion", false);
+        waitAndClick(".application-panel__helper-container--main-action .button--primary-function");
+        waitAndClick("input.form-field--new-discussion-thread-title");
+        sendKeys("input.form-field--new-discussion-thread-title", "Test title for discussion");
+        addTextToCKEditor("Test text for discussion.");
+        click("a.button--standard-ok");
+        waitForPresent("h3.text--discussion-current-thread-title");
+        assertText("h3.text--discussion-current-thread-title", "Test title for discussion");
+        waitForPresent(".text--item-article p");
+        assertTextIgnoreCase(".text--item-article p", "Test text for discussion.");
+      }finally {
+        deleteDiscussion(discussionGroup.getId(), discussion.getId());
+      }
     } finally {
-      cleanUpDiscussions();
+      deleteDiscussionGroup(discussionGroup.getId());
       mockBuilder.wiremockReset();
     }
 
@@ -110,19 +114,24 @@ public class DiscussionTestsBase extends AbstractUITest {
     DiscussionGroup discussionGroup = createDiscussionGroup("test group");
     try {
       Discussion discussion = createDiscussion(discussionGroup.getId(), "test discussion");
-      createDiscussionThread(discussionGroup.getId(), discussion.getId(), "Testing",
+      DiscussionThread thread = createDiscussionThread(discussionGroup.getId(), discussion.getId(), "Testing",
           "<p>Testing testing daa daa</p>", false, false);
-
-      navigate("/discussion", false);
-      waitAndClick(".message__title--category-1 ");
-      waitAndClick(".link--application-list-item-footer:nth-child(1)");
-      addTextToCKEditor("Test reply for test.");
-      click(".button--standard-ok");
-      waitForPresent(".application-list__item--discussion-reply .application-list__item__body article p");
-      assertText(".application-list__item--discussion-reply .application-list__item__body article p", "Test reply for test.");
+      try{
+        navigate("/discussion", false);
+        waitAndClick(".message__title");
+        waitAndClick(".link--application-list-item-footer:nth-child(1)");
+        addTextToCKEditor("Test reply for test.");
+        click(".button--standard-ok");
+        waitForPresent(".application-list__item--discussion-reply .application-list__item__body article p");
+        assertText(".application-list__item--discussion-reply .application-list__item__body article p", "Test reply for test.");
+      } catch (Exception e) {
+        deleteDiscussionThread(discussionGroup.getId(), discussion.getId(), thread.getId());
     } finally {
-      cleanUpDiscussions();
-      mockBuilder.wiremockReset();
+      deleteDiscussion(discussionGroup.getId(), discussion.getId());
+    }
+  } finally {
+    deleteDiscussionGroup(discussionGroup.getId());
+    mockBuilder.wiremockReset();
     }
   }
   
@@ -133,31 +142,37 @@ public class DiscussionTestsBase extends AbstractUITest {
         TestEnvironments.Browser.FIREFOX,
         TestEnvironments.Browser.INTERNET_EXPLORER,
         TestEnvironments.Browser.EDGE,
-        TestEnvironments.Browser.SAFARI
+        TestEnvironments.Browser.SAFARI,
+        TestEnvironments.Browser.CHROME_HEADLESS
       }
     )
   public void discussionDeleteThreadTest() throws Exception {
-    loginAdmin();
+    MockStaffMember admin = new MockStaffMember(4l, 4l, "Admin", "User", UserRole.ADMINISTRATOR, "121212-1234", "admin@example.com", Sex.MALE);
+    Builder mockBuilder = mocker();
+    mockBuilder.addStaffMember(admin).mockLogin(admin).build();
+    login();
     DiscussionGroup discussionGroup = createDiscussionGroup("test group");
     try {
       Discussion discussion = createDiscussion(discussionGroup.getId(), "test discussion");
       DiscussionThread thread = createDiscussionThread(discussionGroup.getId(), discussion.getId(), "Testing",
-            "<p>Testing testing daa daa</p>", false, false);
-        try {
-          navigate("/discussion", false);
-          waitAndClick(".di-message-meta-topic>span");
-          waitAndClick(".di-remove-thread-link");
-          waitAndClick(".delete-button>span");
-          waitForPresent(".mf-content-empty>h3");
-          assertNotPresent(".di-threads .di-message");
-        } catch (Exception e) {
-          deleteDiscussionThread(discussionGroup.getId(), discussion.getId(), thread.getId());
-      } finally {
-        deleteDiscussion(discussionGroup.getId(), discussion.getId());
-      }
+          "<p>Testing testing daa daa</p>", false, false);
+      try{
+        navigate("/discussion", false);
+        waitAndClick(".message__title");
+        waitAndClick(".link--application-list-item-footer:nth-child(4)");
+        waitForPresentAndVisible(".dialog--delete-area .button--standard-ok");
+        waitAndClick(".button--standard-ok");
+        waitForNotVisible(".dialog--delete-area");
+        waitForPresentAndVisible(".application-panel__content .application-panel__main-container.loader-empty");
+        assertNotPresent(".message__title");
+      } catch (Exception e) {
+        deleteDiscussionThread(discussionGroup.getId(), discussion.getId(), thread.getId());
     } finally {
-      deleteDiscussionGroup(discussionGroup.getId());
-      WireMock.reset();
+      deleteDiscussion(discussionGroup.getId(), discussion.getId());
+    }
+  } finally {
+    deleteDiscussionGroup(discussionGroup.getId());
+    mockBuilder.wiremockReset();
     }
   }
 
@@ -168,30 +183,37 @@ public class DiscussionTestsBase extends AbstractUITest {
         TestEnvironments.Browser.FIREFOX,
         TestEnvironments.Browser.INTERNET_EXPLORER,
         TestEnvironments.Browser.EDGE,
-        TestEnvironments.Browser.SAFARI
+        TestEnvironments.Browser.SAFARI,
+        TestEnvironments.Browser.CHROME_HEADLESS
       }
     )
   public void discussionReplyReplyTest() throws Exception {
-    loginAdmin();
-
+    MockStaffMember admin = new MockStaffMember(4l, 4l, "Admin", "User", UserRole.ADMINISTRATOR, "121212-1234", "admin@example.com", Sex.MALE);
+    MockStudent student = new MockStudent(2l, 2l, "Student", "Tester", "student@example.com", 1l, OffsetDateTime.of(1990, 2, 2, 0, 0, 0, 0, ZoneOffset.UTC), "121212-1212", Sex.FEMALE, TestUtilities.toDate(2012, 1, 1), TestUtilities.getNextYear());
+    Builder mockBuilder = mocker();
+    mockBuilder.addStaffMember(admin).addStudent(student).mockLogin(admin).build();
+    login();
     DiscussionGroup discussionGroup = createDiscussionGroup("test group");
     try {
       Discussion discussion = createDiscussion(discussionGroup.getId(), "test discussion");
       try {
         DiscussionThread thread = createDiscussionThread(discussionGroup.getId(), discussion.getId(), "Testing",
             "<p>Testing testing daa daa</p>", false, false);
+        logout();
+        mockBuilder.mockLogin(student);
+        login();
         try {
           navigate("/discussion", false);
-          waitAndClick(".di-message-meta-topic>span");
-          waitAndClick(".di-message-reply-link");
+          waitAndClick(".message__title");
+          waitAndClick(".link--application-list-item-footer:nth-child(1)");
           addTextToCKEditor("Test reply for test.");
-          click("*[name='send']");
-          waitForPresent(".di-replies-container .mf-item-content-text p");
-          waitAndClick(".di-replies-page .di-reply-answer-link>span");
-          addTextToCKEditor("Test reply reply for test.");
-          click("*[name='send']");
-          waitForPresent(".di-replies-container .di-reply-reply .mf-item-content-text p");
-          assertText(".di-replies-container .di-reply-reply .mf-item-content-text p", "Test reply reply for test.");
+          click(".button--standard-ok");
+          waitForPresentAndVisible(".application-list .application-list__item--discussion-reply");
+          waitAndClick(".application-list .application-list__item--discussion-reply .link--application-list-item-footer:nth-child(1)");
+          addTextToCKEditor("Test reply to reply.");
+          click(".button--standard-ok");
+          waitForPresentAndVisible(".application-list__item--discussion-reply .message--discussion-reply-of-reply");
+          assertText(".application-list__item--discussion-reply .message--discussion-reply-of-reply .application-list__item__body article", "Test reply to reply.");
         } catch (Exception e) {
           deleteDiscussionThread(discussionGroup.getId(), discussion.getId(), thread.getId());
         } finally {
@@ -202,7 +224,51 @@ public class DiscussionTestsBase extends AbstractUITest {
       }
     } finally {
       deleteDiscussionGroup(discussionGroup.getId());
-      WireMock.reset();
+      mockBuilder.wiremockReset();
+      }
+  }
+
+  @Test
+  @TestEnvironments (
+      browsers = {
+        TestEnvironments.Browser.CHROME,
+        TestEnvironments.Browser.FIREFOX,
+        TestEnvironments.Browser.INTERNET_EXPLORER,
+        TestEnvironments.Browser.EDGE,
+        TestEnvironments.Browser.SAFARI,
+        TestEnvironments.Browser.CHROME_HEADLESS
+      }
+    )
+  public void discussionQuoteTest() throws Exception {
+    MockStaffMember admin = new MockStaffMember(4l, 4l, "Admin", "User", UserRole.ADMINISTRATOR, "121212-1234", "admin@example.com", Sex.MALE);
+    Builder mockBuilder = mocker();
+    mockBuilder.addStaffMember(admin).mockLogin(admin).build();
+    login();
+    DiscussionGroup discussionGroup = createDiscussionGroup("test group");
+    try {
+      Discussion discussion = createDiscussion(discussionGroup.getId(), "test discussion");
+      try {
+        DiscussionThread thread = createDiscussionThread(discussionGroup.getId(), discussion.getId(), "Testing",
+            "<p>Testing testing daa daa</p>", false, false);
+        try {
+          navigate("/discussion", false);
+          waitAndClick(".message__title");
+          waitAndClick(".link--application-list-item-footer:nth-child(2)");
+          addToEndCKEditor("Test with quote.");
+          waitAndClick(".button--standard-ok");
+          waitForPresent(".application-list__item--discussion-reply .application-list__item__body article");
+          assertText(".application-list__item--discussion-reply .application-list__item__body article blockquote p strong", "Admin User");
+          assertText(".application-list__item--discussion-reply .application-list__item__body article blockquote p:nth-child(2)", "Testing testing daa daa");
+          assertText(".application-list__item--discussion-reply .application-list__item__body article>p", "Test with quote.");
+        } finally {
+          deleteDiscussionThread(discussionGroup.getId(), discussion.getId(), thread.getId());
+        }
+      } finally {
+        deleteDiscussion(discussionGroup.getId(), discussion.getId());
+      }
+    } finally {
+      deleteDiscussionGroup(discussionGroup.getId());
+      mockBuilder.wiremockReset();
     }
   }
   
