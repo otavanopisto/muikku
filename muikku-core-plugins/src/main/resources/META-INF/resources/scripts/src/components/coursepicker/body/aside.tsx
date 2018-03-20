9fbb53f39463fc1ec8997e0b5ea15bb2026a9696
onzag <ejgonzalezviera@gmail.com>
2018-03-20 14:58:11 +0200

    removed small bug with curriculum selection

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/coursepicker/body/aside.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/coursepicker/body/aside.tsx
@@ -35,7 +35,7 @@ class NavigationAside extends React.Component<NavigationAsideProps, NavigationAs
       <NavigationTopic name={this.props.i18n.text.get( 'plugin.coursepicker.filters.curriculum' )}>
         {this.props.courses.avaliableFilters.curriculums.map( ( curriculum: CourseCurriculumFilterType ) => {
           let isActive = this.props.courses.activeFilters.curriculumFilters.includes( curriculum.identifier );
-          let hash = "#" + ( isActive ?
+          let hash = "?" + ( isActive ?
             queryString.stringify( Object.assign( {}, locationData, { c: ( locationData.c || [] ).filter( ( c: string ) => c !== curriculum.identifier ) } ), { arrayFormat: 'bracket' } ) :
             queryString.stringify( Object.assign( {}, locationData, { c: ( locationData.c || [] ).concat( curriculum.identifier ) } ), { arrayFormat: 'bracket' } ) );
           return <NavigationElement key={curriculum.identifier} isActive={isActive} hash={hash}>{curriculum.name}</NavigationElement>
