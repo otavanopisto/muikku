9283d854e9812a8daf737497354ff64c2b923c39
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2018-04-25 10:10:36 +0300

    Cleanup

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/ckplugins/muikku-mathexercisefield/lang/en.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/ckplugins/muikku-mathexercisefield/lang/en.js
@@ -1,10 +1,4 @@
 CKEDITOR.plugins.setLang('muikku-mathexercisefield', 'en', {
   uiElement: 'Math Exercise Field',
-  toolbarMenu: 'Math Exercise Field',
-  propertiesMenu: 'Muikku Math Exercise Field Properties',
-  propertiesDialogTitle: 'Muikku Math Exercise Field Properties', 
-  propertiesDialogRows: 'Rows',
-  propertiesDialogCols: 'Columns',
-  propertiesDialogExample: 'Example',
-  propertiesDialogRichEdit: 'Use Rich Text Editor'
+  toolbarMenu: 'Math Exercise Field'
 });
--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/ckplugins/muikku-mathexercisefield/lang/fi.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/ckplugins/muikku-mathexercisefield/lang/fi.js
@@ -1,10 +1,4 @@
 CKEDITOR.plugins.setLang('muikku-mathexercisefield', 'fi', {
   uiElement: 'Matematiikkatehtäväkenttä',
-  toolbarMenu: 'Matematiikkatehtäväkenttä',
-  propertiesMenu: 'Matematiikkatehtäväkentän asetukset',
-  propertiesDialogTitle: 'Matematiikkatehtäväkentän asetukset', 
-  propertiesDialogRows: 'Rivejä',
-  propertiesDialogCols: 'Sarakkeita',
-  propertiesDialogExample: 'Mallivastaus',
-  propertiesDialogRichEdit: 'Käytä tekstieditoria'
+  toolbarMenu: 'Matematiikkatehtäväkenttä'
 });
