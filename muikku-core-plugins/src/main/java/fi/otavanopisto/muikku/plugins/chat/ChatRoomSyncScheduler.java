6f620cf9f88858639948c5c7ed4659776b4a116a
O-EURILM\Saara Lepistö <saara.lepisto@hotmail.com>
2018-06-06 10:41:18 +0300

    Closes #3932 Create chat rooms for existing workspaces

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/chat/ChatRoomSyncScheduler.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/chat/ChatRoomSyncScheduler.java
@@ -1,8 +1,10 @@
 package fi.otavanopisto.muikku.plugins.chat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
@@ -14,18 +16,19 @@ import javax.ejb.TransactionAttributeType;
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.commons.lang3.StringUtils;
 import org.infinispan.configuration.global.ShutdownHookBehavior;
 
 import fi.otavanopisto.muikku.controller.PluginSettingsController;
 import fi.otavanopisto.muikku.model.users.UserSchoolDataIdentifier;
+import fi.otavanopisto.muikku.model.workspace.WorkspaceAccess;
 import fi.otavanopisto.muikku.model.workspace.WorkspaceEntity;
 import fi.otavanopisto.muikku.openfire.rest.client.RestApiClient;
 import fi.otavanopisto.muikku.openfire.rest.client.entity.AuthenticationToken;
 import fi.otavanopisto.muikku.openfire.rest.client.entity.MUCRoomEntity;
-import fi.otavanopisto.muikku.plugins.coursepicker.CoursePickerRESTService;
-import fi.otavanopisto.muikku.rest.model.Student;
 import fi.otavanopisto.muikku.schooldata.CourseMetaController;
 import fi.otavanopisto.muikku.schooldata.SchoolDataIdentifier;
 import fi.otavanopisto.muikku.schooldata.WorkspaceController;
@@ -36,7 +39,7 @@ import fi.otavanopisto.muikku.schooldata.entity.WorkspaceUser;
 import fi.otavanopisto.muikku.search.SearchProvider;
 import fi.otavanopisto.muikku.search.SearchProvider.Sort;
 import fi.otavanopisto.muikku.search.SearchResult;
-import fi.otavanopisto.muikku.users.UserController;
+import fi.otavanopisto.muikku.session.SessionController;
 
 @Stateless
 public class ChatRoomSyncScheduler {
@@ -54,37 +57,29 @@ public class ChatRoomSyncScheduler {
   private WorkspaceController workspaceController;
   
   @Inject
-  private CoursePickerRESTService coursePickerRESTService;
+  private SessionController sessionController;
   
+  @Inject
   private CourseMetaController courseMetaController;
-  
+    
   @Inject
   @Any
   private Instance<SearchProvider> searchProviders;
 
-
-  @Schedule(second = "0", minute = "*/15", hour = "*", persistent = false)
+  @Schedule(second = "0", minute = "*/30", hour = "*", persistent = false)
   @TransactionAttribute(TransactionAttributeType.REQUIRED)
   public void updateChatRooms() {
-	
-	List<Curriculum> curriculums = courseMetaController.listCurriculums();
-	
+		
     String enabledUsersCsv = pluginSettingsController.getPluginSetting("chat", "enabledUsers");
     if (enabledUsersCsv == null) {
       return;
     }
     List<String> enabledUsers = Arrays.asList(enabledUsersCsv.split(","));
 
-//    String enabledWorkspacesCsv = pluginSettingsController.getPluginSetting("chat", "enabledWorkspaces");
-//    if (enabledWorkspacesCsv == null) {
-//      return;
-//    }
-//    List<String> enabledWorkspaces = Arrays.asList(enabledWorkspacesCsv.split(","));
-
-    curriculums.removeIf((curriculum) -> "OPS2005".equals(curriculum.getName()));
-
-    List<Workspace> enabledWorkspaces = workspaceController.listWorkspaces();
+	List<Curriculum> curriculums = courseMetaController.listCurriculums();
     
+    curriculums.removeIf((curriculum) -> "OPS2005".equals(curriculum.getName()));
+        
     String openfireToken = pluginSettingsController.getPluginSetting("chat", "openfireToken");
     if (openfireToken == null) {
       logger.log(Level.INFO, "No openfire token set, skipping room sync");
@@ -115,63 +110,86 @@ public class ChatRoomSyncScheduler {
       SearchProvider searchProvider = searchProviderIterator.next();
       SearchResult searchResult = null;
       
-      searchResult = searchProvider.searchWorkspaces(schoolDataSource, subjects, identifiers, educationTypeIdentifiers, curriculums, null, accesses, accessUser, includeUnpublished, start, maxResults, sorts);
-          }
-
-    
-    
-    for (Workspace enabledWorkspace : enabledWorkspaces) {
-      try {
-        // Checking before creating is subject to a race condition, but in the worst
-        // case
-        // the creation just fails, resulting in a log entry
-        MUCRoomEntity chatRoomEntity = client.getChatRoom(enabledWorkspace.getIdentifier());
-        if (chatRoomEntity == null) {
-          logger.log(Level.INFO, "Syncing chat workspace " + enabledWorkspace);
-          SchoolDataIdentifier identifier = SchoolDataIdentifier.fromId(enabledWorkspace.getIdentifier());
-          if (identifier == null) {
-            logger.log(Level.WARNING, "Invalid workspace identifier " + enabledWorkspace + ", skipping...");
-            continue;
-          }
-          WorkspaceEntity workspaceEntity = workspaceEntityController.findWorkspaceByIdentifier(identifier);
-          if (workspaceEntity == null) {
-            logger.log(Level.WARNING, "No workspace entity found for identifier " + enabledWorkspace + ", skipping...");
-            continue;
-          }
-
-          Workspace workspace = workspaceController.findWorkspace(workspaceEntity);
-          chatRoomEntity = new MUCRoomEntity(enabledWorkspace.getIdentifier(), workspace.getName(), workspace.getDescription());
-          client.createChatRoom(chatRoomEntity);
-
-          List<WorkspaceUser> workspaceUsers = workspaceController.listWorkspaceStudents(workspaceEntity);
-          List<WorkspaceUser> workspaceStaffs = workspaceController.listWorkspaceStaffMembers(workspaceEntity);
-
-          for (WorkspaceUser workspaceStaff : workspaceStaffs) {
-
-            SchoolDataIdentifier memberIdentifier = workspaceStaff.getUserIdentifier();
-
-            if (enabledUsers.contains(memberIdentifier.toId())) {
-
-              client.addAdmin(enabledWorkspace.getIdentifier(), memberIdentifier.toId());
-
-            }
-          }
-
-          for (WorkspaceUser workspaceUser : workspaceUsers) {
-
-            SchoolDataIdentifier memberIdentifier = workspaceUser.getUserIdentifier();
-
-            if (enabledUsers.contains(memberIdentifier.toId())) {
-
-              client.addMember(enabledWorkspace.getIdentifier(), memberIdentifier.toId());
-
-            }
-          }
+      List<SchoolDataIdentifier> curriculumIdentifiers = null;
+      if (curriculums != null) {
+        curriculumIdentifiers = new ArrayList<>(curriculums.size());
+        for (Curriculum curriculum : curriculums) {
+          SchoolDataIdentifier curriculumIdentifier = SchoolDataIdentifier.fromId(curriculum.getIdentifier().toString());
+          if (curriculumIdentifier != null && !"OPS2005".equals(curriculum.getName())) {
+            curriculumIdentifiers.add(curriculumIdentifier);
+          } 
         }
-
-      } catch (Exception e) {
-        logger.log(Level.INFO, "Exception when syncing chat workspace " + enabledWorkspace, e);
       }
-    }
+      
+      String searchString = null;
+      boolean includeUnpublished = true;
+	  int firstResult = 0;
+  	  int maxResults = 100;
+	  List<Sort> sorts = null;
+	  searchResult = searchProvider.searchWorkspaces(null, null, null, null, curriculumIdentifiers, searchString, null, null, includeUnpublished, firstResult, maxResults, sorts);          
+	  
+	  List<Map<String, Object>> results = searchResult.getResults();
+      for (Map<String, Object> result : results) {
+    	  String searchId = (String) result.get("id");
+          if (StringUtils.isNotBlank(searchId)) {
+            String[] id = searchId.split("/", 2);
+            if (id.length == 2) {
+              String dataSource = id[1];
+              String identifier = id[0];
+              WorkspaceEntity workspaceEntity = workspaceEntityController.findWorkspaceByDataSourceAndIdentifier(dataSource, identifier);
+            
+              String name = (String) result.get("name");
+                
+        	  try {
+        		// Checking before creating is subject to a race condition, but in the worst
+        	    // case
+                // the creation just fails, resulting in a log entry
+       	        MUCRoomEntity chatRoomEntity = client.getChatRoom(identifier);
+        		  if (chatRoomEntity == null) {
+        		    logger.log(Level.INFO, "Syncing chat workspace " + name);
+        		    // SchoolDataIdentifier identifier = SchoolDataIdentifier.fromId(enabledWorkspace.getIdentifier());
+        		    if (identifier == null) {
+        		      logger.log(Level.WARNING, "Invalid workspace identifier " + identifier + ", skipping...");
+        		      continue;
+        		    }
+        		  
+        		    if (workspaceEntity == null) {
+        		      logger.log(Level.WARNING, "No workspace entity found for identifier " + identifier + ", skipping...");
+        		      continue;
+        		    }
+
+        		    Workspace workspace = workspaceController.findWorkspace(workspaceEntity);
+        		      chatRoomEntity = new MUCRoomEntity(identifier, workspace.getName(), workspace.getDescription());
+        		      client.createChatRoom(chatRoomEntity);
+
+        		      List<WorkspaceUser> workspaceUsers = workspaceController.listWorkspaceStudents(workspaceEntity);
+        		      List<WorkspaceUser> workspaceStaffs = workspaceController.listWorkspaceStaffMembers(workspaceEntity);
+
+        		      for (WorkspaceUser workspaceStaff : workspaceStaffs) {
+
+        		        SchoolDataIdentifier memberIdentifier = workspaceStaff.getUserIdentifier();
+
+        		        if (enabledUsers.contains(memberIdentifier.toId())) {
+        		          client.addAdmin(identifier, memberIdentifier.toId());
+        		        }
+        		      }
+
+        		      for (WorkspaceUser workspaceUser : workspaceUsers) {
+
+        		        SchoolDataIdentifier memberIdentifier = workspaceUser.getUserIdentifier();
+
+        		        if (enabledUsers.contains(memberIdentifier.toId())) {
+        		          client.addMember(identifier, memberIdentifier.toId());
+        		        }
+        		      }
+        		    }
+
+        		    } catch (Exception e) {
+        		      logger.log(Level.INFO, "Exception when syncing chat workspace " + name, e);
+        		    }
+            }
+          }
+      }
+    } 
   }
 }
