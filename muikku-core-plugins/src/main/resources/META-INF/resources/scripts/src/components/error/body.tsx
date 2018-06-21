9b2f90bbc5a9acb90361727150206037be04967f
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2018-06-21 10:31:38 +0300

    * Error page layout fixes

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/error/body.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/error/body.tsx
@@ -3,6 +3,11 @@ import * as React from 'react';
 import ScreenContainer from '~/components/general/screen-container';
 
 import '~/sass/elements/container.scss';
+import '~/sass/elements/buttons.scss';
+import '~/sass/elements/text.scss';
+import '~/sass/elements/ordered-container.scss';
+import '~/sass/elements/panel.scss';
+
 import { StateType } from '~/reducers';
 import { Dispatch, connect } from 'react-redux';
 import { ErrorType } from '~/reducers/base/error';
@@ -20,10 +25,17 @@ class ErrorBody extends React.Component<ErrorBodyProps,ErrorBodyState> {
     return (<div className="container container--full">
       <MainFunctionNavbar/>
         
-      <ScreenContainer fullHeight={false}>
-        <div className="container container--error">
-          <h2>{this.props.error.title}</h2>
-          <p>{this.props.error.description}</p>
+      <ScreenContainer fullHeight={true}>
+        <div className="ordered-container ordered-container--error-panels">
+          <div className="ordered-container__item ordered-container__item--error">
+            <div className="text text--error-title">
+              <span className="text__panel-icon text__panel-icon--error icon-error"></span>
+              <span className="text__panel-title">{this.props.error.title}</span>
+            </div>
+            <div className="panel panel--error">
+              <div className="text text--error-body">{this.props.error.description}</div>
+            </div>
+          </div>
         </div>
       </ScreenContainer>
     </div>);
