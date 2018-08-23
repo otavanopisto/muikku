c5d22a0c1f7673ec1d0125029216e1537998a97c
O-EURILM\Saara Lepistö <saara.lepisto@hotmail.com>
2018-08-23 08:29:22 +0300

    FTR #4078 Study program leader should be able to create permanen chat room

--- muikku/src/main/webapp/resources/scripts/gui/chat.js
+++ muikku/src/main/webapp/resources/scripts/gui/chat.js
@@ -2,8 +2,10 @@
   'use strict';
   
   /* global converse */
-  
+
   mApi().chat.status.read().callback(function(err, result) {
+	 
+	  
     if (result && result.enabled) {
       converse.initialize({
         bosh_service_url : '/http-bind/',
@@ -20,8 +22,9 @@
         auto_minimize: true,
         i18n: getLocale() === "fi" ? "fi" : "en",
         hide_occupants:true,
-        limit_room_controls:true
+        limit_room_controls:true,
+        show_checkbox_persistent: CHAT_CHECKBOX_PERMISSION
       });
     }
   });
-}).call(this);
+  }).call(this);
