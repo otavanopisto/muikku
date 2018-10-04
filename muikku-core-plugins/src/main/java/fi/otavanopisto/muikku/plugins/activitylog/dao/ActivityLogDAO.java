e9f37e075113026d48338b52630c974a084bad37
vladislav <vladislav1996.06@gmail.com>
2018-10-04 09:23:10 +0300

    First REST created

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/activitylog/dao/ActivityLogDAO.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/activitylog/dao/ActivityLogDAO.java
@@ -1,9 +1,15 @@
 package fi.otavanopisto.muikku.plugins.activitylog.dao;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-import fi.otavanopisto.muikku.model.users.UserEntity;
-import fi.otavanopisto.muikku.model.workspace.WorkspaceEntity;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import fi.otavanopisto.muikku.plugins.CorePluginsDAO;
 import fi.otavanopisto.muikku.plugins.activitylog.model.ActivityLog;
 import fi.otavanopisto.muikku.plugins.activitylog.model.ActivityLogType;
@@ -13,13 +19,44 @@ public class ActivityLogDAO extends CorePluginsDAO<ActivityLog>{
   
   private static final long serialVersionUID = 7895651610938408594L;
   
-  public ActivityLog create(Long userEntityId, ActivityLogType type, Long workspaceId, Long contextId) {
+  public ActivityLog create(Long userEntityId, ActivityLogType type, Long workspaceEntityId, Long contextId) {
     ActivityLog activityLog = new ActivityLog();
     activityLog.setUserEntityId(userEntityId);
     activityLog.setActivityLogType(type);
     activityLog.setTimestamp(new Date());
-    activityLog.setWorkspaceId(workspaceId);
+    activityLog.setWorkspaceId(workspaceEntityId);
     activityLog.setContextId(contextId);
     return persist(activityLog);
+  }
+  
+  public List<ActivityLog> listActivityLogs(Long userEntityId, Long workspaceEntityId, Date from, Date to) {
+    EntityManager entityManager = getEntityManager();
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<ActivityLog> criteria = criteriaBuilder.createQuery(ActivityLog.class);
+    Root<ActivityLog> root = criteria.from(ActivityLog.class);
+    criteria.select(root);
+    List<Predicate> predicates = new ArrayList<Predicate>();
+    predicates.add(criteriaBuilder.equal(root.get(ActivityLog_.userEntityId), userEntityId));
+    predicates.add(criteriaBuilder.greaterThan(root.get(ActivityLog_.timestamp), from));
+    predicates.add(criteriaBuilder.lessThan(root.get(ActivityLog_.timestamp), to));
+    if(workspaceEntityId == null)
+      predicates.add(criteriaBuilder.isNull(root.get(ActivityLog_.workspaceId)));
+    else
+      predicates.add(criteriaBuilder.equal(root.get(ActivityLog_.workspaceId), workspaceEntityId));
+    criteria.where(criteriaBuilder.and(predicates.toArray(new Predicate[0])));
+    criteria.orderBy(criteriaBuilder.asc(root.get(ActivityLog_.timestamp)));
+    return entityManager.createQuery(criteria).getResultList();
+  }
+  
+  //TODO: Add date parameters?
+  //Will CriteriaQuery<Long> work?
+  public List<Long> listWorkspaces(Long userEntityId) {
+    EntityManager entityManager = getEntityManager();
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Long> criteria = criteriaBuilder.createQuery(Long.class);
+    Root<ActivityLog> root = criteria.from(ActivityLog.class);
+    criteria.multiselect(root.get(ActivityLog_.workspaceId)).distinct(true);
+    criteria.where(criteriaBuilder.equal(root.get(ActivityLog_.userEntityId), userEntityId));
+    return entityManager.createQuery(criteria).getResultList();
   }
 }
--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/activitylog/rest/ActivityLogRESTService.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/activitylog/rest/ActivityLogRESTService.java
@@ -1,5 +1,63 @@
 package fi.otavanopisto.muikku.plugins.activitylog.rest;
 
-public class ActivityLogRESTService {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import fi.otavanopisto.muikku.plugin.PluginRESTService;
+import fi.otavanopisto.muikku.plugins.activitylog.ActivityLogController;
+import fi.otavanopisto.muikku.plugins.activitylog.model.ActivityLog;
+import fi.otavanopisto.security.rest.RESTPermit;
+import fi.otavanopisto.security.rest.RESTPermit.Handling;
+
+@RequestScoped
+@Stateful
+@Path("/activitylogs")
+@Produces("application/json")
+
+public class ActivityLogRESTService extends PluginRESTService {
   
+  private static final long serialVersionUID = 6892435956970819197L;
+  
+  @Inject
+  private Logger logger;
+  
+  @Inject
+  private ActivityLogController activityLogController;
+  
+  //TODO permissions? which ones?
+  @GET
+  @Path("/user/{USERENTITYID}/workspace/")
+  @RESTPermit(handling=Handling.INLINE)
+  public Response listWorkspaceActivityLogs(@PathParam("USERENTITYID") Long userEntityId,
+      @QueryParam("workspaceEntityId") Long workspaceEntityId,
+      @QueryParam("from") String from,
+      @QueryParam("to") String to) {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    Date beginDate;
+    Date endDate;
+    try {
+      beginDate = sdf.parse(from);
+      endDate = sdf.parse(to);
+    }
+    catch (ParseException e) {
+      return Response.status(Status.BAD_REQUEST).build();
+    }
+    List<ActivityLog> workspaceActivityLogs = activityLogController.listActivityLogsByUserEntityIdAndworkspaceEntityId(userEntityId, workspaceEntityId, beginDate, endDate);
+    return Response.ok(workspaceActivityLogs).build();
+  }
 }
