35de3ef3750165cf9190a3382d1046c2938fbb70
Ilmo Euro <ilmo.euro@gmail.com>
2018-10-24 17:23:58 +0300

    Metatavu/muikku#1 - DB updates for saved matriculation enrollments

--- matriculation-plugin-persistence/src/main/java/fi/otavanopisto/muikku/matriculation/persistence/model/SavedMatriculationEnrollment.java
+++ matriculation-plugin-persistence/src/main/java/fi/otavanopisto/muikku/matriculation/persistence/model/SavedMatriculationEnrollment.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
@@ -47,6 +48,6 @@ public class SavedMatriculationEnrollment {
   @Column(nullable = false, unique = true)
   private String userIdentifier;
   
-  @Column
+  @Column(columnDefinition = "TEXT")
   private String savedEnrollmentJson;
 }
