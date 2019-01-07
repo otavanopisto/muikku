ef109a7c82ac7fb63a3c8dcc1dd81c63758a3e64
onzag <ejgonzalezviera@gmail.com>
2019-01-07 13:44:53 +0200

    done file field

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-loader/fields/file-field.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-loader/fields/file-field.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { i18nType } from "~/reducers/base/i18n";
 import Link from "~/components/general/link";
+import $ from '~/lib/jquery';
+let ProgressBarLine = require('react-progressbar.js').Line;
 
 interface FileFieldProps {
   type: string,
@@ -10,17 +12,23 @@ interface FileFieldProps {
   i18n: i18nType,
   
   readOnly?: boolean,
-  value?: string
+  value?: string,
+  onChange?: (context: React.Component<any, any>, name: string, newValue: any)=>any
 }
 
 interface FileFieldState {
-  value: {
+  values: Array<{
+    //might not be there while uploading
     fileId?: string,
     name: string,
-    contentType: string
-  },
-  isUploading: boolean,
-  progress: number,
+    contentType: string,
+    uploading?: boolean,
+        
+    //only has a value while uploading
+    progress?: number,
+    file?: File
+  }>,
+  
   modified: boolean,
   synced: boolean
 }
@@ -30,27 +38,99 @@ export default class FileField extends React.Component<FileFieldProps, FileField
     super(props);
     
     this.state = {
-      value: (props.value && JSON.parse(props.value)[0]) || null,
-      isUploading: false,
-      progress: 0,
+      values: (props.value && JSON.parse(props.value)) || [],
       modified: false,
       synced: true
     }
+    
+    this.onFileChanged = this.onFileChanged.bind(this);
+    this.processFileAt = this.processFileAt.bind(this);
+    this.checkDoneAndRunOnChange = this.checkDoneAndRunOnChange.bind(this);
   }
   onFileChanged(e: React.ChangeEvent<HTMLInputElement>){
-    let file = e.target.files[0];
-    this.setState({
-      value: {
+    let newValues = Array.from(e.target.files).map((file)=>{
+      return {
         name: file.name,
-        contentType: file.type
-      },
-      isUploading: true
+        contentType: file.type,
+        uploading: true,
+        progress: 0,
+        file
+      }
+    });
+
+    let originalLenght = this.state.values.length;
+    this.setState({values: this.state.values.concat(newValues)}, ()=>{
+      newValues.forEach((value, index)=>{
+        let realIndex = index + originalLenght;
+        this.processFileAt(realIndex);
+      })
     });
   }
+  checkDoneAndRunOnChange(){
+    for (let value of this.state.values){
+      if (value.uploading){
+        return;
+      }
+    }
+    let result = JSON.stringify(this.state.values.map((value)=>{
+      let {
+        fileId,
+        name,
+        contentType,
+      } = value;
+      return {
+        fileId, name, contentType
+      }
+    }));
+    
+    this.props.onChange(this, this.props.content.name, result);
+  }
+  processFileAt(index: number){
+    let formData = new FormData();
+    let file:File = this.state.values[index].file;
+    formData.append("file", file);
+    $.ajax({
+      url: '/tempFileUploadServlet/',
+      type: 'POST',
+      data: formData,
+      success: (dataString: string)=>{
+        var data = JSON.parse(dataString);
+        let newValues = [...this.state.values];
+        newValues[index] = {...this.state.values[index]}
+        newValues[index].uploading = false;
+        newValues[index].fileId = data.id;
+        this.setState({
+          values: newValues
+        }, this.checkDoneAndRunOnChange)
+      },
+      error: (xhr:any, err:Error)=>{
+        
+      },
+      xhr: ()=>{
+        let xhr = new (window as any).XMLHttpRequest();
+        //Upload progress
+        xhr.upload.addEventListener("progress", (evt:any)=>{
+          if (evt.lengthComputable) {
+            let percentComplete = evt.loaded / evt.total;
+            let newValues = [...this.state.values];
+            newValues[index] = {...this.state.values[index]}
+            newValues[index].progress = percentComplete;
+            this.setState({
+              values: newValues
+            })
+          }
+        }, false);
+        return xhr;
+      },
+      cache: false,
+      contentType: false,
+      processData: false
+    })
+  }
   componentWillReceiveProps(nextProps: FileFieldProps){
-    let value = (nextProps.value && JSON.parse(nextProps.value)[0]) || null;
-    if (value !== this.state.value){
-      this.setState({value});
+    let values = (nextProps.value && JSON.parse(nextProps.value)) || null;
+    if (values !== this.state.values){
+      this.setState({values});
     }
     
     this.setState({
@@ -60,18 +140,42 @@ export default class FileField extends React.Component<FileFieldProps, FileField
   }
   render(){
     let dataInContainer = null;
-    if (this.state.value){
-      let isImage = this.state.value && this.state.value.contentType.indexOf("image") === 0;
-      if (!this.state.isUploading){
-        //You should set a class and set it up so that images can have a max width, right now it is so that the image can be humongous 
-        dataInContainer = <Link href={`/rest/workspace/fileanswer/${this.state.value.fileId}`} openInNewTab={this.state.value.name}>
-          {!isImage ? this.state.value.name : <img src={`/rest/workspace/fileanswer/${this.state.value.fileId}`}/>}
-        </Link>;
-      }
+    if (this.state.values.length){
+      dataInContainer = this.state.values.map((value, index)=>{
+        let isImage = value.contentType.indexOf("image") === 0;
+        
+        if (!value.uploading){
+          //You should set a class and set it up so that images can have a max width, right now it is so that the image can be humongous 
+          return <Link key={value.fileId} href={`/rest/workspace/fileanswer/${value.fileId}`} openInNewTab={value.name}>
+            {!isImage ? value.name : <img src={`/rest/workspace/fileanswer/${value.fileId}`}/>}
+          </Link>;
+        } else {
+          return <Link key={index}>
+            <ProgressBarLine containerClassName="clip flex-row flex-align-items-center" options={{
+              strokeWidth: 1,
+              duration: 1000,
+              color: "#ff9900",
+              trailColor: "#f5f5f5",
+              trailWidth: 1,
+              svgStyle: {width: "100%", height: "4px"},
+              text: {
+                className: "time-text-or-something",
+                style: {
+                   right: "100%"
+                }
+              }
+            }}
+            strokeWidth={1} easing="easeInOut" duration={1000} color="#ff9900" trailColor="#f5f5f5"
+            trailWidth={1} svgStyle={{width: "100%", height: "4px"}}
+            text={(value.progress * 100) + "%"}
+             progress={value.progress}/>
+          </Link>;
+        }
+      });
     }
     return <div className="muikku-file-input-field-file-uploader-container">
-      {this.props.readOnly ? null : <input type="file" onChange={this.onFileChanged}/>}
-      {!this.state.value ? 
+      {this.props.readOnly ? null : <input type="file" onChange={this.onFileChanged} multiple/>}
+      {!this.state.values.length ? 
         <span className="muikku-file-input-field-description">{this.props.i18n.text.get("plugin.workspace.fileField.fieldHint")}</span> : null}
       <div className="muikku-file-input-field-file-files-container">{dataInContainer}</div>
     </div>
