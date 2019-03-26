59019188f2ddcbbc6739f44549b304dc3d8c67b0
onzag <ejgonzalezviera@gmail.com>
2019-03-26 09:09:08 +0200

    begin permissions

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/workspace/workspacePermissions/index.tsx
@@ -0,0 +1,25 @@
+import WorkspaceNavbar from '~/components/base/workspace/navbar';
+import ScreenContainer from '~/components/general/screen-container';
+
+import * as React from 'react';
+
+interface WorkspacePermissionsBodyProps {
+  workspaceUrl: string
+}
+
+interface WorkspacePermissionsBodyState {
+
+}
+
+export default class WorkspacePermissionsBody extends React.Component<WorkspacePermissionsBodyProps, WorkspacePermissionsBodyState> {
+  constructor(props: WorkspacePermissionsBodyProps){
+    super(props);
+  }
+  render(){
+    return (<div>
+      <WorkspaceNavbar activeTrail="permissions" workspaceUrl={this.props.workspaceUrl}/>
+      <ScreenContainer viewModifiers="permissions">
+      </ScreenContainer>
+    </div>);
+  }
+}
