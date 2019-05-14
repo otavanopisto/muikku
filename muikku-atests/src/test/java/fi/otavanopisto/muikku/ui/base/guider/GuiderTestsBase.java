c912e621f406c59683e6135b7f9e82a5991480fb
petja-laitila <petja.laitila@otavanopisto.fi>
2019-05-14 14:21:51 +0300

    ** Tests that grade is shown in guider

--- muikku-atests/src/test/java/fi/otavanopisto/muikku/ui/base/guider/GuiderTestsBase.java
+++ muikku-atests/src/test/java/fi/otavanopisto/muikku/ui/base/guider/GuiderTestsBase.java
@@ -8,9 +8,13 @@ import java.time.ZoneOffset;
 
 import org.junit.Test;
 
+import fi.otavanopisto.muikku.TestEnvironments;
 import fi.otavanopisto.muikku.TestUtilities;
 import fi.otavanopisto.muikku.atests.Workspace;
+import fi.otavanopisto.muikku.atests.WorkspaceFolder;
+import fi.otavanopisto.muikku.atests.WorkspaceHtmlMaterial;
 import fi.otavanopisto.muikku.mock.PyramusMock.Builder;
+import fi.otavanopisto.muikku.mock.model.MockCourse;
 import fi.otavanopisto.muikku.mock.model.MockCourseStudent;
 import fi.otavanopisto.muikku.mock.model.MockStaffMember;
 import fi.otavanopisto.muikku.mock.model.MockStudent;
@@ -134,4 +138,115 @@ public class GuiderTestsBase extends AbstractUITest {
       mockBuilder.wiremockReset();
     }
   }
+  
+  @Test
+  @TestEnvironments (
+    browsers = {
+      TestEnvironments.Browser.CHROME,
+      TestEnvironments.Browser.CHROME_HEADLESS,
+      TestEnvironments.Browser.FIREFOX,
+      TestEnvironments.Browser.INTERNET_EXPLORER,
+      TestEnvironments.Browser.EDGE,
+      TestEnvironments.Browser.SAFARI
+    }
+  )
+  public void gradeShownInGuiderTest() throws Exception {
+    MockStaffMember admin = new MockStaffMember(1l, 1l, "Admin", "User", UserRole.ADMINISTRATOR, "121212-1234", "admin@example.com", Sex.MALE);
+    MockStudent student = new MockStudent(2l, 2l, "Student", "Tester", "student@example.com", 1l, OffsetDateTime.of(1990, 2, 2, 0, 0, 0, 0, ZoneOffset.UTC), "121212-1212", Sex.FEMALE, TestUtilities.toDate(2012, 1, 1), TestUtilities.getNextYear());
+    OffsetDateTime date = OffsetDateTime.of(2016, 11, 10, 1, 1, 1, 1, ZoneOffset.UTC);
+    Builder mockBuilder = mocker();
+    try{
+      mockBuilder.addStudent(student).addStaffMember(admin).mockLogin(admin).build();
+      Long courseId = 2l;
+      
+      login();
+      
+      Workspace workspace = createWorkspace("testcourses", "test course for testing", String.valueOf(courseId), Boolean.TRUE);
+      OffsetDateTime created = OffsetDateTime.of(2015, 10, 12, 12, 12, 0, 0, ZoneOffset.UTC);
+      OffsetDateTime begin = OffsetDateTime.of(2015, 10, 12, 12, 12, 0, 0, ZoneOffset.UTC);
+      OffsetDateTime end = OffsetDateTime.of(2045, 10, 12, 12, 12, 0, 0, ZoneOffset.UTC);
+      MockCourse mockCourse = new MockCourse(workspace.getId(), workspace.getName(), created, "test course", begin, end);
+      
+      MockCourseStudent courseStudent = new MockCourseStudent(2l, courseId, student.getId());
+      CourseStaffMember courseStaffMember = new CourseStaffMember(1l, courseId, admin.getId(), 7l);
+      mockBuilder
+        .addCourseStaffMember(courseId, courseStaffMember)
+        .addCourseStudent(courseId, courseStudent)
+        .build();
+
+      WorkspaceFolder workspaceFolder1 = createWorkspaceFolder(workspace.getId(), null, Boolean.FALSE, 1, "Test Course material folder", "DEFAULT");
+      
+      WorkspaceHtmlMaterial htmlMaterial = createWorkspaceHtmlMaterial(workspace.getId(), workspaceFolder1.getId(), 
+        "Test", "text/html;editor=CKEditor", 
+        "<p><object type=\"application/vnd.muikku.field.text\"><param name=\"type\" value=\"application/json\" /><param name=\"content\" value=\"{&quot;name&quot;:&quot;muikku-field-nT0yyez23QwFXD3G0I8HzYeK&quot;,&quot;rightAnswers&quot;:[],&quot;columns&quot;:&quot;&quot;,&quot;hint&quot;:&quot;&quot;}\" /></object></p>", 1l, 
+        "EVALUATED");
+      
+      try {
+        logout();
+        mockBuilder.mockLogin(student);
+        login();
+      
+        navigate(String.format("/workspace/%s/materials", workspace.getUrlName()), false);
+        waitForPresent(String.format("#page-%d", htmlMaterial.getId()));
+        
+        assertVisible(String.format("#page-%d .muikku-text-field", htmlMaterial.getId()));
+        assertValue(String.format("#page-%d .muikku-text-field", htmlMaterial.getId()), "");
+        assertClassNotPresent(String.format("#page-%d .muikku-text-field", htmlMaterial.getId()), "muikku-field-saved");
+        sendKeys(String.format("#page-%d .muikku-text-field", htmlMaterial.getId()), "field value");
+        waitClassPresent(String.format("#page-%d .muikku-text-field", htmlMaterial.getId()), "muikku-field-saved");
+        waitAndClick(String.format("#page-%d .muikku-submit-assignment", htmlMaterial.getId()));
+        waitForPresentAndVisible(".notification-queue-item-success");
+        waitForElementToBeClickable(String.format("#page-%d .muikku-withdraw-assignment", htmlMaterial.getId()));
+        mockBuilder
+          .mockAssessmentRequests(student.getId(), courseId, courseStudent.getId(), "Hello!", false, false, date)
+          .mockCompositeGradingScales()
+          .addCompositeCourseAssessmentRequest(student.getId(), courseId, courseStudent.getId(), "Hello!", false, false, TestUtilities.courseFromMockCourse(mockCourse), student, date)
+          .mockCompositeCourseAssessmentRequests()
+          .addStaffCompositeAssessmentRequest(student.getId(), courseId, courseStudent.getId(), "Hello!", false, false, TestUtilities.courseFromMockCourse(mockCourse), student, admin.getId(), date)
+          .mockStaffCompositeCourseAssessmentRequests();
+        
+        logout();
+        mockBuilder.mockLogin(admin);
+        login();
+//        navigate(String.format("/evaluation2"), false);
+//        waitAndClick(".evaluate-button");
+//        
+//        waitForPresent(".eval-modal-evaluate-buttonset .button-start-evaluation");
+//        waitAndClick(".eval-modal-evaluate-buttonset .button-start-evaluation");
+//        
+//        waitUntilAnimationIsDone(".eval-modal #workspaceGradeEditorContainer");
+//        if(getBrowser().equals("chrome_headless")) {
+//          sleep(500);
+//        }
+//  
+//        waitForPresent("#workspaceGradeEditorContainer .eval-modal-evaluate-workspace-content #cke_workspaceGradeText .cke_contents");
+//        getWebDriver().switchTo().frame(findElementByCssSelector("#workspaceGradeEditorContainer .eval-modal-evaluate-workspace-content #cke_workspaceGradeText .cke_wysiwyg_frame"));
+//        sendKeys(".cke_contents_ltr", "Test evaluation.");
+//        getWebDriver().switchTo().defaultContent();
+//        
+//        selectOption("#workspaceGradeGrade", "PYRAMUS-1@PYRAMUS-1");
+//        
+        mockBuilder
+        .addStaffCompositeAssessmentRequest(student.getId(), courseId, courseStudent.getId(), "Hello!", false, true, TestUtilities.courseFromMockCourse(mockCourse), student, admin.getId(), date)
+        .mockStaffCompositeCourseAssessmentRequests()
+        .mockAssessmentRequests(student.getId(), courseId, courseStudent.getId(), "Hello!", false, true, date);
+      
+        mockBuilder.mockCourseAssessments(courseStudent, admin);          
+//        waitAndClick("#workspaceGradeSave");
+//        waitForPresent(".notification-queue-item-success");
+        navigate("/guider", false);
+        waitAndClick(".application-list__header-primary>span");
+        waitForPresent(".application-list__header-secondary.workspace-activity .workspace-student__assessment-state>span>span");
+        assertText(".application-list__header-secondary.workspace-activity .workspace-student__assessment-state>span>span", "E");
+      }finally {
+        deleteWorkspaceHtmlMaterial(workspace.getId(), htmlMaterial.getId());
+        deleteWorkspace(workspace.getId());
+      }
+    }finally {
+      archiveUserByEmail(student.getEmail());
+      mockBuilder.wiremockReset();  
+    }
+  }
+      
+  
 }
