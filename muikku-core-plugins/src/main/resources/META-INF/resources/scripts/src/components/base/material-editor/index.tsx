b0d55bcf2374e3978a72615848e31cf9edb58c67
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2019-05-21 15:10:26 +0300

    * Producers styles

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-editor/index.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-editor/index.tsx
@@ -347,7 +347,7 @@ class MaterialEditor extends React.Component<MaterialEditorProps, MaterialEditor
   
                 <div className="material-editor__list-producers-container">
                   {this.props.editorState.currentDraftNodeValue.producers.map((p, index) => {
-                    return <div className="material-editor__producer" key={index}>{p.name}<span className="material-editor__remove-producer icon-close" onClick={this.removeProducer.bind(this, index)}></span></div>
+                    return <div className="material-editor__producer" key={index}>{p.name}<ButtonPill buttonModifiers="material-editor-remove-producer" onClick={this.removeProducer.bind(this, index)} icon="close"/></div>
                   })}
                 </div>
               </div>
@@ -382,7 +382,7 @@ class MaterialEditor extends React.Component<MaterialEditorProps, MaterialEditor
 
       return <div className={`material-editor ${this.props.editorState.opened ? "material-editor--visible" : ""}`}>
         <Tabs modifier="material-editor" activeTab={this.state.tab} onTabChange={this.changeTab} tabs={allTabs}>
-          <ButtonPill buttonModifiers="material-page-close-editor" onClick={this.close} icon="close"/>
+          <ButtonPill buttonModifiers="material-page-close-editor" onClick={this.close} icon="arrow-left-thin"/>
         </Tabs>
 
         <ConfirmPublishPageWithAnswersDialog/>
