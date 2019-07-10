030cc5dacef37e867ec42b0f1194f381bdf5192c
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2019-07-10 11:08:19 +0300

    * Added incomplete to grade component with proper styling and locales.

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-loader/grade.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-loader/grade.tsx
@@ -9,7 +9,9 @@ export function MaterialLoaderGrade(props: MaterialLoaderGradeProps) {
     (props.compositeReplies && props.compositeReplies.evaluationInfo && props.compositeReplies.evaluationInfo.grade);
   
   if (!grade) {
-    return null;
+    return (<div className="material-page__assignment-assessment-grade">
+      <span className="material-page__assignment-assessment-grade-data material-page__assignment-assessment-grade-data--incomplete"> {props.i18n.text.get("plugin.workspace.materialsLoader.evaluation.grade.incomplete")}</span>
+    </div>);
   }
 
   return (<div className="material-page__assignment-assessment-grade">
