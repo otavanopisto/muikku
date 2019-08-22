892e0300cb15ef5dd88e9d0089c0a24bcba5829f
petja-laitila <petja.laitila@otavanopisto.fi>
2019-08-22 09:33:38 +0300

    ** Test fix

--- muikku-atests/src/test/java/fi/otavanopisto/muikku/ui/base/tor/ToRTestsBase.java
+++ muikku-atests/src/test/java/fi/otavanopisto/muikku/ui/base/tor/ToRTestsBase.java
@@ -195,7 +195,7 @@ public class ToRTestsBase extends AbstractUITest {
   @Test
   public void studiesSummaryTest() throws Exception {
     MockStaffMember admin = new MockStaffMember(1l, 1l, "Admin", "User", UserRole.ADMINISTRATOR, "121212-1234", "admin@example.com", Sex.MALE);
-    MockStudent student = new MockStudent(2l, 2l, "Student", "Tester", "student@example.com", 1l, OffsetDateTime.of(1990, 2, 2, 0, 0, 0, 0, ZoneOffset.UTC), "121212-1212", Sex.FEMALE, TestUtilities.toDate(2012, 1, 1), TestUtilities.toDate(2035, 1, 1));
+    MockStudent student = new MockStudent(4l, 4l, "Studenter", "Tester", "studenter@example.com", 1l, OffsetDateTime.of(1990, 2, 2, 0, 0, 0, 0, ZoneOffset.UTC), "121212-1212", Sex.FEMALE, TestUtilities.toDate(2012, 1, 1), TestUtilities.toDate(2035, 1, 1));
     OffsetDateTime date = OffsetDateTime.of(2016, 11, 10, 1, 1, 1, 1, ZoneOffset.UTC);
     Builder mockBuilder = mocker();
     try{
@@ -210,7 +210,7 @@ public class ToRTestsBase extends AbstractUITest {
       login();
       
       Workspace workspace = createWorkspace(course1, Boolean.TRUE);
-      MockCourseStudent courseStudent = new MockCourseStudent(2l, courseId, student.getId());
+      MockCourseStudent courseStudent = new MockCourseStudent(4l, courseId, student.getId());
       CourseStaffMember courseStaffMember = new CourseStaffMember(1l, courseId, admin.getId(), 7l);
       mockBuilder
         .addCourseStaffMember(courseId, courseStaffMember)
