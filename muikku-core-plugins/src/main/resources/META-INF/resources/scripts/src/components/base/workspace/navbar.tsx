a6c7a00b03374eea539eff20a3dc5734165ac5a4
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2019-10-31 14:30:54 +0200

    * Altered workspace navbar so announcer and settings are their own separate icon and admin dropdown is gone. Permissions will be added to the settings view as a new section.

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/workspace/navbar.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/workspace/navbar.tsx
@@ -154,6 +154,14 @@ class WorkspaceNavbar extends React.Component<WorkspaceNavbarProps, WorkspaceNav
   }
   render(){
     const itemData: ItemDataElement[] = [{
+      modifier: "settings",
+      trail: "workspace-management",
+      text: 'plugin.workspace.dock.workspace-edit',
+      href: "/workspace/" + this.props.workspaceUrl + "/workspace-management",
+      icon: "cogs",
+      to: true,
+      condition: this.props.status.permissions.WORKSPACE_MANAGE_WORKSPACE,
+    },{
       modifier: "home",
       trail: "index",
       text: 'plugin.workspace.dock.home',
@@ -202,6 +210,14 @@ class WorkspaceNavbar extends React.Component<WorkspaceNavbarProps, WorkspaceNav
       to: true,
       condition: this.props.status.permissions.WORKSPACE_JOURNAL_VISIBLE
     },{
+      modifier: "announcer",
+      trail: "workspace-announcer",
+      text: 'plugin.workspace.dock.workspace-announcements',
+      href: "/workspace/" + this.props.workspaceUrl + "/announcer",
+      icon: "paper-plane",
+      to: true,
+      condition: this.props.status.permissions.WORKSPACE_ANNOUNCER_TOOL
+    },{
       //Evaluation is also an external
       modifier: "evaluation",
       trail: "evaluation",
@@ -210,7 +226,7 @@ class WorkspaceNavbar extends React.Component<WorkspaceNavbarProps, WorkspaceNav
       icon: "evaluate",
       condition: this.props.status.permissions.EVALUATION_VIEW_INDEX,
       openInNewTab: "_blank"
-    }, ];
+    }];
 
   let assessmentRequestItem = this.props.currentWorkspace &&
     this.props.status.permissions.WORKSPACE_REQUEST_WORKSPACE_ASSESSMENT ? {
@@ -231,106 +247,35 @@ class WorkspaceNavbar extends React.Component<WorkspaceNavbarProps, WorkspaceNav
     <span className="link--menu__text">{getTextForAssessmentState(this.props.currentWorkspace.studentAssessments.assessmentState, this.props.i18n)}</span>
   </Link>) : null;
 
-  let managementItemList:Array<{
-    icon: string,
-    modifier: string,
-    href: string,
-    text: string,
-    visible: boolean,
-    trail: string,
-    to?: boolean
-  }> = [
-    {
-      icon: "cogs",
-      modifier: "navigation",
-      href: "/workspace/" + this.props.workspaceUrl + "/workspace-management",
-      text: this.props.i18n.text.get("plugin.workspace.dock.workspace-edit"),
-      visible: this.props.status.permissions.WORKSPACE_MANAGE_WORKSPACE,
-      trail: "workspace-management",
-      to: true
-    },
-    {
-      icon: "paper-plane",
-      modifier: "navigation",
-      href: "/workspace/" + this.props.workspaceUrl + "/announcer",
-      text: this.props.i18n.text.get("plugin.workspace.dock.workspace-announcements"),
-      visible: this.props.status.permissions.WORKSPACE_ANNOUNCER_TOOL,
-      trail: "workspace-announcer",
-      to: true
-    },
-    {
-      icon: "key",
-      modifier: "navigation",
-      href: "/workspace/" + this.props.workspaceUrl + "/permissions",
-      text: this.props.i18n.text.get("plugin.workspace.dock.workspace-permissions"),
-      visible: this.props.status.permissions.WORKSPACE_MANAGE_PERMISSIONS,
-      trail: "workspace-permissions",
-      to: true
-    }
-  ]
+  let trueNavigation:Array<React.ReactElement<any>> = [];
+  if (this.props.navigation){
+    trueNavigation.push(this.props.navigation);
+  }
 
-  let managementItem = this.props.currentWorkspace &&
-    (this.props.status.permissions.WORKSPACE_MANAGE_WORKSPACE ||
-    this.props.status.permissions.WORKSPACE_MANAGE_PERMISSIONS || 
-    this.props.status.permissions.WORKSPACE_MANAGE_WORKSPACE_MATERIALS ||
-    this.props.status.permissions.WORKSPACE_MANAGE_WORKSPACE_FRONTPAGE || 
-    this.props.status.permissions.WORKSPACE_MANAGE_WORKSPACE_HELP ||
-    this.props.status.permissions.WORKSPACE_ANNOUNCER_TOOL) ? {
-      modifier: "workspace-management",
-      item: (<Dropdown key="workspace-management" modifier="navigation" items={managementItemList.map(item=>{
-        if (!item.visible){
-          return null
-        }
-        return <Link className={`link link--full link--navigation-dropdown link--${item.modifier} ${this.props.activeTrail === item.trail ? "active" : ""}`}
-          href={this.props.activeTrail !== item.trail ? item.href : null} to={item.to && this.props.activeTrail !== item.trail ? item.href : null}>
-          <span className={`link__icon icon-${item.icon}`}></span>
-          <span>{item.text}</span>
-        </Link>
-      })}>
-        <Link className={`link link--icon link--full link--workspace-navbar`}>
-          <span className="link__icon icon-cogs"/>
-        </Link>
-      </Dropdown>)
-    } : null;
+  let editModeSwitch = null;
+  if (this.props.workspaceEditMode.available){
+     editModeSwitch = (<input key="3" type="checkbox"
+         className={`button-pill button-pill--editing-master-switch ${this.props.workspaceEditMode.active ? "button-pill--editing-master-switch-active" : ""}`}
+         onChange={this.toggleEditModeActive}
+         checked={this.props.workspaceEditMode.active}/>)
+  }
 
-    let trueNavigation:Array<React.ReactElement<any>> = [];
-    if (this.props.navigation){
-      trueNavigation.push(this.props.navigation);
+  return <Navbar mobileTitle={this.props.title}
+    modifier="workspace" navigation={trueNavigation} navbarItems={[
+      assessmentRequestItem,
+      //managementItem
+    ].concat(itemData.map((item)=>{
+    if (!item.condition){
+      return null;
     }
-    if (managementItem){
-      trueNavigation.push(<Navigation>
-        <NavigationTopic icon="cogs" name={this.props.i18n.text.get("plugin.workspace.dock.workspace-edit")}>
-          {managementItemList.map((item, index)=>{
-            return <NavigationElement isActive={this.props.activeTrail === item.trail} key={index} href={item.href}>{item.text}</NavigationElement>
-          })}
-        </NavigationTopic>
-      </Navigation>)
+    return {
+      modifier: item.modifier,
+      item: (<Link  openInNewTab={item.openInNewTab} href={this.props.activeTrail !== item.trail ? item.href : null} to={item.to && this.props.activeTrail !== item.trail ? item.href : null} className={`link link--icon link--full link--workspace-navbar ${this.props.activeTrail === item.trail ? 'active' : ''}`}
+        aria-label={this.props.i18n.text.get(item.text)}>
+        <span className={`link__icon icon-${item.icon}`}/>
+        {item.badge ? <span className="indicator indicator--workspace">{(item.badge >= 100 ? "99+" : item.badge)}</span> : null}
+      </Link>)
     }
-
-    let editModeSwitch = null;
-    if (this.props.workspaceEditMode.available){
-       editModeSwitch = (<input key="3" type="checkbox"
-           className={`button-pill button-pill--editing-master-switch ${this.props.workspaceEditMode.active ? "button-pill--editing-master-switch-active" : ""}`}
-           onChange={this.toggleEditModeActive}
-           checked={this.props.workspaceEditMode.active}/>)
-    }
-
-    return <Navbar mobileTitle={this.props.title}
-      modifier="workspace" navigation={trueNavigation} navbarItems={[
-        assessmentRequestItem,
-        managementItem
-      ].concat(itemData.map((item)=>{
-      if (!item.condition){
-        return null;
-      }
-      return {
-        modifier: item.modifier,
-        item: (<Link  openInNewTab={item.openInNewTab} href={this.props.activeTrail !== item.trail ? item.href : null} to={item.to && this.props.activeTrail !== item.trail ? item.href : null} className={`link link--icon link--full link--workspace-navbar ${this.props.activeTrail === item.trail ? 'active' : ''}`}
-          aria-label={this.props.i18n.text.get(item.text)}>
-          <span className={`link__icon icon-${item.icon}`}/>
-          {item.badge ? <span className="indicator indicator--workspace">{(item.badge >= 100 ? "99+" : item.badge)}</span> : null}
-        </Link>)
-      }
     }))} defaultOptions={this.props.status.loggedIn ? [editModeSwitch] : [
       (<LoginButton modifier="login-main-function" key="0"/>),
       (<ForgotPasswordDialog key="1"><Link className="link link--forgot-password link--forgot-password-main-function">
