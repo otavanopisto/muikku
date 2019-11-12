9bc24adbac26e01ca43158b874b3ce339b0b186a
Pasi Kukkonen <pasi.kukkonen@otavanopisto.fi>
2019-11-12 10:46:15 +0200

    - multiple help pages support

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/WorkspaceHelpPageManagementBackingBean.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/WorkspaceHelpPageManagementBackingBean.java
@@ -1,6 +1,5 @@
 package fi.otavanopisto.muikku.plugins.workspace;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,7 +16,6 @@ import org.ocpsoft.rewrite.annotation.RequestAction;
 
 import fi.otavanopisto.muikku.jsf.NavigationRules;
 import fi.otavanopisto.muikku.model.workspace.WorkspaceEntity;
-import fi.otavanopisto.muikku.plugins.workspace.model.WorkspaceMaterial;
 import fi.otavanopisto.muikku.plugins.workspace.model.WorkspaceRootFolder;
 import fi.otavanopisto.muikku.schooldata.WorkspaceController;
 import fi.otavanopisto.muikku.security.MuikkuPermissions;
@@ -73,8 +71,7 @@ public class WorkspaceHelpPageManagementBackingBean extends AbstractWorkspaceBac
     workspaceName = workspaceBackingBean.getWorkspaceName();
 
     try {
-      WorkspaceMaterial helpPage = workspaceMaterialController.ensureWorkspaceHelpPageExists(workspaceEntity);
-      contentNodes = Arrays.asList(workspaceMaterialController.createContentNode(helpPage, null));
+      contentNodes = workspaceMaterialController.listWorkspaceHelpPagesAsContentNodes(workspaceEntity);
     }
     catch (WorkspaceMaterialException e) {
       logger.log(Level.SEVERE, "Error loading materials", e);
