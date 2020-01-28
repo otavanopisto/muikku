b0959c717fd80bde4a6751d120afd005ccff2d73
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2020-01-28 14:49:19 +0200

    * Fixed controlbox subpanel z-index so new room creation panel can be closed * Set converse to use sessionStorage instead of localStorage so in development we can clean browser storage easier * Added config commented option to converse that should be activated in production, persistent_store: "IndexedDB" to we use indexedDB instead of localStorage

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/util/base-main-function.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/util/base-main-function.ts
@@ -50,8 +50,10 @@ export default function(store: Store<StateType>){
             muc_domain : "conference." + location.hostname,
             muc_history_max_stanzas: 0, // Should be 0 if MAM (message_archiving: "always") is in use
             muc_show_join_leave: true,
+            persistent_store: "IndexedDB",
             ping_interval: 45,
             prebind_url : "/rest/chat/prebind",
+            //trusted: "on",
             // websocket_url: This is worth to check out if we can use websockets instead of BOSH
             whitelisted_plugins: ["myplugin","addRoom", "profileChatSettings"]
           });
