7250364ed43d8460353d7901ab397073440a7214
petja-laitila <petja.laitila@otavanopisto.fi>
2020-02-11 09:34:49 +0200

    ** base-main-function.ts changes for chat initialisation

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/util/base-main-function.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/util/base-main-function.ts
@@ -31,29 +31,35 @@ export default function(store: Store<StateType>){
     store.dispatch(<Action>updateUnreadMessageThreadsCount());
 
     if (state.status.loggedIn){
-      mApi().chat.status.read().callback(function(err:Error, result:{mucNickName:string,enabled:true}) {
+      mApi().chat.status.read().callback(function(err:Error, result:{mucNickName:string,nick:string,enabled:true}) {
         if (result && result.enabled) {
           converse.initialize({
             authentication : "prebind",
+            prebind_url : "/rest/chat/prebind",
+            bosh_service_url: "/http-bind/",
+            default_domain: location.hostname,
             auto_away: 300,
             auto_login: true,
             auto_reconnect: true,
-            bosh_service_url: "/http-bind/",
             fullname: result.mucNickName,
-            hide_muc_server: true,
+            nickname: result.nick,
             i18n: state.locales.current,
             jid: state.status.userSchoolDataIdentifier,
             keepalive: true,
             locales: ["fi", "en"],
             logLevel: "debug", // Needs to be set to info when in production mode
-            // message_archiving: "always",
+            message_archiving: "always",
+            hide_muc_server: false,
+            allow_muc: true,
             muc_domain : "conference." + location.hostname,
-            muc_history_max_stanzas: 25, // Should be 0 if MAM (message_archiving: "always") is in use
+//			muc_history_max_stanzas: 25,            
+			muc_history_max_stanzas: 0, // Should be 0 if MAM (message_archiving: "always") is in use
             muc_show_join_leave: true,
-            // persistent_store: "IndexedDB", // Activate this for production
+//            muc_nickname_from_jid: true,
+//            allow_non_roster_messaging: true,
+            persistent_store: "localStorage", // Activate this for production (It'll use either or anyway, so might as well have something here)
             ping_interval: 45,
-            prebind_url : "/rest/chat/prebind",
-            trusted: "off", // Force sessionStorage instead of localStorage or IndexedDB - FOR DEVELOPMENT ONLY
+//            trusted: "off", // Tämä oli päällä. Force sessionStorage instead of localStorage or IndexedDB - FOR DEVELOPMENT ONLY
             whitelisted_plugins: ["myplugin","addRoom", "profileChatSettings"]
           });
         }
