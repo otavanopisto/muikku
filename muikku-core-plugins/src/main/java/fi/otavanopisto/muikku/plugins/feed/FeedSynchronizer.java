c7d9a7eaafc66e11569ee4939a43c7b51b2615ad
Pasi Kukkonen <pasi.kukkonen@otavanopisto.fi>
2020-02-19 10:24:40 +0200

    - feed sync + privacy url

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/feed/FeedSynchronizer.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/feed/FeedSynchronizer.java
@@ -1,24 +1,19 @@
 package fi.otavanopisto.muikku.plugins.feed;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.util.List;
 import java.util.logging.Logger;
 
-import javax.annotation.Resource;
-import javax.ejb.EJBContext;
 import javax.ejb.Schedule;
-import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
+import javax.ejb.Singleton;
 import javax.inject.Inject;
-import javax.ws.rs.RedirectionException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Entities.EscapeMode;
@@ -27,76 +22,93 @@ import org.jsoup.safety.Whitelist;
 
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 
 import fi.otavanopisto.muikku.plugins.feed.model.Feed;
+import fi.otavanopisto.muikku.plugins.feed.model.FeedItem;
 
-@Stateless
+@Singleton
 public class FeedSynchronizer {
 
   @Inject
-  private FeedDAO feedDao;
+  private FeedDAO feedDAO;
 
   @Inject
-  private FeedItemDAO feedItemDao;
+  private FeedItemDAO feedItemDAO;
 
   @Inject
   private Logger logger;
   
-  @Resource
-  EJBContext ejbContext;
-  
   private String clean(String html) {
     Document doc = Jsoup.parse(html);
-    doc = new Cleaner(Whitelist.basic()).clean(doc);
+    doc = new Cleaner(Whitelist.basicWithImages()).clean(doc);
     doc.select("a[target]").attr("rel", "noopener noreferer");
     doc.outputSettings().escapeMode(EscapeMode.xhtml);
     return doc.body().html();
   }
 
   @Schedule(second = "0", minute = "0", hour = "*", persistent = false)
-  @TransactionAttribute(TransactionAttributeType.REQUIRED)
   public void updateFeeds() {
     Client client = ClientBuilder.newClient();
-    
-    feedItemDao.deleteAll();
-
-    List<Feed> feeds = feedDao.listAll();
-
+    List<Feed> feeds = feedDAO.listAll();
     for (Feed feed : feeds) {
-      
       WebTarget target = client.target(feed.getUrl());
       Reader reader = null;
       try {
+        
+        // Read feed
+        
         InputStream stream = target.request("*").get(InputStream.class);
         reader = new XmlReader(stream);
         SyndFeedInput input = new SyndFeedInput();
         SyndFeed syndFeed = input.build(reader);
         List<SyndEntry> entries = syndFeed.getEntries();
         
+        // List old entries
+        
+        List<FeedItem> feedItems = feedItemDAO.listByFeed(feed);
+
+        // Create/update entries from feed
+        
         for (SyndEntry entry : entries) {
-          feedItemDao.create(
-              entry.getTitle(),
-              entry.getLink(),
-              entry.getAuthor(),
-              entry.getDescription() == null
-                  ? null
-                  : clean(entry.getDescription().getValue()),
-              entry.getPublishedDate(),
-              (String)null,
-              feed);
+          FeedItem existingItem = feedItems.stream().filter(feedItem -> StringUtils.equals(feedItem.getLink(), entry.getLink())).findFirst().orElse(null);
+          if (existingItem == null) {
+            feedItemDAO.create(
+                entry.getTitle(),
+                entry.getLink(),
+                entry.getAuthor(),
+                entry.getDescription() == null ? null : clean(entry.getDescription().getValue()),
+                entry.getPublishedDate(),
+                null, // TODO thumbnail image
+                feed);
+          }
+          else {
+            feedItemDAO.update(
+                existingItem,
+                entry.getTitle(),
+                entry.getLink(),
+                entry.getAuthor(),
+                entry.getDescription() == null ? null : clean(entry.getDescription().getValue()),
+                entry.getPublishedDate(),
+                null);  // TODO thumbnail image
+            feedItems.remove(existingItem);
+          }
+        }
+        
+        // Remove deprecated entries
+
+        for (FeedItem feedItem : feedItems) {
+          feedItemDAO.delete(feedItem);
         }
       }
-      catch (IOException | IllegalArgumentException | FeedException | RedirectionException e) {
-        logger.warning(String.format("Error while synchronizing feeds: %s", e.getMessage()));
-        
-        ejbContext.setRollbackOnly();
+      catch (Exception e) {
+        logger.warning(String.format("Error synchronizing feed %ss: %s", feed.getName(), e.getMessage()));
       }
       finally {
         IOUtils.closeQuietly(reader);
       }
     }
   }
+
 }
