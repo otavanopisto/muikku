769c4dfc36b340162c2d1ddc0f2387c133c580f7
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2020-04-22 10:10:01 +0300

    * image zoom adjustments

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/zoom.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/zoom.tsx
@@ -13,39 +13,39 @@ interface ZoomState {
 export default class Zoom extends React.Component<ZoomProps, ZoomState>{
   constructor(props: ZoomProps){
     super(props);
-    
+
     this.state = {
       zoomed: false,
     }
-    
+
     this.toggleZoom = this.toggleZoom.bind(this);
   }
-  
+
   toggleZoom() {
     this.setState({
       zoomed: !this.state.zoomed,
     });
   }
-    
+
   render() {
     let zoomComponent: React.ReactNode = null;
     if (this.state.zoomed) {
-      const img = <div className="zoom-image" style={{backgroundImage: "url(" + this.props.imgsrc + ")"}}/>
-      zoomComponent = <div className="zoom">
-        <div className="zoom-children">
+      const img = <img className="zoom__zoomed-item" src={this.props.imgsrc}/> //  style={{backgroundImage: "url(" + this.props.imgsrc + ")"}}
+      zoomComponent = <div className="zoom__zoomed-item-overlay">
+        <div className="zoom__zoomed-item-container">
           {img}
-          <div className="zoom-close icon-cross"/>
+          <div className="zoom__zoomed-item-close icon-cross"/>
         </div>
       </div>
     }
     return(
-      <div onClick={this.toggleZoom} className="zoom-container">
-        <div className="zoom-container-clickable-item">
+      <div onClick={this.toggleZoom} className="zoom">
+        <div className="zoom__clickable-item">
           {this.props.children}
         </div>
         {zoomComponent}
       </div>
     )
   }
-  
+
 }
