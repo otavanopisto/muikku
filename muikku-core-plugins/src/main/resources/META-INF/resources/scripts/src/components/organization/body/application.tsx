b0da5814e71402cb82a9acf44e6d27a27592d7d1
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2020-06-02 14:30:18 +0300

    comments, removal of unneeded code

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/organization/body/application.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/organization/body/application.tsx
@@ -19,8 +19,6 @@ import '~/sass/elements/application-panel.scss';
 import '~/sass/elements/loaders.scss';
 
 
-const tabNames = ["summary", "users", "courses", "reports"];
-
 interface OrganizationManagementApplicationProps {
   aside: React.ReactElement<any>,
   i18n: i18nType
@@ -47,6 +45,9 @@ class OrganizationManagementApplication extends React.Component<OrganizationMana
 
   render(){
         let title = <h2 className="application-panel__header-title">{this.props.i18n.text.get('plugin.organization.pageTitle')}</h2>;
+        
+        // This "Mode" could be just changed to "userData" and the dialog just gets by that the mode is "edit". But that belongs to the user edit issue. 
+        
         let usersPrimaryAction = <UserDialog mode="CREATE"><ButtonPill buttonModifiers="organization" icon="plus" /></UserDialog>;
         let coursesPrimaryAction = <WorkspaceDialog><ButtonPill buttonModifiers="organization" icon="plus" /></WorkspaceDialog>;
         let coursesToolbar = <ApplicationPanelToolbar>
