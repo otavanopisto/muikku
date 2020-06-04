b362bbd42ce15640a4bca75b3a46714aded47a92
anttiviljakainen <antti.viljakainen@otavanopisto.fi>
2020-06-04 13:44:56 +0300

    Restrict environment roles from all workspace access'

--- muikku-atests-plugin/src/main/java/fi/otavanopisto/muikku/atests/PermissionsAcceptanceTestsRESTService.java
+++ muikku-atests-plugin/src/main/java/fi/otavanopisto/muikku/atests/PermissionsAcceptanceTestsRESTService.java
@@ -2,13 +2,19 @@ package fi.otavanopisto.muikku.atests;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import fi.otavanopisto.muikku.model.workspace.WorkspaceEntity;
 import fi.otavanopisto.muikku.plugin.PluginRESTService;
+import fi.otavanopisto.muikku.schooldata.WorkspaceEntityController;
+import fi.otavanopisto.muikku.session.SessionController;
 import fi.otavanopisto.security.rest.RESTPermit;
 import fi.otavanopisto.security.rest.RESTPermit.Handling;
 
@@ -21,6 +27,12 @@ public class PermissionsAcceptanceTestsRESTService extends PluginRESTService {
 
   private static final long serialVersionUID = 8129061062696701210L;
 
+  @Inject
+  private SessionController sessionController;
+
+  @Inject
+  private WorkspaceEntityController workspaceEntityController;
+  
   @GET
   @Path("/restpermit/loggedin")
   @RESTPermit (handling = Handling.INLINE, requireLoggedIn = true)
@@ -62,5 +74,32 @@ public class PermissionsAcceptanceTestsRESTService extends PluginRESTService {
   public Response restPermitEveryone() {
     return Response.noContent().build();
   }
+  
+  @GET
+  @Path("/environmentpermissions/{PERMISSIONNAME}")
+  @RESTPermit (handling = Handling.UNSECURED)
+  public Response testEnvironmentRestPermit(@PathParam("PERMISSIONNAME") String permissionName) {
+    if (sessionController.hasEnvironmentPermission(permissionName)) {
+      return Response.noContent().build();
+    } else {
+      return Response.status(Status.FORBIDDEN).build();
+    }
+  }
 
+  @GET
+  @Path("/workspaces/{WORKSPACEID}/permissions/{PERMISSIONNAME}")
+  @RESTPermit (handling = Handling.UNSECURED)
+  public Response testWorkspaceRestPermit(@PathParam("WORKSPACEID") Long workspaceId, @PathParam("PERMISSIONNAME") String permissionName) {
+    WorkspaceEntity workspaceEntity = workspaceEntityController.findWorkspaceEntityById(workspaceId);
+
+    if (workspaceEntity == null) {
+      return Response.status(Status.BAD_REQUEST).entity("Given workspace was not found.").build();
+    }
+
+    if (sessionController.hasWorkspacePermission(permissionName, workspaceEntity)) {
+      return Response.noContent().build();
+    } else {
+      return Response.status(Status.FORBIDDEN).build();
+    }
+  }
 }
