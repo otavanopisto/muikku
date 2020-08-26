adb5362a8ddc41b633eb0fbc08f1d3e87edb6fe9
Jari Lankinen <lankinen@pop-os.localdomain>
2020-08-26 13:45:33 +0300

    * Formatting

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/util/base-main-function.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/util/base-main-function.ts
@@ -48,76 +48,76 @@ export default function(store: Store<StateType>, options: {
       mApi().chat.status.read().callback(function(err:Error, result:{mucNickName:string,nick:string,enabled:boolean}) {
         if (result && result.enabled) {
           converse.initialize({
-              // * * * * * * * * * * * * * * *
-              // Thou shall not touch these
-              // * * * * * * * * * * * * * * *
-              muc_domain : "conference." + location.hostname,
-              authentication : "prebind",
-              prebind_url : "/rest/chat/prebind",
-              bosh_service_url: "/http-bind/",
-              default_domain: location.hostname,
-              auto_login: true,
-              auto_reconnect: true,
-              fullname: result.mucNickName,
-              nickname: result.nick,
-              i18n: state.locales.current,
-              //jid: state.status.userSchoolDataIdentifier,
-              keepalive: true,
-              // This forces the use of nickname set in Muikku user's profile view
-              // This is default value but added here as a reminder not to touch this as Muikku Chat UI requires this setting for displaying nick names/real names.
-              muc_nickname_from_jid: false,
-              // * * * * * * * * * * * * * * *
+            // * * * * * * * * * * * * * * *
+            // Thou shall not touch these
+            // * * * * * * * * * * * * * * *
+            muc_domain : "conference." + location.hostname,
+            authentication : "prebind",
+            prebind_url : "/rest/chat/prebind",
+            bosh_service_url: "/http-bind/",
+            default_domain: location.hostname,
+            auto_login: true,
+            auto_reconnect: true,
+            fullname: result.mucNickName,
+            nickname: result.nick,
+            i18n: state.locales.current,
+            //jid: state.status.userSchoolDataIdentifier,
+            keepalive: true,
+            // This forces the use of nickname set in Muikku user's profile view
+            // This is default value but added here as a reminder not to touch this as Muikku Chat UI requires this setting for displaying nick names/real names.
+            muc_nickname_from_jid: false,
+            // * * * * * * * * * * * * * * *
 
-              // - - - - - - - - - - - - - - -
-              // Thou shall touch these
-              // - - - - - - - - - - - - - - -
+            // - - - - - - - - - - - - - - -
+            // Thou shall touch these
+            // - - - - - - - - - - - - - - -
 
-              // Prevents converse to create instant rooms automatically. This prevents converse to create instant rooms automatically based on local/sessionStorage information.
-              // If room is set as disabled via workspace settings and room gets deleted from openfore properly, converse added room back as an instant room.
-              muc_instant_rooms: false,
-              muc_respect_autojoin: false,
+            // Prevents converse to create instant rooms automatically. This prevents converse to create instant rooms automatically based on local/sessionStorage information.
+            // If room is set as disabled via workspace settings and room gets deleted from openfore properly, converse added room back as an instant room.
+            muc_instant_rooms: false,
+            muc_respect_autojoin: false,
 
-              // Sets timeout when user is marked away automatically
-              auto_away: 300,
+            // Sets timeout when user is marked away automatically
+            auto_away: 300,
 
-              // Supported locales for converse. If Muikku has more langauges in future we need to update this.
-              locales: ["fi", "en"],
+            // Supported locales for converse. If Muikku has more langauges in future we need to update this.
+            locales: ["fi", "en"],
 
-              // Needs to be set to "info" when in production mode
-              loglevel: "debug",
+            // Needs to be set to "info" when in production mode
+            loglevel: "debug",
 
-              // We try to archive every message, openfire needs to have archiving turned on with chat rooms also
-              message_archiving: "always",
+            // We try to archive every message, openfire needs to have archiving turned on with chat rooms also
+            message_archiving: "always",
 
-              // This defines the maximum amount of archived messages to be returned per query.
-              archived_messages_page_size: 50,
+            // This defines the maximum amount of archived messages to be returned per query.
+            archived_messages_page_size: 50,
 
-              // Should be 0 if MAM (message_archiving: "always") is in use
-              muc_history_max_stanzas: 0,
+            // Should be 0 if MAM (message_archiving: "always") is in use
+            muc_history_max_stanzas: 0,
 
-              // If you set this setting to true, then you will be notified of all messages received in a room.
-              //notify_all_room_messages: true,
+            // If you set this setting to true, then you will be notified of all messages received in a room.
+            //notify_all_room_messages: true,
 
-              // This allows students and teachers private messages between each other without adding recipient to contacts
-              allow_non_roster_messaging: true,
+            // This allows students and teachers private messages between each other without adding recipient to contacts
+            allow_non_roster_messaging: true,
 
-              // We could set this to IndexedDB so we don't bump into 5MB limit of local storage (not very like to happen).
-              // IndexedDB is also async and might be more future proof.
-              //persistent_store: "localStorage",
-              persistent_store: "IndexedDB",
+            // We could set this to IndexedDB so we don't bump into 5MB limit of local storage (not very like to happen).
+            // IndexedDB is also async and might be more future proof.
+            //persistent_store: "localStorage",
+            persistent_store: "IndexedDB",
 
-              // How often converse pings in milliseconds
-              ping_interval: 45,
+            // How often converse pings in milliseconds
+            ping_interval: 45,
 
-              // Force sessionStorage instead of localStorage or IndexedDB - FOR DEVELOPMENT ONLY.
-              // For production this option needs to be removed.
-              //trusted: "false",
+            // Force sessionStorage instead of localStorage or IndexedDB - FOR DEVELOPMENT ONLY.
+            // For production this option needs to be removed.
+            //trusted: "false",
 
-              // Plugins that can be used
-              whitelisted_plugins: ["muikku-chat-ui"],
-              // - - - - - - - - - - - - - - -
+            // Plugins that can be used
+            whitelisted_plugins: ["muikku-chat-ui"],
+            // - - - - - - - - - - - - - - -
 
-              blacklisted_plugins: ["converse-bookmarks"]
+            blacklisted_plugins: ["converse-bookmarks"]
           });
         }
       });
