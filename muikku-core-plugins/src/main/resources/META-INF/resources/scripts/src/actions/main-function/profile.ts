eac7fba6376557e92a5fc3fc7ca38fbbe7bfd9fa
onzag <ejgonzalezviera@gmail.com>
2020-10-14 14:09:55 +0300

    dispose of converse

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/actions/main-function/profile.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/actions/main-function/profile.ts
@@ -2,12 +2,10 @@ import promisify, { promisifyNewConstructor } from '~/util/promisify';
 import actions from '../base/notifications';
 import {AnyActionType, SpecificActionType} from '~/actions';
 import mApi, { MApiError } from '~/lib/mApi';
-import {UserType, StudentUserAddressType,StudentUserProfileChatType, UserWithSchoolDataType, UserChatSettingsType} from '~/reducers/main-function/user-index';
+import { StudentUserAddressType, UserWithSchoolDataType, UserChatSettingsType} from '~/reducers/main-function/user-index';
 import { StateType } from '~/reducers';
-import $ from '~/lib/jquery';
 import { resize } from '~/util/modifiers';
 import { updateStatusProfile, updateStatusHasImage } from '~/actions/base/status';
-import converse from '~/lib/converse';
 
 export interface LoadProfilePropertiesSetTriggerType {
   ():AnyActionType
