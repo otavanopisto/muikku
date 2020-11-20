d40f7523a2f687b75ed341d839a5a7e7f8d287a6
Jari Lankinen <lankinen@pop-os.localdomain>
2020-11-20 13:26:21 +0200

    * Fixed empty fodler not showing search results

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/communicator/body/application/messages.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/communicator/body/application/messages.tsx
@@ -90,7 +90,7 @@ class CommunicatorMessages extends BodyScrollLoader<CommunicatorMessagesProps, C
       //TODO: put a translation here please! this happens when messages fail to load, a notification shows with the error
       //message but here we got to put something
       return <div className="empty"><span>{"ERROR"}</span></div>
-    } else if (this.props.threads.length === 0 && !this.props.currentThread) {
+    } else if (this.props.threads.length === 0 && !this.props.currentThread && !this.props.searchMessages) {
       return <div className="empty"><span>{this.props.i18n.text.get("plugin.communicator.empty.topic")}</span></div>
     }
 
