862179a0e8159df0c2915ebfb528bb0257d98e7e
petja-laitila <petja.laitila@otavanopisto.fi>
2020-12-16 10:45:07 +0200

    Snapshot deploys for devel pushes

--- prepare-maven.py
+++ prepare-maven.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 
 ns = 'http://maven.apache.org/SETTINGS/1.0.0'
 
-if (("OOM2_USER" in os.environ) and ("OOM2_PASS" in os.environ)):
+if (("OOM2_USER" in os.environ) and ("OOM2_PASSWORD" in os.environ)):
   homedir = os.path.expanduser("~")
 
   ET.register_namespace('', ns)
@@ -19,17 +19,17 @@ if (("OOM2_USER" in os.environ) and ("OOM2_PASS" in os.environ)):
   oo_snapshots = ET.SubElement(servers, 'server')
   ET.SubElement(oo_snapshots, 'id').text = 'otavanopisto-snapshots'
   ET.SubElement(oo_snapshots, 'username').text = os.environ['OOM2_USER']
-  ET.SubElement(oo_snapshots, 'password').text = os.environ['OOM2_PASS']
+  ET.SubElement(oo_snapshots, 'password').text = os.environ['OOM2_PASSWORD']
 
   oo_releases = ET.SubElement(servers, 'server')
   ET.SubElement(oo_releases, 'id').text = 'otavanopisto-releases'
   ET.SubElement(oo_releases, 'username').text = os.environ['OOM2_USER']
-  ET.SubElement(oo_releases, 'password').text = os.environ['OOM2_PASS']
+  ET.SubElement(oo_releases, 'password').text = os.environ['OOM2_PASSWORD']
 
   m2.write(homedir + '/.m2/mySettings.xml',
           xml_declaration = True,
           encoding = 'utf-8',
           method = 'xml')
 else:
-  print("no secure env vars available, skipping deployment")
+  print("no secure environment variables available, skipping deployment")
   sys.exit()
