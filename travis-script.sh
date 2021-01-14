fd5611192d6ea38a5efa06f04457695e7de10740
petja <petja.laitila@otavanopisto.fi>
2021-01-14 13:25:20 +0200

    Removes all travis related files

--- travis-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ $run_tests == "true" && $test_suite == $suite || $run_tests == "true" && $it_profile == "rest-it" ]]; then
-  echo would run tests
-else
-  echo Skipping testing.
-fi;
