aa367957395ff3ae7061fbd1e8251a8b88ae62a2
onzag <ejgonzalezviera@gmail.com>
2021-02-17 13:09:29 +0200

    pure carousel

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/frontpage/body/videos.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/frontpage/body/videos.tsx
@@ -13,22 +13,22 @@ interface FrontpageVideosState {
 export default class FrontpageVideos extends React.Component<FrontpageVideosProps, FrontpageVideosState> {
   render() {
     return <section id="videos" className="screen-container__section" aria-label={this.props.i18n.text.get("plugin.wcag.frontPageSectionVideosLabel")}>
-      <Carousel>
-        <CarouselVideoItem>
+      <Carousel naturalSlideHeight={720} naturalSlideWidth={1280} totalSlides={3}>
+        <CarouselVideoItem index={0}>
           <iframe width="1280" height="720"
             src="https://www.youtube.com/embed/IvP595DJ5fM?rel=0&amp;showinfo=0"
             style={{ border: 0 }}
             allowFullScreen={true}
             title="Nettilukio.fi - Treenien rinnalla | Robert Ven"></iframe>
         </CarouselVideoItem>
-        <CarouselVideoItem>
+        <CarouselVideoItem index={1}>
           <iframe width="1280" height="720"
             src="https://www.youtube.com/embed/CJcpWZD0VT8?rel=0&amp;showinfo=0"
             style={{ border: 0 }}
             allowFullScreen={true}
             title="Nettilukiolaisten ajatuksia"></iframe>
         </CarouselVideoItem>
-        <CarouselVideoItem>
+        <CarouselVideoItem index={2}>
           <iframe width="1280" height="720"
             src="https://www.youtube.com/embed/iOKUoAAQ7Uk?rel=0&amp;showinfo=0"
             style={{ border: 0 }}
