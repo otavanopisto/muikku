661f418a9290c5af4ba612ecd053209f79453a6a
petja <petja.laitila@otavanopisto.fi>
2021-02-22 13:28:58 +0200

    Removes unused scriptUrl from AbstractWCAGTest

--- muikku-atests/src/test/java/fi/otavanopisto/muikku/wcag/AbstractWCAGTest.java
+++ muikku-atests/src/test/java/fi/otavanopisto/muikku/wcag/AbstractWCAGTest.java
@@ -8,8 +8,6 @@ import org.junit.Before;
 import fi.otavanopisto.muikku.ui.AbstractUITest;
 
 public class AbstractWCAGTest extends AbstractUITest {
-
-  protected static final URL scriptUrl = AbstractWCAGTest.class.getResource("/axe.min.js");
   
   @Before
   public void setUp() {
