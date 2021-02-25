d6b66cb7884fe3ca36b16f22bf47871476da6d81
petja <petja.laitila@otavanopisto.fi>
2021-02-25 08:59:59 +0200

    Don't use explicit URLs so tests will work in different environments
    as long as there is one message thread

--- muikku-atests/src/test/java/fi/otavanopisto/muikku/wcag/discussions/DiscussionsAT.java
+++ muikku-atests/src/test/java/fi/otavanopisto/muikku/wcag/discussions/DiscussionsAT.java
@@ -11,13 +11,13 @@ public class DiscussionsAT extends AbstractWCAGTest{
   @Test
   public void discussionsTest() throws FileNotFoundException {
     login(getTestStudent(), getTestStudentPassword(), true);
-    navigate("/discussion", false);
+    navigate("/discussion", true);
     waitForVisible(".application-panel--discussion");
     testAccessibility("Discussions view");
-    navigate("/discussion#1/1/1/1", true);
+    waitAndClick(".application-list__item.message:first-child");
     waitForVisible(".application-list__item--discussion-message");
     testAccessibility("Discussions single thread");
-    navigate("/discussion", false);
+    navigate("/discussion", true);
     waitAndClick(".application-panel__helper-container--discussion a.button--primary-function");
     waitForVisible(".env-dialog__header");
     testAccessibility("Discussions new message");
@@ -26,17 +26,20 @@ public class DiscussionsAT extends AbstractWCAGTest{
   @Test
   public void discussionsDeleteMessageDialogTest() throws FileNotFoundException {
     login(getTestAdmin(), getTestAdminPassword(), true);
-    navigate("/discussion#1/1/1/1", true);
+    navigate("/discussion", true);
+    waitAndClick(".application-list__item.message:first-child");
     waitAndClick(".application-list__item--discussion-message .link--application-list-item-footer:nth-child(4)");
     waitForVisible(".dialog--delete-area .button--standard-ok");
     testAccessibility("Discussions delete message dialog");
 
-    navigate("/discussion#1/1/1/1", true);
+    navigate("/discussion", true);
+    waitAndClick(".application-list__item.message:first-child");
     waitAndClick(".application-list__item--discussion-message .link--application-list-item-footer:nth-child(3)");
     waitForVisible(".env-dialog__row--new-discussion-thread-states");
     testAccessibility("Discussions edit message dialog");
     
-    navigate("/discussion#1/1/1/1", true);
+    navigate("/discussion", true);
+    waitAndClick(".application-list__item.message:first-child");
     waitAndClick(".application-list .application-list__item--discussion-message+div.application-list__item--discussion-reply .link--application-list-item-footer:nth-child(4)");
     waitForVisible(".dialog--delete-area .button--standard-ok");
     testAccessibility("Discussions delete message reply dialog");
