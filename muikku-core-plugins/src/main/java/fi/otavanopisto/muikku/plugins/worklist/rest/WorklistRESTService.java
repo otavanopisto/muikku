a51e24c18b4e3a227d43915b26fbce48df8e68d9
Pasi Kukkonen <pasi.kukkonen@otavanopisto.fi>
2021-03-08 10:57:00 +0200

    - resolves #5375

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/worklist/rest/WorklistRESTService.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/worklist/rest/WorklistRESTService.java
@@ -84,7 +84,7 @@ public class WorklistRESTService {
    * Output: New worklist item
    * 
    * {id: 123
-   *  entryDate: 2021-03-01T12:43:23
+   *  entryDate: 2021-03-01
    *  description: "Course assessment"
    *  price: 75
    *  factor: 1
@@ -114,7 +114,7 @@ public class WorklistRESTService {
   /**
    * PUT mApi().worklist.worklistItems
    * 
-   * Updates an existing worklist item.
+   * Updates an existing worklist item (entryDate, description, price, factor).
    * 
    * Payload: An existing worklist item
    * 
