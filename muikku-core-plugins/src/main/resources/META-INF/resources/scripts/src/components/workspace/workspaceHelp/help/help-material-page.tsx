c254bf393a8ba20fcc35c7bcb12c4b8e59ed77c8
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2021-04-20 14:32:13 +0300

    plugins disabled when creating pages in help page

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/workspace/workspaceHelp/help/help-material-page.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/workspace/workspaceHelp/help/help-material-page.tsx
@@ -4,8 +4,16 @@ import { Dispatch, connect } from "react-redux";
 import { i18nType } from "~/reducers/base/i18n";
 
 import MaterialLoader from "~/components/base/material-loader";
-import { MaterialContentNodeType, WorkspaceType, MaterialCompositeRepliesType, WorkspaceEditModeStateType } from "~/reducers/workspaces";
-import { setCurrentWorkspace, SetCurrentWorkspaceTriggerType } from "~/actions/workspaces";
+import {
+  MaterialContentNodeType,
+  WorkspaceType,
+  MaterialCompositeRepliesType,
+  WorkspaceEditModeStateType,
+} from "~/reducers/workspaces";
+import {
+  setCurrentWorkspace,
+  SetCurrentWorkspaceTriggerType,
+} from "~/actions/workspaces";
 import { bindActionCreators } from "redux";
 import { MaterialLoaderEditorButtonSet } from "~/components/base/material-loader/editor-buttonset";
 import { MaterialLoaderTitle } from "~/components/base/material-loader/title";
@@ -20,84 +28,118 @@ import LazyLoader from "~/components/general/lazy-loader";
 import { StatusType } from "~/reducers/base/status";
 
 interface HelpMaterialProps {
-  i18n: i18nType,
-  status: StatusType,
-  workspaceEditMode: WorkspaceEditModeStateType,
-  materialContentNode: MaterialContentNodeType,
-  folder: MaterialContentNodeType,
-  isViewRestricted: boolean,
-  workspace: WorkspaceType,
-  setCurrentWorkspace: SetCurrentWorkspaceTriggerType,
+  i18n: i18nType;
+  status: StatusType;
+  workspaceEditMode: WorkspaceEditModeStateType;
+  materialContentNode: MaterialContentNodeType;
+  folder: MaterialContentNodeType;
+  isViewRestricted: boolean;
+  workspace: WorkspaceType;
+  setCurrentWorkspace: SetCurrentWorkspaceTriggerType;
 }
 
-interface HelpMaterialState {
-}
+interface HelpMaterialState {}
 
-class WorkspaceMaterial extends React.Component<HelpMaterialProps, HelpMaterialState> {
-  constructor(props: HelpMaterialProps){
+class WorkspaceMaterial extends React.Component<
+  HelpMaterialProps,
+  HelpMaterialState
+> {
+  constructor(props: HelpMaterialProps) {
     super(props);
     this.updateWorkspaceActivity = this.updateWorkspaceActivity.bind(this);
   }
-  updateWorkspaceActivity(){
+  updateWorkspaceActivity() {
     //This function is very efficient and reuses as much data as possible so it won't call anything from the server other than
     //to refresh the activity and that's because we are forcing it to do so
-    this.props.setCurrentWorkspace({workspaceId: this.props.workspace.id, refreshActivity: true});
+    this.props.setCurrentWorkspace({
+      workspaceId: this.props.workspace.id,
+      refreshActivity: true,
+    });
   }
-  render(){
-    const isAssignment = this.props.materialContentNode.assignmentType === "EVALUATED";
+  render() {
+    const isAssignment =
+      this.props.materialContentNode.assignmentType === "EVALUATED";
     const isBinary = this.props.materialContentNode.type === "binary";
-    let evalStateClassName:string = "";
-    let evalStateIcon:string = "";
+    let evalStateClassName: string = "";
+    let evalStateIcon: string = "";
 
-    return <LazyLoader useChildrenAsLazy={true} className="material-lazy-loader-container">
-      {(loaded: boolean) => {
-        return <MaterialLoader canPublish
-          canRevert canCopy={!isBinary} canHide canDelete canRestrictView canChangePageType={!isBinary}
-          canChangeExerciseType={!isBinary} canSetLicense={!isBinary} canSetProducers={!isBinary}
-          canAddAttachments={!isBinary} canEditContent={!isBinary} folder={this.props.folder}
-          editable={this.props.workspaceEditMode.active}
-          material={this.props.materialContentNode} workspace={this.props.workspace}
-          answerable={this.props.status.loggedIn}
-          readOnly={!this.props.status.loggedIn}
-          onAssignmentStateModified={this.updateWorkspaceActivity}
-          invisible={!loaded} isViewRestricted={this.props.isViewRestricted}>
-          {(props, state, stateConfiguration) => {
-            return <div>
-              <MaterialLoaderEditorButtonSet {...props} {...state}/>
-              <MaterialLoaderTitle {...props} {...state}/>
-              <MaterialLoaderContent {...props} {...state} stateConfiguration={stateConfiguration}/>
-              <div className="material-page__de-floater"></div>
-              <MaterialLoaderCorrectAnswerCounter {...props} {...state}/>
-              {isAssignment ?
-                <div className={`material-page__assignment-assessment ${evalStateClassName}`}>
-                  <div className={`material-page__assignment-assessment-icon ${evalStateIcon}`}></div>
-                  <MaterialLoaderDate {...props} {...state}/>
-                  <MaterialLoaderGrade {...props} {...state}/>
-                  <MaterialLoaderAssesment {...props} {...state}/>
-                </div>
-              : null}
-              <MaterialLoaderProducersLicense {...props} {...state}/>
-            </div>
-          }}
-        </MaterialLoader>
-      }}
-    </LazyLoader>
+    return (
+      <LazyLoader
+        useChildrenAsLazy={true}
+        className="material-lazy-loader-container"
+      >
+        {(loaded: boolean) => {
+          return (
+            <MaterialLoader
+              canPublish
+              canRevert
+              canCopy={!isBinary}
+              canHide
+              canDelete
+              canRestrictView
+              disablePlugins
+              canChangePageType={!isBinary}
+              canChangeExerciseType={!isBinary}
+              canSetLicense={!isBinary}
+              canSetProducers={!isBinary}
+              canAddAttachments={!isBinary}
+              canEditContent={!isBinary}
+              folder={this.props.folder}
+              editable={this.props.workspaceEditMode.active}
+              material={this.props.materialContentNode}
+              workspace={this.props.workspace}
+              answerable={this.props.status.loggedIn}
+              readOnly={!this.props.status.loggedIn}
+              onAssignmentStateModified={this.updateWorkspaceActivity}
+              invisible={!loaded}
+              isViewRestricted={this.props.isViewRestricted}
+            >
+              {(props, state, stateConfiguration) => {
+                return (
+                  <div>
+                    <MaterialLoaderEditorButtonSet {...props} {...state} />
+                    <MaterialLoaderTitle {...props} {...state} />
+                    <MaterialLoaderContent
+                      {...props}
+                      {...state}
+                      stateConfiguration={stateConfiguration}
+                    />
+                    <div className="material-page__de-floater"></div>
+                    <MaterialLoaderCorrectAnswerCounter {...props} {...state} />
+                    {isAssignment ? (
+                      <div
+                        className={`material-page__assignment-assessment ${evalStateClassName}`}
+                      >
+                        <div
+                          className={`material-page__assignment-assessment-icon ${evalStateIcon}`}
+                        ></div>
+                        <MaterialLoaderDate {...props} {...state} />
+                        <MaterialLoaderGrade {...props} {...state} />
+                        <MaterialLoaderAssesment {...props} {...state} />
+                      </div>
+                    ) : null}
+                    <MaterialLoaderProducersLicense {...props} {...state} />
+                  </div>
+                );
+              }}
+            </MaterialLoader>
+          );
+        }}
+      </LazyLoader>
+    );
   }
 }
 
-function mapStateToProps(state: StateType){
+function mapStateToProps(state: StateType) {
   return {
     i18n: state.i18n,
     workspaceEditMode: state.workspaces.editMode,
     status: state.status,
-  }
-};
+  };
+}
 
-function mapDispatchToProps(dispatch: Dispatch<any>){
-  return bindActionCreators({setCurrentWorkspace}, dispatch);
-};
+function mapDispatchToProps(dispatch: Dispatch<any>) {
+  return bindActionCreators({ setCurrentWorkspace }, dispatch);
+}
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(WorkspaceMaterial);
+export default connect(mapStateToProps, mapDispatchToProps)(WorkspaceMaterial);
