745fc1bc6a82ea2e8aebbdd92fc426ee1009764a
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2021-04-23 12:19:02 +0300

    Removed unnecessary console.logs

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/actions/main-function/messages/index.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/actions/main-function/messages/index.ts
@@ -206,7 +206,6 @@ let toggleAllMessageItems: ToggleSelectAllMessageThreadsTriggerType = function t
 }
 
 let sendMessage: SendMessageTriggerType = function sendMessage( message ) {
-  console.log("message.to",message.to);
   let recepientWorkspaces = message.to.filter( x => x.type === "workspace" ).map( x => x.value.id )
   let data = {
     caption: message.subject,
@@ -217,7 +216,6 @@ let sendMessage: SendMessageTriggerType = function sendMessage( message ) {
     recipientStudentsWorkspaceIds: recepientWorkspaces,
     recipientTeachersWorkspaceIds: recepientWorkspaces
   };
-  console.log("data",data);
 
   return async ( dispatch: ( arg: AnyActionType ) => any, getState: () => StateType ) => {
     if (!message.subject){
