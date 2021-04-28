ed530d8877e0c0963b2ff0a9f55d2d76bd27c0eb
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2021-04-28 11:19:15 +0300

    More code fine tuning and cleaning

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/index/body/announcements-panel.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/index/body/announcements-panel.tsx
@@ -41,7 +41,6 @@ class AnnouncementsPanel extends React.Component<AnnouncementsPanelProps, Announ
    * @param prevState
    */
   componentDidUpdate(prevProps: AnnouncementsPanelProps, prevState: AnnouncementsPanelState){
-    console.log("componentDidUpdate 1");
     if(JSON.stringify(prevProps.announcements) !== JSON.stringify(this.props.announcements)){
       this.setState({
         announcements: this.props.announcements
@@ -75,34 +74,37 @@ class AnnouncementsPanel extends React.Component<AnnouncementsPanelProps, Announ
      * renders announcements
      */
     const renderAnnouncements = currentAnnouncements.map((announcement: AnnouncementType)=>{
-      let extraWorkspaces = announcement.workspaces && announcement.workspaces.length ? announcement.workspaces.length - 1 : 0;
-      return <Link key={announcement.id} className={`item-list__item item-list__item--announcements ${announcement.workspaces.length ? "item-list__item--has-workspaces" : ""}`}
-        href={`/announcements#${announcement.id}`} to={`/announcements#${announcement.id}`}>
-        <span className="item-list__icon item-list__icon--announcements icon-paper-plane"></span>
-        <span className="item-list__text-body item-list__text-body--multiline">
-          <span className="item-list__announcement-caption">
-            {announcement.caption}
+      const extraWorkspaces = announcement.workspaces && announcement.workspaces.length ? announcement.workspaces.length - 1 : 0;
+      return (
+        <Link key={announcement.id} className={`item-list__item item-list__item--announcements ${announcement.workspaces.length ? "item-list__item--has-workspaces" : ""}`}
+          href={`/announcements#${announcement.id}`} to={`/announcements#${announcement.id}`}>
+          <span className="item-list__icon item-list__icon--announcements icon-paper-plane"></span>
+          <span className="item-list__text-body item-list__text-body--multiline">
+            <span className="item-list__announcement-caption">
+              {announcement.caption}
+            </span>
+            <span className="item-list__announcement-date">
+              {this.props.i18n.time.format(announcement.startDate)}
+            </span>
+            {announcement.workspaces && announcement.workspaces.length ?
+              <div className="labels item-list__announcement-workspaces">
+                <span className="label">
+                  <span className="label__icon label__icon--announcement-workspace icon-books"></span>
+                  <span className="label__text label__text--announcement-workspace">{announcement.workspaces[0].name} {announcement.workspaces[0].nameExtension ? "(" + announcement.workspaces[0].nameExtension + ")" : null }</span>
+                </span>
+                {extraWorkspaces ? <span className="label">{"(+" + extraWorkspaces + ")"}</span> : null}
+              </div> : null}
           </span>
-          <span className="item-list__announcement-date">
-            {this.props.i18n.time.format(announcement.startDate)}
-          </span>
-          {announcement.workspaces && announcement.workspaces.length ?
-            <div className="labels item-list__announcement-workspaces">
-              <span className="label">
-                <span className="label__icon label__icon--announcement-workspace icon-books"></span>
-                <span className="label__text label__text--announcement-workspace">{announcement.workspaces[0].name} {announcement.workspaces[0].nameExtension ? "(" + announcement.workspaces[0].nameExtension + ")" : null }</span>
-              </span>
-              {extraWorkspaces ? <span className="label">{"(+" + extraWorkspaces + ")"}</span> : null}
-            </div> : null}
-        </span>
-      </Link>
+        </Link>
+      )
     })
 
     /**
      * renders pagination body as one of announcements list item
      */
-    const renderPaginationBody = (<div className="item-list__item item-list__item--announcements" >
-      <span className="item-list__text-body item-list__text-body--multiline" style={{display:"inline-block", textAlign:"center", margin:0}}>
+    const renderPaginationBody = (
+      <div className="item-list__item item-list__item--announcements" >
+        <span className="item-list__text-body item-list__text-body--multiline" style={{display:"inline-block", textAlign:"center", margin:0}}>
           <Pagination 
             innerClass="pagination"
             activePage={currentPage}
@@ -112,26 +114,29 @@ class AnnouncementsPanel extends React.Component<AnnouncementsPanelProps, Announ
             onChange={this.handlePageChange}
            />
         </span>
-      </div>);
-
-    return (<div className="panel panel--announcements">
-      <div className="panel__header">
-        <div className="panel__header-icon panel__header-icon--announcements icon-paper-plane"></div>
-        <h2 className="panel__header-title">{this.props.i18n.text.get('plugin.frontPage.announcements.title')}</h2>
       </div>
-      {this.props.announcements.length ? (
-        <div className="panel__body">
-          <div className="item-list item-list--panel-announcements">
-            {renderAnnouncements}
-          </div>
-          {renderPaginationBody}        
+    );
+
+    return (
+      <div className="panel panel--announcements">
+        <div className="panel__header">
+          <div className="panel__header-icon panel__header-icon--announcements icon-paper-plane"></div>
+          <h2 className="panel__header-title">{this.props.i18n.text.get('plugin.frontPage.announcements.title')}</h2>
         </div>
-        ) : (
-          <div className="panel__body panel__body--empty">
-            {this.props.i18n.text.get("plugin.frontPage.announcements.noAnnouncements")}
+        {this.props.announcements.length ? (
+          <div className="panel__body">
+            <div className="item-list item-list--panel-announcements">
+              {renderAnnouncements}
+            </div>
+            {renderPaginationBody}        
           </div>
-      )}
-    </div>);
+          ) : (
+            <div className="panel__body panel__body--empty">
+              {this.props.i18n.text.get("plugin.frontPage.announcements.noAnnouncements")}
+            </div>
+        )}
+      </div>
+    );
   }
 }
 
