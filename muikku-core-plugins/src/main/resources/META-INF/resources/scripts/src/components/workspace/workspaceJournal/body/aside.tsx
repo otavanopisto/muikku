1e93f78bfea6133f72d1038591ef38b2a7dcf3cb
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2021-05-06 14:00:35 +0300

    Teachers journal mobile view reworked: - Student list is moved to mobile side menu - Student select menu removed from mobile view size - minor journal list styling changes

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/workspace/workspaceJournal/body/aside.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/workspace/workspaceJournal/body/aside.tsx
@@ -8,32 +8,127 @@ import '~/sass/elements/buttons.scss';
 import '~/sass/elements/item-list.scss';
 import { StateType } from '~/reducers';
 import Navigation, { NavigationTopic, NavigationElement } from '~/components/general/navigation';
+import { WorkspaceType } from '../../../../reducers/workspaces/index';
+import { StatusType } from '../../../../reducers/base/status';
+import { getName } from '../../../../util/modifiers';
+import { bindActionCreators } from 'redux';
+import { loadStudentsOfWorkspace, loadCurrentWorkspaceJournalsFromServer } from '~/actions/workspaces';
+import { LoadUsersOfWorkspaceTriggerType, LoadCurrentWorkspaceJournalsFromServerTriggerType } from '../../../../actions/workspaces/index';
+import { WorkspaceStudentListType } from '../../../../reducers/user-index';
 
 interface NavigationAsideProps {
-  i18n: i18nType,
+  i18n: i18nType;
+  workspace: WorkspaceType;
+  status: StatusType;
+  loadStudents: LoadUsersOfWorkspaceTriggerType;
+  loadCurrentWorkspaceJournalsFromServer: LoadCurrentWorkspaceJournalsFromServerTriggerType;
 }
 
 interface NavigationAsideState {
+  students: WorkspaceStudentListType | null
 }
 
 class NavigationAside extends React.Component<NavigationAsideProps, NavigationAsideState> {
-  render() {
-    return <Navigation>
-      <NavigationTopic>
+  
+  constructor(props:NavigationAsideProps){
+    super(props);
 
-      </NavigationTopic>
+    this.state = {
+      students: null
+    }
+
+  }
+
+  componentDidUpdate(prevProps: NavigationAsideProps, prevState: NavigationAsideState){
+    if(JSON.stringify(prevProps.workspace) !== JSON.stringify(this.props.workspace)){
+      
+      this.setState({
+        students: this.props.workspace.students
+      })
+    }
+  }
+
+  /**
+   * Handles student changes and loads specific students journals to global state
+   * @param id
+   * @returns
+   */
+  handleOnStudentClick = (id:number | null) => (e: React.MouseEvent) => {
+
+    this.props.loadCurrentWorkspaceJournalsFromServer(id);
+
+  }
+  
+  /**
+   * render
+   * @returns
+   */
+  render() {
+    const { students } = this.state
+
+    const navigationElementList: JSX.Element[] = [];
+
+    /*
+      const allSelected = this.props.workspace.journals !== null && this.props.workspace.journals.userEntityId === null;
+    */
+
+    navigationElementList.push(<li key="showAll" onClick={this.handleOnStudentClick(null)} className={`item-list__item item-list__item--aside-navigation`}>
+      <span className="item-list__icon icon-user"></span>
+      <span className="link link--full link--menu ">{this.props.i18n.text.get("plugin.workspace.journal.studentFilter.showAll")}</span></li>);
+
+    if(students !== null){
+      !this.props.status.isStudent && students  ? (students.results)
+      .filter((student, index, array) =>
+        array.findIndex((otherStudent) => otherStudent.userEntityId === student.userEntityId) === index
+      ).map((student) => {
+        const selected = student.userEntityId === this.props.workspace.journals.userEntityId;
+
+        navigationElementList.push(<li onClick={this.handleOnStudentClick(student.userEntityId)} className={`item-list__item item-list__item--aside-navigation ${selected && "active"}`} key={student.userEntityId}>
+          <span className="item-list__icon icon-user"></span>
+          <span className="link link--full link--menu">{getName(student, true)}</span></li>)
+      }) : undefined;
+    }
+      
+    return <Navigation>
+      {!this.props.status.isStudent && (
+        <div className="journal__student__list__container">
+          <NavigationTopic name="Opiskelijat">
+            <ul className="journal__student__List">
+              {navigationElementList}
+            </ul>
+          </NavigationTopic>
+        </div>
+      )}
     </Navigation>
   }
 }
 
+/**
+ * mapStateToProps
+ * @param state
+ * @returns
+ */
 function mapStateToProps( state: StateType ) {
   return {
-    i18n: state.i18n
+    i18n: state.i18n,
+    workspace: state.workspaces.currentWorkspace,
+    status: state.status
   }
 };
 
+/**
+ * mapDispatchToProps
+ * @param dispatch
+ * @returns
+ */
 function mapDispatchToProps( dispatch: Dispatch<any> ) {
-  return {};
+  return bindActionCreators(
+    {
+      loadStudents: loadStudentsOfWorkspace,
+      loadCurrentWorkspaceJournalsFromServer
+    },
+    dispatch
+  );;
 };
 
 export default connect(
