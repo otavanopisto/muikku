5b9f3e62a4ae89d401fcb51727879baff5955bb0
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2021-06-03 12:10:35 +0300

    Subpanel component without styling, locales summary

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/organization/body/application/summary.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/organization/body/application/summary.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import {StateType} from '~/reducers';
-import {connect, Dispatch} from 'react-redux';
+import { StateType } from '~/reducers';
+import { connect, Dispatch } from 'react-redux';
 import { i18nType } from "~/reducers/base/i18n";
-import ApplicationSubPanel, {ApplicationSubPanelItem}  from "~/components/general/application-sub-panel";
+import ApplicationSubPanel, { ApplicationSubPanelItem } from "~/components/general/application-sub-panel";
 
 
 interface SummaryProps {
@@ -16,33 +16,60 @@ interface SummaryState {
 
 class Summary extends React.Component<SummaryProps, SummaryState> {
 
-  
-  render(){
+
+  render() {
     return (
-       <div>
+      <div>
         <ApplicationSubPanel i18n={this.props.i18n} modifier="workspace-users" bodyModifier="workspace-staff-members" title={this.props.i18n.text.get('plugin.organization.summary.billing.title')}>
-          <ApplicationSubPanelItem i18n={this.props.i18n} title={this.props.i18n.text.get('plugin.organization.summary.billing.subtitle')} ></ApplicationSubPanelItem>
-          <ApplicationSubPanelItem i18n={this.props.i18n} title={this.props.i18n.text.get('plugin.organization.summary.billing.subtitle')} ></ApplicationSubPanelItem>
-          <ApplicationSubPanelItem i18n={this.props.i18n} title={this.props.i18n.text.get('plugin.organization.summary.billing.subtitle')} ></ApplicationSubPanelItem>
+          <ApplicationSubPanelItem title={this.props.i18n.text.get('plugin.organization.summary.billing.subtitle.activeBillingPeriod')}>
+            <ApplicationSubPanelItem.Content>
+              DD.MM.YYYY - DD.MM.YYYY
+            </ApplicationSubPanelItem.Content>
+            <ApplicationSubPanelItem.Content label={this.props.i18n.text.get('plugin.organization.summary.billing.detail.nextBillingSeason')} >
+              DD.MM.YYYY - DD.MM.YYYY
+            </ApplicationSubPanelItem.Content>
+          </ApplicationSubPanelItem>
+          <ApplicationSubPanelItem title={this.props.i18n.text.get('plugin.organization.summary.billing.subtitle.accumulatedBill')} >
+            <ApplicationSubPanelItem.Content>1000€</ApplicationSubPanelItem.Content>
+            <ApplicationSubPanelItem.Content>
+              {this.props.i18n.text.get('plugin.organization.summary.billing.detail.accumulated', 10, 5, 3)}
+            </ApplicationSubPanelItem.Content>
+          </ApplicationSubPanelItem>
+          <ApplicationSubPanelItem title={this.props.i18n.text.get('plugin.organization.summary.billing.subtitle.previousBills')} >
+            <ApplicationSubPanelItem.Content>1000€</ApplicationSubPanelItem.Content>
+            <ApplicationSubPanelItem.Content>
+              {this.props.i18n.text.get('plugin.organization.summary.billing.detail.accumulated', 10, 5, 3)}
+            </ApplicationSubPanelItem.Content>
+          </ApplicationSubPanelItem>
         </ApplicationSubPanel>
         <ApplicationSubPanel i18n={this.props.i18n} modifier="workspace-users" bodyModifier="workspace-staff-members" title={this.props.i18n.text.get('plugin.organization.summary.contact.title')}>
-          <ApplicationSubPanelItem i18n={this.props.i18n} title={this.props.i18n.text.get('plugin.organization.summary.contact.subtitle')} > </ApplicationSubPanelItem>
-          <ApplicationSubPanelItem i18n={this.props.i18n} title={this.props.i18n.text.get('plugin.organization.summary.contact.subtitle')} ></ApplicationSubPanelItem>
+          <ApplicationSubPanelItem title={this.props.i18n.text.get('plugin.organization.summary.contact.subtitle')} >
+            <ApplicationSubPanelItem.Content>
+              <div>Musta kaapu</div>
+              <div>2043i2340i12430</div>
+              <div>qwqweqweqwe@asdas.net</div>
+            </ApplicationSubPanelItem.Content>
+          </ApplicationSubPanelItem>
+          <ApplicationSubPanelItem title={this.props.i18n.text.get('plugin.organization.summary.contact.subtitle')} >
+            <ApplicationSubPanelItem.Content>
+              <div>Musta kaapu</div>
+              <div>2043i2340i12430</div>
+              <div>qwqweqweqwe@asdas.net</div>
+            </ApplicationSubPanelItem.Content>
+          </ApplicationSubPanelItem>
         </ApplicationSubPanel>
-        <ApplicationSubPanel i18n={this.props.i18n} modifier="workspace-users" bodyModifier="workspace-staff-members" title={this.props.i18n.text.get('plugin.organization.summary.stats.title')}>
-        </ApplicationSubPanel>
-       </div>
+      </div>
     );
   }
 }
 
-function mapStateToProps(state: StateType){
+function mapStateToProps(state: StateType) {
   return {
     i18n: state.i18n,
   }
 };
 
-function mapDispatchToProps(dispatch: Dispatch<any>){
+function mapDispatchToProps(dispatch: Dispatch<any>) {
   return {
   };
 };
