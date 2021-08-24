2cbc06941393778981d813208405fbdabc840f27
Pasi Kukkonen <pasi.kukkonen@otavanopisto.fi>
2021-08-24 10:52:56 +0300

    isDefaultOrganization variable to whoami endpoint
    - resolves #5683

--- muikku-rest/src/main/java/fi/otavanopisto/muikku/rest/user/UserRESTService.java
+++ muikku-rest/src/main/java/fi/otavanopisto/muikku/rest/user/UserRESTService.java
@@ -1621,10 +1621,27 @@ public class UserRESTService extends AbstractRESTService {
     if (user == null) {
       return Response.status(Response.Status.NOT_FOUND).build();
     }
+    
+    String organizationIdentifier = user.getOrganizationIdentifier().toId();
+    String defaultOrganizationIdentifier = systemSettingsController.getSetting("defaultOrganization");
+    boolean isDefaultOrganization = StringUtils.equals(organizationIdentifier,  defaultOrganizationIdentifier);
 
     boolean hasImage = userEntityFileController.hasProfilePicture(userEntity);
+    
+    UserWhoAmIInfo whoamiInfo = new UserWhoAmIInfo(
+        userEntity.getId(),
+        user.getFirstName(),
+        user.getLastName(),
+        user.getNickName(),
+        user.getStudyProgrammeName(),
+        hasImage,
+        user.getHasEvaluationFees(),
+        user.getCurriculumIdentifier(),
+        organizationIdentifier,
+        isDefaultOrganization); 
+
     return Response
-        .ok(new UserWhoAmIInfo(userEntity.getId(), user.getFirstName(), user.getLastName(), user.getNickName(), user.getStudyProgrammeName(), hasImage, user.getHasEvaluationFees(), user.getCurriculumIdentifier(), user.getOrganizationIdentifier().toId()))
+        .ok(whoamiInfo)
         .cacheControl(cacheControl)
         .tag(tag)
         .build();
