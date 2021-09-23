b96829aba7f29db88fcf4f0087f379092d76999d
aviljakaine <antti.viljakainen@otavanopisto.fi>
2021-09-23 09:40:20 +0300

    Show exercise evaluation in materials

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/WorkspaceRESTService.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/WorkspaceRESTService.java
@@ -1842,7 +1842,7 @@ public class WorkspaceRESTService extends PluginRESTService {
         
         // Evaluation info for evaluable materials
         
-        if (reply.getWorkspaceMaterial().getAssignmentType() == WorkspaceMaterialAssignmentType.EVALUATED) {
+        if (reply.getWorkspaceMaterial().getAssignmentType() == WorkspaceMaterialAssignmentType.EVALUATED || reply.getWorkspaceMaterial().getAssignmentType() == WorkspaceMaterialAssignmentType.EXERCISE) {
           compositeReply.setEvaluationInfo(evaluationController.getEvaluationInfo(userEntity, reply.getWorkspaceMaterial()));
         }
         
