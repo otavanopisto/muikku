9f1a2007bb6564f6d4f4d8ebfa5716ba6da4e717
jjlankinen <jari.lankinen@otavanopisto.fi>
2021-10-18 14:14:32 +0300

    * Changed preload back to metadata thanks to bacnend now returning range better

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-loader/base/index.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-loader/base/index.tsx
@@ -114,7 +114,7 @@ function preprocessor($html: any): any {
   });
 
   $html.find('audio').each(function () {
-    $(this).attr("preload", "none");
+    $(this).attr("preload", "metadata");
   })
 
   $html.find('source').each(function () {
@@ -494,7 +494,7 @@ export default class Base extends React.Component<BaseProps, BaseState> {
       {
         shouldProcessHTMLElement: (tagname) => tagname === "audio",
         preprocessReactProperties: (tag, props, children, element) => {
-          props.preload = "none";
+          props.preload = "metadata";
         },
         processingFunction: (tag, props, children, element) => {
           return (<AudioPoolComponent {...props} invisible={invisible}>{children}</AudioPoolComponent>);
