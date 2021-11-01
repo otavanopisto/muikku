add5cc56f838d2f94b3fa35ecb0f817595d49a84
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2021-11-01 14:31:26 +0200

    Changes: - communicator and discussions works now more or less like emails. When answering, modifying or quiting the editor is now right below last message item

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/discussion/body/application/modify-reply-thread-drawer.tsx
@@ -0,0 +1,240 @@
+import { i18nType } from "~/reducers/base/i18n";
+import * as React from "react";
+import { DiscussionThreadReplyType } from "~/reducers/discussion";
+import { Dispatch, connect } from "react-redux";
+import { AnyActionType } from "~/actions";
+import { bindActionCreators } from "redux";
+import CKEditor from "~/components/general/ckeditor";
+import {
+  modifyReplyFromCurrentThread,
+  ModifyReplyFromCurrentThreadTriggerType,
+} from "~/actions/discussion";
+import { StateType } from "~/reducers";
+import SessionStateComponent from "~/components/general/session-state-component";
+import Button from "~/components/general/button";
+
+import "~/sass/elements/form-elements.scss";
+import "~/sass/elements/form.scss";
+
+/**
+ * ModifyThreadReplyDrawerProps
+ */
+interface ModifyThreadReplyDrawerProps {
+  i18n: i18nType;
+  reply?: DiscussionThreadReplyType;
+  modifyReplyFromCurrentThread: ModifyReplyFromCurrentThreadTriggerType;
+  onClickCancel: () => void;
+}
+
+/**
+ * ModifyThreadReplyDrawerState
+ */
+interface ModifyThreadReplyDrawerState {
+  text: string;
+  locked: boolean;
+}
+
+/**
+ * ModifyThreadReplyDrawer
+ */
+class ModifyThreadReplyDrawer extends SessionStateComponent<
+  ModifyThreadReplyDrawerProps,
+  ModifyThreadReplyDrawerState
+> {
+  /**
+   * constructor
+   * @param props
+   */
+  constructor(props: ModifyThreadReplyDrawerProps) {
+    super(props, "discussion-modify-thread-reply");
+
+    this.onCKEditorChange = this.onCKEditorChange.bind(this);
+    this.modifyReply = this.modifyReply.bind(this);
+    this.clearUp = this.clearUp.bind(this);
+    this.checkAgainstStoredState = this.checkAgainstStoredState.bind(this);
+
+    this.state = this.getRecoverStoredState(
+      {
+        locked: false,
+        text: props.reply.message,
+      },
+      props.reply.id
+    );
+  }
+
+  /**
+   * checkAgainstStoredState
+   */
+  checkAgainstStoredState() {
+    this.checkStoredAgainstThisState(
+      {
+        text: this.props.reply.message,
+      },
+      this.props.reply.id
+    );
+  }
+
+  /**
+   * clearUp
+   */
+  clearUp() {
+    this.setStateAndClear(
+      {
+        text: this.props.reply.message,
+      },
+      this.props.reply.id
+    );
+  }
+
+  /**
+   * componentWillReceiveProps
+   * @param nextProps
+   */
+  componentWillReceiveProps(nextProps: ModifyThreadReplyDrawerProps) {
+    if (nextProps.reply.id !== this.props.reply.id) {
+      this.setState(
+        this.getRecoverStoredState(
+          {
+            text: nextProps.reply.message,
+          },
+          nextProps.reply.id
+        )
+      );
+    }
+  }
+
+  /**
+   * onCKEditorChange
+   * @param text
+   */
+  onCKEditorChange(text: string) {
+    this.setStateAndStore({ text }, this.props.reply.id);
+  }
+
+  /**
+   * modifyReply
+   */
+  modifyReply() {
+    this.setState({
+      locked: true,
+    });
+    this.props.modifyReplyFromCurrentThread({
+      reply: this.props.reply,
+      message: this.state.text,
+      success: () => {
+        this.props.onClickCancel && this.props.onClickCancel();
+        this.justClear(["text"], this.props.reply.id);
+        this.setState({
+          locked: false,
+        });
+      },
+      fail: () => {
+        this.setState({
+          locked: false,
+        });
+      },
+    });
+  }
+
+  handleOnCancelClick = () => {
+    this.props.onClickCancel && this.props.onClickCancel();
+  };
+
+  /**
+   * render
+   * @returns JSX.Element
+   */
+  render() {
+    let editorTitle =
+      this.props.i18n.text.get("plugin.discussion.reply.edit.topic") +
+      " - " +
+      this.props.i18n.text.get("plugin.discussion.createmessage.content");
+
+    /**
+     * content
+     */
+    let content = (
+      <div className="env-dialog__row env-dialog__row--ckeditor" key="3">
+        <div className="env-dialog__form-element-container">
+          <label className="env-dialog__label">
+            {this.props.i18n.text.get(
+              "plugin.discussion.createmessage.content"
+            )}
+          </label>
+          <CKEditor
+            editorTitle={editorTitle}
+            autofocus
+            key="1"
+            onChange={this.onCKEditorChange}
+          >
+            {this.state.text}
+          </CKEditor>
+        </div>
+      </div>
+    );
+
+    /**
+     * footer
+     */
+    let footer = (
+      <div className="env-dialog__actions">
+        <Button
+          buttonModifiers="dialog-execute"
+          onClick={this.modifyReply.bind(this)}
+          disabled={this.state.locked}
+        >
+          {this.props.i18n.text.get("plugin.discussion.createmessage.send")}
+        </Button>
+        <Button
+          buttonModifiers="dialog-cancel"
+          onClick={this.handleOnCancelClick}
+          disabled={this.state.locked}
+        >
+          {this.props.i18n.text.get("plugin.discussion.createmessage.cancel")}
+        </Button>
+        {this.recovered ? (
+          <Button
+            buttonModifiers="dialog-clear"
+            onClick={this.clearUp}
+            disabled={this.state.locked}
+          >
+            {this.props.i18n.text.get(
+              "plugin.discussion.createmessage.clearDraft"
+            )}
+          </Button>
+        ) : null}
+      </div>
+    );
+
+    return (
+      <div>
+        {editorTitle}
+        {content}
+        {footer}
+      </div>
+    );
+  }
+}
+
+/**
+ * mapStateToProps
+ * @param state
+ */
+function mapStateToProps(state: StateType) {
+  return {
+    i18n: state.i18n,
+  };
+}
+
+/**
+ * mapDispatchToProps
+ * @param dispatch
+ */
+function mapDispatchToProps(dispatch: Dispatch<AnyActionType>) {
+  return bindActionCreators({ modifyReplyFromCurrentThread }, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ModifyThreadReplyDrawer);
