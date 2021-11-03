4b55d039ea1e44e771665b59a7858429985c7c40
jjlankinen <jari.lankinen@otavanopisto.fi>
2021-11-03 09:40:37 +0200

    * Audio field component's comments

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-loader/fields/audio-field/index.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/material-loader/fields/audio-field/index.tsx
@@ -10,10 +10,11 @@ import ConfirmRemoveDialog from "./confirm-remove-dialog";
 import Synchronizer from "../base/synchronizer";
 import { AudioPoolComponent } from "~/components/general/audio-pool-component";
 import { FieldStateStatus } from "~/@types/shared";
+import { createFieldSavedStateClass } from "../../base/index";
 
-//so we use the media recorder
-//the media recorder is polyfilled
-//if it's not there
+// so we use the media recorder
+// the media recorder is polyfilled
+// if it's not there
 if (!(window as any).MediaRecorder) {
   let script = document.createElement('script');
   script.src = (window as any).CONTEXTPATH + "/javax.faces.resource/scripts/dist/polyfill-mediarecorder.js.jsf";
@@ -39,27 +40,27 @@ interface AudioFieldProps {
 interface AudioFieldState {
   recording: boolean,
   values: Array<{
-    //might not be there while uploading
+    // might not be there while uploading
     id?: string,
     name?: string,
     contentType: string,
     uploading?: boolean,
     failed?: boolean,
 
-    //only has a value while uploading
+    // only has a value while uploading
     progress?: number,
     file?: File,
     blob?: Blob,
 
-    //utility
+    // utility
     url?: string
   }>,
   supportsMediaAPI: ()=>boolean,
   time: number,
 
-  //This state comes from the context handler in the base
-  //We can use it but it's the parent managing function that modifies them
-  //We only set them up in the initial state
+  // This state comes from the context handler in the base
+  // We can use it but it's the parent managing function that modifies them
+  // We only set them up in the initial state
   modified: boolean,
   synced: boolean,
   syncError: string,
@@ -67,7 +68,7 @@ interface AudioFieldState {
   fieldSavedState: FieldStateStatus,
 }
 
-//this is the maximum recording time in seconds
+// this is the maximum recording time in seconds
 const MAX_RECORDING_TIME_IN_SECONDS = 60*5;
 
 export default class AudioField extends React.Component<AudioFieldProps, AudioFieldState> {
@@ -89,14 +90,14 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
         }
       }),
 
-      //modified synced and syncerror are false, true and null by default
+      // modified synced and syncerror are false, true and null by default
       modified: false,
       synced: true,
       syncError: null,
 
       fieldSavedState: null,
 
-      //so even
+      // so even
       supportsMediaAPI: this.getSupportsMediaAPI
     }
 
@@ -107,34 +108,54 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
     this.processFileAt = this.processFileAt.bind(this);
     this.onFieldSavedStateChange = this.onFieldSavedStateChange.bind(this);
   }
+
+  /**
+   * onFieldSavedStateChange
+   * @param savedState
+   */
   onFieldSavedStateChange(savedState: FieldStateStatus){
     this.setState({
       fieldSavedState: savedState
     });
   }
+
+  /**
+   * shouldComponentUpdate
+   * @param nextProps
+   * @param nextState
+   * @returns
+   */
   shouldComponentUpdate(nextProps: AudioFieldProps, nextState: AudioFieldState){
     return !equals(nextProps.content, this.props.content) || this.props.readOnly !== nextProps.readOnly || !equals(nextState, this.state) || nextProps.invisible !== this.props.invisible;
   }
+
+  /**
+   * getSupportsMediaAPI
+   * @returns
+   */
   getSupportsMediaAPI(){
-    //dang must be a fast AF browser for this to happen
+    // dang must be a fast AF browser for this to happen
     if (!(window as any).MediaRecorder){
-      //force update after 500 ms give it time
-      //this function will be called again
-      //by the render
-      //its a hack but well
+      // force update after 500 ms give it time
+      // this function will be called again
+      // by the render
+      // its a hack but well
       setTimeout(this.forceUpdate, 500);
 
-      //lets assume true
-      //after all a fast browser that got into here
-      //likely supports it
+      // lets assume true
+      // after all a fast browser that got into here
+      // likely supports it
       return true;
     }
     return !(window as any).MediaRecorder.notSupported
   }
 
-  //so here we start the stream
+  /**
+   * start - here we start the stream
+   * @returns
+   */
   async start(){
-    //we create it
+    // we create it
     try {
       this.stream = this.stream || await navigator.mediaDevices.getUserMedia({
         audio: true
@@ -143,17 +164,17 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
       console.error(err.stack);
       return;
     }
-    //start the recorder
+    // start the recorder
     this.recorder = new (window as any).MediaRecorder(this.stream);
 
-    //and add a event listener on dataavaliable
+    // and add a event listener on dataavaliable
     this.recorder.addEventListener('dataavailable', (e: Event)=>{
-      //where we take the blob generated
+      // where we take the blob generated
       let blob = (e as any).data as Blob;
 
-      //set the state as that blob, with the given local blob url
-      //the content type and the fact that is uploading
-      //with all the values concatted
+      // set the state as that blob, with the given local blob url
+      // the content type and the fact that is uploading
+      // with all the values concatted
       this.setState({
         values: this.state.values.concat([{
           blob,
@@ -162,21 +183,21 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
           uploading: true
         }])
       }, ()=>{
-        //and we tell it to process the last file
-        //just like a file would
+        // and we tell it to process the last file
+        // just like a file would
         this.processFileAt(this.state.values.length - 1);
       })
     });
 
-    //we start the recorder
+    // we start the recorder
     this.recorder.start();
-    //and set the time
+    // and set the time
     this.setState({
       recording: true,
       time: 0
     });
-    //set a interval every second
-    //just to check the time and update the timer
+    // set a interval every second
+    // just to check the time and update the timer
     this.interval = setInterval(()=>{
       let nTime = this.state.time+1;
       if (nTime === MAX_RECORDING_TIME_IN_SECONDS){
@@ -187,33 +208,44 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
       })
     }, 1000) as any;
   }
-  //stop the recording
+
+  /**
+   * stop - here we stop the recording
+   */
   stop(){
-    //clear the interval that checks the time
+    // clear the interval that checks the time
     clearInterval(this.interval);
-    //stop the recorder, this should trigger the dataavailable event
+    // stop the recorder, this should trigger the dataavailable event
     this.recorder.stop();
     this.stream.getTracks().forEach((t) => t.stop());
 
-    //delet stuff
+    // delete stuff
     delete this["recorder"];
     delete this["stream"];
 
-    //and stop the fact it is recording
+    // and stop the fact it is recording
     this.setState({
       recording: false
     });
   }
-  //remove a clip
+
+  /**
+   * removeClipAt - Here we remote the clip
+   * @param index
+   */
   removeClipAt(index: number){
     const newValues = this.state.values.filter((a, i) => i !== index);
     this.setState({
       values: newValues,
     }, this.checkDoneAndRunOnChange);
   }
-  //when the file changes (old method for bad browsers)
+
+  /**
+   * onFileChanged - when the file changes (old method for bad browsers)
+   * @param e
+   */
   onFileChanged(e: React.ChangeEvent<HTMLInputElement>){
-    //we do it similarly to the file field
+    // we do it similarly to the file field
     let newValues = Array.from(e.target.files).map((file)=>{
       return {
         name: file.name,
@@ -225,7 +257,7 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
       }
     });
 
-    //check the method at the file field for an in depth explanation
+    // check the method at the file field for an in depth explanation
     let originalLenght = this.state.values.length;
     this.setState({values: this.state.values.concat(newValues)}, ()=>{
       newValues.forEach((value, index)=>{
@@ -234,38 +266,40 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
       })
     });
   }
-  //processing a file, this function
-  //doesn't care on whether it came from the local stream
-  //or a file element
+
+  /**
+   * processFileAt - processing a file, this function doesn't care on whether it came from the local stream or a file element
+   * @param index
+   */
   processFileAt(index: number){
-    //create the form data
+    // create the form data
     let formData = new FormData();
-    //the file can be the file itself as it was given or the blob as given by the steam
-    //both different types
+    // the file can be the file itself as it was given or the blob as given by the steam
+    // both different types
     let file:any = this.state.values[index].file || this.state.values[index].blob;
-    //we add it to the file
+    // we add it to the file
     formData.append("file", file);
-    //and do the thing
+    // and do the thing
     $.ajax({
       url: this.props.status.contextPath + '/tempFileUploadServlet',
       type: 'POST',
       data: formData,
       success: (data: any)=>{
-        //we change this
+        // we change this
         let newValues = [...this.state.values];
         newValues[index] = {...this.state.values[index]}
         newValues[index].uploading = false;
         newValues[index].id = data.fileId;
-        //if the server does not return a content type we'll use whatever the blob recorded, this shouldn't be the case the server should return somethings
+        // if the server does not return a content type we'll use whatever the blob recorded, this shouldn't be the case the server should return somethings
         newValues[index].contentType = data.fileContentType || file.type;
-        //if user didn't provide a name we will give one, this happens when recording in place, such is the default behaviour
+        // if user didn't provide a name we will give one, this happens when recording in place, such is the default behaviour
         newValues[index].name = newValues[index].name || data.fileId; //NO extension, we don't need it
 
         this.setState({
           values: newValues
         }, this.checkDoneAndRunOnChange)
       },
-      //in case of error
+      // in case of error
       error: (xhr:any, err:Error)=>{
         let newValues = [...this.state.values];
         newValues[index] = {...this.state.values[index]}
@@ -277,7 +311,7 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
       },
       xhr: ()=>{
         let xhr = new (window as any).XMLHttpRequest();
-        //Upload progress same as in the file field
+        // Upload progress same as in the file field
         xhr.upload.addEventListener("progress", (evt:any)=>{
           if (evt.lengthComputable) {
             let percentComplete = evt.loaded / evt.total;
@@ -296,11 +330,16 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
       processData: false
     })
   }
+
+  /**
+   * checkDoneAndRunOnChange
+   * @returns
+   */
   checkDoneAndRunOnChange(){
     if (!this.props.content) {
       return;
     }
-    //if something is uploading, wait
+    // if something is uploading, wait
     for (let value of this.state.values){
       if (value.uploading){
         return;
@@ -310,7 +349,7 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
       return;
     }
 
-    //get the result
+    // get the result
     let result = JSON.stringify(this.state.values.filter((value)=>{
       return !value.failed;
     }).map((value)=>{
@@ -324,16 +363,21 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
       }
     }));
 
-    //and trigger onchange
+    // and trigger onchange
     this.props.onChange(this, this.props.content.name, result);
   }
+
+  /**
+   * render
+   * @returns
+   */
   render(){
     if (this.props.invisible){
       let emptyData = null;
       if (this.state.values.length){
-        //we need to map them
+        // we need to map them
         emptyData = this.state.values.map((value, index)=>{
-          //if the value is not uploading, we set it as static
+          // if the value is not uploading, we set it as static
           return <span className="material-page__audiofield-file-container" key={index}/>;
         });
       }
@@ -347,17 +391,17 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
         </span>
       </span>
     }
-    //rendering things here
-    //this is the data that it has already created
+    // rendering things here
+    // this is the data that it has already created
     let dataInContainer = null;
     let recordingInContainer = null;
 
-    //if we have values
+    // if we have values
     if (this.state.values.length){
-      //we need to map them
+      // we need to map them
       dataInContainer = this.state.values.map((value, index)=>{
         if (!value.uploading) {
-          //if the value is not uploading, we set it as static
+          // if the value is not uploading, we set it as static
           return <span className="material-page__audiofield-file-container" key={index}>
             <AudioPoolComponent className="material-page__audiofield-file" controls src={value.url} preload="metadata"/>
             <Link className="material-page__audiofield-download-file-button icon-download"
@@ -370,14 +414,14 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
               </ConfirmRemoveDialog> : null }
           </span>;
         } else if (value.failed) {
-          //if the value failed we add a message, you can get the value name there so use it to say which file
+          // if the value failed we add a message, you can get the value name there so use it to say which file
           return <span className="material-page__audiofield-file-container">
             <span className="material-page__audiofield-file material-page__audiofield-file--FAILED-TO-UPLOAD">
               {this.props.i18n.text.get("plugin.workspace.audioField.uploadFailed")}
             </span>
           </span>;
         } else {
-          //if the value is uploading
+          // if the value is uploading
           return <span className="material-page__audiofield-file-container material-page__audiofield-file-container--uploading" key={index}>
             <span className="material-page__audiofield-file material-page__audiofield-file--uploading">
                <ProgressBarLine containerClassName="material-page__audiofield-file-upload-progressbar" options={{
@@ -432,19 +476,12 @@ export default class AudioField extends React.Component<AudioFieldProps, AudioFi
         </span>
       </span>: null }
 
-    //if elements is disabled
+    // if elements is disabled
     let ElementDisabledState = this.props.readOnly ? "material-page__taskfield-disabled" : "";
 
-    let fieldSavedStateClass = "";
-    if (this.state.fieldSavedState === "ERROR") {
-      fieldSavedStateClass = "state-ERROR";
-    } else if (this.state.fieldSavedState === "SAVING") {
-      fieldSavedStateClass = "state-SAVING";
-    } else if (this.state.fieldSavedState === "SAVED") {
-      fieldSavedStateClass = "state-SAVED";
-    }
+    let fieldSavedStateClass = createFieldSavedStateClass(this.state.fieldSavedState);
 
-    //and this is the container
+    // and this is the container
     return <span className={`material-page__audiofield-wrapper ${fieldSavedStateClass}`}>
       <Synchronizer
         synced={this.state.synced}
