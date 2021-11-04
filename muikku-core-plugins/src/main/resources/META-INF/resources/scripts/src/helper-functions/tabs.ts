5273b71d092c5ab41ebd1badcde91b0a6bb52cfe
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2021-11-04 09:00:44 +0200

    Documentatiton

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/helper-functions/tabs.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/helper-functions/tabs.ts
@@ -1,5 +1,9 @@
 import { TabType } from "~/components/general/tabs";
-
+/**
+ * Creates a array from tab ids from given tabs
+ *
+ * @returns an array of strings
+ */
 export const createAllTabs = (tabs: TabType[]) => {
   const tabStrings: string[] = []
   for (let i = 0; i < tabs.length; i++) {
