0ee303c859c29e6c8814b5f82bc1a2bbf8234f6c
petja <petja.laitila@otavanopisto.fi>
2021-11-10 15:23:12 +0200

    Modern browsers like FF use filename* header parameter wich can be utf-8

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/WorkspaceRESTService.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/WorkspaceRESTService.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
@@ -2038,18 +2040,23 @@ public class WorkspaceRESTService extends PluginRESTService {
     if (content == null) {
       return Response.status(Status.NOT_FOUND).build();
     }
-    
-    String filename = new String(answerFile.getFileName().getBytes(Charset.forName("US-ASCII")));
-    if (StringUtils.isEmpty(answerFile.getContentType())) {
-      return Response.ok(content)
-        .header("Content-Disposition", "attachment; filename=\"" + filename.replaceAll("\"", "\\\"") + "\"")
-        .build();
-    }
-    else {
-      return Response.ok(content)
-        .type(answerFile.getContentType())
-        .header("Content-Disposition", "attachment; filename=\"" + filename.replaceAll("\"", "\\\"") + "\"")
-        .build();
+    String filename = new String(answerFile.getFileName().getBytes(Charset.forName("US-ASCII"))).replaceAll("\"", "\\\"");
+    try {
+      String encFilename = URLEncoder.encode(answerFile.getFileName(), "utf-8").replaceAll("\\+", "%20").replaceAll("\"", "\\\"");
+      if (StringUtils.isEmpty(answerFile.getContentType())) {
+        return Response.ok(content)
+          .header("Content-Disposition", "attachment; filename=\"" + filename + "\"; filename*=utf-8\"" + encFilename + "\"")
+          .build();
+      }
+      else {
+        return Response.ok(content)
+          .type(answerFile.getContentType())
+          .header("Content-Disposition", "attachment; filename=\"" + filename + "\"; filename*=utf-8''\"" + encFilename + "\"")
+          .build();
+      }
+    } catch (UnsupportedEncodingException e) {
+      logger.warning("Unsupported character encoding: " + e.getMessage());
+      return Response.status(Status.INTERNAL_SERVER_ERROR).build();
     }
   }
 
