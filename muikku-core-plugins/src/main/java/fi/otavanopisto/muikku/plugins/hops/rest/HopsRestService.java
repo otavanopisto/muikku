aac68f6fd5aa1346d4561ff3fed7d25ee197b3b5
Saara Lepistö <saara.lepisto@hotmail.com>
2021-12-17 07:58:10 +0200

    * Added condition for access check in listWorkspaceSuggestions()

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/hops/rest/HopsRestService.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/hops/rest/HopsRestService.java
@@ -341,12 +341,14 @@ public class HopsRestService {
   @GET
   @Path("/listWorkspaceSuggestions")
   @RESTPermit (handling = Handling.INLINE, requireLoggedIn = true)
-  public Response listWorkspaceSuggestions(@QueryParam("subject") String subject, @QueryParam("courseNumber") Integer courseNumber) {
+  public Response listWorkspaceSuggestions(@QueryParam("subject") String subject, @QueryParam("courseNumber") Integer courseNumber, String studentIdentifier) {
 
     // Access check
     
     if (!sessionController.hasEnvironmentPermission(MuikkuPermissions.HOPS_SUGGEST_WORKSPACES)) {
-      return Response.status(Status.FORBIDDEN).build();
+      if (!StringUtils.equals(SchoolDataIdentifier.fromId(studentIdentifier).getIdentifier(), sessionController.getLoggedUserIdentifier())) {
+        return Response.status(Status.FORBIDDEN).build();
+      }
     }
     
     List<SuggestedWorkspaceRestModel> suggestedWorkspaces = new ArrayList<>();
