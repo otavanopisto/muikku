22a0e30b5008a7b22ce8ca0b8587dc379f6c0087
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2021-12-28 13:57:03 +0200

    5594-changes: - added custom feedback element for ckeditor and exercise type assignments which is shown only if assignments has been reviewed. Also it is hide again if answer is changed after initial answering

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/ckplugins/styles/workspace-material-styles.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/ckplugins/styles/workspace-material-styles.js
@@ -32,7 +32,8 @@ CKEDITOR.stylesSet.add( 'workspace-material-styles', [
   { name: getLocaleText("plugin.workspace.htmlMaterialEditor.stylesSets.citationBasic"), element: 'div', attributes: { 'class': 'material-citation-basic material-styles-text' } },
   { name: getLocaleText("plugin.workspace.htmlMaterialEditor.stylesSets.longCitation"), element: 'div', attributes: { 'class': 'material-citation-long material-styles-text' } },
   { name: getLocaleText("plugin.workspace.htmlMaterialEditor.stylesSets.exampleBox"), element: 'div', attributes: { 'class': 'material-example material-styles-text' } },
-  { name: getLocaleText("plugin.workspace.htmlMaterialEditor.stylesSets.exerciseBox"), element: 'div', attributes: { 'class': 'material-exercise material-styles-text' } }
+  { name: getLocaleText("plugin.workspace.htmlMaterialEditor.stylesSets.exerciseBox"), element: 'div', attributes: { 'class': 'material-exercise material-styles-text' } },
+  { name: "Harjoitustehtävä esimerkki vastaus", element: 'div', attributes: { 'class': 'material-exercise-feedback material-styles-block', 'data-show': "false" } }
 
   /* Inline Styles */
 
