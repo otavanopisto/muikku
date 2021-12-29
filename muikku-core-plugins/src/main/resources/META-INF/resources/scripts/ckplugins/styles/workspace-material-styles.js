adf99651d4b78dbdefcab75953fd8a9d2ecdc163
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2021-12-29 07:59:50 +0200

    Default data-show attribute for material-exercise-feedback set to be true, so editor can also show it

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/ckplugins/styles/workspace-material-styles.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/ckplugins/styles/workspace-material-styles.js
@@ -33,7 +33,7 @@ CKEDITOR.stylesSet.add( 'workspace-material-styles', [
   { name: getLocaleText("plugin.workspace.htmlMaterialEditor.stylesSets.longCitation"), element: 'div', attributes: { 'class': 'material-citation-long material-styles-text' } },
   { name: getLocaleText("plugin.workspace.htmlMaterialEditor.stylesSets.exampleBox"), element: 'div', attributes: { 'class': 'material-example material-styles-text' } },
   { name: getLocaleText("plugin.workspace.htmlMaterialEditor.stylesSets.exerciseBox"), element: 'div', attributes: { 'class': 'material-exercise material-styles-text' } },
-  { name: "Harjoitustehtävä esimerkki vastaus", element: 'div', attributes: { 'class': 'material-exercise-feedback material-styles-block', 'data-show': "false" } }
+  { name: "Harjoitustehtävä esimerkki vastaus", element: 'div', attributes: { 'class': 'material-exercise-feedback material-styles-block', 'data-show': "true" } }
 
   /* Inline Styles */
 
