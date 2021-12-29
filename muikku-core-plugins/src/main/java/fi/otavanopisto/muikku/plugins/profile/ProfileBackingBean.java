9ae16a5ac64a7cf163dc1c5f324a62839d100579
Pasi Kukkonen <pasi.kukkonen@otavanopisto.fi>
2021-12-29 09:00:52 +0200

    - work in progress

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/profile/ProfileBackingBean.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/profile/ProfileBackingBean.java
@@ -1,30 +1,12 @@
 package fi.otavanopisto.muikku.plugins.profile;
 
-import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.annotation.RequestAction;
 
-import fi.otavanopisto.muikku.model.users.UserEntity;
-import fi.otavanopisto.muikku.plugins.chat.ChatController;
-import fi.otavanopisto.muikku.plugins.chat.model.UserChatSettings;
-import fi.otavanopisto.muikku.plugins.chat.model.UserChatVisibility;
-import fi.otavanopisto.muikku.schooldata.SchoolDataIdentifier;
-import fi.otavanopisto.muikku.schooldata.entity.User;
-import fi.otavanopisto.muikku.schooldata.entity.UserAddress;
-import fi.otavanopisto.muikku.schooldata.entity.UserPhoneNumber;
-import fi.otavanopisto.muikku.session.SessionController;
-import fi.otavanopisto.muikku.users.UserController;
-import fi.otavanopisto.muikku.users.UserEmailEntityController;
-import fi.otavanopisto.muikku.users.UserEntityController;
 import fi.otavanopisto.security.LoggedIn;
 
 @Named
@@ -33,99 +15,10 @@ import fi.otavanopisto.security.LoggedIn;
 @Join (path = "/profile", to = "/jsf/profile/profile.jsf")
 public class ProfileBackingBean {
   
-  @Inject
-  private SessionController sessionController;
-  
-  @Inject
-  private UserController userController;
-
-  @Inject
-  private UserEntityController userEntityController;
-  
-  @Inject
-  private UserEmailEntityController userEmailEntityController;
-
-  @Inject
-  private ChatController chatController;
-
   @RequestAction
   @LoggedIn
   public String init() {
-    UserEntity userEntity = sessionController.getLoggedUserEntity();
-    User user = userController.findUserByDataSourceAndIdentifier(sessionController.getLoggedUserSchoolDataSource(), sessionController.getLoggedUserIdentifier());
-    List<UserAddress> userAddresses = userController.listUserAddresses(user);
-    List<UserPhoneNumber> userPhoneNumbers = userController.listUserPhoneNumbers(user);
-    
-    displayName = user.getNickName() == null ? user.getDisplayName() : String.format("%s %s (%s)", user.getNickName(), user.getLastName(), user.getStudyProgrammeName());
-    
-    studyStartDate = user.getStudyStartDate();
-    studyTimeEnd = user.getStudyTimeEnd();
-    studyTimeLeftStr = userEntityController.getStudyTimeEndAsString(studyTimeEnd);
-    
-    addresses = new ArrayList<>();
-    for (UserAddress userAddress : userAddresses) {
-      addresses.add(String.format("%s %s %s %s", userAddress.getStreet(), userAddress.getPostalCode(), userAddress.getCity(), userAddress.getCountry()));
-    }
-    
-    phoneNumbers = new ArrayList<>();
-    for (UserPhoneNumber userPhoneNumber : userPhoneNumbers) {
-      phoneNumbers.add(userPhoneNumber.getNumber());
-    }
-    
-    SchoolDataIdentifier identifier = userEntity.defaultSchoolDataIdentifier();
-    emails = userEmailEntityController.getUserEmailAddresses(identifier);
-    
-    UserChatSettings userChatSettings = chatController.findUserChatSettings(userEntity);
-    
-    chatEnabled = false;
-    if (userChatSettings != null) {
-      UserChatVisibility visibility = userChatSettings.getVisibility();
-
-      if (visibility == UserChatVisibility.VISIBLE_TO_ALL) {
-        chatEnabled = true;
-      }
-    } 
     return null;
   }
-  
-  public String getDisplayName() {
-    return displayName;
-  }
 
-  public List<String> getAddresses() {
-    return addresses;
-  }
-  
-  public List<String> getEmails() {
-    return emails;
-  }
-  
-  public List<String> getPhoneNumbers() {
-    return phoneNumbers;
-  }
-  
-  public Date getStudyStartDate() {
-    return studyStartDate != null ? Date.from(studyStartDate.toInstant()) : null;
-  }
-
-  public Date getStudyTimeEnd() {
-    return studyTimeEnd != null ? Date.from(studyTimeEnd.toInstant()) : null;
-  }
-
-  public String getStudyTimeLeftStr() {
-    return studyTimeLeftStr;
-  }
-  
-  public boolean isChatEnabled() {
-	  return chatEnabled;
-  }
-
-  private String displayName;
-  private List<String> emails;
-  private List<String> addresses;
-  private List<String> phoneNumbers;
-  private OffsetDateTime studyStartDate;
-  private OffsetDateTime studyTimeEnd;
-  private String studyTimeLeftStr;
-  private boolean chatEnabled;
 }
--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/transcriptofrecords/TranscriptofRecordsBackingBean.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/transcriptofrecords/TranscriptofRecordsBackingBean.java
@@ -1,36 +1,15 @@
 package fi.otavanopisto.muikku.plugins.transcriptofrecords;
 
-import java.time.OffsetDateTime;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.apache.commons.lang3.StringUtils;
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.annotation.RequestAction;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import fi.otavanopisto.muikku.jsf.NavigationRules;
-import fi.otavanopisto.muikku.model.users.UserEntity;
-import fi.otavanopisto.muikku.plugins.transcriptofrecords.model.TranscriptOfRecordsFile;
-import fi.otavanopisto.muikku.schooldata.GradingController;
-import fi.otavanopisto.muikku.schooldata.SchoolDataIdentifier;
-import fi.otavanopisto.muikku.schooldata.entity.GradingScale;
-import fi.otavanopisto.muikku.schooldata.entity.GradingScaleItem;
-import fi.otavanopisto.muikku.schooldata.entity.User;
 import fi.otavanopisto.muikku.session.SessionController;
-import fi.otavanopisto.muikku.users.UserController;
-import fi.otavanopisto.muikku.users.UserEntityController;
 import fi.otavanopisto.security.LoggedIn;
 
 @Named
@@ -41,142 +20,14 @@ import fi.otavanopisto.security.LoggedIn;
 public class TranscriptofRecordsBackingBean {
   
   @Inject
-  private Logger logger;
-
-  @Inject
   private SessionController sessionController;
-
-  @Inject
-  private GradingController gradingController;
-  
-  @Inject
-  private TranscriptOfRecordsFileController transcriptOfRecordsFileController;
-
-  @Inject
-  private TranscriptOfRecordsController transcriptOfRecordsController;
-
-  @Inject
-  private UserController userController;
-
-  @Inject
-  private UserEntityController userEntityController;
   
   @RequestAction
   public String init() {
     if (!sessionController.hasEnvironmentPermission(TranscriptofRecordsPermissions.TRANSCRIPT_OF_RECORDS_VIEW)) {
       return NavigationRules.ACCESS_DENIED;
     }
-    
-    Map<String, Grade> grades = new HashMap<>();
-    
-    List<GradingScale> gradingScales = gradingController.listGradingScales();
-    for (GradingScale gradingScale : gradingScales) {
-      List<GradingScaleItem> scaleItems = gradingController.listGradingScaleItems(gradingScale);
-      for (GradingScaleItem scaleItem : scaleItems) {
-        String id = StringUtils.join(new String[] { 
-            gradingScale.getSchoolDataSource(), 
-            gradingScale.getIdentifier(), 
-            scaleItem.getSchoolDataSource(), 
-            scaleItem.getIdentifier() }, '-');
-        String grade = scaleItem.getName();
-        String scale = gradingScale.getName();
-        Boolean passing = scaleItem.isPassingGrade();
-        
-        grades.put(id, new Grade(grade, scale, passing));
-      }
-    }
-    
-    try {
-      this.grades = new ObjectMapper().writeValueAsString(grades);
-    } catch (JsonProcessingException e) {
-      logger.log(Level.SEVERE, "Failed to serialize grades", e);
-      return NavigationRules.INTERNAL_ERROR;
-    }
-    
-    UserEntity loggedEntity = sessionController.getLoggedUserEntity();
-    User user = userController.findUserByDataSourceAndIdentifier(sessionController.getLoggedUserSchoolDataSource(), sessionController.getLoggedUserIdentifier());
-    
-    studyStartDate = user.getStudyStartDate();
-    studyEndDate = user.getStudyEndDate();
-    studyTimeEnd = user.getStudyTimeEnd();
-    studyTimeLeftStr = userEntityController.getStudyTimeEndAsString(studyTimeEnd);
-    
-    List<TranscriptOfRecordsFile> transcriptOfRecordsFiles;
-    if (loggedEntity != null) {
-      transcriptOfRecordsFiles = transcriptOfRecordsFileController.listFiles(loggedEntity);
-    } else {
-      transcriptOfRecordsFiles = Collections.emptyList();
-    }
-
-    try {
-      files = new ObjectMapper().writeValueAsString(transcriptOfRecordsFiles);
-    } catch (JsonProcessingException e) {
-      logger.log(Level.SEVERE, "Failed to serialize files", e);
-      return NavigationRules.INTERNAL_ERROR;
-    }
-    
     return null;
   }
-  
-  public String getGrades() {
-    return grades;
-  }
-  
-  public String getFiles() {
-    return files;
-  }
-  
-  public Boolean getShowStudies() {
-    SchoolDataIdentifier userIdentifier = sessionController.getLoggedUser();
-    User user = userController.findUserByIdentifier(userIdentifier);
-    return transcriptOfRecordsController.shouldShowStudies(user);
-  }
-  
-  public String getStudyStartDate() {
-    return studyStartDate != null ? studyStartDate.toString() : null;
-  }
 
-  public String getStudyTimeEnd() {
-    return studyTimeEnd != null ? studyTimeEnd.toString() : null;
-  }
-
-  public String getStudyTimeLeftStr() {
-    return studyTimeLeftStr;
-  }
-  
-  public String getStudyEndDate() {
-    return studyEndDate != null ? studyEndDate.toString() : null;
-  }
-
-  private String grades;
-  private String files;
-  private OffsetDateTime studyStartDate;
-  private OffsetDateTime studyEndDate;
-  private OffsetDateTime studyTimeEnd;
-  private String studyTimeLeftStr;
-  
-  public static class Grade {
-    
-    public Grade(String grade, String scale, Boolean passing) {
-      this.scale = scale;
-      this.grade = grade;
-      this.passing = passing;
-    }
-    
-    public String getScale() {
-      return scale;
-    }
-
-    public String getGrade() {
-      return grade;
-    }
-    
-    public Boolean getPassing() {
-      return passing;
-    }
-    
-    private String scale;
-    private String grade;
-    private Boolean passing;
-  }
 }
