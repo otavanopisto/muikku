6bce409a47fe5f6862f1f444525eaabd1a679637
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2022-01-10 13:41:25 +0200

    Options for fullcalendar

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/body/application/toolbar/guidance-event.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/body/application/toolbar/guidance-event.tsx
@@ -6,7 +6,9 @@ import '~/sass/elements/link.scss';
 import { bindActionCreators } from "redux";
 import { StateType } from '~/reducers';
 import { GuiderType, GuiderStudentType } from "~/reducers/main-function/guider";
-import { ButtonPill } from "~/components/general/button";
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { EventInput } from "@fullcalendar/common"
 
 interface GuidanceEventProps {
   i18n: i18nType,
@@ -15,24 +17,33 @@ interface GuidanceEventProps {
 }
 
 interface GuidanceEventState {
+  newEvents: EventInput[],
+  backgroundEvents: EventInput[],
 }
 
 class GuidanceEvent extends React.Component<GuidanceEventProps, GuidanceEventState> {
   constructor(props: GuidanceEventProps) {
     super(props);
 
-    this.state = {
-      isOpen: false
-    }
-
   }
 
   render() {
-    const content = () => <div>Muuu</div>;
+    const content = () => <FullCalendar
+      plugins={[timeGridPlugin]}
+      initialView="timeGridWeek"
+      allDaySlot={false}
+      locale={"fi"}
+      slotMinTime={"09:00:00"}
+      slotMaxTime={"16:00:00"}
+      slotDuration={"01:00:00"}
+      height={"auto"}
+      weekends={false}
+      firstDay={1}
+    />;
     const footer = (closeDialog: () => any) => <div>Niii</div>;
 
     return <EnvironmentDialog modifier="guidance-event"
-      title={this.props.i18n.text.get('plugin.communicator.createmessage.label')}
+      title={this.props.i18n.text.get('plugin.guider.user.actions.reserveGuidanceTime.title')}
       content={content} footer={footer}
     >
       {this.props.children}
