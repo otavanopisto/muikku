fa6f09f35f3726f3a44afaf6b29651a554815513
jjlankinen <jari.lankinen@otavanopisto.fi>
2022-02-08 12:05:38 +0200

    * Removed playgroud * Removed unnecessary *.py files * Updated package-lock

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/organization/body.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/organization/body.tsx
@@ -1,25 +1,26 @@
-import MainFunctionNavbar from '~/components/base/main-function/navbar';
-import * as React from 'react';
-import Playground from '../__playground';
-import Application from './body/application';
-import Aside from './body/aside';
-import ScreenContainer from '../general/screen-container';
+import MainFunctionNavbar from "~/components/base/main-function/navbar";
+import * as React from "react";
+import Application from "./body/application";
+import Aside from "./body/aside";
+import ScreenContainer from "../general/screen-container";
 
-interface OrganizationManagementBodyProps {
-}
+interface OrganizationManagementBodyProps {}
 
-interface OrganizationManagementBodyState {
-}
+interface OrganizationManagementBodyState {}
 
-export default class OrganizationManagementBody extends React.Component<OrganizationManagementBodyProps, OrganizationManagementBodyState> {
-
+export default class OrganizationManagementBody extends React.Component<
+  OrganizationManagementBodyProps,
+  OrganizationManagementBodyState
+> {
   render() {
-    let aside = <Aside />
-    return (<div>
-      <MainFunctionNavbar navigation={aside} activeTrail="organization" />
-      <ScreenContainer>
-        <Application aside={aside} />
-      </ScreenContainer>
-    </div>);
+    let aside = <Aside />;
+    return (
+      <div>
+        <MainFunctionNavbar navigation={aside} activeTrail="organization" />
+        <ScreenContainer>
+          <Application aside={aside} />
+        </ScreenContainer>
+      </div>
+    );
   }
 }
