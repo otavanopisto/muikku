9c4dc9caa5ba0f84d08229815bbdd76bfab15aa6
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2022-03-10 07:10:50 +0200

    Lint errors

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/tabs.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/tabs.tsx
@@ -67,7 +67,7 @@ export const Tabs: React.FC<TabsProps> = (props) => {
     children,
   } = props;
 
-  const mobileBreakpoint = parseInt(variables.mobileBreakpoint); //Parse a breakpoint from scss to a number
+  const mobileBreakpoint = parseInt(variables.mobilebreakpoint); //Parse a breakpoint from scss to a number
 
   const isMobileWidth = useIsAtBreakpoint(mobileBreakpoint);
 
