32f277d164b02609941844c8e6e0a6888e34974a
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2022-04-01 10:14:36 +0300

    merge bug fixes

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/dialogs/student.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/dialogs/student.tsx
@@ -151,7 +151,7 @@ class StudentDialog extends React.Component<
      * Content
      * @returns JSX.Element
      */
-    const content = () => (
+    const content = () =>(
       <Tabs
         modifier="guider-student"
         tabs={tabs}
