b2cae3782cd2dbaefbb8481e0c4cce61e9238e1d
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2022-04-12 10:06:07 +0300

    contact event classnames

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/dialogs/student/tabs/contact-events/contact-event.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/dialogs/student/tabs/contact-events/contact-event.tsx
@@ -1,27 +1,47 @@
 import * as React from "react";
 
+export type contactEventReply = {
+  date: string;
+  creator: string;
+  content: string;
+};
 export interface ContactEvent {
   date: string;
   type: string;
   creator: string;
   content: string;
+  replies?: contactEventReply[];
 }
-
 interface ContactEventProps {
   event: ContactEvent;
+  modifier: string;
 }
 
 const ContactEvent: React.FC<ContactEventProps> = (props) => {
-  const { date, type, creator, content } = props.event;
+  const { date, type, creator, content, replies } = props.event;
+  const modifier = props.modifier;
 
   return (
-    <div>
-      <div>
-        <div>{date}</div>
-        <div>{type}</div>
-        <div>{creator}</div>
+    <div className="contact-event">
+      <div className="contact-event__header">
+        <div className="contact-event__date">{date}</div>
+        <div className="contact-event__type">{type}</div>
+        <div className="contact-event__creator">{creator}</div>
       </div>
-      <div>{content}</div>
+      <div className="contact-event__body">{content}</div>
+      {replies
+        ? replies.map((reply) => (
+            <div className="contact-event__reply">
+              <div className="contact-event__header contact-event__header--reply">
+                <div className="contact-event__date">{reply.date}</div>
+                <div className="contact-event__creator">{reply.creator}</div>
+              </div>
+              <div className="contact-event__body contact-event__body--reply">
+                {reply.content}
+              </div>
+            </div>
+          ))
+        : null}
     </div>
   );
 };
