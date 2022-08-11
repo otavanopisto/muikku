980fc4c67354ed6a07888fee875be13b1d2b9a9c
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2022-08-11 12:48:55 +0300

    Base rules and components for ckeditor loader

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/ckeditor-loader/content.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import { StateType } from "~/reducers";
+import { i18nType } from "~/reducers/base/i18n";
+import Base from "./base";
+
+/**
+ * MaterialLoaderContentProps
+ */
+interface CkeditorLoaderContentProps {
+  html: string;
+  i18n: i18nType;
+}
+
+/**
+ * stopPropagation
+ * @param e e
+ */
+function stopPropagation(e: React.MouseEvent<HTMLDivElement>) {
+  e.stopPropagation();
+}
+
+/**
+ * CkeditorLoaderContent
+ * @param props props
+ */
+function CkeditorLoaderContent(props: CkeditorLoaderContentProps) {
+  return (
+    <div className="react-required-container">
+      <div className="react-required-container" onClick={stopPropagation}>
+        <Base html={props.html} i18n={props.i18n} usedAs="default" />
+      </div>
+    </div>
+  );
+}
+
+/**
+ * mapStateToProps
+ * @param state state
+ * @returns object
+ */
+function mapStateToProps(state: StateType) {
+  return {
+    i18n: state.i18n,
+  };
+}
+
+/**
+ * mapDispatchToProps
+ * @returns object
+ */
+function mapDispatchToProps() {
+  return {};
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CkeditorLoaderContent);
---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/ckeditor-loader/static/link.tsx
@@ -0,0 +1,118 @@
+import * as React from "react";
+import { HTMLAttributeAnchorTarget } from "react";
+import { i18nType } from "~/reducers/base/i18n";
+import {
+  HTMLtoReactComponent,
+  HTMLToReactComponentRule,
+} from "~/util/modifiers";
+
+/**
+ * LinkProps
+ */
+interface LinkProps {
+  element: HTMLElement;
+  path: string;
+  dataset: {
+    //Someone thought it was smart to set up two versions of data
+    url?: string;
+  };
+  i18n: i18nType;
+  processingRules: HTMLToReactComponentRule[];
+}
+
+/**
+ * Link
+ */
+export default class Link extends React.Component<
+  LinkProps,
+  Record<string, unknown>
+> {
+  /**
+   * constructor
+   * @param props props
+   */
+  constructor(props: LinkProps) {
+    super(props);
+  }
+
+  /**
+   * renderAccessibilityIndicator
+   * @param target target
+   * @returns target accessibility indicator
+   */
+  renderAccessibilityIndicatorByTarget = (
+    target: HTMLAttributeAnchorTarget
+  ) => {
+    switch (target) {
+      case "_blank":
+        return (
+          <>
+            <span className="icon-external-link"></span>
+            <span className="visually-hidden">Opens to new tab</span>
+          </>
+        );
+
+      default:
+        return null;
+    }
+  };
+
+  /**
+   * render
+   */
+  render() {
+    const newRules = this.props.processingRules.filter(
+      (r) => r.id !== "link-rule"
+    );
+    newRules.push({
+      /**
+       * shouldProcessHTMLElement
+       * @param tag tag
+       * @param element element
+       */
+      shouldProcessHTMLElement: (tag, element) =>
+        tag === "a" &&
+        element.getAttribute("href") &&
+        element.getAttribute("href")[0] !== "#",
+      /**
+       * preprocessReactProperties
+       * @param tag tag
+       * @param props props
+       * @param children children
+       * @param element element
+       */
+      preprocessReactProperties: (tag, props, children, element) => {
+        const isAbsolute =
+          props.href.indexOf("/") == 0 ||
+          props.href.indexOf("mailto:") == 0 ||
+          props.href.indexOf("data:") == 0 ||
+          props.href.match("^(?:[a-zA-Z]+:)?//");
+        if (!isAbsolute) {
+          props.href = this.props.path + "/" + props.href;
+        }
+      },
+
+      /**
+       * processingFunction
+       * @param Tag Tag
+       * @param props props
+       * @param children children
+       * @param element element
+       * @returns any
+       */
+      processingFunction: (Tag, props, children, element) => {
+        if (props.target) {
+          return (
+            <Tag {...props}>
+              {children}{" "}
+              {this.renderAccessibilityIndicatorByTarget(props.target)}
+            </Tag>
+          );
+        }
+
+        return <Tag {...props}>{children}</Tag>;
+      },
+    });
+    return HTMLtoReactComponent(this.props.element, newRules);
+  }
+}
