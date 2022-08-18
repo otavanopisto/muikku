51c62fb15663c90007443a19d414d741b624c914
jjlankinen <jari.lankinen@otavanopisto.fi>
2022-08-18 11:37:34 +0300

    * Fixed image overflow when added via CKE

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/announcements/body/application/announcements.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/announcements/body/application/announcements.tsx
@@ -53,48 +53,46 @@ class Announcement extends React.Component<
       );
     }
     return (
-      <section>
-        <article className="article">
-          <header className="article__header">
-            {this.props.announcement.caption}
-          </header>
-          {this.props.announcement.workspaces.length ||
-          this.props.announcement.userGroupEntityIds.length ? (
-            <div className="labels">
-              {this.props.announcement.workspaces.map((workspace) => (
-                <span className="label" key={workspace.id}>
-                  <span className="label__icon label__icon--announcement-workspace icon-books"></span>
-                  <span className="label__text label__text--announcement-workspace">
-                    {workspace.name}{" "}
-                    {workspace.nameExtension
-                      ? "(" + workspace.nameExtension + ")"
-                      : null}
+      <article className="article">
+        <header className="article__header">
+          {this.props.announcement.caption}
+        </header>
+        {this.props.announcement.workspaces.length ||
+        this.props.announcement.userGroupEntityIds.length ? (
+          <div className="labels">
+            {this.props.announcement.workspaces.map((workspace) => (
+              <span className="label" key={workspace.id}>
+                <span className="label__icon label__icon--announcement-workspace icon-books"></span>
+                <span className="label__text label__text--announcement-workspace">
+                  {workspace.name}{" "}
+                  {workspace.nameExtension
+                    ? "(" + workspace.nameExtension + ")"
+                    : null}
+                </span>
+              </span>
+            ))}
+            {this.props.announcement.userGroupEntityIds.map((userGroupId) => {
+              if (!this.props.userIndex.groups[userGroupId]) {
+                return null;
+              }
+              return (
+                <span className="label" key={userGroupId}>
+                  <span className="label__icon label__icon--announcement-usergroup icon-users"></span>
+                  <span className="label__text label__text--announcement-usergroup">
+                    {this.props.userIndex.groups[userGroupId].name}
                   </span>
                 </span>
-              ))}
-              {this.props.announcement.userGroupEntityIds.map((userGroupId) => {
-                if (!this.props.userIndex.groups[userGroupId]) {
-                  return null;
-                }
-                return (
-                  <span className="label" key={userGroupId}>
-                    <span className="label__icon label__icon--announcement-usergroup icon-users"></span>
-                    <span className="label__text label__text--announcement-usergroup">
-                      {this.props.userIndex.groups[userGroupId].name}
-                    </span>
-                  </span>
-                );
-              })}
-            </div>
-          ) : null}
-          <div className="article__date">
-            {this.props.i18n.time.format(this.props.announcement.startDate)}
+              );
+            })}
           </div>
-          <section className="article__body rich-text">
-            <CkeditorLoaderContent html={this.props.announcement.content} />
-          </section>
-        </article>
-      </section>
+        ) : null}
+        <div className="article__date">
+          {this.props.i18n.time.format(this.props.announcement.startDate)}
+        </div>
+        <section className="article__body rich-text">
+          <CkeditorLoaderContent html={this.props.announcement.content} />
+        </section>
+      </article>
     );
   }
 }
