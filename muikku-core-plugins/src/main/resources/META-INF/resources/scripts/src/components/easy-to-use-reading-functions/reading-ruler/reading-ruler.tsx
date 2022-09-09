8b2fdcc954e515c0a478be431c11741260843993
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2022-09-09 08:49:18 +0300

    Localization support for reading ruler

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/easy-to-use-reading-functions/reading-ruler/reading-ruler.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/easy-to-use-reading-functions/reading-ruler/reading-ruler.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import Portal from "~/components/general/portal";
 import { ReadingRulerBase } from "./reading-ruler-base";
+import { bindActionCreators, Dispatch } from "redux";
+import { StateType } from "~/reducers";
+import { AnyActionType } from "~/actions";
+import { connect } from "react-redux";
+import { i18nType } from "~/reducers/base/i18n";
 
 /**
  * ReadingRulerPortalProps
  */
 interface ReadingRulerPortalProps {
   active: boolean;
+  i18n: i18nType;
   onClose?: () => void;
 }
 
@@ -17,6 +23,32 @@ interface ReadingRulerPortalProps {
  */
 export const ReadingRuler: React.FC<ReadingRulerPortalProps> = (props) => (
   <Portal isOpen={props.active}>
-    {<ReadingRulerBase active={props.active} onClose={props.onClose} />}
+    {
+      <ReadingRulerBase
+        active={props.active}
+        i18n={props.i18n}
+        onClose={props.onClose}
+      />
+    }
   </Portal>
 );
+
+/**
+ * mapStateToProps
+ * @param state state
+ */
+function mapStateToProps(state: StateType) {
+  return {
+    i18n: state.i18n,
+  };
+}
+
+/**
+ * mapDispatchToProps
+ * @param dispatch dispatch
+ */
+function mapDispatchToProps(dispatch: Dispatch<AnyActionType>) {
+  return bindActionCreators({}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReadingRuler);
