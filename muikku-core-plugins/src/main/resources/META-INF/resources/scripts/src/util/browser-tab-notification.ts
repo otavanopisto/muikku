0612eb786623225353fd90dfdf8f6c4be8c3e2ba
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2022-10-12 11:48:50 +0300

    comments the browser tab notification

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/util/browser-tab-notification.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/util/browser-tab-notification.ts
@@ -1,4 +1,11 @@
 /**
+ * As of this moment, this is only used in the chat.
+ * If there's ever any other use for this, this needs to be further developed
+ * so that there's a namespace for all the intervals this creates.
+ * Otherwise there will be many timeouts switching the document title and it'll be messy
+ */
+
+/**
  * Blinking notification for browser tab
  */
 export class BrowserTabNotification {
