b0295c66d8cfecf9aec8eb225877c441d4161d84
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2022-10-21 12:19:48 +0300

    unneeded imports in continue studies panel

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/index/body/continue-studies-panel.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/index/body/continue-studies-panel.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
-import { connect, Dispatch } from "react-redux";
-import { AnyActionType } from "~/actions";
-import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 import { i18nType } from "~/reducers/base/i18n";
 import { StatusType } from "~/reducers/base/status";
 import { WorkspaceMaterialReferenceType } from "~/reducers/workspaces";
