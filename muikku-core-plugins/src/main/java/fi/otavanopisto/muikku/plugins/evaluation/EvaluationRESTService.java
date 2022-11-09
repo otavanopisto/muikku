06b49056200b8aa359b20019fcabae118a70686c
Saara Lepistö <saara.lepisto@hotmail.com>
2022-11-09 13:09:50 +0200

    *

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/evaluation/EvaluationRESTService.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/evaluation/EvaluationRESTService.java
@@ -1093,9 +1093,7 @@ public class EvaluationRESTService extends PluginRESTService {
     WorkspaceJournalFeedback journalFeedback = evaluationController.findWorkspaceJournalFeedbackByStudentAndWorkspace(studentEntity.getId(), workspaceEntity.getId());
     
     if (journalFeedback == null) {
-      return Response.status(Status.NOT_FOUND)
-          .entity("Could not find journal feedback")
-          .build();
+      return Response.status(Status.OK).entity("Could not find feedback with given entities").build();
     }
     return Response.ok(createRestModel(journalFeedback)).build();
   }
