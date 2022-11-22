9c6d90054b9cd9fb924523969701b8eb4a7f8ee4
aviljakaine <antti.viljakainen@otavanopisto.fi>
2022-11-22 10:12:19 +0200

    Removed debug code

--- muikku-elastic-search/src/main/java/fi/otavanopisto/muikku/plugins/search/ElasticSearchProvider.java
+++ muikku-elastic-search/src/main/java/fi/otavanopisto/muikku/plugins/search/ElasticSearchProvider.java
@@ -387,8 +387,6 @@ public class ElasticSearchProvider implements SearchProvider {
             )
         );
       }
-            
-      System.out.println(query.toString());
       
       SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder()
           .query(query)
