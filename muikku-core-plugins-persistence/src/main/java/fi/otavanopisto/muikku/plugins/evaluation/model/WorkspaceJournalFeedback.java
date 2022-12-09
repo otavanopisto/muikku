f3a55219c50b55444314827b8316890843dbe0c0
Saara Lepistö <saara.lepisto@hotmail.com>
2022-12-09 11:07:17 +0200

    * Changes based on review

--- muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/evaluation/model/WorkspaceJournalFeedback.java
+++ muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/evaluation/model/WorkspaceJournalFeedback.java
@@ -18,10 +18,6 @@ public class WorkspaceJournalFeedback {
   public Long getId() {
     return id;
   }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
   
   public Long getStudent() {
     return student;
