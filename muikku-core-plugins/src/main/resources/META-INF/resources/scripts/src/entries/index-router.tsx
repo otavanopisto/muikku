1a4ec614cc8e0ab1cdd21e5d98fdeb7d7c688fd0
aviljakaine <antti.viljakainen@otavanopisto.fi>
2023-06-08 14:41:28 +0300

    Frontpage from root

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/entries/index-router.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/entries/index-router.tsx
@@ -2,14 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
-import { prepareApp } from '~/run';
-
-import Front from "~/containers/index.frontpage";
-import { Action } from "redux";
-import reducer from "~/reducers/index.frontpage";
-import { loadLocale } from "~/actions/base/locales";
-import tabOrMouse from "~/util/tab-or-mouse";
-
+import { renderFront } from './prep_frontpage';
 import { renderMainFunction } from './prep_mainfunction';
 import { renderWorkspaces } from "~/entries/prep_workspace"
 
@@ -25,30 +18,10 @@ class Page2 extends React.Component {
   }
 }
 
-function renderFront() {
-  return prepareApp(reducer, Front, async (store) => {
-    // For user that are not logged in, we need to load the locale
-    // data
-    store.dispatch(loadLocale() as Action);
-    // hack to remove the session if it still exists, this might
-    // happen if the user session expired and he got to the homepage
-    // but the chat was still active
-    tabOrMouse();
-    window.sessionStorage.removeItem("strophe-bosh-session");
-  });  
-}
-
-/*
-  Front fails to initialize
-*/
-
 ReactDOM.render(
     <BrowserRouter basename="/">
       <div>
-          <Route exact path="/" component={Page} />
-          <Route path="/test" component={Page2} />
-
-          <Route path="/front" render={renderFront} />
+          <Route exact path="/" render={renderFront} />
 
           <Route path="/organization" render={renderMainFunction} />
           <Route path="/coursepicker" render={renderMainFunction} />
