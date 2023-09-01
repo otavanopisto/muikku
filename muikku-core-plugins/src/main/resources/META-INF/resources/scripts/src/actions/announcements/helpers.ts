49be25fb57105f279c4545bd7977128d334e0feb
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2023-09-01 12:14:44 +0300

    Type guards for error handling. ResponseError has access to response that contains status codes

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/actions/announcements/helpers.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/actions/announcements/helpers.ts
@@ -4,13 +4,12 @@ import {
   AnnouncementsStatePatch,
   AnnouncerNavigationItemType,
 } from "~/reducers/announcements";
-import { MApiError } from "~/lib/mApi";
 import notificationActions from "~/actions/base/notifications";
 import { StateType } from "~/reducers";
 import { loadUserGroupIndex } from "~/actions/user-index";
 import { GetAnnouncementsRequest } from "~/generated/client";
 import { Dispatch } from "react-redux";
-import MApi from "~/api/api";
+import MApi, { isMApiError, isResponseError } from "~/api/api";
 
 /**
  * loadAnnouncementsHelper
@@ -123,9 +122,10 @@ export async function loadAnnouncementsHelper(
       payload,
     });
   } catch (err) {
-    if (!(err instanceof MApiError)) {
+    if (!isMApiError(err)) {
       throw err;
     }
+
     //Error :(
     dispatch(
       notificationActions.displayNotification(
