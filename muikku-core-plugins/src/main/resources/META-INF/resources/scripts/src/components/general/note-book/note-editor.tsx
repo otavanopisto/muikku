ec42d6d68a1d9c524ec500b3a79fc3f4c39e6c37
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2023-09-21 13:26:41 +0300

    Notebook locale fixes

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/note-book/note-editor.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/note-book/note-editor.tsx
@@ -422,7 +422,6 @@ class NoteEditor extends SessionStateComponent<
                 onClick={this.handleCancelClick}
               >
                 {this.props.t("actions.cancel")}
-                Peruuta
               </Button>
 
               {this.recovered && (
@@ -433,9 +432,8 @@ class NoteEditor extends SessionStateComponent<
                 >
                   {this.props.t("actions.remove", {
                     ns: "common",
-                    context: "draf",
+                    context: "draft",
                   })}
-                  Poista luonnos
                 </Button>
               )}
             </div>
