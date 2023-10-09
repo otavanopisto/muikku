5bedaf72265a497b36a6ec5c115ff6022c8f27b2
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2023-10-09 12:17:07 +0300

    * Prettier

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/evaluation/body/application/evaluation/evaluation-journal-event-list.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/evaluation/body/application/evaluation/evaluation-journal-event-list.tsx
@@ -252,13 +252,13 @@ const EvaluationJournalEventList: React.FC<EvaluationDiaryEventListProps> = (
                   onClick={handleJournalFeedbackEditorStateClick}
                   disabled={feedbackEditorOpen}
                 >
-                  {t("actions.edit", {ns: "common"})}
+                  {t("actions.edit", { ns: "common" })}
                 </Link>
 
                 {!feedbackEditorOpen && (
                   <DeleteJournalFeedback journalFeedback={journalFeedback}>
                     <Link className="link link--evaluation link--evaluation-delete">
-                      {t("actions.remove", {ns: "common"})}
+                      {t("actions.remove", { ns: "common" })}
                     </Link>
                   </DeleteJournalFeedback>
                 )}
