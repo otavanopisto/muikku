19deefd39d788ec4a4e5f5d2d90d5382ef014970
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2023-10-27 10:03:38 +0300

    Test removed

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/dialog.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/dialog.tsx
@@ -138,7 +138,6 @@ export default class Dialog extends React.Component<DialogProps, DialogState> {
               : this.props.modifier;
           return (
             <FocusTrap active={this.state.visible}>
-              <div>asd</div>
               <div
                 className={`dialog ${(modifiers || [])
                   .map((s) => `dialog--${s}`)
