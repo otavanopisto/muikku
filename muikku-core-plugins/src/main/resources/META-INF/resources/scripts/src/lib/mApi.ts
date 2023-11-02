5ebc56b014bebfedc8cc6bbbc15d1c773fedd7e7
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2023-11-02 09:01:29 +0200

    All old mApi related remains removed

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/lib/mApi.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/lib/mApi.ts
@@ -1,19 +1,16 @@
 /**
  * MApiError
  */
-export class MApiError extends Error {
-  /**
-   * constructor
-   * @param m m
-   */
+/* export class MApiError extends Error {
+
   constructor(m: string) {
     super(m);
     Object.setPrototypeOf(this, MApiError.prototype);
   }
-}
+} */
 
-(<any>window).MAPI_OVERRIDE_RETURN_ACTUAL_ERRORS = true;
+/* (<any>window).MAPI_OVERRIDE_RETURN_ACTUAL_ERRORS = true;
 (<any>window).MAPI_OVERRIDE_RETURN_ACTUAL_ERRORS_W_CLASS = MApiError;
 const mApi = (<any>window).mApi;
 
-export default mApi;
+export default mApi; */
