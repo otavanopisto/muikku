a6c7c433e907063f80b42c7e061e366356f8acc1
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2023-11-21 07:35:42 +0200

    reverts summar y to baseline after testing reverts loadrecords to baseline

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/containers/main-function.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/containers/main-function.tsx
@@ -344,7 +344,8 @@ export default class MainFunction extends React.Component<
         setLocationToInfoInTranscriptOfRecords() as Action
       );
       this.props.store.dispatch(updateSummary(userId) as Action);
-    } // this.props.store.dispatch(updateHops() as Action);
+    }
+    this.props.store.dispatch(updateHops() as Action);
   }
 
   /**
