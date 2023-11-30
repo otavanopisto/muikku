2eca6d4c6d62dff47fc7bcb722cecb2303339fd7
aviljakaine <antti.viljakainen@otavanopisto.fi>
2023-11-30 10:15:45 +0200

    Tests fix

--- muikku-atests/src/test/java/fi/otavanopisto/muikku/rest/users/test/UserRESTServiceTestsIT.java
+++ muikku-atests/src/test/java/fi/otavanopisto/muikku/rest/users/test/UserRESTServiceTestsIT.java
@@ -5,10 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
-import io.restassured.response.Response;
-
 import fi.otavanopisto.muikku.AbstractRESTTest;
-import fi.otavanopisto.muikku.model.users.EnvironmentRoleArchetype;
+import io.restassured.response.Response;
 
 public class UserRESTServiceTestsIT extends AbstractRESTTest {
 
@@ -28,7 +26,6 @@ public class UserRESTServiceTestsIT extends AbstractRESTTest {
   }
   
   /**
-   * /user/users
    * /user/users/{ID}
    */
   
@@ -45,134 +42,13 @@ public class UserRESTServiceTestsIT extends AbstractRESTTest {
   }
 
   @Test
-  public void testSearchUsers() throws NoSuchFieldException {
-    Response response = asAdmin()
-        .get("/user/users");
-
-    response.then().statusCode(200);
-
-    String expected = "["
-        + "{'id':4,'firstName':'Test','lastName':'Administrator','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'ad...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':2,'firstName':'Test','lastName':'Staff1member','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'te...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':3,'firstName':'Test','lastName':'Staff2member','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'ma...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':1,'firstName':'Test','lastName':'User','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'te...@example.com'}]";
-    
-    JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
-  }
-
-  @Test
-  public void testSearchUsersAsStudent() throws NoSuchFieldException {
-    Response response = asStudent()
-        .get("/user/users");
-
-    response.then().statusCode(200);
-
-    String expected = "["
-        + "{'id':4,'firstName':'Test','lastName':'Administrator','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'ad...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':2,'firstName':'Test','lastName':'Staff1member','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'te...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':3,'firstName':'Test','lastName':'Staff2member','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'ma...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':1,'firstName':'Test','lastName':'User','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'te...@example.com'}]";
-    JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
-  }
-
-  @Test
-  public void testSearchUsersWithUserGroups() throws NoSuchFieldException {
-    Long userGroupId = 2l;
-    
-    Response response = asAdmin()
-        .param("userGroupIds", userGroupId)
-        .get("/user/users");
-
-    response.then().statusCode(200);
-
-    String expected = "["
-        + "{'id':4,'firstName':'Test','lastName':'Administrator','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'ad...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':1,'firstName':'Test','lastName':'User','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'te...@example.com'}]";
-    JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
-  }
-
-  @Test
-  public void testSearchUsersWithMyUserGroups() throws NoSuchFieldException {
-    Response response = asAdmin()
-        .param("myUserGroups", true)
-        .get("/user/users");
-
-    response.then().statusCode(200);
-
-    String expected = "["
-        + "{'id':4,'firstName':'Test','lastName':'Administrator','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'ad...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':1,'firstName':'Test','lastName':'User','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'te...@example.com'}]";
-    JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
-  }
-  
-  @Test
-  public void testSearchUsersWithWorkspaces() throws NoSuchFieldException {
-    Long workspaceId = 1l;
-    
-    Response response = asAdmin()
-        .param("workspaceIds", workspaceId)
-        .get("/user/users");
-
-    response.then().statusCode(200);
-
-    String expected = "["
-        + "{'id':4,'firstName':'Test','lastName':'Administrator','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'ad...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':1,'firstName':'Test','lastName':'User','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'te...@example.com'}]";
-    
-    JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
-  }
-
-  @Test
-  public void testSearchUsersWithMyWorkspaces() throws NoSuchFieldException {
-    Response response = asAdmin()
-        .param("myWorkspaces", true)
-        .get("/user/users");
-
-    response.then().statusCode(200);
-
-    String expected = "["
-        + "{'id':4,'firstName':'Test','lastName':'Administrator','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'ad...@example.com','studyStartDate':null,'studyTimeEnd':null},"
-        + "{'id':1,'firstName':'Test','lastName':'User','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'te...@example.com'}]";
-    
-    JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
-  }
-
-  @Test
-  public void testSearchUsersWithSearchString() throws NoSuchFieldException {
-    String searchString = "a";
-    
-    Response response = asAdmin()
-        .param("q", searchString)
-        .get("/user/users");
-
-    response.then().statusCode(200);
-    
-    String expected = "[{'id':4,'firstName':'Test','lastName':'Administrator','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'ad...@example.com','studyStartDate':null,'studyTimeEnd':null}]";
-    JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
-  }
-
-  @Test
-  public void testSearchUsersWithArchetype() throws NoSuchFieldException {
-    String archetype = EnvironmentRoleArchetype.STUDENT.name();
-    
-    Response response = asAdmin()
-        .param("archetype", archetype)
-        .get("/user/users");
-
-    response.then().statusCode(200);
-
-    String expected = "[{'id':1,'firstName':'Test','lastName':'User','hasImage':false,'nationality':null,'language':null,'municipality':null,'school':null,'email':'te...@example.com'}]";
-    JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
-  }
-
-  @Test
   public void testFindStudent1AsAdmin() throws NoSuchFieldException {
     String studentId = "PYRAMUS-STUDENT-1";
     
     Response response = asAdmin().get("/user/students/{ID}", studentId);
     
     response.then().statusCode(200);
-    String expected = "{\"id\":\"PYRAMUS-STUDENT-1\",\"firstName\":\"Test\",\"lastName\":\"User\",\"nickName\":null,\"studyProgrammeName\":\"Test Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"te...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2070-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":1,\"flags\":null,\"organization\":{\"id\":1,\"name\":\"Default\"}}";
+    String expected = "{\"id\":\"PYRAMUS-STUDENT-1\",\"firstName\":\"Test\",\"lastName\":\"User\",\"nickName\":null,\"studyProgrammeName\":\"Test Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"te...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2070-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":1,\"organization\":{\"id\":1,\"name\":\"Default\"}}";
     JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
   }
 
@@ -183,7 +59,7 @@ public class UserRESTServiceTestsIT extends AbstractRESTTest {
     Response response = asAdmin().get("/user/students/{ID}", studentId);
     
     response.then().statusCode(200);
-    String expected = "{\"id\":\"PYRAMUS-STUDENT-6\",\"firstName\":\"Hidden\",\"lastName\":\"Dragon\",\"nickName\":null,\"studyProgrammeName\":\"Test Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"cr...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2011-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":6,\"flags\":null,\"organization\":{\"id\":1,\"name\":\"Default\"}}";
+    String expected = "{\"id\":\"PYRAMUS-STUDENT-6\",\"firstName\":\"Hidden\",\"lastName\":\"Dragon\",\"nickName\":null,\"studyProgrammeName\":\"Test Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"cr...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2011-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":6,\"organization\":{\"id\":1,\"name\":\"Default\"}}";
     JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
   }
   
@@ -194,7 +70,7 @@ public class UserRESTServiceTestsIT extends AbstractRESTTest {
     Response response = asAdmin().get("/user/students/{ID}", studentId);
     
     response.then().statusCode(200);
-    String expected = "{\"id\":\"PYRAMUS-STUDENT-7\",\"firstName\":\"Constipated\",\"lastName\":\"Moose\",\"nickName\":null,\"studyProgrammeName\":\"Secondary Organization Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"co...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2011-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":7,\"flags\":null,\"organization\":{\"id\":2,\"name\":\"Secondary Test Organization\"}}";
+    String expected = "{\"id\":\"PYRAMUS-STUDENT-7\",\"firstName\":\"Constipated\",\"lastName\":\"Moose\",\"nickName\":null,\"studyProgrammeName\":\"Secondary Organization Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"co...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2011-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":7,\"organization\":{\"id\":2,\"name\":\"Secondary Test Organization\"}}";
     JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
   }
   
@@ -205,7 +81,7 @@ public class UserRESTServiceTestsIT extends AbstractRESTTest {
     Response response = asStudent().get("/user/students/{ID}", studentId);
     
     response.then().statusCode(200);
-    String expected = "{\"id\":\"PYRAMUS-STUDENT-1\",\"firstName\":\"Test\",\"lastName\":\"User\",\"nickName\":null,\"studyProgrammeName\":\"Test Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"te...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2070-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":1,\"flags\":null,\"organization\":{\"id\":1,\"name\":\"Default\"}}";
+    String expected = "{\"id\":\"PYRAMUS-STUDENT-1\",\"firstName\":\"Test\",\"lastName\":\"User\",\"nickName\":null,\"studyProgrammeName\":\"Test Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"te...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2070-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":1,\"organization\":{\"id\":1,\"name\":\"Default\"}}";
     JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
   }
 
@@ -216,7 +92,7 @@ public class UserRESTServiceTestsIT extends AbstractRESTTest {
     Response response = asAdmin().get("/user/students/{ID}", studentId);
     
     response.then().statusCode(200);
-    String expected = "{\"id\":\"PYRAMUS-STUDENT-6\",\"firstName\":\"Hidden\",\"lastName\":\"Dragon\",\"nickName\":null,\"studyProgrammeName\":\"Test Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"cr...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2011-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":6,\"flags\":null,\"organization\":{\"id\":1,\"name\":\"Default\"}}";
+    String expected = "{\"id\":\"PYRAMUS-STUDENT-6\",\"firstName\":\"Hidden\",\"lastName\":\"Dragon\",\"nickName\":null,\"studyProgrammeName\":\"Test Study Programme\",\"hasImage\":false,\"nationality\":null,\"language\":null,\"municipality\":null,\"school\":null,\"email\":\"cr...@example.com\",\"studyStartDate\":\"2010-01-01T00:00:00.000+00:00\",\"studyEndDate\":\"2011-01-01T00:00:00.000+00:00\",\"studyTimeEnd\":null,\"curriculumIdentifier\":null,\"updatedByStudent\":false,\"userEntityId\":6,\"organization\":{\"id\":1,\"name\":\"Default\"}}";
     JSONAssert.assertEquals(expected, response.body().asString(), STRICT_JSON);
   }
 
