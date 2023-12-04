47824e65a34a44efa371758c73f228e479008b9d
Petja Laitila <petja.laitila@otavanopisto.fi>
2023-12-04 12:05:24 +0200

    Records view WCAG tests

--- muikku-atests/src/test/java/fi/otavanopisto/muikku/wcag/records/RecordsAT.java
+++ muikku-atests/src/test/java/fi/otavanopisto/muikku/wcag/records/RecordsAT.java
@@ -10,17 +10,34 @@ public class RecordsAT extends AbstractWCAGTest{
   public void recordsTest() throws Exception {
     login(getTestStudent(), getTestStudentPassword(), true);
     navigate("/records", true);
-    waitForPresent(".application-sub-panel__card-highlight--summary-activity");
+    waitForVisible(".application-sub-panel__card-highlight--summary-activity");
+    waitForVisible(".notes__item");
     testAccessibility("Records summary view");
-    navigate("/records#hops", true);
-    waitForPresent(".form-element__radio-option-container #goalMatriculationExamyes");
-    testAccessibility("Records view HOPS");
+    
     navigate("/records#records", true);
-    waitForPresent(".application-list__header-primary");
+    refresh();
+    waitForVisible(".application-list__content .application-list__header-primary-meta--records");
     testAccessibility("Records records view");
-    waitAndClick(".course--studies:first-child");
-    waitForPresent(".workspace-assessment__literal");
-    waitForPresent(".application-list__item.assignment:first-child");
-    testAccessibility("Records single course view");   
+    
+    waitAndClick(".application-list__item:nth-child(2)");
+    waitForVisible(".workspace-assessment__grade-data");
+    testAccessibility("Records with course opened");
+    
+    waitAndClick(".application-list__item:nth-child(2) .button--assignments-and-exercises");
+    waitForVisible(".application-list__content");
+    waitAndClick(".application-sub-panel__header--studies-assignments .link--studies-open-close:first-child");
+    waitForVisible(".application-list__content .material-page__content");
+    testAccessibility("Evaluated exercises opened");
+  
+    navigate("/records#hops", true);
+    refresh();
+    waitForVisible("#goalSecondarySchoolDegreeyes");
+    waitForVisible("#additionalInfo");
+    testAccessibility("Records view HOPS");
+    
+    navigate("/records#yo", true);
+    refresh();
+    waitForVisible(".application-sub-panel--yo-status-container");
+    testAccessibility("Records yo view");
   }
 }
