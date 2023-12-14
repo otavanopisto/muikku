c21b10dda3cc6a3c4cbda0692967099137b2103f
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2023-12-14 12:58:23 +0200

    * Prettier formatting

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/dialog.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/dialog.tsx
@@ -138,7 +138,10 @@ export default class Dialog extends React.Component<DialogProps, DialogState> {
               ? [this.props.modifier]
               : this.props.modifier;
           return (
-            <FocusTrap active={this.state.visible} focusTrapOptions={{allowOutsideClick: true}}>
+            <FocusTrap
+              active={this.state.visible}
+              focusTrapOptions={{ allowOutsideClick: true }}
+            >
               <div
                 role="dialog"
                 className={`dialog ${(modifiers || [])
--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/environment-dialog.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/environment-dialog.tsx
@@ -24,35 +24,38 @@ export default class EnvironmentDialog extends Dialog {
         closeOnEsc
       >
         {(closePortal: () => any) => (
-          <FocusTrap active={this.state.visible} focusTrapOptions={{allowOutsideClick: true}}>
-          <div
-            className={`env-dialog env-dialog--mainfunction env-dialog--${
-              this.props.modifier
-            } ${this.state.visible ? "visible" : ""}`}
-            onClick={this.onOverlayClick.bind(this, closePortal)}
+          <FocusTrap
+            active={this.state.visible}
+            focusTrapOptions={{ allowOutsideClick: true }}
           >
-            <section
-              role="dialog"
-              className="env-dialog__wrapper"
-              aria-labelledby={`dialog-title-${this.props.modifier}`}
-              aria-modal="true"
+            <div
+              className={`env-dialog env-dialog--mainfunction env-dialog--${
+                this.props.modifier
+              } ${this.state.visible ? "visible" : ""}`}
+              onClick={this.onOverlayClick.bind(this, closePortal)}
             >
-              <div role="form" className="env-dialog__content">
-                <header
-                  className="env-dialog__header"
-                  id={`dialog-title-${this.props.modifier}`}
-                >
-                  {this.props.title}
-                </header>
-                <section className="env-dialog__body">
-                  {this.props.content(closePortal)}
-                </section>
-                <footer className="env-dialog__footer">
-                  {this.props.footer && this.props.footer(closePortal)}
-                </footer>
-              </div>
-            </section>
-          </div>
+              <section
+                role="dialog"
+                className="env-dialog__wrapper"
+                aria-labelledby={`dialog-title-${this.props.modifier}`}
+                aria-modal="true"
+              >
+                <div role="form" className="env-dialog__content">
+                  <header
+                    className="env-dialog__header"
+                    id={`dialog-title-${this.props.modifier}`}
+                  >
+                    {this.props.title}
+                  </header>
+                  <section className="env-dialog__body">
+                    {this.props.content(closePortal)}
+                  </section>
+                  <footer className="env-dialog__footer">
+                    {this.props.footer && this.props.footer(closePortal)}
+                  </footer>
+                </div>
+              </section>
+            </div>
           </FocusTrap>
         )}
       </Portal>
