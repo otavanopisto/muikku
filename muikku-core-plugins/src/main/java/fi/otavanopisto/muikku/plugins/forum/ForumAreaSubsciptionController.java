8c9b574e5c66f7d29647af94cec8666b1ae9fb52
Pasi Kukkonen <pasi.kukkonen@otavanopisto.fi>
2024-02-19 07:40:47 +0200

    - resolves #6967

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/forum/ForumAreaSubsciptionController.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/forum/ForumAreaSubsciptionController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import javax.inject.Inject;
 
 import fi.otavanopisto.muikku.model.users.UserEntity;
+import fi.otavanopisto.muikku.model.workspace.WorkspaceEntity;
+import fi.otavanopisto.muikku.plugins.forum.dao.WorkspaceForumAreaDAO;
 import fi.otavanopisto.muikku.plugins.forum.model.ForumArea;
+import fi.otavanopisto.muikku.plugins.forum.model.WorkspaceForumArea;
 import fi.otavanopisto.muikku.plugins.forum.wall.ForumAreaSubscription;
 import fi.otavanopisto.muikku.plugins.forum.wall.ForumAreaSubscriptionDAO;
 
@@ -13,7 +16,29 @@ public class ForumAreaSubsciptionController {
 
   @Inject
   private ForumAreaSubscriptionDAO forumAreaSubscriptionDAO;
+  
+  @Inject
+  private WorkspaceForumAreaDAO workspaceForumAreaDAO;
+  
+  public void removeAreaSubscriptions(UserEntity userEntity) {
+    List<ForumAreaSubscription> subscriptions = listByUser(userEntity);
+    for (ForumAreaSubscription subscription : subscriptions) {
+      deleteSubscription(subscription);
+    }
+  }
 
+  public void removeAreaSubscriptions(UserEntity userEntity, WorkspaceEntity workspaceEntity) {
+    List<ForumAreaSubscription> subscriptions = listByUser(userEntity);
+    if (subscriptions.isEmpty()) {
+      return;
+    }
+    List<WorkspaceForumArea> areas = workspaceForumAreaDAO.listByWorkspaceEntity(workspaceEntity);
+    for (ForumAreaSubscription subscription : subscriptions) {
+      if (areas.stream().filter(a -> a.getId().equals(subscription.getForumArea().getId())).findFirst().orElse(null) != null) {
+        deleteSubscription(subscription);
+      }
+    }
+  }
   
   public ForumAreaSubscription createForumAreaSubsciption(ForumArea forumArea, UserEntity userEntity) {
     return forumAreaSubscriptionDAO.create(userEntity, forumArea);
--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/InactiveUserListener.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/InactiveUserListener.java
@@ -5,11 +5,15 @@ import java.util.List;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import fi.otavanopisto.muikku.model.users.UserEntity;
 import fi.otavanopisto.muikku.model.users.UserSchoolDataIdentifier;
 import fi.otavanopisto.muikku.model.workspace.WorkspaceUserEntity;
+import fi.otavanopisto.muikku.plugins.forum.ForumAreaSubsciptionController;
+import fi.otavanopisto.muikku.plugins.forum.ForumThreadSubsciptionController;
 import fi.otavanopisto.muikku.plugins.search.UserIndexer;
 import fi.otavanopisto.muikku.schooldata.SchoolDataIdentifier;
 import fi.otavanopisto.muikku.schooldata.events.SchoolDataUserInactiveEvent;
+import fi.otavanopisto.muikku.users.UserEntityController;
 import fi.otavanopisto.muikku.users.UserSchoolDataIdentifierController;
 import fi.otavanopisto.muikku.users.WorkspaceUserEntityController;
 
@@ -17,12 +21,21 @@ public class InactiveUserListener {
 
   @Inject
   private UserIndexer userIndexer;
+  
+  @Inject
+  private UserEntityController userEntityController;
 
   @Inject
   private UserSchoolDataIdentifierController userSchoolDataIdentifierController;
   
   @Inject
   private WorkspaceUserEntityController workspaceUserEntityController;
+
+  @Inject
+  private ForumAreaSubsciptionController forumAreaSubsciptionController;
+  
+  @Inject
+  private ForumThreadSubsciptionController forumThreadSubsciptionController;
   
   public void onSchoolDataUserInactiveEvent(@Observes SchoolDataUserInactiveEvent event) {
     SchoolDataIdentifier schoolDataIdentifier = new SchoolDataIdentifier(event.getIdentifier(), event.getDataSource());
@@ -36,8 +49,10 @@ public class InactiveUserListener {
     if (userSchoolDataIdentifier != null) {
       userIndexer.indexUser(userSchoolDataIdentifier.getUserEntity());
     }
+    // #6967: Remove user's all forum subscriptions
+    UserEntity userEntity = userEntityController.findUserEntityByDataSourceAndIdentifier(event.getDataSource(), event.getIdentifier());
+    forumAreaSubsciptionController.removeAreaSubscriptions(userEntity);
+    forumThreadSubsciptionController.removeThreadSubscriptions(userEntity);
   }
-
-  
 
 }
