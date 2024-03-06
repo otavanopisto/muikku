8f26ba707d26b8792b351fe155ca589dc0ee5e35
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2024-03-06 08:58:06 +0200

    * Raised dragging timer to 150ms * Fixed incorrect locale in study history

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/draggable.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/draggable.tsx
@@ -448,7 +448,7 @@ export default class Draggable extends React.Component<
     }
 
     if (this.state.isDragging) {
-      if (new Date().getTime() - this.timer <= 70) {
+      if (new Date().getTime() - this.timer <= 150) {
         this.props.onClick && this.props.onClick(e as any);
         this.setState(
           {
