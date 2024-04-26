1bf8ccd2d924f38cb7b3293b0d36b04df84ae083
aviljakaine <antti.viljakainen@otavanopisto.fi>
2024-04-26 08:19:31 +0300

    Fixed Workspace announcements for guardian

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/WorkspaceRESTService.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/WorkspaceRESTService.java
@@ -789,24 +789,18 @@ public class WorkspaceRESTService extends PluginRESTService {
   @Path("/workspaces/{URLNAME}/basicInfo")
   @RESTPermit (handling = Handling.INLINE)
   public Response getWorkspaceBasicInfo(@PathParam("URLNAME") String urlName) {
-    schoolDataBridgeSessionController.startSystemSession();
-    try {
-      WorkspaceEntity workspaceEntity = workspaceController.findWorkspaceEntityByUrlName(urlName);
+    WorkspaceEntity workspaceEntity = workspaceController.findWorkspaceEntityByUrlName(urlName);
 
-      if (workspaceEntity == null) {
-        return Response.status(Status.NOT_FOUND).build();
-      }
-      
-      WorkspaceBasicInfo workspaceBasicInfo = workspaceRESTModelController.workspaceBasicInfo(workspaceEntity.getId());
-      if (workspaceBasicInfo == null) {
-        return Response.status(Status.NOT_FOUND).build();
-      }
-  
-      return Response.ok(workspaceBasicInfo).build();
+    if (workspaceEntity == null) {
+      return Response.status(Status.NOT_FOUND).build();
     }
-    finally {
-      schoolDataBridgeSessionController.endSystemSession();
+    
+    WorkspaceBasicInfo workspaceBasicInfo = workspaceRESTModelController.workspaceBasicInfo(workspaceEntity.getId());
+    if (workspaceBasicInfo == null) {
+      return Response.status(Status.NOT_FOUND).build();
     }
+
+    return Response.ok(workspaceBasicInfo).build();
   }
 
   @GET
--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/model/WorkspaceRESTModelController.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/model/WorkspaceRESTModelController.java
@@ -3,6 +3,7 @@ package fi.otavanopisto.muikku.plugins.workspace.rest.model;
 import javax.inject.Inject;
 
 import fi.otavanopisto.muikku.model.workspace.WorkspaceEntity;
+import fi.otavanopisto.muikku.schooldata.SchoolDataBridgeSessionController;
 import fi.otavanopisto.muikku.schooldata.WorkspaceController;
 import fi.otavanopisto.muikku.schooldata.WorkspaceEntityController;
 import fi.otavanopisto.muikku.schooldata.entity.Workspace;
@@ -15,19 +16,28 @@ public class WorkspaceRESTModelController {
   @Inject
   private WorkspaceEntityController workspaceEntityController;
   
+  @Inject
+  private SchoolDataBridgeSessionController schoolDataBridgeSessionController;
+  
   public WorkspaceBasicInfo workspaceBasicInfo(Long workspaceId) {
-    WorkspaceEntity workspaceEntity = workspaceEntityController.findWorkspaceEntityById(workspaceId);
-    if (workspaceEntity != null) {
-      Workspace workspace = workspaceController.findWorkspace(workspaceEntity);      
-
-      if (workspace != null) {
-        return new WorkspaceBasicInfo(
-            workspaceEntity.getId(), 
-            workspaceEntity.getUrlName(), 
-            workspace.getName(), 
-            workspace.getNameExtension());
+    schoolDataBridgeSessionController.startSystemSession();
+    try {
+      WorkspaceEntity workspaceEntity = workspaceEntityController.findWorkspaceEntityById(workspaceId);
+      if (workspaceEntity != null) {
+        Workspace workspace = workspaceController.findWorkspace(workspaceEntity);      
+  
+        if (workspace != null) {
+          return new WorkspaceBasicInfo(
+              workspaceEntity.getId(), 
+              workspaceEntity.getUrlName(), 
+              workspace.getName(), 
+              workspace.getNameExtension());
+        }
       }
+      return null;
     }
-    return null;
+    finally {
+      schoolDataBridgeSessionController.endSystemSession();
+    }
   }
 }
