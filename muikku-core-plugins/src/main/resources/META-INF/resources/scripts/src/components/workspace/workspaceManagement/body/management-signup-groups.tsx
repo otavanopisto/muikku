748abdd25dcf45ff77b9dff377e26dca53ed56f8
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2024-08-15 14:46:14 +0300

    roll back for Sign up rights

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/workspace/workspaceManagement/body/management-signup-groups.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/workspace/workspaceManagement/body/management-signup-groups.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import CKEditor from "~/components/general/ckeditor";
-import Dropdown from "~/components/general/dropdown";
 import { SearchFormElement } from "~/components/general/form-element";
 import { WorkspaceSignupGroup } from "~/generated/client";
 import { filterHighlight, filterMatch } from "~/util/modifiers";
@@ -25,9 +23,7 @@ interface ManagementSignupGroupsProps {
  */
 const ManagementSignupGroups = (props: ManagementSignupGroupsProps) => {
   const { t } = useTranslation(["workspace"]);
-
   const { onChange } = props;
-
   const [workspaceSignupGroupFilter, setWorkspaceSignupGroupFilter] =
     React.useState<string>("");
 
@@ -42,21 +38,7 @@ const ManagementSignupGroups = (props: ManagementSignupGroupsProps) => {
   // Expensive operation, memoize the list
   const memoizedList = React.useMemo(
     () =>
-      props.workspaceSignupGroups
-        .map((permission) => {
-          if (permission.signupMessage === null) {
-            permission.signupMessage = {
-              caption: "",
-              content: "",
-              enabled: false,
-            };
-
-            return permission;
-          }
-
-          return permission;
-        })
-        .filter((permission) =>
+      props.workspaceSignupGroups.filter((permission) =>
           filterMatch(permission.userGroupName, workspaceSignupGroupFilter)
         )
         .sort((a, b) => a.userGroupName.localeCompare(b.userGroupName)),
@@ -128,8 +110,6 @@ interface ManagementSignupGroupItem {
 const ManagementSignupGroupItem = (props: ManagementSignupGroupItem) => {
   const { workspaceSignupGroup } = props;
 
-  const { t } = useTranslation(["workspace"]);
-
   /**
    * Handles toggle
    * @param event event
@@ -145,62 +125,9 @@ const ManagementSignupGroupItem = (props: ManagementSignupGroupItem) => {
     props.onChange(updatedWorkspaceSignupGroup);
   };
 
-  /**
-   * Handles workspace signup group message caption change
-   * @param signupGroup signupGroup
-   */
-  const handleWorkspaceSignupGroupCaptionChange =
-    (signupGroup: WorkspaceSignupGroup) =>
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const updatedWorkspaceSignupGroup = {
-        ...signupGroup,
-        signupMessage: {
-          ...signupGroup.signupMessage,
-          caption: event.target.value,
-        },
-      };
-
-      props.onChange(updatedWorkspaceSignupGroup);
-    };
-
-  /**
-   * Handles workspace signup group message caption change
-   * @param signupGroup signupGroup
-   */
-  const handleWorkspaceSignupGroupContentChange =
-    (signupGroup: WorkspaceSignupGroup) => (text: string) => {
-      const updatedWorkspaceSignupGroup = {
-        ...signupGroup,
-        signupMessage: {
-          ...signupGroup.signupMessage,
-          content: text,
-        },
-      };
-
-      props.onChange(updatedWorkspaceSignupGroup);
-    };
-
-  /**
-   * Handles workspace signup group enabled change
-   * @param signupGroup signupGroup
-   */
-  const handleWorkspaceSignupGroupMessageToggle =
-    (signupGroup: WorkspaceSignupGroup) => (event: React.ChangeEvent) => {
-      const updatedWorkspaceSignupGroup = {
-        ...signupGroup,
-        signupMessage: {
-          ...signupGroup.signupMessage,
-          enabled: !signupGroup.signupMessage.enabled,
-        },
-      };
-
-      props.onChange(updatedWorkspaceSignupGroup);
-    };
-
   return (
-    <details>
-      <summary>
-        <span className="form-element form-element--checkbox-radiobutton-inside-summary">
+      <div>
+        <span className="form-element form-element--checkbox-radiobutton">
           <input
             id={`usergroup${workspaceSignupGroup.userGroupEntityId}`}
             type="checkbox"
@@ -215,89 +142,8 @@ const ManagementSignupGroupItem = (props: ManagementSignupGroupItem) => {
             )}
           </label>
         </span>
+      </div>  );
 
-        <Dropdown
-          modifier="instructions"
-          openByHover
-          alignSelfVertically="top"
-          content={
-            <p>
-              {t("content.workspaceSignupGroupMessageInfo", {
-                ns: "workspace",
-              })}
-            </p>
-          }
-        >
-          <span>
-            <label htmlFor="enable-signup-message" className="visually-hidden">
-              {t("labels.activateSignupMessage", {
-                ns: "workspace",
-              })}
-            </label>
-            <input
-              id="enable-signup-message"
-              type="checkbox"
-              className={`button-pill button-pill--autoreply-switch ${
-                workspaceSignupGroup.signupMessage.enabled
-                  ? "button-pill--autoreply-switch-active"
-                  : ""
-              }`}
-              checked={workspaceSignupGroup.signupMessage.enabled}
-              disabled={
-                workspaceSignupGroup.signupMessage.caption === "" ||
-                workspaceSignupGroup.signupMessage.content === ""
-              }
-              onChange={handleWorkspaceSignupGroupMessageToggle(
-                workspaceSignupGroup
-              )}
-            />
-          </span>
-        </Dropdown>
-      </summary>
-
-      <div className="details__content">
-        <div className="form__container">
-          <div className="form__row">
-            <div className="form-element form-element--sign-up-message">
-              <label htmlFor="message-caption">
-                {t("labels.workspaceSignupMessageTitle", {
-                  ns: "workspace",
-                })}
-              </label>
-              <input
-                id="message-caption"
-                className="form-element__input"
-                value={workspaceSignupGroup.signupMessage.caption}
-                onChange={handleWorkspaceSignupGroupCaptionChange(
-                  workspaceSignupGroup
-                )}
-              />
-            </div>
-          </div>
-          <div className="form__row">
-            <div className="form-element form-element--sign-up-message">
-              <label>
-                {t("labels.workspaceSignupMessageContent", {
-                  ns: "workspace",
-                })}
-              </label>
-              <CKEditor
-                editorTitle={t("labels.workspaceSignupMessageContent", {
-                  ns: "workspace",
-                })}
-                ancestorHeight={200}
-                onChange={handleWorkspaceSignupGroupContentChange(
-                  workspaceSignupGroup
-                )}
-              >
-                {workspaceSignupGroup.signupMessage.content}
-              </CKEditor>
-            </div>
-          </div>
-        </div>
-      </div>
-    </details>
-  );
 };
 
 // Memoized component, using deepEqual to compare props for memoization
