4848b424beb52d46c09f9a7aa943688a8f00f5f4
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2024-11-08 09:05:48 +0200

    guider view change

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/body.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/body.tsx
@@ -1,9 +1,9 @@
 import MainFunctionNavbar from "~/components/base/main-function/navbar";
 import Application from "./body/application";
 import Aside from "./body/aside";
-
 import * as React from "react";
 import { useTranslation } from "react-i18next";
+import { GuiderContext, GuiderViews } from "./context";
 
 /**
  * GuiderBodyProps
@@ -16,16 +16,18 @@ interface GuiderBodyProps {}
  */
 const GuiderBody = (props: GuiderBodyProps) => {
   const { t } = useTranslation("common");
-
   const aside = <Aside />;
+  const [view, setView] = React.useState("students" as GuiderViews);
   return (
     <div>
-      <MainFunctionNavbar
-        title={t("labels.guider")}
-        activeTrail="guider"
-        navigation={aside}
-      />
-      <Application aside={aside} />
+      <GuiderContext.Provider value={{ view, setView }}>
+        <MainFunctionNavbar
+          title={t("labels.guider")}
+          activeTrail="guider"
+          navigation={aside}
+        />
+        <Application aside={aside} />
+      </GuiderContext.Provider>
     </div>
   );
 };
