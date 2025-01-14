b9eff6d16381f3184bd302675022b3eedf8a42ec
aviljakaine <antti.viljakainen@otavanopisto.fi>
2025-01-14 12:57:53 +0200

    Added missing u18compulsory states for different evaluation types

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/evaluation/EvaluationRESTService.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/evaluation/EvaluationRESTService.java
@@ -1960,7 +1960,7 @@ public class EvaluationRESTService extends PluginRESTService {
     restAssessmentRequest.setSubjects(subjects);
     boolean hasPedagogyForm = pedagogyController.hasPedagogyForm(workspaceUserEntity.getUserSchoolDataIdentifier().getUserEntity().getId());
     restAssessmentRequest.setHasPedagogyForm(hasPedagogyForm);
-    
+    restAssessmentRequest.setU18Compulsory(userEntityController.isUnder18CompulsoryEducationStudent(workspaceUserEntity.getUserSchoolDataIdentifier().schoolDataIdentifier()));
     return restAssessmentRequest;
   }
   
@@ -2041,7 +2041,7 @@ public class EvaluationRESTService extends PluginRESTService {
     restAssessmentRequest.setSubjects(subjects);
     boolean hasPedagogyForm = pedagogyController.hasPedagogyForm(workspaceUserEntity.getUserSchoolDataIdentifier().getUserEntity().getId());
     restAssessmentRequest.setHasPedagogyForm(hasPedagogyForm);
-    
+    restAssessmentRequest.setU18Compulsory(userEntityController.isUnder18CompulsoryEducationStudent(workspaceUserEntity.getUserSchoolDataIdentifier().schoolDataIdentifier()));
     return restAssessmentRequest;
   }
 
