98fe430bff02be5ba8c4a79aa303ff7ca0b74235
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2025-01-23 13:49:18 +0200

    type guard formatting

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/helper-functions/type-guards.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/helper-functions/type-guards.ts
@@ -4,4 +4,5 @@ import { User, Student } from "~/generated/client";
  * isUser
  * @param item item
  */
-export const isUser = (item: Student | User): item is User => typeof item.id === 'number';
+export const isUser = (item: Student | User): item is User =>
+  typeof item.id === "number";
