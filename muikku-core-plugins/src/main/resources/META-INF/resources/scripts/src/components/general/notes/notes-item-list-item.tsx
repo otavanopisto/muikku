d131d21d17ff22c2c5137c1c55f4e5640c5dc061
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2025-01-28 14:43:11 +0200

    status setting rights

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/notes/notes-item-list-item.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/notes/notes-item-list-item.tsx
@@ -529,7 +529,7 @@ const NotesListItem = React.forwardRef<HTMLDivElement, NotesListItemProps>(
                 aria-label={t("wcag.archive")}
               />
             )}
-            {specificRecipient && renderUpdateStatus()}
+            {(specificRecipient || !multiUserNote) && renderUpdateStatus()}
           </div>
         </div>
 
@@ -581,7 +581,7 @@ const NotesListItem = React.forwardRef<HTMLDivElement, NotesListItemProps>(
           {!loggedUserIsCreator ? creatorName : null}
         </div>
         <div className="notes__item-footer">
-          {specificRecipient && renderStatus()}
+          {(specificRecipient || !multiUserNote) && renderStatus()}
         </div>
       </div>
     );
