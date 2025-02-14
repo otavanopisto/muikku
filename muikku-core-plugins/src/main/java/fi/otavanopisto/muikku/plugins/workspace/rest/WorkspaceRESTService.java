768b52aae0db763577dd2bceb1ba44899ee7e773
pkukkon <pasi.kukkonen@otavanopisto.fi>
2025-02-14 10:07:40 +0200

    - yawn

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/WorkspaceRESTService.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/rest/WorkspaceRESTService.java
@@ -1990,7 +1990,8 @@ public class WorkspaceRESTService extends PluginRESTService {
           entity.getAssignmentType(),
           entity.getCorrectAnswers(),
           entity.getTitleLanguage(),
-          entity.getMaxPoints());
+          entity.getMaxPoints(),
+          entity.getAi());
       if (entity.getNextSiblingId() != null) {
         WorkspaceNode nextSibling = workspaceMaterialController.findWorkspaceNodeById(entity.getNextSiblingId());
         if (nextSibling == null) {
@@ -2025,7 +2026,8 @@ public class WorkspaceRESTService extends PluginRESTService {
                     workspaceMaterial.getAssignmentType(),
                     workspaceMaterial.getCorrectAnswers(),
                     workspaceMaterial.getLanguage(),
-                    workspaceMaterial.getMaxPoints());
+                    workspaceMaterial.getMaxPoints(),
+                    workspaceMaterial.getAi());
               }
             }
           }
@@ -2740,7 +2742,7 @@ public class WorkspaceRESTService extends PluginRESTService {
     return new fi.otavanopisto.muikku.plugins.workspace.rest.model.WorkspaceMaterial(workspaceMaterial.getId(), workspaceMaterial.getMaterialId(),
         workspaceMaterial.getParent() != null ? workspaceMaterial.getParent().getId() : null, nextSiblingId, workspaceMaterial.getHidden(),
         workspaceMaterial.getAssignmentType(), workspaceMaterial.getCorrectAnswers(), workspaceMaterial.getPath(), workspaceMaterial.getTitle(),
-        workspaceNode.getLanguage(), workspaceMaterial.getMaxPoints());
+        workspaceNode.getLanguage(), workspaceMaterial.getMaxPoints(), workspaceMaterial.getAi());
   }
 
   private fi.otavanopisto.muikku.plugins.workspace.rest.model.Workspace createRestModel(
@@ -3082,7 +3084,8 @@ public class WorkspaceRESTService extends PluginRESTService {
         restWorkspaceMaterial.getCorrectAnswers(),
         restWorkspaceMaterial.getTitle(),
         restWorkspaceMaterial.getTitleLanguage(),
-        restWorkspaceMaterial.getMaxPoints());
+        restWorkspaceMaterial.getMaxPoints(),
+        restWorkspaceMaterial.getAi());
     restWorkspaceMaterial.setPath(workspaceNode.getPath());
 
     // #6440: If the material is a journal page whose title is changed, update respective journal entry titles
