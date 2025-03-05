f5e91edd17a73a27309a466930e3ccd6119a93dd
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2025-03-05 12:39:12 +0200

    Webpack dev server websocket proxys works now without errors

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/webpack-config/dev_server.ts
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/webpack-config/dev_server.ts
@@ -3,11 +3,14 @@ import { Configuration } from "webpack-dev-server";
 // Defines the dev server configuration for webpack dev server
 const devServer: Configuration = {
   port: 8000,
+  host: "dev.muikkuverkko.fi",
   hot: "only",
   historyApiFallback: true,
-  host: "dev.muikkuverkko.fi",
   client: {
     overlay: true,
+    logging: "none",
+    webSocketTransport: "ws",
+    webSocketURL: "ws://dev.muikkuverkko.fi:8000/ws",
   },
   proxy: [
     {
@@ -24,7 +27,16 @@ const devServer: Configuration = {
       secure: false,
       changeOrigin: true,
     },
+    {
+      context: ["/ws/socket/**"],
+      target: "wss://dev.muikkuverkko.fi",
+      ws: true,
+      secure: true,
+      changeOrigin: true,
+      logLevel: "debug",
+    },
   ],
+  webSocketServer: "ws",
 };
 
 export default devServer;
