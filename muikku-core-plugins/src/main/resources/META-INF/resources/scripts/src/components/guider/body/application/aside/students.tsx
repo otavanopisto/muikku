8e9b686fbd2547004773ec7ad126949ec2d6c222
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2025-03-05 12:39:01 +0200

    guider student aside to arrow function

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/body/application/aside/students.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/guider/body/application/aside/students.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
-import { Action, bindActionCreators, Dispatch } from "redux";
-import { AnyActionType } from "~/actions";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import * as queryString from "query-string";
 import "~/sass/elements/item-list.scss";
-import { GuiderState } from "~/reducers/main-function/guider";
 import LabelUpdateDialog from "../../../dialogs/label-update";
 import { StateType } from "~/reducers";
 import Navigation, {
@@ -12,44 +9,23 @@ import Navigation, {
   NavigationElement,
 } from "~/components/general/navigation";
 import { UserGroup } from "~/generated/client";
-import { withTranslation, WithTranslation } from "react-i18next";
 import { GuiderContext, GuiderNotesState } from "../../../context";
-import {
-  loadNotes,
-  LoadNotesTriggerType,
-} from "~/actions/main-function/guider";
-import { StatusType } from "~/reducers/base/status";
-
-/**
- * NavigationProps
- */
-interface NavigationProps extends WithTranslation<["common"]> {
-  loadNotes: LoadNotesTriggerType;
-  guider: GuiderState;
-  status: StatusType;
-}
-
-/**
- * NavigationState
- */
-interface NavigationState {}
+import { useTranslation } from "react-i18next";
 
 /**
  * NavigationAside
  */
-class NavigationAside extends React.Component<
-  NavigationProps,
-  NavigationState
-> {
-  // These are required to make the types work in the context
-  static contextType = GuiderContext;
-  context!: React.ContextType<typeof GuiderContext>;
+const StudentNavigationAside = () => {
+  const { view, filters, dispatch } = React.useContext(GuiderContext);
+  const { status, guider } = useSelector((state: StateType) => state);
+  const actionDispatch = useDispatch();
+  const { t } = useTranslation(["flags"]);
 
   /**
    * @param filter state filter
    */
-  handleStateFilterChange = (filter: GuiderNotesState) => {
-    this.context.dispatch({
+  const handleStateFilterChange = (filter: GuiderNotesState) => {
+    dispatch({
       type: "SET_STATE_FILTER",
       payload: filter,
     });
@@ -57,173 +33,134 @@ class NavigationAside extends React.Component<
   /**
    * render
    */
-  render() {
-    const locationData = queryString.parse(
-      document.location.hash.split("?")[1] || "",
-      { arrayFormat: "bracket" }
-    );
-    return (
-      <Navigation>
-        {this.props.guider.availableFilters.labels.length > 0 && (
-          <NavigationTopic
-            name={this.props.i18n.t("labels.flags", { ns: "flags" })}
-          >
-            {this.props.guider.availableFilters.labels.map((label) => {
-              const isActive =
-                this.props.guider.activeFilters.labelFilters.includes(label.id);
-              const hash = isActive
-                ? queryString.stringify(
-                    Object.assign({}, locationData, {
-                      c: "",
-                      l: (locationData.l || []).filter(
-                        (i: string) => parseInt(i) !== label.id
-                      ),
-                    }),
-                    { arrayFormat: "bracket" }
-                  )
-                : queryString.stringify(
-                    Object.assign({}, locationData, {
-                      c: "",
-                      l: (locationData.l || []).concat(label.id),
-                    }),
-                    { arrayFormat: "bracket" }
-                  );
-              return (
-                <NavigationElement
-                  modifiers="aside-navigation-guider-flag"
-                  icon="flag"
-                  key={label.id}
-                  iconColor={label.color}
-                  isActive={isActive}
-                  hash={"?" + hash}
-                  editableWrapper={LabelUpdateDialog}
-                  editableWrapperArgs={{ label: label }}
-                  isEditable
-                >
-                  {label.name}
-                </NavigationElement>
-              );
-            })}
-          </NavigationTopic>
-        )}
-
-        {this.props.guider.availableFilters.workspaces.length > 0 && (
-          <NavigationTopic
-            name={this.props.i18n.t("labels.workspaces", { ns: "workspace" })}
-          >
-            {this.props.guider.availableFilters.workspaces.map((workspace) => {
-              const isActive =
-                this.props.guider.activeFilters.workspaceFilters.includes(
-                  workspace.id
-                );
-              const hash = isActive
-                ? queryString.stringify(
-                    Object.assign({}, locationData, {
-                      c: "",
-                      w: (locationData.w || []).filter(
-                        (w: string) => parseInt(w) !== workspace.id
-                      ),
-                    }),
-                    { arrayFormat: "bracket" }
-                  )
-                : queryString.stringify(
-                    Object.assign({}, locationData, {
-                      c: "",
-                      w: (locationData.w || []).concat(workspace.id),
-                    }),
-                    { arrayFormat: "bracket" }
-                  );
-              return (
-                <NavigationElement
-                  modifiers="aside-navigation-guider-course"
-                  icon="books"
-                  key={workspace.id}
-                  isActive={isActive}
-                  hash={"?" + hash}
-                >
-                  {workspace.name +
-                    (workspace.nameExtension
-                      ? " (" + workspace.nameExtension + ")"
-                      : "")}
-                </NavigationElement>
-              );
-            })}
-          </NavigationTopic>
-        )}
-
-        {this.props.guider.availableFilters.userGroups.length > 0 && (
-          <NavigationTopic
-            name={this.props.i18n.t("labels.studentGroups", { ns: "users" })}
-          >
-            {this.props.guider.availableFilters.userGroups.map(
-              (userGroup: UserGroup) => {
-                const isActive =
-                  this.props.guider.activeFilters.userGroupFilters.includes(
-                    userGroup.id
-                  );
-                const hash = isActive
-                  ? queryString.stringify(
-                      Object.assign({}, locationData, {
-                        c: "",
-                        u: (locationData.u || []).filter(
-                          (u: string) => parseInt(u) !== userGroup.id
-                        ),
-                      }),
-                      { arrayFormat: "bracket" }
-                    )
-                  : queryString.stringify(
-                      Object.assign({}, locationData, {
-                        c: "",
-                        u: (locationData.u || []).concat(userGroup.id),
-                      }),
-                      { arrayFormat: "bracket" }
-                    );
-                return (
-                  <NavigationElement
-                    modifiers="aside-navigation-guider-user-group"
-                    icon="users"
-                    key={userGroup.id}
-                    isActive={isActive}
-                    hash={"?" + hash}
-                  >
-                    {userGroup.name}
-                  </NavigationElement>
-                );
-              }
-            )}
-          </NavigationTopic>
-        )}
-      </Navigation>
-    );
-  }
-}
-
-NavigationAside.contextType = GuiderContext;
-
-/**
- * mapStateToProps
- * @param state state
- */
-function mapStateToProps(state: StateType) {
-  return {
-    guider: state.guider,
-    status: state.status,
-  };
-}
-
-/**
- * mapDispatchToProps
- * @param dispatch dispatch
- */
-function mapDispatchToProps(dispatch: Dispatch<Action<AnyActionType>>) {
-  return bindActionCreators(
-    {
-      loadNotes,
-    },
-    dispatch
+  const locationData = queryString.parse(
+    document.location.hash.split("?")[1] || "",
+    { arrayFormat: "bracket" }
   );
-}
+  return (
+    <Navigation>
+      {guider.availableFilters.labels.length > 0 && (
+        <NavigationTopic name={t("labels.flags", { ns: "flags" })}>
+          {guider.availableFilters.labels.map((label) => {
+            const isActive = guider.activeFilters.labelFilters.includes(
+              label.id
+            );
+            const hash = isActive
+              ? queryString.stringify(
+                  Object.assign({}, locationData, {
+                    c: "",
+                    l: (locationData.l || []).filter(
+                      (i: string) => parseInt(i) !== label.id
+                    ),
+                  }),
+                  { arrayFormat: "bracket" }
+                )
+              : queryString.stringify(
+                  Object.assign({}, locationData, {
+                    c: "",
+                    l: (locationData.l || []).concat(label.id),
+                  }),
+                  { arrayFormat: "bracket" }
+                );
+            return (
+              <NavigationElement
+                modifiers="aside-navigation-guider-flag"
+                icon="flag"
+                key={label.id}
+                iconColor={label.color}
+                isActive={isActive}
+                hash={"?" + hash}
+                editableWrapper={LabelUpdateDialog}
+                editableWrapperArgs={{ label: label }}
+                isEditable
+              >
+                {label.name}
+              </NavigationElement>
+            );
+          })}
+        </NavigationTopic>
+      )}
 
-export default withTranslation(["guider"])(
-  connect(mapStateToProps, mapDispatchToProps)(NavigationAside)
-);
+      {guider.availableFilters.workspaces.length > 0 && (
+        <NavigationTopic name={t("labels.workspaces", { ns: "workspace" })}>
+          {guider.availableFilters.workspaces.map((workspace) => {
+            const isActive = guider.activeFilters.workspaceFilters.includes(
+              workspace.id
+            );
+            const hash = isActive
+              ? queryString.stringify(
+                  Object.assign({}, locationData, {
+                    c: "",
+                    w: (locationData.w || []).filter(
+                      (w: string) => parseInt(w) !== workspace.id
+                    ),
+                  }),
+                  { arrayFormat: "bracket" }
+                )
+              : queryString.stringify(
+                  Object.assign({}, locationData, {
+                    c: "",
+                    w: (locationData.w || []).concat(workspace.id),
+                  }),
+                  { arrayFormat: "bracket" }
+                );
+            return (
+              <NavigationElement
+                modifiers="aside-navigation-guider-course"
+                icon="books"
+                key={workspace.id}
+                isActive={isActive}
+                hash={"?" + hash}
+              >
+                {workspace.name +
+                  (workspace.nameExtension
+                    ? " (" + workspace.nameExtension + ")"
+                    : "")}
+              </NavigationElement>
+            );
+          })}
+        </NavigationTopic>
+      )}
+
+      {guider.availableFilters.userGroups.length > 0 && (
+        <NavigationTopic name={t("labels.studentGroups", { ns: "users" })}>
+          {guider.availableFilters.userGroups.map((userGroup: UserGroup) => {
+            const isActive = guider.activeFilters.userGroupFilters.includes(
+              userGroup.id
+            );
+            const hash = isActive
+              ? queryString.stringify(
+                  Object.assign({}, locationData, {
+                    c: "",
+                    u: (locationData.u || []).filter(
+                      (u: string) => parseInt(u) !== userGroup.id
+                    ),
+                  }),
+                  { arrayFormat: "bracket" }
+                )
+              : queryString.stringify(
+                  Object.assign({}, locationData, {
+                    c: "",
+                    u: (locationData.u || []).concat(userGroup.id),
+                  }),
+                  { arrayFormat: "bracket" }
+                );
+            return (
+              <NavigationElement
+                modifiers="aside-navigation-guider-user-group"
+                icon="users"
+                key={userGroup.id}
+                isActive={isActive}
+                hash={"?" + hash}
+              >
+                {userGroup.name}
+              </NavigationElement>
+            );
+          })}
+        </NavigationTopic>
+      )}
+    </Navigation>
+  );
+};
+
+export default StudentNavigationAside;
