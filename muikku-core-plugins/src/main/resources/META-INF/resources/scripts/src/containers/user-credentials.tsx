8af9bfd4028990b15e6b2df9094c95c6effec932
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2025-03-11 08:03:04 +0200

    Reset crendentials and error pages with app title and favicon

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/containers/user-credentials.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/containers/user-credentials.tsx
@@ -6,18 +6,21 @@ import { Action, Store } from "redux";
 import Body from "../components/credentials/body";
 import * as React from "react";
 import "~/sass/util/base.scss";
+import { withTranslation, WithTranslation } from "react-i18next";
+import { Helmet } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet-async";
 
 /**
  * UserCredentialsProps
  */
-interface UserCredentialsProps {
+interface UserCredentialsProps extends WithTranslation {
   store: Store<StateType>;
 }
 
 /**
  * UserCredentials
  */
-export default class UserCredentials extends React.Component<
+class UserCredentials extends React.Component<
   UserCredentialsProps,
   Record<string, unknown>
 > {
@@ -35,12 +38,66 @@ export default class UserCredentials extends React.Component<
    * @returns JSX.Element
    */
   render() {
+    const { t } = this.props;
+
+    const appName = t("appName", { ns: "pageTitles" });
+    const title = `${t("userCredentialsReset", { ns: "pageTitles" })} | ${appName}`;
+
     return (
-      <div id="root">
-        <Notifications></Notifications>
-        <DisconnectedWarningDialog />
-        <Body></Body>
-      </div>
+      <HelmetProvider>
+        <Helmet>
+          <title>{title}</title>
+
+          {/* For all browsers */}
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/gfx/favicons/favicon-16x16.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/gfx/favicons/favicon-32x32.png"
+          />
+
+          {/* For Google and Android */}
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="48x48"
+            href="/gfx/favicons/favicon-48x48.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/gfx/favicons/favicon-192x192.png"
+          />
+
+          {/* For iPad */}
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="167x167"
+            href="/gfx/favicons/favicon-167x167.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="180x180"
+            href="/gfx/favicons/favicon-180x180.png"
+          />
+        </Helmet>
+        <div id="root">
+          <Notifications></Notifications>
+          <DisconnectedWarningDialog />
+          <Body></Body>
+        </div>
+      </HelmetProvider>
     );
   }
 }
+
+export default withTranslation(["common"])(UserCredentials);
