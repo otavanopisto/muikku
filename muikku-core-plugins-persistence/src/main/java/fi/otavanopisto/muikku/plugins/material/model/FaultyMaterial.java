60f4c1a26953f8e9908ba5ec15f904554c56b2c7
pkukkon <pasi.kukkonen@otavanopisto.fi>
2025-03-24 09:56:13 +0200

    - entity changes

--- muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/material/model/FaultyMaterial.java
+++ muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/material/model/FaultyMaterial.java
@@ -4,18 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
-@PrimaryKeyJoinColumn(name="id")
 public class FaultyMaterial {
 
   public Long getId() {
     return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
   }
 
   @Id
