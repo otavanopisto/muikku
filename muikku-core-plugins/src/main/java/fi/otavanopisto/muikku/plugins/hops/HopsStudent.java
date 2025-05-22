a9c4cb39600dc910a24de192ec5ab46af848cc40
pkukkon <pasi.kukkonen@otavanopisto.fi>
2025-05-22 11:43:03 +0300

    - clarification

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/hops/HopsStudent.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/hops/HopsStudent.java
@@ -24,6 +24,6 @@ public class HopsStudent {
   }
 
   private Long userEntityId;
-  private String category;
+  private String category; // studyProgramme.getCategory().getEducationType().getCode()
 
 }
