6eb1c5f2df957634eacafacbb08e8cd55aac1f41
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2025-09-03 12:28:42 +0300

    Exam lists added to evaluation

---
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/evaluation/body/application/evaluation/evaluation-exams-list-item.tsx
@@ -0,0 +1,208 @@
+import * as React from "react";
+import AnimateHeight from "react-animate-height";
+import { useTranslation } from "react-i18next";
+import {
+  ExamAttendance,
+  MaterialCompositeReply,
+  MaterialContentNode,
+} from "~/generated/client";
+import { localize } from "~/locales/i18n";
+import { WorkspaceDataType } from "~/reducers/workspaces";
+import EvaluationMaterial from "./evaluation-material";
+import "~/sass/elements/evaluation.scss";
+
+/**
+ * EvaluationExamsListItemProps
+ */
+interface EvaluationExamsListItemProps {
+  exam: ExamAttendance;
+  studentUserEntityId: number;
+  workspace: WorkspaceDataType;
+  evaluationCompositeReplies: MaterialCompositeReply[];
+}
+
+/**
+ * EvaluationExamsListItemProps
+ * @param props props
+ */
+const EvaluationExamsListItem = React.forwardRef(
+  (props: EvaluationExamsListItemProps, ref: React.Ref<HTMLDivElement>) => {
+    const { studentUserEntityId, workspace, exam, evaluationCompositeReplies } =
+      props;
+    const { contents } = exam;
+
+    const { t } = useTranslation(["evaluation", "common"]);
+
+    const [showContent, setShowContent] = React.useState(false);
+
+    /**
+     * toggleOpen
+     * @param type "open" | "close"
+     */
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const toggleContent = (type?: "open" | "close") => {
+      setShowContent(type === "open");
+    };
+
+    /**
+     * handleToggleOpen
+     */
+    const handleToggleContent = () => {
+      setShowContent(!showContent);
+    };
+
+    const assignmentItems =
+      contents && contents.length > 0 ? (
+        contents.map((content) => {
+          const compositeReply = evaluationCompositeReplies.find(
+            (cReply) => cReply.workspaceMaterialId === content.materialId
+          );
+
+          return (
+            <AssignmentItem
+              key={content.materialId}
+              content={content}
+              studentUserEntityId={studentUserEntityId}
+              workspace={workspace}
+              compositeReply={compositeReply}
+            />
+          );
+        })
+      ) : (
+        <div className="empty">
+          <span>
+            {t("content.empty", { ns: "evaluation", context: "assignments" })}
+          </span>
+        </div>
+      );
+
+    return (
+      <>
+        <div
+          ref={ref}
+          className="evaluation-modal__item"
+          onClick={handleToggleContent}
+        >
+          <div className="evaluation-modal__item-header">
+            <div className="evaluation-modal__item-header-title">
+              {props.exam.name}
+            </div>
+          </div>
+        </div>
+
+        <AnimateHeight duration={400} height={showContent ? "auto" : 0}>
+          {assignmentItems}
+        </AnimateHeight>
+      </>
+    );
+  }
+);
+
+/**
+ * AssignmentItemProps
+ */
+interface AssignmentItemProps {
+  studentUserEntityId: number;
+  content: MaterialContentNode;
+  compositeReply?: MaterialCompositeReply;
+  workspace: WorkspaceDataType;
+}
+
+/**
+ * AssignmentItem
+ * @param props props
+ */
+const AssignmentItem = (props: AssignmentItemProps) => {
+  const { content, compositeReply, studentUserEntityId, workspace } = props;
+
+  const [contentOpen, setContentOpen] = React.useState(false);
+
+  const { t } = useTranslation(["evaluation", "common"]);
+
+  /**
+   * handleToggleContent
+   */
+  const handleToggleContent = () => {
+    setContentOpen(!contentOpen);
+  };
+
+  /**
+   * assignmentTypeClass
+   * @returns string
+   */
+  const materialTypeClass = () => {
+    if (content.assignmentType === "EVALUATED") {
+      return "assignment";
+    }
+    return "exercise";
+  };
+
+  /**
+   * renderAssignmentStatus
+   * @returns JSX.Element
+   */
+  const renderAssignmentMeta = () => {
+    if (!compositeReply) {
+      return null;
+    }
+
+    // Checking if assigments is submitted at all.
+    // Its date string
+    const hasSubmitted = compositeReply.submitted;
+
+    return (
+      <div className="evaluation-modal__item-meta">
+        {hasSubmitted === null ||
+        (hasSubmitted !== null && compositeReply.state === "WITHDRAWN") ? (
+          <div className="evaluation-modal__item-meta-item">
+            <span className="evaluation-modal__item-meta-item-data">
+              {t("labels.notDone", { ns: "evaluation" })}
+            </span>
+          </div>
+        ) : (
+          hasSubmitted && (
+            <div className="evaluation-modal__item-meta-item">
+              <span className="evaluation-modal__item-meta-item-label">
+                {t("labels.done", { ns: "evaluation" })}
+              </span>
+              <span className="evaluation-modal__item-meta-item-data">
+                {localize.date(hasSubmitted)}
+              </span>
+            </div>
+          )
+        )}
+      </div>
+    );
+  };
+
+  const titleModifiers = [materialTypeClass()];
+
+  return (
+    <div className="evaluation-modal__item">
+      <div className="evaluation-modal__item-header">
+        <div
+          onClick={handleToggleContent}
+          className={`evaluation-modal__item-header-title ${titleModifiers.map((modifier) => `evaluation-modal__item-header-title--${modifier}`).join(" ")}`}
+        >
+          {content.title}
+          {renderAssignmentMeta()}
+        </div>
+      </div>
+
+      <AnimateHeight duration={400} height={contentOpen ? "auto" : 0}>
+        {workspace && content && (
+          <EvaluationMaterial
+            material={content}
+            workspace={workspace}
+            compositeReply={compositeReply}
+            userEntityId={studentUserEntityId}
+          />
+        )}
+      </AnimateHeight>
+    </div>
+  );
+};
+
+EvaluationExamsListItem.displayName = "EvaluationExamsListItem";
+
+export default EvaluationExamsListItem;
