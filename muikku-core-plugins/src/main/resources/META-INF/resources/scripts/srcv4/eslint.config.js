8e5f3b40788975537534d9c637e6ef7eb9b271f4
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2025-09-23 08:58:21 +0300

    eslint config updated

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/srcv4/eslint.config.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/srcv4/eslint.config.js
@@ -1,23 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      // Remove tseslint.configs.recommended and replace with this
+      tseslint.configs.recommendedTypeChecked,
+
+      // Alternatively, use this for stricter rules. Can be enabled if needed.
+      //tseslint.configs.strictTypeChecked,
+
+      // Optionally, add this for stylistic rules
+      tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+
+      // Enable lint rules for React
+      reactX.configs["recommended-typescript"],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
     ],
     languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
