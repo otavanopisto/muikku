f31b901e75ca387a4bf0c508976b266816a85ced
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2025-09-24 10:59:05 +0300

    Eslint ignore updated

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/srcv4/eslint.config.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/srcv4/eslint.config.js
@@ -8,7 +8,7 @@ import reactX from "eslint-plugin-react-x";
 import reactDom from "eslint-plugin-react-dom";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "generated"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
