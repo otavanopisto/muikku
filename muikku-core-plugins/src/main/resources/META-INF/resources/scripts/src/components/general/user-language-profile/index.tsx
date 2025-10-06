d0dfd00ea2f1e60338e3c93cabaf943659a5c1fe
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2025-10-06 13:42:09 +0300

    user language profile cv styling

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/user-language-profile/index.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/general/user-language-profile/index.tsx
@@ -16,6 +16,7 @@ import "~/sass/elements/user-language-profile.scss";
 import Accordion from "./components/accordion";
 import Sample from "~/components/language-profile/body/application/languages/sample";
 import { LanguageData } from "~/@types/shared";
+import Stars from "~/components/language-profile/body/application/cv/stars";
 
 /**
  * UserLanguageProfileProps
@@ -40,6 +41,7 @@ const UserLanguageProfile = (props: UserLanguageProfileProps) => {
     skillGoals,
     languages,
     samples,
+    cv,
   }: LanguageProfileData = useSelector(
     (state: StateType) => state.languageProfile.data
   );
@@ -382,6 +384,117 @@ const UserLanguageProfile = (props: UserLanguageProfileProps) => {
               </div>
             </Accordion>
           ))}
+          <Accordion
+            id="language-CV-drawer"
+            title={t("labels.languageCv", { ns: "languageProfile" })}
+          >
+            <div className="user-language-profile-container__row">
+              <label
+                htmlFor="cv-general"
+                className="user-language-profile__label"
+              >
+                {t("labels.general", {
+                  ns: "languageProfile",
+                })}
+              </label>
+              <p>{cv.general}</p>
+            </div>
+            <div className="user-language-profile-container__row">
+              {cv.languages.length > 0 &&
+                cv.languages.map((language) => (
+                  <div
+                    className="user-language-profile__cv-language"
+                    key={language.code}
+                  >
+                    <h4>
+                      {t(`languages.${language.code}`, {
+                        ns: "languageProfile",
+                        defaultValue: language.code,
+                      })}
+                    </h4>
+                    <div className="user-language-profile__cv-language-skill">
+                      <fieldset className="user-language-profile__skills-container">
+                        <div className="user-language-profile__label">
+                          {t("labels.skillLevel", {
+                            ns: "languageProfile",
+                          })}
+                        </div>
+                        <div className="user-language-profile__skill-row">
+                          <StarDisplayer
+                            label={t("labels.skillInteraction", {
+                              ns: "languageProfile",
+                            })}
+                            value={parseInt(language.interaction)}
+                          />
+
+                          <StarDisplayer
+                            label={t("labels.skillVocalization", {
+                              ns: "languageProfile",
+                            })}
+                            value={parseInt(language.vocal)}
+                          />
+                          <StarDisplayer
+                            label={t("labels.skillWriting", {
+                              ns: "languageProfile",
+                            })}
+                            value={parseInt(language.writing)}
+                          />
+                          <StarDisplayer
+                            label={t("labels.skillReadingComprehension", {
+                              ns: "languageProfile",
+                            })}
+                            value={parseInt(language.reading)}
+                          />
+                          <StarDisplayer
+                            label={t("labels.skillListeningComprehension", {
+                              ns: "languageProfile",
+                            })}
+                            value={parseInt(language.listening)}
+                          />
+                        </div>
+                        <div className="user-language-profile__label">
+                          {t("labels.skillEstimate", {
+                            ns: "languageProfile",
+                          })}
+                          <div className="user-language-profile__skill-row"></div>
+                        </div>
+
+                        <div className="user-language-profile__label">
+                          {t("labels.descriptionOfCompetence", {
+                            ns: "languageProfile",
+                          })}
+                        </div>
+                        <div className="user-language-profile__skill-row"></div>
+                        {language.samples?.length && (
+                          <>
+                            <div className="user-language-profile__label">
+                              {t("labels.samples", {
+                                ns: "languageProfile",
+                              })}
+                            </div>
+                            {language.samples?.map((sample, index) => (
+                              <div
+                                key={"sample-link" + index}
+                                className="user-language-profile__skill-row user-language-profile__skill-row--sample"
+                              >
+                                <a
+                                  href={sample.url}
+                                  rel="noreferrer"
+                                  target="_blank"
+                                  className="user-language-profile__sample-link"
+                                >
+                                  {sample.name}
+                                </a>
+                              </div>
+                            ))}
+                          </>
+                        )}
+                      </fieldset>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </Accordion>
         </>
       )}
     </div>
