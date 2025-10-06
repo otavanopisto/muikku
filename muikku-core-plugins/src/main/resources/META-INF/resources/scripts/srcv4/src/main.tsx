3b9af750d2371347656cd4c716403b8814bd1967
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2025-10-06 11:41:29 +0300

    Theme for Provider

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/srcv4/src/main.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/srcv4/src/main.tsx
@@ -1,18 +1,19 @@
+//import "@mantine/core/styles.layer.css";
+import "@mantine/core/styles.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { MantineProvider } from "@mantine/core";
 import { Provider } from "jotai";
 import { DevTools } from "jotai-devtools";
 import { jotaiStore } from "./jotaiStore";
 import { DebugAtoms } from "./utils/DebugAtoms";
 import App from "./App.tsx";
-import "@mantine/core/styles.css";
 import "jotai-devtools/styles.css";
+import { theme } from "./theme";
 
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <Provider store={jotaiStore}>
         {process.env.NODE_ENV === "development" && (
           <>
