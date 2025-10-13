3adee360f2ea43491e71466e31e7cf56d245ab7a
saaralepisto <saara.lepisto@hotmail.com>
2025-10-13 14:03:02 +0300

    index

--- muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/announcer/model/AnnouncementRecipient.java
+++ muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/announcer/model/AnnouncementRecipient.java
@@ -7,12 +7,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(indexes = @Index(columnList = "userEntityId"))
+
 public class AnnouncementRecipient {
 
   public Long getId() {
