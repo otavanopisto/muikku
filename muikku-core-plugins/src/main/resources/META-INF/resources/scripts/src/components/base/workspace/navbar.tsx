ae6258d0fb201af7cf393e4d12b17ac2bffa14c2
pkukkon <pasi.kukkonen@otavanopisto.fi>
2025-10-20 23:15:06 +0300

    - fricking newlines

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/workspace/navbar.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/base/workspace/navbar.tsx
@@ -178,8 +178,7 @@ class WorkspaceNavbar extends React.Component<
         href: "/workspace/" + this.props.workspaceUrl + "/exams",
         icon: "exams",
         to: true,
-        condition:
-          this.props.status.permissions.WORKSPACE_IS_WORKSPACE_STUDENT_WITH_EXAMS
+        condition: this.props.status.permissions.WORKSPACE_IS_WORKSPACE_STUDENT_WITH_EXAMS
       },
       {
         modifier: "discussion",
