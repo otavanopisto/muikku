67c46e4360cd08a5485dc1dfb9ea39e0daeadef3
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2025-10-31 12:26:58 +0200

    announcements aside to function component

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/announcements/body/aside.tsx
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/src/components/announcements/body/aside.tsx
@@ -13,173 +13,144 @@ import { AnyActionType } from "~/actions";
 import { Action, Dispatch } from "redux";
 import { connect } from "react-redux";
 import PagerV2 from "~/components/general/pagerV2";
+import { useTranslation } from "react-i18next";
 
 /**
  * AnnouncementsAsideProps
  */
-interface AnnouncementsAsideProps extends WithTranslation {
+interface AnnouncementsAsideProps {
   announcements: AnnouncementsState;
 }
 
 /**
- * AnnouncementsAsideState
- */
-interface AnnouncementsAsideState {
-  currentPage: number;
-  itemsPerPage: number;
-}
-
-/**
  * AnnouncementsAside
+ * @param props props
+ * @returns component
  */
-class AnnouncementsAside extends React.Component<
-  AnnouncementsAsideProps,
-  AnnouncementsAsideState
-> {
+const AnnouncementsAside: React.FC<AnnouncementsAsideProps> = (props) => {
+  const { t } = useTranslation("messaging");
+  const [currentPage, setCurrentPage] = React.useState(0);
+  const { announcements } = props;
+  const itemsPerPage = 10;
   /**
-   * AnnouncementsPanelProps
-   * @param props props
+   * Calculates amount of pages
+   * depends how many items there is per page
    */
-  constructor(props: AnnouncementsAsideProps) {
-    super(props);
+  const pageCount = Math.ceil(
+    announcements.announcements.length / itemsPerPage
+  );
 
-    this.state = {
-      itemsPerPage: 10,
-      currentPage: 0,
-    };
-  }
   /**
    * handles page changes,
    * sets selected page as currentPage to state
    * @param selectedItem selectedItem
    * @param selectedItem.selected selected
    */
-  handlePageChange = (selectedItem: { selected: number }) => {
-    this.setState({
-      currentPage: selectedItem.selected,
-    });
+  const handlePageChange = (selectedItem: { selected: number }) => {
+    setCurrentPage(selectedItem.selected);
   };
 
   /**
    * Gets the current announcements to display
    * @returns an array of announcements depending on current page and items per page
    */
-  getCurrentAnnouncements = () => {
-    const { currentPage, itemsPerPage } = this.state;
+  const paginatedAnnouncements = React.useMemo(() => {
     const offset = currentPage * itemsPerPage;
-    return this.props.announcements.announcements.slice(
-      offset,
-      offset + itemsPerPage
-    );
-  };
+    return announcements.announcements.slice(offset, offset + itemsPerPage);
+  }, [announcements.announcements, currentPage, itemsPerPage]);
+
+  const renderPaginationBody = (
+    <div
+      className="item-list__item item-list__item--announcements"
+      aria-label={t("wcag.pager", { ns: "messaging" })}
+    >
+      <span className="item-list__text-body item-list__text-body--multiline--footer">
+        <PagerV2
+          previousLabel=""
+          nextLabel=""
+          breakLabel="..."
+          initialPage={currentPage}
+          forcePage={currentPage}
+          marginPagesDisplayed={1}
+          pageCount={pageCount}
+          pageRangeDisplayed={2}
+          onPageChange={handlePageChange}
+        />
+      </span>
+    </div>
+  );
+
   /**
-   * Component render method
-   * @returns JSX.Element
+   * renders pagination body as one of announcements list item
    */
-  render() {
-    const { currentPage, itemsPerPage } = this.state;
-    /**
-     * Calculates amount of pages
-     * depends how many items there is per page
-     */
-    const pageCount = Math.ceil(
-      this.props.announcements.announcements.length / itemsPerPage
-    );
-    /**
-     * renders pagination body as one of announcements list item
-     */
-    const renderPaginationBody = (
-      <div
-        className="item-list__item item-list__item--announcements"
-        aria-label={this.props.t("wcag.pager", { ns: "messaging" })}
-      >
-        <span className="item-list__text-body item-list__text-body--multiline--footer">
-          <PagerV2
-            previousLabel=""
-            nextLabel=""
-            breakLabel="..."
-            initialPage={currentPage}
-            forcePage={currentPage}
-            marginPagesDisplayed={1}
-            pageCount={pageCount}
-            pageRangeDisplayed={2}
-            onPageChange={this.handlePageChange}
-          />
-        </span>
-      </div>
-    );
-    return (
-      <>
-        {this.props.announcements.announcements.length !== 0 ? (
-          <>
-            <div className="item-list item-list--panel-announcements">
-              {this.getCurrentAnnouncements().map(
-                (announcement: Announcement) => {
-                  const extraWorkspaces =
-                    announcement.workspaces && announcement.workspaces.length
-                      ? announcement.workspaces.length - 1
-                      : 0;
-                  return (
-                    <Link
-                      key={announcement.id}
-                      className={`item-list__item item-list__item--announcements ${
-                        announcement.workspaces.length
-                          ? "item-list__item--has-workspaces"
-                          : ""
-                      } ${announcement.unread ? "item-list__item--unread" : ""}`}
-                      href={`#${announcement.id}`}
-                    >
-                      <span className="item-list__icon item-list__icon--announcements icon-paper-plane"></span>
-                      <span className="item-list__text-body item-list__text-body--multiline">
-                        <span className="item-list__announcement-caption">
-                          {announcement.caption}
+
+  return (
+    <>
+      {announcements.announcements.length !== 0 ? (
+        <>
+          <div className="item-list item-list--panel-announcements">
+            {paginatedAnnouncements.map((announcement: Announcement) => {
+              const extraWorkspaces =
+                announcement.workspaces && announcement.workspaces.length
+                  ? announcement.workspaces.length - 1
+                  : 0;
+              return (
+                <Link
+                  key={announcement.id}
+                  className={`item-list__item item-list__item--announcements ${
+                    announcement.workspaces.length
+                      ? "item-list__item--has-workspaces"
+                      : ""
+                  } ${announcement.unread ? "item-list__item--unread" : ""}`}
+                  href={`#${announcement.id}`}
+                >
+                  <span className="item-list__icon item-list__icon--announcements icon-paper-plane"></span>
+                  <span className="item-list__text-body item-list__text-body--multiline">
+                    <span className="item-list__announcement-caption">
+                      {announcement.caption}
+                    </span>
+                    <span className="item-list__announcement-date">
+                      {localize.date(announcement.startDate)}
+                    </span>
+                    {announcement.workspaces &&
+                    announcement.workspaces.length ? (
+                      <div className="labels item-list__announcement-workspaces">
+                        <span className="label">
+                          <span className="label__icon label__icon--workspace icon-books"></span>
+                          <span className="label__text label__text--workspace">
+                            {announcement.workspaces[0].name}{" "}
+                            {announcement.workspaces[0].nameExtension
+                              ? "(" +
+                                announcement.workspaces[0].nameExtension +
+                                ")"
+                              : null}
+                          </span>
                         </span>
-                        <span className="item-list__announcement-date">
-                          {localize.date(announcement.startDate)}
-                        </span>
-                        {announcement.workspaces &&
-                        announcement.workspaces.length ? (
-                          <div className="labels item-list__announcement-workspaces">
-                            <span className="label">
-                              <span className="label__icon label__icon--workspace icon-books"></span>
-                              <span className="label__text label__text--workspace">
-                                {announcement.workspaces[0].name}{" "}
-                                {announcement.workspaces[0].nameExtension
-                                  ? "(" +
-                                    announcement.workspaces[0].nameExtension +
-                                    ")"
-                                  : null}
-                              </span>
-                            </span>
-                            {extraWorkspaces ? (
-                              <span className="label">
-                                {"(+" + extraWorkspaces + ")"}
-                              </span>
-                            ) : null}
-                          </div>
+                        {extraWorkspaces ? (
+                          <span className="label">
+                            {"(+" + extraWorkspaces + ")"}
+                          </span>
                         ) : null}
-                      </span>
-                      {announcement.pinned && (
-                        <span className="icon icon-pin"></span>
-                      )}
-                    </Link>
-                  );
-                }
-              )}
-            </div>
-            {this.props.announcements.announcements.length > itemsPerPage
-              ? renderPaginationBody
-              : null}
-          </>
-        ) : (
-          <div>
-            {this.props.i18n.t("content.empty", { context: "announcements" })}
+                      </div>
+                    ) : null}
+                  </span>
+                  {announcement.pinned && (
+                    <span className="icon icon-pin"></span>
+                  )}
+                </Link>
+              );
+            })}
           </div>
-        )}
-      </>
-    );
-  }
-}
+          {announcements.announcements.length > itemsPerPage
+            ? renderPaginationBody
+            : null}
+        </>
+      ) : (
+        <div>{t("content.empty", { context: "announcements" })}</div>
+      )}
+    </>
+  );
+};
 
 /**
  * mapStateToProps
