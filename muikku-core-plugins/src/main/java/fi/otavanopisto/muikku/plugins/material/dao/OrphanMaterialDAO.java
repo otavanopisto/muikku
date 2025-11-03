2f9ec412acfbbc01669066a7b16f8030868d8e30
pkukkon <pasi.kukkonen@otavanopisto.fi>
2025-11-03 13:00:41 +0200

    - wrong DAO

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/material/dao/OrphanMaterialDAO.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/material/dao/OrphanMaterialDAO.java
@@ -3,17 +3,16 @@ package fi.otavanopisto.muikku.plugins.material.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import fi.otavanopisto.muikku.dao.CoreDAO;
+import fi.otavanopisto.muikku.plugins.CorePluginsDAO;
 import fi.otavanopisto.muikku.plugins.material.model.OrphanMaterial;
 
-public class OrphanMaterialDAO extends CoreDAO<OrphanMaterial> {
+public class OrphanMaterialDAO extends CorePluginsDAO<OrphanMaterial> {
   
-  private static final long serialVersionUID = -5759085487898738000L;
+  private static final long serialVersionUID = -3468160385318100755L;
 
   public List<OrphanMaterial> getBatch(int count) {
     EntityManager entityManager = getEntityManager();
@@ -21,13 +20,11 @@ public class OrphanMaterialDAO extends CoreDAO<OrphanMaterial> {
     CriteriaQuery<OrphanMaterial> criteria = criteriaBuilder.createQuery(OrphanMaterial.class);
     Root<OrphanMaterial> root = criteria.from(OrphanMaterial.class);
     criteria.select(root);
-    TypedQuery<OrphanMaterial> query = entityManager.createQuery(criteria);
-    query.setMaxResults(count);
-    return query.getResultList();
+    return entityManager.createQuery(criteria).setMaxResults(count).getResultList();
   }
 
   public void delete(OrphanMaterial orphanMaterial) {
     super.delete(orphanMaterial);
   }
-
+  
 }
