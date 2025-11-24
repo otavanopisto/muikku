a170eeba1a8b77b181401d64f9365c7306d765a5
saaralepisto <saara.lepisto@hotmail.com>
2025-11-24 07:50:39 +0200

    deleting index

--- muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/announcer/model/AnnouncementRecipient.java
+++ muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/announcer/model/AnnouncementRecipient.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -16,7 +15,7 @@ import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(indexes = @Index(columnList = "userEntityId"), uniqueConstraints = {
+@Table(uniqueConstraints = {
     @UniqueConstraint( columnNames = {"userEntityId", "announcement_id"} )
   })
 
