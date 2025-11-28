447539a5144c036340d4d545a5a14a759e6707b3
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2025-11-28 13:01:26 +0200

    Added jose for jwt token encryption. Helper methods to generate smowl registration and monitoring link.

---
+++ muikku-frontend/muikkuV3/api_smowl/helper.ts
@@ -0,0 +1,124 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as jose from "jose";
+
+export const MONITORING_ENDPOINT = "https://swl.smowltech.net/monitor/";
+export const REGISTRATION_ENDPOINT = "https://swl.smowltech.net/register/";
+const ISSUER = "smowl_custom_integration";
+const AUDIENCE = "smowl_platform_domain";
+const JWT_SECRET = "smowl_provided_jwt_secret";
+
+/**
+ * Parameters for the monitoring JWT token
+ */
+interface MonitorringJwtParams {
+  entityKey: string;
+  userId: string;
+  /**
+   * The type of activity, e.g.: "quiz", "course", "test"...
+   */
+  activityType: string;
+  activityId: string;
+  activityContainerId: string;
+  /**
+   * The monitoring mode, 0: Instructions mode, 1: Monitoring mode
+   */
+  isMonitoring: number;
+}
+
+/**
+ * Parameters for the monitoring get parameters
+ */
+interface MonitoringGetParams {
+  entityName: string;
+  userName: string;
+  userEmail: string;
+  /**
+   * The language of the user, e.g.: "en", "es", "fr", "pt", "it", "de"...
+   */
+  lang: string;
+  type: number;
+}
+
+/**
+ * Parameters for the registration JWT token
+ */
+interface RegistrationJwtParams {
+  entityKey: string;
+  userId: string;
+  /**
+   * The type of activity, e.g.: "quiz", "course", "test"...
+   */
+  activityType?: string;
+  activityId?: string;
+  activityContainerId?: string;
+}
+
+/**
+ * Parameters for the registration get parameters
+ */
+interface RegistrationGetParams {
+  entityName: string;
+  userName: string;
+  userEmail: string;
+  /**
+   * The language of the user, e.g.: "en", "es", "fr", "pt", "it", "de"...
+   */
+  lang: string;
+  type: number;
+  /**
+   * The URL of the activity where to redirect the user after registration
+   */
+  activityUrl: string;
+}
+
+/**
+ * Formats a link with a JWT token and additional parameters
+ * @param endpoint - The base URL for the endpoint
+ * @param jwtParams - The parameters for the JWT token
+ * @param getParams - The additional parameters for the link
+ * @returns The formatted link
+ */
+export const formLink = async (
+  endpoint: string,
+  jwtParams: Record<string, any>,
+  getParams: Record<string, any>
+) => {
+  const controllerUrl = new URL(endpoint);
+  const secret = new TextEncoder().encode(JWT_SECRET);
+  const jwt = await new jose.SignJWT({ data: jwtParams })
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuer(ISSUER)
+    .setAudience(AUDIENCE)
+    .setIssuedAt()
+    .setExpirationTime("12h")
+    .sign(secret);
+  controllerUrl.searchParams.append("token", jwt);
+
+  for (const key in getParams) {
+    controllerUrl.searchParams.append(key, getParams[key]);
+  }
+
+  return controllerUrl.href;
+};
+
+/**
+ * Generates the monitoring link. Used with monitoring iframe.
+ * @param jwtParams - The parameters for the JWT token
+ * @param getParams - The additional parameters for the link
+ * @returns The monitoring link
+ */
+export const generateMonitoringLink = async (
+  jwtParams: MonitorringJwtParams,
+  getParams: MonitoringGetParams
+) => formLink(MONITORING_ENDPOINT, jwtParams, getParams);
+
+/**
+ * Generates the registration link
+ * @param jwtParams - The parameters for the JWT token
+ * @param getParams - The additional parameters for the link
+ * @returns The registration link
+ */
+export const generateRegistrationLink = async (
+  jwtParams: RegistrationJwtParams,
+  getParams: RegistrationGetParams
+) => formLink(REGISTRATION_ENDPOINT, jwtParams, getParams);
