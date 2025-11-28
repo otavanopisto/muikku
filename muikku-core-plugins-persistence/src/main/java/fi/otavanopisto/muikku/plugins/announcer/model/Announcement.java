cb3d7fb558bad977c3b78dbf060358ae86fe1580
saaralepisto <saara.lepisto@hotmail.com>
2025-11-28 14:24:27 +0200

    Removed unused import

--- muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/announcer/model/Announcement.java
+++ muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/announcer/model/Announcement.java
@@ -3,7 +3,6 @@ package fi.otavanopisto.muikku.plugins.announcer.model;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
