8fb21012a94fb75bd24301a8e719c24cb203586f
Antti Viljakainen <antti.viljakainen@otavanopisto.fi>
2025-12-09 11:56:39 +0200

    Added Smowl JWT sign endpoint

--- muikku-rest/src/main/java/fi/otavanopisto/muikku/rest/SmowlRESTService.java
+++ muikku-rest/src/main/java/fi/otavanopisto/muikku/rest/SmowlRESTService.java
@@ -1,9 +1,19 @@
 package fi.otavanopisto.muikku.rest;
 
 import java.io.UnsupportedEncodingException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,6 +28,12 @@ import javax.ws.rs.core.Response.Status;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
 import fi.otavanopisto.muikku.controller.SystemSettingsController;
 import fi.otavanopisto.muikku.model.users.EnvironmentRoleArchetype;
 import fi.otavanopisto.muikku.session.SessionController;
@@ -29,16 +45,90 @@ import fi.otavanopisto.security.rest.RESTPermit.Handling;
 public class SmowlRESTService extends AbstractRESTService {
   
   @Inject
+  private Logger logger;
+  
+  @Inject
   private SessionController sessionController;
 
   @Inject
   private SystemSettingsController systemSettingsController; 
 
-  // insert into SystemSetting (settingKey, settingValue) values ('smowl.entityName', ''), ('smowl.apiKey', '');
+  /*
+  insert into SystemSetting (settingKey, settingValue) values ('smowl.entityName', ''), ('smowl.apiKey', ''), 
+    ('smowl.audience', ''), ('smowl.licenseKey', ''), ('smowl.jwtSecret', '');
+  */
+  
+  @POST
+  @Path("/signjwt")
+  @RESTPermit(handling = Handling.INLINE, requireLoggedIn = true)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response signJWT(Map<String, String> payload) {
+    // Student-only endpoint
+    if (!sessionController.hasRole(EnvironmentRoleArchetype.STUDENT)) {
+      return Response.status(Status.FORBIDDEN).entity("Caller is not a student").build();
+    }
 
+    final String issuer = "smowl_custom_integration";
+    final String audience = systemSettingsController.getSetting("smowl.audience");
+    final String licenseKey = systemSettingsController.getSetting("smowl.licenseKey");
+    final String jwtSecret = systemSettingsController.getSetting("smowl.jwtSecret");
+    
+    if (StringUtils.isAnyBlank(issuer, audience, licenseKey, jwtSecret)) {
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity("Smowl credentials are not set.").build();
+    }
+    
+    Instant now = Instant.now();
+
+    Set<String> allowedClaims = Set.of("activityType", "activityId", "activityContainerId", "isMonitoring");
+
+    Map<String, String> claims = new HashMap<>();
+    if (payload != null) {
+      for (Entry<String, String> entry : payload.entrySet()) {
+        if (allowedClaims.contains(entry.getKey())) {
+          claims.put(entry.getKey(), entry.getValue());
+        }
+        else {
+          return Response.status(Status.BAD_REQUEST).entity(String.format("Claim not allowed: %s", entry.getKey())).build();
+        }
+      }
+    }    
+    claims.put("entityKey", licenseKey);
+    claims.put("userId", sessionController.getLoggedUserEntity().getId().toString());
+    
+    try {
+      JWTClaimsSet.Builder claimBuilder = new JWTClaimsSet.Builder()
+//          .jwtID(UUID.randomUUID().toString())
+          .issuer(issuer)
+          .audience(audience)
+          .issueTime(Date.from(now))
+          .expirationTime(Date.from(now.plus(12, ChronoUnit.HOURS)));
+
+      claims.forEach((key, value) -> claimBuilder.claim(key, value));
+
+      // Sign and return
+      
+      SignedJWT signedJWT = new SignedJWT(
+          new JWSHeader(JWSAlgorithm.HS256),
+          claimBuilder.build()
+      );
+      signedJWT.sign(new MACSigner(jwtSecret.getBytes()));
+      String token = signedJWT.serialize();
+
+      Map<String, Object> ret = new HashMap<>();
+      ret.put("claims", claims);
+      ret.put("token", token);
+      
+      return Response.ok().entity(ret).build();
+    }
+    catch (Exception e) {
+      logger.log(Level.SEVERE, "Failed to sign JWT.", e);
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity("Smowl credentials are not set.").build();
+    }
+  }
+  
   @POST
   @Path("/alarms/{PATH: .*}")
-  @RESTPermit(handling = Handling.INLINE)
+  @RESTPermit(handling = Handling.INLINE, requireLoggedIn = true)
   public Response alarmsProxy(@PathParam("PATH") String smowlPath,
       String entityBody) {
     
@@ -52,7 +142,7 @@ public class SmowlRESTService extends AbstractRESTService {
 
   @POST
   @Path("/configs/{PATH: .*}")
-  @RESTPermit(handling = Handling.INLINE)
+  @RESTPermit(handling = Handling.INLINE, requireLoggedIn = true)
   public Response configsProxy(@PathParam("PATH") String smowlPath,
       String entityBody) {
     
