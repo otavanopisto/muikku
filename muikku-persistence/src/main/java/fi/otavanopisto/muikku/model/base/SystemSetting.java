30ee9c532a206a7e0701850a0a2517082a89878c
saaralepisto <saara.lepisto@hotmail.com>
2025-12-12 12:16:54 +0200

    cleaning

--- muikku-persistence/src/main/java/fi/otavanopisto/muikku/model/base/SystemSetting.java
+++ muikku-persistence/src/main/java/fi/otavanopisto/muikku/model/base/SystemSetting.java
@@ -5,20 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Index;
 import javax.persistence.Lob;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import javax.validation.constraints.NotEmpty;
 
 @Entity
-@Table (
-    indexes = {
-      @Index(columnList = "settingKey", unique = true)
-    }
-  )
-
 public class SystemSetting {
 
   public Long getId() {
