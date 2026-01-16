c0ac2c9b08536aa516488bcc2841a3555f64d728
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2026-01-16 11:01:20 +0200

    Validation fix, opintopolkuUrl textarea to input field

--- muikku-frontend/muikkuV3/components/general/matriculationExaminationWizard/matriculationExaminationSelectLists/matriculation-examination-selector-components.tsx
+++ muikku-frontend/muikkuV3/components/general/matriculationExaminationWizard/matriculationExaminationSelectLists/matriculation-examination-selector-components.tsx
@@ -202,7 +202,7 @@ interface MatriculationExaminationFinishedInputGroupProps {
   readOnly?: boolean;
   onSubjectGroupChange: <T extends keyof MatriculationExamFinishedSubject>(
     key: T,
-    value: MatriculationExamFinishedSubject[T] | null,
+    value: MatriculationExamFinishedSubject[T],
     index: number
   ) => void;
   onClickDeleteRow: (index: number) => (e: React.MouseEvent) => void;
