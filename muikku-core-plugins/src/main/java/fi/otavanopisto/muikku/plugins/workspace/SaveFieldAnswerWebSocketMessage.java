929871327cd2d60822844e20910b682ae3678111
pkukkon <pasi.kukkonen@otavanopisto.fi>
2026-05-22 10:45:13 +0300

    - remove unused websocket message fields

--- muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/SaveFieldAnswerWebSocketMessage.java
+++ muikku-core-plugins/src/main/java/fi/otavanopisto/muikku/plugins/workspace/SaveFieldAnswerWebSocketMessage.java
@@ -1,7 +1,5 @@
 package fi.otavanopisto.muikku.plugins.workspace;
 
-import fi.otavanopisto.muikku.plugins.workspace.model.WorkspaceMaterialReplyState;
-
 public class SaveFieldAnswerWebSocketMessage {
 
   public String getAnswer() {
@@ -10,14 +8,6 @@ public class SaveFieldAnswerWebSocketMessage {
 
   public void setAnswer(String answer) {
     this.answer = answer;
-  }
-
-  public Long getMaterialId() {
-    return materialId;
-  }
-
-  public void setMaterialId(Long materialId) {
-    this.materialId = materialId;
   }
 
   public String getFieldName() {
@@ -36,14 +26,6 @@ public class SaveFieldAnswerWebSocketMessage {
     this.originTicket = originTicket;
   }
   
-  public Long getWorkspaceEntityId() {
-    return workspaceEntityId;
-  }
-  
-  public void setWorkspaceEntityId(Long workspaceEntityId) {
-    this.workspaceEntityId = workspaceEntityId;
-  }
-  
   public Long getWorkspaceMaterialId() {
     return workspaceMaterialId;
   }
@@ -60,20 +42,10 @@ public class SaveFieldAnswerWebSocketMessage {
     this.userEntityId = userEntityId;
   }
 
-  public WorkspaceMaterialReplyState getState() {
-    return state;
-  }
-
-  public void setState(WorkspaceMaterialReplyState state) {
-    this.state = state;
-  }
-
   private String answer;
-  private Long materialId;
   private String fieldName;
   private String originTicket;
   private Long workspaceMaterialId;
-  private Long workspaceEntityId;
-  private WorkspaceMaterialReplyState state;
   private Long userEntityId;
+
 }
