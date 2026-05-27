eeb170cfeb1fb03440153b23ca9eee2cbcb246c1
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2026-05-27 11:10:44 +0300

    import missing

--- muikku-frontend/muikkuV3/components/guardian/body/application/summary.tsx
+++ muikku-frontend/muikkuV3/components/guardian/body/application/summary.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
 import { Instructions } from "~/components/general/instructions";
 import { localize } from "~/locales/i18n";
 import { StateType } from "~/reducers";
