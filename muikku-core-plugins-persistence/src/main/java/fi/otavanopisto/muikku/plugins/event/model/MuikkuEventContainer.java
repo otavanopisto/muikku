ca8f69bd37191198d0b7b3684c3db8cdd25e922d
saaralepisto <saara.lepisto@hotmail.com>
2026-05-29 08:30:14 +0300

    Review changes pt2

--- muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/event/model/MuikkuEventContainer.java
+++ muikku-core-plugins-persistence/src/main/java/fi/otavanopisto/muikku/plugins/event/model/MuikkuEventContainer.java
@@ -5,8 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
+@Table(uniqueConstraints = {
+    @UniqueConstraint( columnNames = {"workspaceEntityId", "userEntityId"} )
+  })
+
 public class MuikkuEventContainer {
 
   public Long getId() {
