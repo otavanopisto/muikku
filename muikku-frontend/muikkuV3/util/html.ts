fed6058e0ff58499d19543cf277e75f48cc4b5ff
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2026-06-03 11:08:56 +0300

    Fix: isValidHTML function now checks if all nodes are elements

--- muikku-frontend/muikkuV3/util/html.ts
+++ muikku-frontend/muikkuV3/util/html.ts
@@ -1,9 +1,9 @@
 /**
  * Checks if the string is a valid HTML
- * @param str
+ * @param str string to check
  * @returns boolean
  */
 export const isValidHTML = (str: string): boolean => {
   const doc = new DOMParser().parseFromString(str, "text/html");
-  return Array.from(doc.body.childNodes).some((node) => node.nodeType === 1);
+  return Array.from(doc.body.childNodes).every((node) => node.nodeType === 1);
 };
