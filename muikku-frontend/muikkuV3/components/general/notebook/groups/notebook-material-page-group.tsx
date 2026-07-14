ae41762a9bfcaaf1b274d1fe909fde374a702996
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2026-07-14 13:25:36 +0300

    Changes: - Added add button to material page group

--- muikku-frontend/muikkuV3/components/general/notebook/groups/notebook-material-page-group.tsx
+++ muikku-frontend/muikkuV3/components/general/notebook/groups/notebook-material-page-group.tsx
@@ -2,6 +2,11 @@ import * as React from "react";
 import { NotebookMaterialPageGroup } from "../helpers/notebook-layout";
 import NotebookNoteItem from "../items/notebook-note-item";
 import { isNotebookDraftId } from "../helpers/notebook-drafts";
+import Dropdown from "../../dropdown";
+import { IconButton } from "../../button";
+import { useDispatch } from "react-redux";
+import { beginNotebookV2MaterialNoteDraft } from "~/actions/notebook/notebookV2";
+import { useTranslation } from "react-i18next";
 
 /**
  * NotebookMaterialPageGroupProps
@@ -23,6 +28,9 @@ const NotebookMaterialPageGroupView = (
   const { group, isOpen, onToggle } = props;
   const { page, materialNotes, contextItems } = group;
 
+  const dispatch = useDispatch();
+  const { t } = useTranslation("notebook");
+
   const hasMaterialNotes = materialNotes.length > 0;
   const hasContextItems = contextItems.length > 0;
 
@@ -30,9 +38,29 @@ const NotebookMaterialPageGroupView = (
     return null;
   }
 
+  /**
+   * Handle add click
+   */
+  const handleAddClick = () => {
+    dispatch(beginNotebookV2MaterialNoteDraft(page.workspaceMaterialId));
+  };
+
   return (
     <div className="notebook__page-group">
-      <h4 className="notebook__page-group-title">{page.title}</h4>
+      <div className="notebook__page-group-header">
+        <h4 className="notebook__page-group-title">{page.title}</h4>
+        <div className="notebook__page-group-actions">
+          <Dropdown openByHover content={<p>{t("actions.add")}</p>}>
+            <IconButton
+              icon="plus"
+              aria-label={t("actions.add")}
+              buttonModifiers={["notebook-action"]}
+              onClick={handleAddClick}
+              disablePropagation={true}
+            />
+          </Dropdown>
+        </div>
+      </div>
 
       {hasMaterialNotes &&
         materialNotes.map((note) => (
