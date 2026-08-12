4dd65515619ae9b2c873806da6e1382eb4dfcf76
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2026-08-12 10:58:19 +0300

    Webpack html-validate ignore warning, disableDotRule true for history api fallback dev server settings

--- muikku-frontend/muikkuV3/webpack-config/dev_server.ts
+++ muikku-frontend/muikkuV3/webpack-config/dev_server.ts
@@ -5,7 +5,9 @@ const devServer: Configuration = {
   port: 8000,
   host: "dev.muikkuverkko.fi",
   hot: "only",
-  historyApiFallback: true,
+  historyApiFallback: {
+    disableDotRule: true,
+  },
   client: {
     overlay: true,
     logging: "none",
