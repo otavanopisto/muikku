1444da6e93fd89467585d5122c83cb3e1d7871a1
Jari Lankinen <jari.lankinen@otavanopisto.fi>
2026-08-14 12:26:37 +0300

    * One more icon change

--- muikku-frontend/muikkuV3/components/general/notebook/groups/notebook-material-page-group.tsx
+++ muikku-frontend/muikkuV3/components/general/notebook/groups/notebook-material-page-group.tsx
@@ -68,7 +68,7 @@ const NotebookMaterialPageGroupView = (
           <div className="notebook__page-group-actions">
             <Dropdown openByHover content={<p>{t("actions.add")}</p>}>
               <IconButton
-                icon="plus"
+                icon="note-add"
                 aria-label={t("actions.add")}
                 buttonModifiers={["notebook-action"]}
                 onClick={handleAddClick}
