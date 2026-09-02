db598fe97c556a14fbbd9fb2b47970b8f573b62f
Tero Tanttu <tero.tanttu@otavanopisto.fi>
2026-09-02 11:26:39 +0300

    Wrong prop definition error fixed

--- muikku-frontend/muikkuV3/components/base/material-loader/base/index.tsx
+++ muikku-frontend/muikkuV3/components/base/material-loader/base/index.tsx
@@ -131,7 +131,7 @@ interface BaseProps extends WithTranslation {
     fieldName: string,
     content: string,
     onSuccess: () => void,
-    onError: (error: Error) => void
+    onFail: () => void
   ) => void;
 }
 
