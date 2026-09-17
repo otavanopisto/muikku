aef5e058f343e31e3ae7008ed67a9b4e87d61c4e
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2026-09-17 14:02:59 +0300

    fixes undefined bug

--- muikku-frontend/muikkuV3/components/general/events/absence-event.tsx
+++ muikku-frontend/muikkuV3/components/general/events/absence-event.tsx
@@ -4,13 +4,10 @@ import "~/sass/elements/note.scss";
 import BaseEvent from "./base/base-event";
 import { MuikkuEvent } from "~/generated/client";
 import "~/sass/elements/muikku-absence-event.scss";
-import {
-  AbsenceEventEnum,
-  AbsenceReasonEnum,
-} from "~/reducers/base/muikku-events";
 import Button from "~/components/general/button";
 import PromptDialog from "~/components/general/prompt-dialog";
 import EditAbsenceDialog from "~/components/workspace/workspaceUsers/dialogs/edit-absence";
+import { absentFromLabel, absenceReasonLabel } from "~/util/events";
 
 /**
  * WallAbsenceEventsProps
@@ -37,41 +34,6 @@ const AbsenceEvent: React.FC<AbsenceEventsProps> = (props) => {
     ? "REVIEWED"
     : "REVIEW-PENDING";
 
-  /**
-   * Returns the absent from label
-   * @returns string
-   */
-  const absentFromLabel = () => {
-    switch (event.title as AbsenceEventEnum) {
-      case AbsenceEventEnum.Lesson:
-        return t("types.LESSON", { ns: "events" });
-      case AbsenceEventEnum.LessonPreArranged:
-        return t("types.LESSON_PRE_ARRANGED", { ns: "events" });
-      case AbsenceEventEnum.Exam:
-        return t("types.EXAM", { ns: "events" });
-      case AbsenceEventEnum.SkillsDemonstrationMeeting:
-        return t("types.SKILLS_DEMONSTRATION_MEETING", { ns: "events" });
-      case AbsenceEventEnum.GuidanceOrSupportSession:
-        return t("types.GUIDANCE_OR_SUPPORT_SESSION", { ns: "events" });
-    }
-  };
-
-  /**
-   * Returns the absence reason value label
-   * @param value absence reason value
-   * @returns string
-   */
-  const absenceReasonLabel = (value: string) => {
-    switch (value as AbsenceReasonEnum) {
-      case AbsenceReasonEnum.Medical:
-        return t("reasons.MEDICAL_REASON", { ns: "events" });
-      case AbsenceReasonEnum.OtherAuthorized:
-        return t("reasons.OTHER_AUTHORIZED_REASON", { ns: "events" });
-      case AbsenceReasonEnum.UnauthorizedExplained:
-        return t("reasons.UNAUTHORIZED_ABSENCE_EXPLAINED", { ns: "events" });
-    }
-  };
-
   const actions = (
     <div className="muikku-absence-event__footer">
       {onUpdate && (
@@ -90,7 +52,7 @@ const AbsenceEvent: React.FC<AbsenceEventsProps> = (props) => {
           content={t("content.removing", {
             ns: "events",
             context: "absence",
-            label: absentFromLabel(),
+            label: absentFromLabel(event),
             userName: event.targetUserName,
           })}
           onExecute={() => onDelete(event.id!)}
@@ -109,7 +71,7 @@ const AbsenceEvent: React.FC<AbsenceEventsProps> = (props) => {
       endDate={event.end}
       modifier={modifier}
       state={absenceState}
-      title={event.targetUserName + " - " + absentFromLabel()}
+      title={event.targetUserName + " - " + absentFromLabel(event)}
     >
       <div className="muikku-absence-event">
         {event.description && (
